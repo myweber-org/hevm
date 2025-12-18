755156a0479a01ff66d5f707b61b5625047398b2
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-18 22:48:52 +0000

    Add word frequency analysis utility

---
+++ test/contracts/pass/WordFrequency.hs
@@ -0,0 +1,23 @@
+module WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord w = Map.insertWith (+) (normalize w) 1
+    normalize = filter isAlpha . map toLower
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+displayFrequency :: [(String, Int)] -> String
+displayFrequency = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequency . topNWords n
