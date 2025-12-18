2cbf40b58a8d840f5153b6e0442782cc5917a62f
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-18 14:14:21 +0000

    Add function to filter and transform list of integers

---
+++ ethjet/DataProcessor.hs
@@ -0,0 +1,7 @@
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
