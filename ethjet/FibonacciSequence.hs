31cc3e2a15fad2bfa9896e76deb02ef5f448354a
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-18 16:20:39 +0000

    Add Fibonacci sequence generator using memoization

---
+++ ethjet/FibonacciSequence.hs
@@ -0,0 +1,19 @@
+module FibonacciSequence where
+
+import Data.Function (fix)
+
+fibonacci :: Int -> Integer
+fibonacci = (map fib [0..] !!)
+  where
+    fib 0 = 0
+    fib 1 = 1
+    fib n = fibonacci (n - 1) + fibonacci (n - 2)
+
+memoizedFibonacci :: Int -> Integer
+memoizedFibonacci = fix (memoize . fib)
+  where
+    fib f 0 = 0
+    fib f 1 = 1
+    fib f n = f (n - 1) + f (n - 2)
+    
+    memoize g = (map g [0..] !!)
