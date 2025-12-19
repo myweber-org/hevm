1c3147f3c93197cb607a3e7dd6b68356cd874aa6
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-19 12:18:47 +0000

    Add function to calculate moving average of a list

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -4,4 +4,23 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processNumbers = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | n > length xs = error "Window size exceeds list length"
+    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
+
+-- Example usage with a helper function
+exampleUsage :: IO ()
+exampleUsage = do
+    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    let windowSize = 3
+    putStrLn $ "Data series: " ++ show dataSeries
+    putStrLn $ "Moving average with window " ++ show windowSize ++ ":"
+    print $ movingAverage windowSize dataSeries
