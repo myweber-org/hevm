42fc959509d518e6d7f4eb8c9f1bd7de359218d3
InkspillVerse <inkspill.verse@outlook.com>
2025-12-19 19:26:55 +0000

    Add function to filter and transform a list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -25,3 +25,10 @@ main = do
     putStrLn $ "Sum of processed even numbers: " ++ show (sumProcessedEvenNumbers numbers)
     putStrLn $ "First element (safe): " ++ show (safeHead numbers)
     putStrLn $ "First element of empty list: " ++ show (safeHead [])
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
