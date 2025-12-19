d0c5c258d6797450cf32e787f2bef9ab1da2f70b
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-19 21:41:18 +0000

    Add word frequency counter for text analysis

--- test/scripts/WordFrequencyCounter.hs
+++ test/scripts/WordFrequencyCounter.hs
@@ -36,4 +36,38 @@ processText = formatOutput . sortByFrequency . countWords
 main :: IO ()
 main = do
     input <- getContents
-    putStr $ processText input
+    putStr $ processText inputmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map (map toLower) wordsList
+        grouped = foldr countHelper [] cleaned
+        sorted = sortOn (Down . snd) grouped
+    in sorted
+  where
+    cleanWord = filter isAlpha
+    countHelper word [] = [(word, 1)]
+    countHelper word ((w, c):rest)
+        | word == w = (w, c + 1) : rest
+        | otherwise = (w, c) : countHelper word rest
+
+formatResults :: [WordCount] -> String
+formatResults counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+analyzeText :: String -> String
+analyzeText = formatResults . countWords
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze:"
+    input <- getContents
+    putStrLn "\nWord frequencies:"
+    putStrLn $ analyzeText input
