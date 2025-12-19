14d4b59c5e3ce848731c7c6e710f509266b4ebf2
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-19 12:09:05 +0000

    Add function to filter and transform list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -11,4 +11,17 @@ main :: IO ()
 main = do
     let numbers = [1..10]
     let result = processNumbers numbers
-    print result
+    print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform isEven square
+    where
+        isEven x = x `mod` 2 == 0
+        square x = x * x
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
