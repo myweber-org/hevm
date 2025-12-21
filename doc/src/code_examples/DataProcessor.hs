11091dc52fd17d2fdb2c484a1b126d79006fed99
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-21 22:22:20 +0000

    Add function to filter and transform a list of integers, extracting even numbers and computing their squares

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -35,3 +35,15 @@ processData = filterAndTransform (> 0) (* 2)
 
 sumProcessedData :: [Int] -> Int
 sumProcessedData = sum . processData
+module DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter even
+
+sampleData :: [Int]
+sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+main :: IO ()
+main = do
+    let result = processData sampleData
+    putStrLn $ "Processed data: " ++ show result
