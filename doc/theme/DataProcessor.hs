cae6d5112c3aa14cd8f034aef2b41df8c3971764
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-21 00:17:45 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -110,4 +110,21 @@ calculateAverages rows = map (\col -> sum col / fromIntegral (length col)) (tran
     transpose xss = map head xss : transpose (map tail xss)
 
 processCSVData :: String -> [Double]
-processCSVData csvData = calculateAverages (parseCSV csvData)
+processCSVData csvData = calculateAverages (parseCSV csvData)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * 2 + 1)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+main :: IO ()
+main = do
+    let numbers = [1..20]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
+    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
