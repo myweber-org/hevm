5198ab7537fa89d2b40e7f06c15af592ac171932
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-21 16:22:16 +0000

    Add a function to generate Fibonacci sequence up to a given number

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -27,4 +27,16 @@ fibonacci n = take n fibs
 main :: IO ()
 main = do
   putStrLn "Fibonacci sequence up to 10 terms:"
-  print $ fibonacci 10
+  print $ fibonacci 10module FibonacciSequence where
+
+fibonacci :: Int -> [Int]
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
+
+fibonacciUpTo :: Int -> [Int]
+fibonacciUpTo limit = takeWhile (<= limit) fibs
+    where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
