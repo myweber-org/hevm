4c70964ab29599fd9e579955e0641d400cb5ee2e
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-21 19:57:18 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/contracts/FibonacciSequence.hs
+++ test/contracts/FibonacciSequence.hs
@@ -5,4 +5,9 @@ fibonacci n
     | n <= 0    = []
     | otherwise = takeWhile (<= n) fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
