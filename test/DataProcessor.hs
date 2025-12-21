57ff44ac4a2714147b0fc135c89ca55630ab3906
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-21 16:14:27 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -77,3 +77,23 @@ main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
     print result
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+processOddCubes :: [Int] -> [Int]
+processOddCubes = filterAndTransform odd (\x -> x * x * x)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    putStrLn "Original list:"
+    print numbers
+    putStrLn "\nEven numbers squared:"
+    print $ processEvenSquares numbers
+    putStrLn "\nOdd numbers cubed:"
+    print $ processOddCubes numbers
