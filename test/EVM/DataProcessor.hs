c2141f5de51ebada6642332d70764d3c737f7847
InkspillVerse <inkspill.verse@outlook.com>
2025-12-21 21:05:25 +0000

    Add data validation and transformation utilities for CSV processing

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -42,4 +42,34 @@ validateInput xs = all (\x -> x >= -100 && x <= 100) xs
 safeProcess :: [Int] -> Maybe [Int]
 safeProcess xs
     | validateInput xs = Just (processData xs)
-    | otherwise = Nothing
+    | otherwise = Nothingmodule DataProcessor where
+
+import Data.Char (isDigit, isAlpha)
+import Data.List (intercalate)
+
+type Row = [String]
+type Table = [Row]
+
+validateRowLength :: Int -> Row -> Bool
+validateRowLength expectedLength row = length row == expectedLength
+
+validateNumericField :: String -> Bool
+validateNumericField = all isDigit
+
+validateAlphaField :: String -> Bool
+validateAlphaField = all isAlpha
+
+transformToUpper :: Row -> Row
+transformToUpper = map (map toUpper)
+
+filterValidRows :: (Row -> Bool) -> Table -> Table
+filterValidRows validator = filter validator
+
+processCSVData :: Table -> Table
+processCSVData = filterValidRows (validateRowLength 3) . map transformToUpper
+
+formatRow :: Row -> String
+formatRow = intercalate ","
+
+formatTable :: Table -> String
+formatTable = unlines . map formatRow
