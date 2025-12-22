6e79436561254a4657851223500a8c5e58c8574c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-22 00:17:08 +0000

    Add a function to generate the nth Fibonacci number using memoization

---
+++ cli/FibonacciSequence.hs
@@ -0,0 +1,10 @@
+module FibonacciSequence where
+
+import Data.Function (fix)
+
+fibMemo :: Int -> Integer
+fibMemo = fix memoize
+  where
+    memoize _ 0 = 0
+    memoize _ 1 = 1
+    memoize f n = f (n - 1) + f (n - 2)
