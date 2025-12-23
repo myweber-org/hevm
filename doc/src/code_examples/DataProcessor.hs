3a363b76e2040d0ae78a0bb3a4e76ecbcaec71a0
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-23 07:56:40 +0000

    Add function to filter and transform list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -46,4 +46,20 @@ sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 main :: IO ()
 main = do
     let result = processData sampleData
-    putStrLn $ "Processed data: " ++ show result
+    putStrLn $ "Processed data: " ++ show resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * 2 + 1)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+main :: IO ()
+main = do
+    let numbers = [1..20]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
+    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
