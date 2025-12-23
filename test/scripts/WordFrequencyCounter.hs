a1a0eb1d770d02f1b96dd634c02e04bc17a3c030
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-23 20:01:27 +0000

    Add word frequency counter module with basic text processing functions

--- test/scripts/WordFrequencyCounter.hs
+++ test/scripts/WordFrequencyCounter.hs
@@ -9,65 +9,22 @@ type WordCount = (String, Int)
 countWords :: String -> [WordCount]
 countWords text = 
     let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleanedWords = filter (all isAlpha) wordsList
-    in aggregateCounts cleanedWords
+        grouped = foldr countHelper [] wordsList
+    in sortOn (Down . snd) grouped
   where
-    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-
-aggregateCounts :: [String] -> [WordCount]
-aggregateCounts = foldr incrementCount []
-  where
-    incrementCount word [] = [(word, 1)]
-    incrementCount word ((w, c):rest)
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : incrementCount word rest
-
-sortByFrequency :: [WordCount] -> [WordCount]
-sortByFrequency = sortOn (Down . snd)
-
-formatOutput :: [WordCount] -> String
-formatOutput counts = unlines $ map formatLine counts
-  where
-    formatLine (word, count) = word ++ ": " ++ show count
-
-processText :: String -> String
-processText = formatOutput . sortByFrequency . countWords
-
-main :: IO ()
-main = do
-    input <- getContents
-    putStr $ processText inputmodule WordFrequencyCounter where
-
-import Data.Char (toLower, isAlpha)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type WordCount = (String, Int)
-
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleaned = map (map toLower) wordsList
-        grouped = foldr countHelper [] cleaned
-        sorted = sortOn (Down . snd) grouped
-    in sorted
-  where
-    cleanWord = filter isAlpha
+    cleanWord = filter isAlpha . map toLower
     countHelper word [] = [(word, 1)]
     countHelper word ((w, c):rest)
-        | word == w = (w, c + 1) : rest
+        | w == word = (w, c + 1) : rest
         | otherwise = (w, c) : countHelper word rest
 
-formatResults :: [WordCount] -> String
-formatResults counts = 
-    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+topNWords :: Int -> String -> [WordCount]
+topNWords n text = take n $ countWords text
 
-analyzeText :: String -> String
-analyzeText = formatResults . countWords
+displayResults :: [WordCount] -> String
+displayResults counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
 
-main :: IO ()
-main = do
-    putStrLn "Enter text to analyze:"
-    input <- getContents
-    putStrLn "\nWord frequencies:"
-    putStrLn $ analyzeText input
+processText :: String -> String
+processText text = 
+    let topWords = topNWords 10 text
+    in "Top 10 most frequent words:\n" ++ displayResults topWords
