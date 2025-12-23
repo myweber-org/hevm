0b4a970de1ed78fb5698299cb25779030785da59
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-23 23:24:09 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,46 +1,9 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 4, -2, 5, 0, 8]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
-    putStrLn $ "First element: " ++ show (safeHead numbers)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5, 0, 7]
-    putStrLn $ "Input: " ++ show input
-    putStrLn $ "Processed: " ++ show (processData input)
-    putStrLn $ "Sum: " ++ show (sumProcessedData input)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
 processEvenSquares :: [Int] -> [Int]
 processEvenSquares = filterAndTransform even (\x -> x * x)
@@ -51,15 +14,12 @@ sumProcessed processor = sum . map processor
 main :: IO ()
 main = do
     let numbers = [1..10]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
-    putStrLn $ "Sum of even squares: " ++ show (sumProcessed (\x -> if even x then x*x else 0) numbers)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+    putStrLn "Original list:"
+    print numbers
+    
+    putStrLn "\nSquares of even numbers:"
+    let squares = processEvenSquares numbers
+    print squares
+    
+    putStrLn "\nSum of squares of even numbers:"
+    print $ sumProcessed (\x -> x * x) (filter even numbers)
