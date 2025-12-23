9870ecfed273d3181fee9f7d77993fb207bc5d92
InkspillVerse <inkspill.verse@outlook.com>
2025-12-23 15:53:58 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- src/FibonacciSequence.hs
+++ src/FibonacciSequence.hs
@@ -3,4 +3,21 @@ module FibonacciSequence where
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module Fibonacci where
+
+import Data.Function (fix)
+
+fibonacci :: Int -> Integer
+fibonacci = (map fib [0..] !!)
+  where
+    fib 0 = 0
+    fib 1 = 1
+    fib n = fibonacci (n - 1) + fibonacci (n - 2)
+
+-- Alternative implementation using fix for memoization
+fibonacciMemoized :: Int -> Integer
+fibonacciMemoized = fix memoFib
+  where
+    memoFib _ 0 = 0
+    memoFib _ 1 = 1
+    memoFib f n = f (n - 1) + f (n - 2)
