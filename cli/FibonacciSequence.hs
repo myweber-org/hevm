44f6628cadd9c0ff370e72e72601e0b94bd7e0d6
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-23 18:41:17 +0000

    Add a function to generate the Fibonacci sequence up to a given number

--- cli/FibonacciSequence.hs
+++ cli/FibonacciSequence.hs
@@ -7,4 +7,9 @@ fibMemo = fix memoize
   where
     memoize _ 0 = 0
     memoize _ 1 = 1
-    memoize f n = f (n - 1) + f (n - 2)
+    memoize f n = f (n - 1) + f (n - 2)module FibonacciSequence where
+
+fibonacci :: Integer -> [Integer]
+fibonacci n = takeWhile (<= n) fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
