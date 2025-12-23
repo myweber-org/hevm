4ed5cb0c0ab9f969d1bae54e6d870d4b89f6c3a8
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-23 14:36:34 +0000

    Add function to filter and transform list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -109,4 +109,27 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * 2 + 1)
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< 0) xs = Nothing
+    | otherwise = Just xs
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn "Original data:"
+            print validData
+            putStrLn "Processed data (even numbers doubled and incremented):"
+            print $ processData validData
+        Nothing -> putStrLn "Invalid input data"
