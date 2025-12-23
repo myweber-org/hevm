f91ab04d999d07b08667262100e4f23c60f01fe9
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-23 13:31:18 +0000

    Add word frequency counter with sorting and filtering capabilities

--- doc/theme/WordFrequencyCounter.hs
+++ doc/theme/WordFrequencyCounter.hs
@@ -23,4 +23,39 @@ displayFrequency freqList =
     unlines $ map (\(word, count) -> word ++ ": " ++ show count) freqList
 
 processText :: Int -> String -> String
-processText n = displayFrequency . topNWords n
+processText n = displayFrequency . topNWords nmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = group $ sort cleanedWords
+    in sortOn (Down . snd) $ map (\ws -> (head ws, length ws)) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+
+filterByMinFrequency :: Int -> [WordCount] -> [WordCount]
+filterByMinFrequency minFreq = filter ((>= minFreq) . snd)
+
+getTopNWords :: Int -> [WordCount] -> [WordCount]
+getTopNWords n = take n
+
+wordFrequencyReport :: String -> Int -> Int -> [WordCount]
+wordFrequencyReport text minFreq topN =
+    let counts = countWords text
+        filtered = filterByMinFrequency minFreq counts
+    in getTopNWords topN filtered
+
+main :: IO ()
+main = do
+    let sampleText = "Hello world! Hello Haskell. Haskell is great. World says hello back."
+    let report = wordFrequencyReport sampleText 1 5
+    
+    putStrLn "Word Frequency Report:"
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) report
