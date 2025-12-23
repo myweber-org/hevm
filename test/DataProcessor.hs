03dd782f0230406dccf4e116e16a67f1a6545a5f
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-23 15:32:20 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -170,3 +170,19 @@ processCSVFile filename = do
     content <- readFile filename
     let parsed = parseCSV content
     return $ calculateColumnAverage parsed 2
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedList :: [Int] -> Int
+sumProcessedList = sum . processEvenSquares
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    let result = sumProcessedList sampleData
+    putStrLn $ "Sum of squares of even numbers from 1 to 10: " ++ show result
