018d5dba3e8d2a3008d82a0b0c31dcd136bbc85d
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-23 18:21:23 +0000

    Add function to filter and transform list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -16,3 +16,18 @@ main = do
     case validateInput sampleData of
         Just validData -> print $ processData validData
         Nothing -> putStrLn "Invalid input: contains zero"
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+safeProcess :: [Int] -> Maybe [Int]
+safeProcess xs
+    | validateInput xs = Just (processData xs)
+    | otherwise = Nothing
