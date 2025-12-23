6c212de895d0de3835ffec1cc30b618291bf7c6e
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-23 07:50:53 +0000

    Add function to parse CSV data and calculate average values

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -56,3 +56,46 @@ main = do
     putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
     putStrLn $ "Sum of even squares: " ++ 
         show (sumProcessed (\x -> x * x) (filter even numbers))
+module DataProcessor where
+
+import Data.List (foldl')
+import Text.Read (readMaybe)
+
+type Record = (String, Double, Double)
+
+parseCSVLine :: String -> Maybe Record
+parseCSVLine line = case wordsWhen (==',') line of
+    [name, val1, val2] -> do
+        v1 <- readMaybe val1
+        v2 <- readMaybe val2
+        return (name, v1, v2)
+    _ -> Nothing
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+    "" -> []
+    s' -> w : wordsWhen p s''
+        where (w, s'') = break p s'
+
+calculateAverages :: [Record] -> (Double, Double)
+calculateAverages records = 
+    let (sum1, sum2, count) = foldl' (\(s1, s2, c) (_, v1, v2) -> (s1 + v1, s2 + v2, c + 1)) (0, 0, 0) records
+    in if count > 0 
+        then (sum1 / fromIntegral count, sum2 / fromIntegral count)
+        else (0, 0)
+
+processCSVData :: String -> Maybe ((Double, Double), [Record])
+processCSVData csvContent = do
+    let lines' = filter (not . null) $ lines csvContent
+    records <- mapM parseCSVLine lines'
+    if null records 
+        then Nothing
+        else Just (calculateAverages records, records)
+
+formatOutput :: ((Double, Double), [Record]) -> String
+formatOutput ((avg1, avg2), records) =
+    "Processed " ++ show (length records) ++ " records\n" ++
+    "Average value 1: " ++ show avg1 ++ "\n" ++
+    "Average value 2: " ++ show avg2 ++ "\n" ++
+    "Individual records:\n" ++
+    unlines (map (\(name, v1, v2) -> name ++ ": " ++ show v1 ++ ", " ++ show v2) records)
