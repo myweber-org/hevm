52e37fe0c9e11f7597e53209c44634262ff36bcc
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-24 18:56:38 +0000

    Implement Sieve of Eratosthenes to generate primes up to a given limit

--- bench/PrimeSieve.hs
+++ bench/PrimeSieve.hs
@@ -11,3 +11,12 @@ sieve limit
 
 primesUpTo :: Int -> [Int]
 primesUpTo = sieve
+module PrimeSieve where
+
+primesUpTo :: Int -> [Int]
+primesUpTo n
+    | n < 2     = []
+    | otherwise = sieve [2..n]
+  where
+    sieve [] = []
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
