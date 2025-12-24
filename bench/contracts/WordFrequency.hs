c8fb1ee55a29a863bd39f2771caea8adbe6d0f1b
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-24 21:04:47 +0000

    Add a function to count word frequencies in a text file

--- bench/contracts/WordFrequency.hs
+++ bench/contracts/WordFrequency.hs
@@ -32,4 +32,26 @@ analyzeText text = do
 main :: IO ()
 main = do
     let sampleText = "Hello world! This is a test. Hello again, world. Testing, testing, one two three."
-    analyzeText sampleText
+    analyzeText sampleTextmodule WordFrequency where
+
+import qualified Data.Map as Map
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlphaNum
+
+getTopWords :: Int -> String -> [(String, Int)]
+getTopWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+processFile :: FilePath -> Int -> IO ()
+processFile path n = do
+    content <- readFile path
+    let topWords = getTopWords n content
+    mapM_ (\(word, freq) -> putStrLn $ word ++ ": " ++ show freq) topWords
