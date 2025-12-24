011740a341f5f6ed6fdc0488bf10ef9ae2179165
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-24 14:16:13 +0000

    Add a word frequency counter for text analysis

--- test/scripts/WordFrequencyCounter.hs
+++ test/scripts/WordFrequencyCounter.hs
@@ -27,4 +27,33 @@ displayResults counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
 processText :: String -> String
 processText text = 
     let topWords = topNWords 10 text
-    in "Top 10 most frequent words:\n" ++ displayResults topWords
+    in "Top 10 most frequent words:\n" ++ displayResults topWordsmodule WordFrequencyCounter where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords text =
+    let wordsList = filter (not . null) $ map normalize $ words text
+    in List.foldl' (\acc w -> Map.insertWith (+) w 1 acc) Map.empty wordsList
+  where
+    normalize = filter Char.isAlpha . map Char.toLower
+
+getTopNWords :: Int -> String -> [(String, Int)]
+getTopNWords n text =
+    take n $ List.sortOn (\(_, freq) -> negate freq) $ Map.toList $ countWords text
+
+printWordFrequencies :: String -> IO ()
+printWordFrequencies text = do
+    let topWords = getTopNWords 10 text
+    putStrLn "Top 10 most frequent words:"
+    mapM_ (\(word, freq) -> putStrLn $ word ++ ": " ++ show freq) topWords
+
+sampleText :: String
+sampleText = "This is a sample text. This text contains some words. Some words are repeated. Repeated words help demonstrate frequency counting."
+
+main :: IO ()
+main = printWordFrequencies sampleText
