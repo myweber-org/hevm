1d9d451453b3c9e02906ed4bc563901ddd44528c
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-24 15:16:52 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -230,4 +230,21 @@ formatReport rows =
     let header = "ID,Name,Amount"
         separator = replicate 30 '-'
         formattedRows = map (intercalate ",") rows
-    in unlines $ header : separator : formattedRows
+    in unlines $ header : separator : formattedRowsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Bool
+validateInput = all (\x -> x >= -100 && x <= 100)
+
+safeProcess :: [Int] -> Maybe Int
+safeProcess xs
+    | validateInput xs = Just (sumProcessedData xs)
+    | otherwise = Nothing
