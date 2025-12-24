e706410e59daf8a363bdfc8b9cf17b73ee450eb1
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-24 14:57:54 +0000

    Add function to calculate moving average of a list

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -8,4 +8,17 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
+
+-- Example usage:
+-- movingAverage 3 [1,2,3,4,5] -> [2.0,3.0,4.0]
