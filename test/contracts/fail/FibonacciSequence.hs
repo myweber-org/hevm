3175308fcfdc63dfd883afb14a15f01a3f630494
InkspillVerse <inkspill.verse@outlook.com>
2025-12-25 23:25:22 +0000

    Add Fibonacci sequence generator with memoization

---
+++ test/contracts/fail/FibonacciSequence.hs
@@ -0,0 +1,11 @@
+module Fibonacci where
+
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    mapM_ (print . fibonacci) [0..19]
