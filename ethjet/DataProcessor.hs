95b27edbe5cd150d79470286a6a4ec02a726bde1
InkspillVerse <inkspill.verse@outlook.com>
2025-12-25 19:24:57 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -23,3 +23,16 @@ main = do
     
     putStrLn "\nSum of squares of even numbers:"
     print $ sumProcessedData even (\x -> x * x) numbers
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [1, -2, 3, -4, 5]
+    let result = processNumbers numbers
+    print result
