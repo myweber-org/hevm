aaa0f3f16a7971f3484dcbe35da81bf1b4987f08
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-25 20:01:03 +0000

    Add word frequency counter for text analysis

--- bench/contracts/WordFrequency.hs
+++ bench/contracts/WordFrequency.hs
@@ -2,56 +2,22 @@ module WordFrequency where
 
 import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type WordCount = (String, Int)
-
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleanedWords = filter (all isAlpha) wordsList
-        frequencyMap = foldr countWord [] cleanedWords
-    in take 10 $ sortOn (Down . snd) frequencyMap
-  where
-    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-    
-    countWord :: String -> [WordCount] -> [WordCount]
-    countWord word [] = [(word, 1)]
-    countWord word ((w, c):rest)
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord word rest
-
-analyzeText :: String -> IO ()
-analyzeText text = do
-    putStrLn "Top 10 most frequent words:"
-    mapM_ printWord $ countWords text
-  where
-    printWord (word, count) = 
-        putStrLn $ word ++ ": " ++ show count
-
-main :: IO ()
-main = do
-    let sampleText = "Hello world! This is a test. Hello again, world. Testing, testing, one two three."
-    analyzeText sampleTextmodule WordFrequency where
-
+import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Char (toLower, isAlphaNum)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
 
-type FrequencyMap = Map.Map String Int
+type FrequencyMap = Map String Int
 
 countWords :: String -> FrequencyMap
-countWords = foldr incrementWord Map.empty . words
+countWords = foldr updateWord Map.empty . words
   where
-    incrementWord word = Map.insertWith (+) (normalize word) 1
-    normalize = map toLower . filter isAlphaNum
+    updateWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
 
-getTopWords :: Int -> String -> [(String, Int)]
-getTopWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (negate . snd) $ Map.toList $ countWords text
 
-processFile :: FilePath -> Int -> IO ()
-processFile path n = do
-    content <- readFile path
-    let topWords = getTopWords n content
-    mapM_ (\(word, freq) -> putStrLn $ word ++ ": " ++ show freq) topWords
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequencies . topNWords n
