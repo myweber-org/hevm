c8f2c528f326a39c021ae6954f1c71fd4075744b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-25 03:24:18 +0000

    Add word frequency analysis function for text processing

--- test/EVM/Test/WordFrequency.hs
+++ test/EVM/Test/WordFrequency.hs
@@ -34,4 +34,28 @@ analyzeText input = do
     mapM_ printWord (countWords input)
   where
     printWord (word, count) = 
-        putStrLn $ word ++ ": " ++ show count
+        putStrLn $ word ++ ": " ++ show countmodule WordFrequency where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords text =
+    let wordsList = filter (not . null) $ map normalize $ words text
+    in List.foldl' (\acc w -> Map.insertWith (+) w 1 acc) Map.empty wordsList
+  where
+    normalize = filter Char.isAlpha . map Char.toLower
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text =
+    take n $ List.sortBy (\(_, c1) (_, c2) -> compare c2 c1) $ Map.toList $ countWords text
+
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies freqList =
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) freqList
+
+analyzeText :: Int -> String -> String
+analyzeText n text = displayFrequencies $ topNWords n text
