8649c86a4b94dceab051361eadcd51d758904b5a
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-25 14:13:35 +0000

    Add function to count word frequencies in a text

---
+++ doc/src/WordFrequency.hs
@@ -0,0 +1,28 @@
+module WordFrequency where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        freqMap = foldr (\word acc -> 
+            case lookup word acc of
+                Just count -> (word, count + 1) : filter ((/= word) . fst) acc
+                Nothing -> (word, 1) : acc) [] wordsList
+    in sortOn (Down . snd) freqMap
+  where
+    cleanWord = map toLower . filter isAlphaNum
+
+printWordFrequencies :: [WordCount] -> IO ()
+printWordFrequencies counts = 
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
+
+main :: IO ()
+main = do
+    let sampleText = "Hello world! Hello Haskell. Haskell is fun. World says hello."
+    let frequencies = countWords sampleText
+    printWordFrequencies frequencies
