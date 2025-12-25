29b750dba962df5838d1acc3e384867853697b5f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-25 21:45:34 +0000

    Add function to count word frequencies in a text

--- test/WordFrequencyCounter.hs
+++ test/WordFrequencyCounter.hs
@@ -18,4 +18,23 @@ topNWords n text = take n $ sortOn (negate . snd) $ Map.toList (countWords text)
 displayFrequencies :: String -> IO ()
 displayFrequencies text = do
   putStrLn "Word frequencies:"
-  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) $ topNWords 10 text
+  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) $ topNWords 10 textmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import qualified Data.Map as Map
+
+countWordFrequencies :: String -> [(String, Int)]
+countWordFrequencies text =
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        frequencyMap = foldr (\word -> Map.insertWith (+) word 1) Map.empty wordsList
+    in sortOn (negate . snd) $ Map.toList frequencyMap
+  where
+    cleanWord = map toLower . filter isAlphaNum
+
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies freqList =
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) freqList
+
+processText :: String -> String
+processText = displayFrequencies . countWordFrequencies
