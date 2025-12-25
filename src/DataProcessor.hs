9e8291493d266d1fee893332c0c94f3b26b7f12b
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-25 23:26:58 +0000

    Add function to filter and transform a list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -21,3 +21,10 @@ main = do
     
     putStrLn "\nSum of doubled numbers:"
     print $ sumProcessed (*2) numbers
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
