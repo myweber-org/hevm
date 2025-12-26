bc8ff19ffd2f4c9baa86d102d2388c189594e418
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-26 15:28:28 +0000

    Add prime factorization function using trial division

---
+++ bench/PrimeFactors.hs
@@ -0,0 +1,10 @@
+module PrimeFactors (primeFactors) where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = factors n 2
+  where
+    factors 1 _ = []
+    factors m p
+      | m < p * p = [m]
+      | m `mod` p == 0 = p : factors (m `div` p) p
+      | otherwise = factors m (p + 1)
