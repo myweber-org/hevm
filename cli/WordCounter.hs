dff958a6aa89983133010bbe2c684b1ae0a0cb49
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-26 15:02:15 +0000

    Add a simple word counting function for text analysis

--- cli/WordCounter.hs
+++ cli/WordCounter.hs
@@ -24,3 +24,20 @@ main = do
     putStrLn $ "Word count: " ++ show (countWords cleaned)
     putStrLn $ "Character count: " ++ show (countCharacters cleaned)
     putStrLn $ "Line count: " ++ show (countLines cleaned)
+module WordCounter where
+
+import Data.Char (isSpace)
+import Data.List (group, sort)
+
+countWords :: String -> [(String, Int)]
+countWords text = 
+    let wordsList = filter (not . all isSpace) $ splitWords text
+        lowerWords = map (map toLower) wordsList
+        sortedWords = sort lowerWords
+        grouped = group sortedWords
+    in map (\ws -> (head ws, length ws)) grouped
+  where
+    splitWords = words . map (\c -> if isSpace c then ' ' else c)
+    toLower c
+        | c >= 'A' && c <= 'Z' = toEnum (fromEnum c + 32)
+        | otherwise = c
