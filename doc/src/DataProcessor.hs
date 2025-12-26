467bff89aae4e21db72bc2d87b52591b1dc756d2
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-26 23:48:16 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -50,3 +50,26 @@ movingAverage n xs
 tails :: [a] -> [[a]]
 tails [] = [[]]
 tails xs@(_:ys) = xs : tails ys
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessed :: (Int -> Int) -> [Int] -> Int
+sumProcessed processor = sum . map processor
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    putStrLn "Original list:"
+    print numbers
+    
+    putStrLn "\nSquares of even numbers:"
+    let squares = processEvenSquares numbers
+    print squares
+    
+    putStrLn "\nSum of squares of even numbers:"
+    print $ sumProcessed (\x -> x * x) (filter even numbers)
