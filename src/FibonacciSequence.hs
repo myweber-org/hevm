6e822a78d756005e950fcbfc8a49d3f7d3b9fe26
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-26 20:10:13 +0000

    Add a function to generate Fibonacci sequence up to a given number

--- src/FibonacciSequence.hs
+++ src/FibonacciSequence.hs
@@ -1,23 +1,8 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
+fibonacci :: Integer -> [Integer]
+fibonacci n
+    | n <= 0    = []
+    | otherwise = takeWhile (<= n) fibs
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module Fibonacci where
-
-import Data.Function (fix)
-
-fibonacci :: Int -> Integer
-fibonacci = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibonacci (n - 1) + fibonacci (n - 2)
-
--- Alternative implementation using fix for memoization
-fibonacciMemoized :: Int -> Integer
-fibonacciMemoized = fix memoFib
-  where
-    memoFib _ 0 = 0
-    memoFib _ 1 = 1
-    memoFib f n = f (n - 1) + f (n - 2)
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
