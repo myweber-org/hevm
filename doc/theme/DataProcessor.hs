d67f2fb3dd0973f19c8afbba50c1efbacd270128
InkspillVerse <inkspill.verse@outlook.com>
2025-12-26 21:11:57 +0000

    Add function to filter and transform a list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -1,5 +1,8 @@
 
 module DataProcessor where
 
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
 processNumbers :: [Int] -> [Int]
-processNumbers = map (^2) . filter (>0)
+processNumbers = filterAndTransform (> 0) (* 2)
