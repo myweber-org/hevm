3e326f78723704e44582809e8eaa2164b8cfc386
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-26 15:40:10 +0000

    Add word frequency counter for text analysis

--- test/WordFrequency.hs
+++ test/WordFrequency.hs
@@ -20,4 +20,35 @@ displayFrequency :: [(String, Int)] -> String
 displayFrequency = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
-analyzeText n = displayFrequency . topWords n
+analyzeText n = displayFrequency . topWords nmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let words' = filter (not . null) $ map cleanWord $ words text
+        cleaned = filter (all isAlpha) words'
+        grouped = foldr countWord [] cleaned
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    
+    countWord :: String -> [WordCount] -> [WordCount]
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+displayFrequency :: [WordCount] -> String
+displayFrequency counts = unlines $ map showCount counts
+  where
+    showCount (word, count) = word ++ ": " ++ show count
+
+analyzeText :: String -> String
+analyzeText text = 
+    let freq = countWords text
+    in "Top 10 most frequent words:\n" ++ displayFrequency freq
