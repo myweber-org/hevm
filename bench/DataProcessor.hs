0288a322c93cc3e7384d5e3e6816ec7422c989e9
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-26 15:13:58 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -14,4 +14,25 @@ computeAverages rows
     transpose = foldr (zipWith (:)) (repeat [])
 
 processCSVData :: String -> [Double]
-processCSVData = computeAverages . parseCSV
+processCSVData = computeAverages . parseCSVmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubled :: [Int] -> Int
+sumPositiveDoubled = sum . processNumbers
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+main :: IO ()
+main = do
+    let numbers = [-3, 2, 0, 5, -1, 8]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
+    putStrLn $ "Sum of positive doubled: " ++ show (sumPositiveDoubled numbers)
+    putStrLn $ "First element: " ++ show (safeHead numbers)
