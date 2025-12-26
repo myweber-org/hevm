ef1aada8e988954e3968030be58cec375b65d9e5
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-26 19:16:36 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -7,28 +7,8 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Bool
-validateInput xs = all (\x -> x >= -100 && x <= 100) xs
-
 main :: IO ()
 main = do
-    let sampleData = [-5, 2, 0, 8, -3, 10]
-    if validateInput sampleData
-        then print $ processData sampleData
-        else putStrLn "Invalid input data"module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
-
-main :: IO ()
-main = do
-    let testData = [1..10]
-    putStrLn $ "Original data: " ++ show testData
-    putStrLn $ "Even squares: " ++ show (processEvenSquares testData)
-    putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) testData)
+    let input = [1, -2, 3, 0, 5, -8]
+    let result = processData input
+    print result
