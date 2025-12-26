75aa5fe3feef5faa930dcaa0e358860f387117f6
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-26 14:39:30 +0000

    Add inorder traversal for binary tree

---
+++ bench/BinaryTreeTraversal.hs
@@ -0,0 +1,14 @@
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
+
+inorder :: BinaryTree a -> [a]
+inorder Empty = []
+inorder (Node x left right) = inorder left ++ [x] ++ inorder right
+
+preorder :: BinaryTree a -> [a]
+preorder Empty = []
+preorder (Node x left right) = [x] ++ preorder left ++ preorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Empty = []
+postorder (Node x left right) = postorder left ++ postorder right ++ [x]
