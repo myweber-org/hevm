e8ce8e4611e4e8f29fd62bd04d20e43542fc5a8a
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-26 21:39:21 +0000

    Add function to calculate moving average of a list

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -32,4 +32,21 @@ validateInput = all (\x -> x >= -100 && x <= 100)
 safeProcessData :: [Int] -> Maybe [Int]
 safeProcessData xs
     | validateInput xs = Just (processData xs)
-    | otherwise = Nothing
+    | otherwise = Nothingmodule DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . tails
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+-- Helper function from Data.List
+tails :: [a] -> [[a]]
+tails [] = [[]]
+tails xs@(_:ys) = xs : tails ys
