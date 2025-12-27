0ffd69dbc630b6433725b75a39932c32c3c9da38
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-27 23:29:05 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -15,4 +15,23 @@ exampleUsage = do
     putStrLn "Original series:"
     print dataSeries
     putStrLn "Moving average (window size 3):"
-    print $ movingAverage 3 dataSeries
+    print $ movingAverage 3 dataSeriesmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+main :: IO ()
+main = do
+    let inputData = [1, -2, 3, 0, 5, -10]
+    if validateInput inputData
+        then do
+            let result = processData inputData
+            putStrLn $ "Processed data: " ++ show result
+        else putStrLn "Invalid input data detected"
