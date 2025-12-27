c8cff28a468f69a8de2acb175c45285bdd503a4e
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-27 01:14:11 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- bench/FibonacciSequence.hs
+++ bench/FibonacciSequence.hs
@@ -1,6 +1,14 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> [Int]
-fibonacci n = takeWhile (<= n) fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibonacci :: Int -> [Integer]
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
+
+main :: IO ()
+main = do
+    putStrLn "Fibonacci sequence up to 10 terms:"
+    print $ fibonacci 10
