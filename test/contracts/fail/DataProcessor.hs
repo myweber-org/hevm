5b4d3c3d4a920551dd6868ae6f167ce118f5b737
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-27 19:34:21 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -24,4 +24,30 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 10) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = 
+    if all (> 0) xs 
+    then Just xs 
+    else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [5, 12, 8, 20, 3, 15]
+    case validateInput sampleData of
+        Just validData -> do
+            let result = sumProcessed validData
+            putStrLn $ "Sum of processed numbers: " ++ show result
+        Nothing -> 
+            putStrLn "Invalid input: all numbers must be positive"
