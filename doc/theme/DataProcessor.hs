2b3b8736b9265b3fdb4a21306e540447a58790e1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-27 22:29:21 +0000

    Add function to calculate moving average of a list

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -1,32 +1,18 @@
-
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | length xs < n = []
+    | otherwise = avg : movingAverage n (tail xs)
+  where
+    window = take n xs
+    avg = sum window / fromIntegral n
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processEvenSquares
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput [] = Nothing
-validateInput xs = Just xs
-
-main :: IO ()
-main = do
-    let sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    case validateInput sampleData of
-        Nothing -> putStrLn "No data to process"
-        Just data' -> do
-            putStrLn $ "Original data: " ++ show data'
-            putStrLn $ "Processed data: " ++ show (processEvenSquares data')
-            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData data')
+-- Example usage with a helper function
+exampleUsage :: IO ()
+exampleUsage = do
+    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    putStrLn "Original series:"
+    print dataSeries
+    putStrLn "Moving average (window size 3):"
+    print $ movingAverage 3 dataSeries
