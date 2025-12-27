4dfdf59e98717f67a3c7c0ac89b0838e98fa15ac
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-27 15:34:35 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -1,20 +1,5 @@
+
 module DataProcessor where
-
-import Data.List.Split (splitOn)
-
-parseCSV :: String -> [[Double]]
-parseCSV csv = map (map read . splitOn ",") (lines csv)
-
-computeAverages :: [[Double]] -> [Double]
-computeAverages rows
-  | null rows = []
-  | otherwise = map average (transpose rows)
-  where
-    average xs = sum xs / fromIntegral (length xs)
-    transpose = foldr (zipWith (:)) (repeat [])
-
-processCSVData :: String -> [Double]
-processCSVData = computeAverages . parseCSVmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
@@ -22,17 +7,12 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
-sumPositiveDoubled :: [Int] -> Int
-sumPositiveDoubled = sum . processNumbers
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
 
 main :: IO ()
 main = do
-    let numbers = [-3, 2, 0, 5, -1, 8]
+    let numbers = [-3, 1, 0, 5, -2, 8]
     putStrLn $ "Original list: " ++ show numbers
     putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of positive doubled: " ++ show (sumPositiveDoubled numbers)
-    putStrLn $ "First element: " ++ show (safeHead numbers)
+    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
