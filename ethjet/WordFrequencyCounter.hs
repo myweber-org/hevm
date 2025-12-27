5278aad28420498c10d6e73ee7e2638605a249d9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-27 23:29:50 +0000

    Add function to count word frequencies in a text

--- ethjet/WordFrequencyCounter.hs
+++ ethjet/WordFrequencyCounter.hs
@@ -31,4 +31,29 @@ sampleText = "Hello world! Hello Haskell. World says hello back."
 main :: IO ()
 main = do
     putStrLn "Word frequency analysis:"
-    putStrLn $ processText sampleText
+    putStrLn $ processText sampleTextmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        frequencyMap = foldr countWord [] wordsList
+    in take 10 $ sortOn (Down . snd) frequencyMap
+  where
+    cleanWord = map toLower . filter isAlphaNum
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+displayCounts :: [WordCount] -> String
+displayCounts counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+processText :: String -> String
+processText = displayCounts . countWords
