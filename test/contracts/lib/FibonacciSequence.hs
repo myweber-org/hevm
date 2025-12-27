7d6bab925426ebf4d684073a2dc1e8ef25793ac6
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-27 23:53:32 +0000

    Add Fibonacci sequence generator with memoization

--- test/contracts/lib/FibonacciSequence.hs
+++ test/contracts/lib/FibonacciSequence.hs
@@ -1,6 +1,11 @@
-module FibonacciSequence where
+module Fibonacci where
 
-fib :: Int -> Integer
-fib n = fibs !! n
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    mapM_ (print . fibonacci) [0..19]
