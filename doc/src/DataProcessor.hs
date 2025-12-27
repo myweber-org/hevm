7589739620cecc4510db5d98172bebbee8c31bfb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-27 22:53:12 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -1,41 +1,20 @@
+
 module DataProcessor where
 
-import Data.Char (isDigit, isAlpha)
-import Data.List (intercalate)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
-data ValidationError = InvalidFormat String | MissingField String
-    deriving (Show, Eq)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-validatePhone :: String -> Either ValidationError String
-validatePhone phone
-    | all isDigit phone && length phone == 10 = Right phone
-    | otherwise = Left $ InvalidFormat "Phone must be 10 digits"
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
 
-validateEmail :: String -> Either ValidationError String
-validateEmail email
-    | '@' `elem` email && '.' `elem` (dropWhile (/= '@') email) = Right email
-    | otherwise = Left $ InvalidFormat "Invalid email format"
+validateInput :: [Int] -> Bool
+validateInput xs = not (null xs) && all (\x -> x >= -100 && x <= 100) xs
 
-transformName :: String -> String
-transformName = unwords . map capitalize . words
-  where
-    capitalize [] = []
-    capitalize (x:xs) = toUpper x : map toLower xs
-    toUpper = toEnum . (+ (-32)) . fromEnum
-    toLower c = if c >= 'A' && c <= 'Z'
-                then toEnum (fromEnum c + 32)
-                else c
-
-processRecord :: [(String, String)] -> Either ValidationError [(String, String)]
-processRecord fields = do
-    phone <- maybe (Left $ MissingField "phone") validatePhone $ lookup "phone" fields
-    email <- maybe (Left $ MissingField "email") validateEmail $ lookup "email" fields
-    let name = maybe "" transformName $ lookup "name" fields
-    return [("name", name), ("phone", phone), ("email", email)]
-
-formatCSV :: [[(String, String)]] -> String
-formatCSV records = 
-    let headers = ["name", "phone", "email"]
-        headerLine = intercalate "," headers
-        recordLines = map (intercalate "," . map snd) records
-    in unlines $ headerLine : recordLines
+safeProcess :: [Int] -> Maybe Int
+safeProcess xs
+    | validateInput xs = Just (sumProcessedData xs)
+    | otherwise = Nothing
