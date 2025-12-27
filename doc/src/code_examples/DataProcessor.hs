8257d134a713f91227d6cf6f1dc95e68f8921fdf
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-27 15:57:03 +0000

    Add function to filter and transform a list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -11,4 +11,10 @@ main :: IO ()
 main = do
     let input = [-3, 1, 0, 5, -2, 8]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
