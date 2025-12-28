f563f205f2990562bfa7a031e1019ed637bf3fc3
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-28 23:52:24 +0000

    Add function to calculate moving average of a list

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -31,4 +31,21 @@ main :: IO ()
 main = do
     let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
+    average zs = sum zs / fromIntegral (length zs)
+
+safeMovingAverage :: Fractional a => Int -> [a] -> Maybe [a]
+safeMovingAverage n xs
+    | n <= 0 = Nothing
+    | otherwise = Just $ movingAverage n xs
+
+testData :: [Double]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
