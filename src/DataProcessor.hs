187636b80510f3c88c56cd5c793239cd05c16789
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-28 14:53:52 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -2,7 +2,8 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
 processEvenSquares :: [Int] -> [Int]
 processEvenSquares = filterAndTransform even (\x -> x * x)
@@ -13,81 +14,7 @@ sumProcessed processor = sum . map processor
 main :: IO ()
 main = do
     let numbers = [1..10]
-    putStrLn "Original list:"
-    print numbers
-    
-    putStrLn "\nSquares of even numbers:"
-    print $ processEvenSquares numbers
-    
-    putStrLn "\nSum of doubled numbers:"
-    print $ sumProcessed (*2) numbers
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, 0, 5, -7]
-    let result = processNumbers input
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Bool
-validateInput xs = all (\x -> x >= -100 && x <= 100) xs
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5, -8]
-    if validateInput sampleData
-        then do
-            putStrLn "Processing valid data..."
-            print $ processData sampleData
-        else putStrLn "Invalid input data detected"
-module DataProcessor where
-
-import Data.List.Split (splitOn)
-import Data.Maybe (mapMaybe)
-
-type Record = (String, Double, Double)
-
-parseCSV :: String -> [Record]
-parseCSV content = mapMaybe parseLine (lines content)
-  where
-    parseLine line = case splitOn "," line of
-      [name, val1, val2] -> 
-        case (reads val1, reads val2) of
-          ([(v1, "")], [(v2, "")]) -> Just (name, v1, v2)
-          _ -> Nothing
-      _ -> Nothing
-
-calculateAverages :: [Record] -> (Double, Double)
-calculateAverages records = (avg firsts, avg seconds)
-  where
-    firsts = map (\(_, v1, _) -> v1) records
-    seconds = map (\(_, _, v2) -> v2) records
-    avg xs = sum xs / fromIntegral (length xs)
-
-processData :: String -> (Double, Double)
-processData = calculateAverages . parseCSV
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
+    putStrLn $ "Sum of doubled evens: " ++ 
+        show (sumProcessed (*2) (filter even numbers))
