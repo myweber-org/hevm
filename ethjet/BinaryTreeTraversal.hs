741237af871cf343bbbb93224ddae972d5f61043
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-28 01:30:24 +0000

    Implement in-order, pre-order, and post-order binary tree traversal functions

---
+++ ethjet/BinaryTreeTraversal.hs
@@ -0,0 +1,23 @@
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
+
+inOrder :: BinaryTree a -> [a]
+inOrder Empty = []
+inOrder (Node val left right) = inOrder left ++ [val] ++ inOrder right
+
+preOrder :: BinaryTree a -> [a]
+preOrder Empty = []
+preOrder (Node val left right) = [val] ++ preOrder left ++ preOrder right
+
+postOrder :: BinaryTree a -> [a]
+postOrder Empty = []
+postOrder (Node val left right) = postOrder left ++ postOrder right ++ [val]
+
+sampleTree :: BinaryTree Int
+sampleTree = Node 1
+                (Node 2
+                    (Node 4 Empty Empty)
+                    (Node 5 Empty Empty))
+                (Node 3
+                    Empty
+                    (Node 6 Empty Empty))
