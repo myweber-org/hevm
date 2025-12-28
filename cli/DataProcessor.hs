e661a8dc4e5f1109a7741893c24995e9e17e64e5
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-28 20:52:59 +0000

    Add function to calculate moving average of a list

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -8,4 +8,21 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | length xs < n = []
+    | otherwise = avg : movingAverage n (tail xs)
+  where
+    window = take n xs
+    avg = sum window / fromIntegral n
+
+-- Example usage with a helper function
+exampleUsage :: IO ()
+exampleUsage = do
+    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    putStrLn "Original series:"
+    print dataSeries
+    putStrLn $ "Moving average (window size 3): " ++ show (movingAverage 3 dataSeries)
+    putStrLn $ "Moving average (window size 5): " ++ show (movingAverage 5 dataSeries)
