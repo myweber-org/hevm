c8be61f91086c16bd281df0a325a7fafe2aef579
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-28 23:05:57 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -4,25 +4,11 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbersmodule DataProcessor where
-
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | length xs < n = []
-    | otherwise = avg : movingAverage n (tail xs)
-  where
-    window = take n xs
-    avg = sum window / fromIntegral n
-
--- Example usage with a helper function
-exampleUsage :: IO ()
-exampleUsage = do
-    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-    putStrLn "Original series:"
-    print dataSeries
-    putStrLn $ "Moving average (window size 3): " ++ show (movingAverage 3 dataSeries)
-    putStrLn $ "Moving average (window size 5): " ++ show (movingAverage 5 dataSeries)
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
