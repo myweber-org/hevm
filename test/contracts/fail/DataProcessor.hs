cbab6a5ba4618c4ea1a0a0618578b9e6ec90abac
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-29 16:49:14 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -1,57 +1,13 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
+filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (*2)
+processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
 
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original: " ++ show numbers
-    putStrLn $ "Processed: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedList :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedList predicate transformer = sum . filterAndTransform predicate transformer
-
 validateInput :: [Int] -> Maybe [Int]
-validateInput [] = Nothing
-validateInput xs = Just xs
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    case validateInput numbers of
-        Nothing -> putStrLn "Empty input list"
-        Just validNumbers -> do
-            putStrLn $ "Original list: " ++ show validNumbers
-            putStrLn $ "Even squares: " ++ show (processEvenSquares validNumbers)
-            putStrLn $ "Sum of even squares: " ++ 
-                show (sumProcessedList even (\x -> x * x) validNumbers)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform even (\x -> x * x + 1)
-
-main :: IO ()
-main = do
-    let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let result = processData input
-    putStrLn $ "Input: " ++ show input
-    putStrLn $ "Result: " ++ show result
+validateInput xs = if all (> -100) xs then Just xs else Nothing
