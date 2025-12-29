a2cf54793ae12c4f94199fe1ace55aef4020d4d3
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-29 22:57:58 +0000

    Implement Sieve of Eratosthenes to generate primes up to a given limit

--- test/contracts/PrimeSieve.hs
+++ test/contracts/PrimeSieve.hs
@@ -16,4 +16,15 @@ main = do
     putStrLn "Enter limit:"
     input <- getLine
     let limit = read input :: Int
-    print $ primesUpTo limit
+    print $ primesUpTo limitmodule PrimeSieve where
+
+sieve :: Int -> [Int]
+sieve limit
+    | limit < 2 = []
+    | otherwise = go [2..limit] []
+  where
+    go []     acc = reverse acc
+    go (p:xs) acc = go [x | x <- xs, x `mod` p /= 0] (p:acc)
+
+primesUpTo :: Int -> [Int]
+primesUpTo = sieve
