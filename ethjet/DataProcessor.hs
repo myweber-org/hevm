0f8dddb730e73c3cdfd124c0b932af1b9b434ec6
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-29 20:14:21 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,27 +1,19 @@
+
 module DataProcessor where
 
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-  where
-    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
-    average zs = sum zs / fromIntegral (length zs)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
--- Alternative implementation using list comprehensions
-movingAverage' :: Fractional a => Int -> [a] -> [a]
-movingAverage' n xs
-    | n <= 0 = error "Window size must be positive"
-    | otherwise = [sum window / fromIntegral n | window <- windows n xs]
-  where
-    windows m ys = [take m (drop i ys) | i <- [0..length ys - m]]
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
--- Helper function to test with sample data
-testMovingAverage :: IO ()
-testMovingAverage = do
-    let testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-    putStrLn "Testing moving average with window size 3:"
-    print $ movingAverage 3 testData
-    putStrLn "Testing moving average with window size 5:"
-    print $ movingAverage 5 testData
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+main :: IO ()
+main = do
+    let numbers = [-3, 1, 4, -1, 5, 9, -2, 6]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
+    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
