65046e551a4e819e39612c1ea96134713c93af2c
InkspillVerse <inkspill.verse@outlook.com>
2025-12-29 18:38:22 +0000

    Add Fibonacci sequence generator with memoization

--- bench/FibonacciSequence.hs
+++ bench/FibonacciSequence.hs
@@ -1,14 +1,10 @@
-module FibonacciSequence where
+module Fibonacci where
 
-fibonacci :: Int -> [Integer]
-fibonacci n
-    | n <= 0    = []
-    | n == 1    = [0]
-    | n == 2    = [0, 1]
-    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-                  in take n fibs
+fib :: Int -> Integer
+fib n = fibs !! n
+  where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 main :: IO ()
 main = do
-    putStrLn "Fibonacci sequence up to 10 terms:"
-    print $ fibonacci 10
+    putStrLn "Fibonacci sequence up to 20:"
+    mapM_ (print . fib) [0..20]
