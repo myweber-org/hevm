a492ecb3babefc31b39c7a6e6aa8f3491668183b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-29 15:33:03 +0000

    Add function to calculate moving average of a list

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,37 +1,4 @@
-
 module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processDatamodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (*2)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print result
-module DataProcessor where
-
-processData :: [Int] -> [Int]
-processData = map (^2) . filter even
-
-main :: IO ()
-main = do
-    let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let result = processData input
-    print resultmodule DataProcessor where
 
 movingAverage :: Fractional a => Int -> [a] -> [a]
 movingAverage n xs
@@ -42,17 +9,19 @@ movingAverage n xs
     windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
     average zs = sum zs / fromIntegral (length zs)
 
-safeMovingAverage :: Fractional a => Int -> [a] -> Maybe [a]
-safeMovingAverage n xs
-    | n <= 0 = Nothing
-    | otherwise = Just $ movingAverage n xs
+-- Alternative implementation using list comprehensions
+movingAverage' :: Fractional a => Int -> [a] -> [a]
+movingAverage' n xs
+    | n <= 0 = error "Window size must be positive"
+    | otherwise = [sum window / fromIntegral n | window <- windows n xs]
+  where
+    windows m ys = [take m (drop i ys) | i <- [0..length ys - m]]
 
-testData :: [Double]
-testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+-- Helper function to test with sample data
+testMovingAverage :: IO ()
+testMovingAverage = do
+    let testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    putStrLn "Testing moving average with window size 3:"
+    print $ movingAverage 3 testData
+    putStrLn "Testing moving average with window size 5:"
+    print $ movingAverage 5 testData
