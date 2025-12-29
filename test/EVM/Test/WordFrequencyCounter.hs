b5e54b7d6030e6c43c004c4ae5bb2e01766f5d4c
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-29 15:11:26 +0000

    Add word frequency counting utility with sorting and filtering

--- test/EVM/Test/WordFrequencyCounter.hs
+++ test/EVM/Test/WordFrequencyCounter.hs
@@ -23,4 +23,35 @@ displayFrequencies freqs =
     unlines $ map (\(word, count) -> word ++ ": " ++ show count) freqs
 
 processText :: String -> String
-processText = displayFrequencies . countWordFrequencies
+processText = displayFrequencies . countWordFrequenciesmodule WordFrequencyCounter where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+type WordCount = Map.Map T.Text Int
+
+countWords :: T.Text -> WordCount
+countWords = Map.fromListWith (+) . map (\w -> (w, 1)) . filter (not . T.null) . map normalize . T.words
+  where
+    normalize = T.toLower . T.filter Char.isLetter
+
+sortByFrequency :: WordCount -> [(T.Text, Int)]
+sortByFrequency = List.sortBy (\(_, c1) (_, c2) -> compare c2 c1) . Map.toList
+
+filterByMinFrequency :: Int -> WordCount -> WordCount
+filterByMinFrequency minFreq = Map.filter (>= minFreq)
+
+processText :: T.Text -> Int -> [(T.Text, Int)]
+processText text minFreq = sortByFrequency $ filterByMinFrequency minFreq $ countWords text
+
+printResults :: [(T.Text, Int)] -> IO ()
+printResults = mapM_ (\(word, count) -> TIO.putStrLn $ T.pack (show count) <> " " <> word)
+
+main :: IO ()
+main = do
+    content <- TIO.readFile "input.txt"
+    let results = processText content 3
+    printResults results
