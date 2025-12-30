f3b098bd13d62f6d800369f8f5f82f0fcaa4f827
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-30 16:39:56 +0000

    Add Fibonacci sequence generator with memoization

--- cli/FibonacciSequence.hs
+++ cli/FibonacciSequence.hs
@@ -1,15 +1,11 @@
 module FibonacciSequence where
 
-import Data.Function (fix)
-
-fibMemo :: Int -> Integer
-fibMemo = fix memoize
-  where
-    memoize _ 0 = 0
-    memoize _ 1 = 1
-    memoize f n = f (n - 1) + f (n - 2)module FibonacciSequence where
-
-fibonacci :: Integer -> [Integer]
-fibonacci n = takeWhile (<= n) fibs
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    mapM_ (print . fibonacci) [0..19]
