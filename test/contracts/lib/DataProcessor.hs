72470fe9c48a68dd3a1dd87990ac8b859bd64d19
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-30 21:05:23 +0000

    Add function to filter and transform a list of integers

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -13,3 +13,10 @@ sumPositiveDoubles = sum . processNumbersmodule DataProcessor where
 
 processData :: [Int] -> [Int]
 processData = map (^2) . filter even
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
