02ba4f9071b0c253cd53b2a946c872cc54882a55
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-30 14:47:15 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -11,4 +11,7 @@ main :: IO ()
 main = do
     let numbers = [-5, 3, 0, 8, -2, 10]
     let result = processNumbers numbers
-    print result
+    print resultmodule DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter (>0)
