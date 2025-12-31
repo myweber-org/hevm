775a619ce82454e078d6a0135b443d51c230fde5
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-31 19:46:48 +0000

    Implement a simple prime sieve using the Sieve of Eratosthenes algorithm

--- test/PrimeSieve.hs
+++ test/PrimeSieve.hs
@@ -24,4 +24,12 @@ primesUpTo n
     | otherwise = sieve [2..n]
   where
     sieve [] = []
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]module PrimeSieve where
+
+primes :: [Int]
+primes = sieve [2..]
+  where
     sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
