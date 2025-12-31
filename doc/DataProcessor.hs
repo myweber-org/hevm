74f9b3b518507ae656419d1bf65f2730a18d692e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-31 17:04:47 +0000

    Add function to filter and transform a list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -1,40 +1,5 @@
+
 module DataProcessor where
-
-import Data.List (tails)
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | n > length xs = error "Window size exceeds list length"
-    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
-  where
-    average :: [Double] -> Double
-    average ys = sum ys / fromIntegral (length ys)
-
-smoothData :: [Double] -> [Double]
-smoothData = movingAverage 3
-
-main :: IO ()
-main = do
-    let sampleData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-    putStrLn "Original data:"
-    print sampleData
-    putStrLn "\nSmoothed data (3-point moving average):"
-    print $ smoothData sampleDatamodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (>0) (*2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just xmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
@@ -44,13 +9,6 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
+    let numbers = [-5, 3, 0, 8, -2, 10]
     let result = processNumbers numbers
     print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
