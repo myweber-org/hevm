5f2767c98fe59d137597366806b295559979ed6c
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-31 00:16:00 +0000

    Add a function to generate Fibonacci sequence up to a given number of terms

--- src/FibonacciSequence.hs
+++ src/FibonacciSequence.hs
@@ -4,10 +4,3 @@ fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-    putStrLn "Enter the number of Fibonacci terms:"
-    input <- getLine
-    let n = read input :: Int
-    print $ fibonacci n
