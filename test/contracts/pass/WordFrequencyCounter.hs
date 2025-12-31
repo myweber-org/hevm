c3e08c2b6e2f544cb09526e7a235c33c7f646848
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-31 23:21:05 +0000

    Add word frequency counter with sorting and filtering capabilities

--- test/contracts/pass/WordFrequencyCounter.hs
+++ test/contracts/pass/WordFrequencyCounter.hs
@@ -35,4 +35,48 @@ main = do
     args <- getArgs
     case args of
         [] -> putStrLn "Usage: wordfreq <file1> [file2 ...]"
-        files -> mapM_ processFile files
+        files -> mapM_ processFile filesmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = group $ sort cleanedWords
+    in map (\ws -> (head ws, length ws)) grouped
+
+cleanWord :: String -> String
+cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+
+sortByFrequency :: [WordCount] -> [WordCount]
+sortByFrequency = sortOn (Down . snd)
+
+sortAlphabetically :: [WordCount] -> [WordCount]
+sortAlphabetically = sortOn fst
+
+filterByMinFrequency :: Int -> [WordCount] -> [WordCount]
+filterByMinFrequency minFreq = filter ((>= minFreq) . snd)
+
+getTopNWords :: Int -> [WordCount] -> [WordCount]
+getTopNWords n = take n . sortByFrequency
+
+processText :: String -> Int -> Int -> [WordCount]
+processText text minFreq topN = 
+    let counted = countWords text
+        filtered = filterByMinFrequency minFreq counted
+    in getTopNWords topN filtered
+
+displayResults :: [WordCount] -> IO ()
+displayResults counts = 
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
+
+sampleAnalysis :: IO ()
+sampleAnalysis = do
+    let sampleText = "Hello world! Hello Haskell. Haskell is great. World says hello to Haskell."
+    putStrLn "Word frequency analysis:"
+    displayResults $ processText sampleText 1 5
