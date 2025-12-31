9a294b9437eab7177b23c0a79fcc356a656b3fe1
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-31 20:46:04 +0000

    Implement in-order, pre-order, and post-order binary tree traversal functions

--- src/BinaryTreeTraversal.hs
+++ src/BinaryTreeTraversal.hs
@@ -10,4 +10,27 @@ inOrderIterative tree = go [] tree []
         [] -> reverse result
         (Node Empty val right):rest -> go rest right (val:result)
         (Node left val right):rest -> go (Node Empty val right:stack) left result
-    go stack (Node left val right) result = go (Node left val right:stack) left result
+    go stack (Node left val right) result = go (Node left val right:stack) left resultmodule BinaryTreeTraversal where
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+
+inOrder :: BinaryTree a -> [a]
+inOrder Empty = []
+inOrder (Node value left right) = inOrder left ++ [value] ++ inOrder right
+
+preOrder :: BinaryTree a -> [a]
+preOrder Empty = []
+preOrder (Node value left right) = [value] ++ preOrder left ++ preOrder right
+
+postOrder :: BinaryTree a -> [a]
+postOrder Empty = []
+postOrder (Node value left right) = postOrder left ++ postOrder right ++ [value]
+
+exampleTree :: BinaryTree Int
+exampleTree = Node 1
+                (Node 2
+                    (Node 4 Empty Empty)
+                    (Node 5 Empty Empty))
+                (Node 3
+                    (Node 6 Empty Empty)
+                    Empty)
