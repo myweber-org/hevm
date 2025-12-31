813e18810ace726c4c71aecd3cf26b3e9b6cf809
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-31 23:38:43 +0000

    Add function to calculate the maximum depth of a binary tree

--- cli/BinaryTreeDepth.hs
+++ cli/BinaryTreeDepth.hs
@@ -16,4 +16,8 @@ sampleTree = Node 1
                     (Node 5 Empty Empty))
                 (Node 3 
                     Empty 
-                    (Node 6 Empty Empty))
+                    (Node 6 Empty Empty))data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
+
+treeDepth :: BinaryTree a -> Int
+treeDepth Empty = 0
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
