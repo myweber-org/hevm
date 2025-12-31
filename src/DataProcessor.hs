3289aa43b189e1da7500c117d7b48a5921c7b3de
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-31 15:44:23 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -11,3 +11,33 @@ main = do
     let numbers = [-3, 1, 0, 5, -2, 8]
     let result = processNumbers numbers
     print result
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processEvenNumbers :: [Int] -> [Int]
+processEvenNumbers = filterAndTransform even (*2)
+
+processOddNumbers :: [Int] -> [Int]
+processOddNumbers = filterAndTransform odd (+1)
+
+sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
+sumProcessedData predicate transformer =
+    sum . filterAndTransform predicate transformer
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    putStrLn "Original data:"
+    print sampleData
+    
+    putStrLn "\nDoubled even numbers:"
+    print $ processEvenNumbers sampleData
+    
+    putStrLn "\nIncremented odd numbers:"
+    print $ processOddNumbers sampleData
+    
+    putStrLn "\nSum of processed even numbers:"
+    print $ sumProcessedData even (*2) sampleData
