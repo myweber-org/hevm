0f4bc2bdd3659341e31a57ecf11a4ebd64a83554
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-31 23:43:27 +0000

    Add function to calculate moving average of a list

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -36,3 +36,28 @@ main = do
     putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
     putStrLn $ "Sum of doubled odds: " ++ 
         show (sumProcessed (\x -> x * 2) (filter odd numbers))
+module DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
+  where
+    average :: Fractional a => [a] -> a
+    average ys = sum ys / fromIntegral (length ys)
+
+smoothData :: Fractional a => Int -> [a] -> [a]
+smoothData windowSize dataPoints = movingAverage windowSize dataPoints
+
+main :: IO ()
+main = do
+    let testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    putStrLn "Original data:"
+    print testData
+    putStrLn "\nMoving average with window size 3:"
+    print $ movingAverage 3 testData
+    putStrLn "\nSmoothed data with window size 4:"
+    print $ smoothData 4 testData
