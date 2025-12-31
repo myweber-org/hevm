e8970ef89349b73bec7c477fa4a4656486bf3ed6
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-31 20:08:43 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -65,4 +65,14 @@ main = do
   putStrLn "First 20 Fibonacci numbers:"
   print $ fibonacciList 20
   putStrLn "\nFibonacci number at position 30:"
-  print $ fibonacci 30
+  print $ fibonacci 30module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+  putStrLn "Fibonacci sequence up to 10 terms:"
+  print $ fibonacci 10
