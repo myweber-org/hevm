548362ed98ea399872d6c57d8f75ac2c0600d3e5
InkspillVerse <inkspill.verse@outlook.com>
2025-12-31 16:25:37 +0000

    Add function to calculate moving average of a list

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -1,10 +1,16 @@
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
+    average zs = sum zs / fromIntegral (length zs)
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+smoothData :: Fractional a => Int -> [a] -> [a]
+smoothData windowSize dataList =
+    let avg = movingAverage windowSize dataList
+        padding = replicate (windowSize `div` 2) (head dataList)
+    in padding ++ avg ++ padding
