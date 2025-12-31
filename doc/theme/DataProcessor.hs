bd85486d34840fda195903d015e352eda2f77bed
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-31 14:57:49 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -51,3 +51,13 @@ main = do
   putStrLn $ "Parsed records: " ++ show records
   putStrLn $ "Average: " ++ show avg
   putStrLn $ "Above average: " ++ show (filterAboveAverage records avg)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
