13595b76817b47f2c643d7aaee6c9641bc943c49
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-31 16:04:12 +0000

    Add function to calculate moving average of a list

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -1,8 +1,17 @@
-
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+import Data.List (tails)
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
+  where
+    average window = sum window / fromIntegral n
+
+-- Example usage (commented out for production):
+-- main :: IO ()
+-- main = do
+--     let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+--     print $ movingAverage 3 dataSeries
