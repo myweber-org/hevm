77b0c399f67dc70c36fc4cb5c9a001410f668a57
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-01 22:35:57 +0000

    Add function to parse CSV data and calculate average values

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -1,18 +1,45 @@
-
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
+import Data.List.Split (splitOn)
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+type CSVRow = [String]
+type CSVData = [CSVRow]
 
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
 
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = 
-    if all (\x -> x >= -100 && x <= 100) xs
-        then Just xs
-        else Nothing
+numericColumns :: CSVData -> [[Double]]
+numericColumns [] = []
+numericColumns rows = 
+    let transposed = transpose rows
+    in map (mapMaybe readDouble) transposed
+  where
+    transpose :: [[a]] -> [[a]]
+    transpose ([]:_) = []
+    transpose x = map head x : transpose (map tail x)
+    
+    readDouble :: String -> Maybe Double
+    readDouble s = case reads s of
+        [(n, "")] -> Just n
+        _ -> Nothing
+
+calculateAverages :: [[Double]] -> [Double]
+calculateAverages columns = 
+    map average columns
+  where
+    average :: [Double] -> Double
+    average [] = 0.0
+    average xs = sum xs / fromIntegral (length xs)
+
+processCSVFile :: String -> IO [Double]
+processCSVFile filename = do
+    content <- readFile filename
+    let parsed = parseCSV content
+    let numericData = numericColumns parsed
+    return $ calculateAverages numericData
+
+mapMaybe :: (a -> Maybe b) -> [a] -> [b]
+mapMaybe _ [] = []
+mapMaybe f (x:xs) = case f x of
+    Just y -> y : mapMaybe f xs
+    Nothing -> mapMaybe f xs
