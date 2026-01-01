8403c998f2f5a3ae352c3f5c258b39b10583c56a
InkspillVerse <inkspill.verse@outlook.com>
2026-01-01 22:14:12 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/FibonacciSequence.hs
+++ test/FibonacciSequence.hs
@@ -11,4 +11,9 @@ fibonacci n
 main :: IO ()
 main = do
     putStrLn "Fibonacci sequence of 10 terms:"
-    print $ fibonacci 10
+    print $ fibonacci 10module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
