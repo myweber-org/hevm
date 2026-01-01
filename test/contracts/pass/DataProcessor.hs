a071217a3489127a466d75ae8484043f2e7ccd5a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-01 14:39:26 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -1,32 +1,14 @@
-module DataProcessor where
 
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-  where
-    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
-    average zs = sum zs / fromIntegral (length zs)
-
-smoothData :: Fractional a => Int -> [a] -> [a]
-smoothData windowSize dataList =
-    let avg = movingAverage windowSize dataList
-        padding = replicate (windowSize `div` 2) (head dataList)
-    in padding ++ avg ++ padding
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-validateData :: [Int] -> Bool
-validateData xs = all (> 0) xs && length xs > 3
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
 
-combineProcessors :: [Int] -> [Int]
-combineProcessors xs = if validateData xs then processData xs else []module DataProcessor where
-
-processData :: [Int] -> [Int]
-processData = map (^2) . filter even
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
