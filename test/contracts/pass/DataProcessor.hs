af53c2798849720c98dba8e0fc6006729d00ca00
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-01 00:25:54 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -14,3 +14,16 @@ smoothData windowSize dataList =
     let avg = movingAverage windowSize dataList
         padding = replicate (windowSize `div` 2) (head dataList)
     in padding ++ avg ++ padding
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
+
+combineProcessors :: [Int] -> [Int]
+combineProcessors xs = if validateData xs then processData xs else []
