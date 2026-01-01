906d4471c2008761cfaa3435214141591c164db8
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-01 22:16:08 +0000

    Add word frequency counting functionality with command-line argument support

--- cli/WordFrequencyCounter.hs
+++ cli/WordFrequencyCounter.hs
@@ -33,4 +33,39 @@ main :: IO ()
 main = do
     let sampleText = "Hello world! Hello Haskell. Haskell is functional. World says hello back."
     putStrLn "Word Frequency Analysis:"
-    putStrLn $ wordFrequencyReport sampleText 1 5
+    putStrLn $ wordFrequencyReport sampleText 1 5module WordFrequencyCounter where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+import System.Environment (getArgs)
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        grouped = foldr countWord [] cleanedWords
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = filter (\c -> isAlphaNum c || c == '\'')
+    countWord w [] = [(w, 1)]
+    countWord w ((x, n):xs)
+        | w == x = (x, n+1):xs
+        | otherwise = (x, n):countWord w xs
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = unlines $ map formatLine counts
+  where
+    formatLine (word, count) = word ++ ": " ++ show count
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [] -> putStrLn "Usage: wordfreq <filename>"
+        (filename:_) -> do
+            content <- readFile filename
+            let frequencies = countWords content
+            putStrLn $ formatOutput frequencies
