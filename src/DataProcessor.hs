4f1daafb15f0ed4b5a48691061fa5bf5750b6d0e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-01 16:54:13 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -1,53 +1,25 @@
-module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
-    let result = processNumbers numbers
-    print result
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = 
     map transformer . filter predicate
 
-processEvenNumbers :: [Int] -> [Int]
-processEvenNumbers = filterAndTransform even (*2)
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
 
-processOddNumbers :: [Int] -> [Int]
-processOddNumbers = filterAndTransform odd (+1)
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processEvenSquares
 
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer =
-    sum . filterAndTransform predicate transformer
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (>0) xs then Just xs else Nothing
 
 main :: IO ()
 main = do
     let sampleData = [1..10]
-    putStrLn "Original data:"
-    print sampleData
-    
-    putStrLn "\nDoubled even numbers:"
-    print $ processEvenNumbers sampleData
-    
-    putStrLn "\nIncremented odd numbers:"
-    print $ processOddNumbers sampleData
-    
-    putStrLn "\nSum of processed even numbers:"
-    print $ sumProcessedData even (*2) sampleData
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumPositiveDoubled :: [Int] -> Int
-sumPositiveDoubled = sum . processNumbers
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original data: " ++ show validData
+            putStrLn $ "Processed data: " ++ show (processEvenSquares validData)
+            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
+        Nothing -> putStrLn "Invalid input: all numbers must be positive"
