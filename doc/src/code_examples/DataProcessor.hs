f04a86dc616e437497cf9ffa7a1f3ab0aee3a5ee
InkspillVerse <inkspill.verse@outlook.com>
2026-01-01 23:20:33 +0000

    Add function to filter and transform a list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -1,45 +1,13 @@
 module DataProcessor where
 
-import Data.List.Split (splitOn)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
 
-type CSVRow = [String]
-type CSVData = [CSVRow]
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-parseCSV :: String -> CSVData
-parseCSV content = map (splitOn ",") (lines content)
-
-numericColumns :: CSVData -> [[Double]]
-numericColumns [] = []
-numericColumns rows = 
-    let transposed = transpose rows
-    in map (mapMaybe readDouble) transposed
-  where
-    transpose :: [[a]] -> [[a]]
-    transpose ([]:_) = []
-    transpose x = map head x : transpose (map tail x)
-    
-    readDouble :: String -> Maybe Double
-    readDouble s = case reads s of
-        [(n, "")] -> Just n
-        _ -> Nothing
-
-calculateAverages :: [[Double]] -> [Double]
-calculateAverages columns = 
-    map average columns
-  where
-    average :: [Double] -> Double
-    average [] = 0.0
-    average xs = sum xs / fromIntegral (length xs)
-
-processCSVFile :: String -> IO [Double]
-processCSVFile filename = do
-    content <- readFile filename
-    let parsed = parseCSV content
-    let numericData = numericColumns parsed
-    return $ calculateAverages numericData
-
-mapMaybe :: (a -> Maybe b) -> [a] -> [b]
-mapMaybe _ [] = []
-mapMaybe f (x:xs) = case f x of
-    Just y -> y : mapMaybe f xs
-    Nothing -> mapMaybe f xs
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processNumbers input
+    print result
