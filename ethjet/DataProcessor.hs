3f15f3ab90805641446078775a4007cd5cc22197
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-01 20:22:32 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -11,4 +11,10 @@ sumPositiveDoubles :: [Int] -> Int
 sumPositiveDoubles = sum . processData
 
 validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (\x -> x >= -100 && x <= 100) xs then Just xs else Nothing
+validateInput xs = if all (\x -> x >= -100 && x <= 100) xs then Just xs else Nothingmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
