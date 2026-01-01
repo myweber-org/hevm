e13b5c02d0e989337bcf574e3b57c5ec3b4dd62e
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-01 18:44:33 +0000

    Add a function to generate the nth Fibonacci number using recursion

--- bench/FibonacciSequence.hs
+++ bench/FibonacciSequence.hs
@@ -7,4 +7,9 @@ fib n = fibs !! n
 main :: IO ()
 main = do
     putStrLn "Fibonacci sequence up to 20:"
-    mapM_ (print . fib) [0..20]
+    mapM_ (print . fib) [0..20]module FibonacciSequence where
+
+fibonacci :: Integer -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
