e5304b7d2d3a5f4dd3957cd94f96a9ffcc642c38
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-01 19:04:27 +0000

    Add function to filter and transform list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -5,30 +5,14 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = 
     map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-module DataProcessor where
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processData
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-main :: IO ()
-main = do
-    let sampleData = [-3, 1, 0, 5, -2, 8]
-    putStrLn $ "Original data: " ++ show sampleData
-    putStrLn $ "Processed data: " ++ show (processData sampleData)
-    putStrLn $ "Sum of positive doubles: " ++ show (sumPositiveDoubles sampleData)
-    putStrLn $ "First element: " ++ show (safeHead sampleData)
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = 
+    if all (\x -> x >= -100 && x <= 100) xs
+        then Just xs
+        else Nothing
