6934c39ac7c32c9a78dd3d71d3674b3952c4e5a8
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-01 01:21:10 +0000

    Add function to calculate moving average of a list

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -1,63 +1,17 @@
 
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 4, -1, 5, 9, -2, 6]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessed :: (Int -> Int) -> [Int] -> Int
-sumProcessed processor = sum . map processor
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
-    putStrLn $ "Sum of doubled odds: " ++ 
-        show (sumProcessed (\x -> x * 2) (filter odd numbers))
-module DataProcessor where
-
-import Data.List (tails)
-
 movingAverage :: Fractional a => Int -> [a] -> [a]
 movingAverage n xs
     | n <= 0 = error "Window size must be positive"
     | length xs < n = []
-    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
+    | otherwise = map average $ windows n xs
   where
-    average :: Fractional a => [a] -> a
-    average ys = sum ys / fromIntegral (length ys)
+    windows m ys = take (length ys - m + 1) $ iterate (tail) ys
+    average zs = sum zs / fromIntegral n
 
 smoothData :: Fractional a => Int -> [a] -> [a]
-smoothData windowSize dataPoints = movingAverage windowSize dataPoints
-
-main :: IO ()
-main = do
-    let testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-    putStrLn "Original data:"
-    print testData
-    putStrLn "\nMoving average with window size 3:"
-    print $ movingAverage 3 testData
-    putStrLn "\nSmoothed data with window size 4:"
-    print $ smoothData 4 testData
+smoothData windowSize dataPoints =
+    let avg = movingAverage windowSize dataPoints
+        padding = replicate (windowSize `div` 2) (head dataPoints)
+    in padding ++ avg ++ padding
