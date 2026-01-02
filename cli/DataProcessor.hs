9ef1ef14b0b449ca0ab5ac54041f9e3d20df2b15
InkspillVerse <inkspill.verse@outlook.com>
2026-01-02 19:17:49 +0000

    Add function to filter and transform list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -26,4 +26,28 @@ validateInput xs = not (null xs) && all (>= -100) xs && all (<= 100) xs
 safeProcess :: [Int] -> Maybe [Int]
 safeProcess xs
     | validateInput xs = Just (processData xs)
-    | otherwise = Nothing
+    | otherwise = Nothingmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles xs = 
+    let processed = processNumbers xs
+    in sum processed
+
+main :: IO ()
+main = do
+    let testData = [1, -2, 3, 0, 5, -8]
+    putStrLn $ "Original: " ++ show testData
+    putStrLn $ "Processed: " ++ show (processNumbers testData)
+    putStrLn $ "Sum: " ++ show (sumPositiveDoubles testData)
+    putStrLn $ "Head: " ++ show (safeHead testData)
