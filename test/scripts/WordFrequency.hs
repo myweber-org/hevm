9410899afec70ac79f61da718eca55353e481e51
InkspillVerse <inkspill.verse@outlook.com>
2026-01-02 16:05:54 +0000

    Add word frequency counting utility with command line interface

--- test/scripts/WordFrequency.hs
+++ test/scripts/WordFrequency.hs
@@ -1,26 +1,31 @@
 module WordFrequency where
 
-import qualified Data.Map.Strict as Map
 import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
 
-countWords :: String -> Map.Map String Int
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
 countWords text = 
     let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleanWord = map toLower . filter isAlpha
-    in Map.fromListWith (+) [(word, 1) | word <- wordsList]
-
-mostFrequent :: Map.Map String Int -> [(String, Int)]
-mostFrequent wordMap = 
-    take 5 $ reverse $ sortBy (comparing snd) $ Map.toList wordMap
+        cleaned = map toLower <$> wordsList
+        grouped = foldr (\w m -> insertWord w m) [] cleaned
+    in sortOn (Down . snd) grouped
   where
-    comparing f x y = compare (f x) (f y)
+    cleanWord = filter isAlpha
+    insertWord w [] = [(w, 1)]
+    insertWord w ((x, n):xs)
+        | w == x = (x, n+1) : xs
+        | otherwise = (x, n) : insertWord w xs
 
-displayResults :: String -> IO ()
-displayResults text = do
-    let freqMap = countWords text
-        topWords = mostFrequent freqMap
-    
-    putStrLn "Top 5 most frequent words:"
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
-    
-    putStrLn $ "\nTotal unique words: " ++ show (Map.size freqMap)
+formatResults :: [WordCount] -> String
+formatResults counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze (press Ctrl+D when finished):"
+    input <- getContents
+    let frequencies = countWords input
+    putStrLn "\nWord frequencies (sorted by count):"
+    putStrLn $ formatResults frequencies
