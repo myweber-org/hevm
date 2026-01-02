9d7c17dd0e3a57c9eaf80fda32c25118839633ae
InkspillVerse <inkspill.verse@outlook.com>
2026-01-02 01:02:40 +0000

    Add function to filter and transform list of integers

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -23,4 +23,14 @@ movingAverage n xs
         windows _ [] = []
         windows m ys
             | length ys < m = []
-            | otherwise = take m ys : windows m (tail ys)
+            | otherwise = take m ys : windows m (tail ys)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
