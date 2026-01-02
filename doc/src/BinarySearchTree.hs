5d5818d6530a6de9e972fe91db8daa030d3984ee
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-02 14:57:02 +0000

    Implement a binary search tree with insertion and in-order traversal

--- doc/src/BinarySearchTree.hs
+++ doc/src/BinarySearchTree.hs
@@ -25,4 +25,23 @@ contains _ Empty = False
 contains x (Node left val right)
     | x == val  = True
     | x < val   = contains x left
-    | otherwise = contains x right
+    | otherwise = contains x rightmodule BinarySearchTree where
+
+data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
+
+insert :: Ord a => a -> BST a -> BST a
+insert x Empty = Node Empty x Empty
+insert x (Node left val right)
+    | x < val   = Node (insert x left) val right
+    | x > val   = Node left val (insert x right)
+    | otherwise = Node left val right
+
+inOrder :: BST a -> [a]
+inOrder Empty = []
+inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
+
+fromList :: Ord a => [a] -> BST a
+fromList = foldr insert Empty
+
+toSortedList :: BST a -> [a]
+toSortedList = inOrder
