a56accd5f52210f2156ff8ee1d6bc6ca872748c0
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-02 00:00:44 +0000

    Add function to calculate moving average of a list

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -72,4 +72,27 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+-- Example usage with test data
+testData :: [Double]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+
+main :: IO ()
+main = do
+    putStrLn "Testing moving average with window size 3:"
+    print $ movingAverage 3 testData
+    putStrLn "\nTesting moving average with window size 5:"
+    print $ movingAverage 5 testData
