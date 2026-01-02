51c44b2a13dc470106359a4eb2971f0dfc5f7e39
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-02 20:22:06 +0000

    Add data validation and transformation utilities for CSV processing

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -6,3 +6,40 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+import Data.Char (isDigit, isSpace)
+import Data.List (intercalate)
+
+type ValidationRule = String -> Bool
+type Transformation = String -> String
+
+validateNumeric :: ValidationRule
+validateNumeric = all (\c -> isDigit c || c == '.')
+
+validateNonEmpty :: ValidationRule
+validateNonEmpty = not . all isSpace
+
+trimWhitespace :: Transformation
+trimWhitespace = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+
+toUpperCase :: Transformation
+toUpperCase = map toUpper
+
+processCSVRow :: [ValidationRule] -> [Transformation] -> [String] -> Either String [String]
+processCSVRow validators transformers row
+    | length row /= length validators = Left "Column count mismatch"
+    | any (== False) validationResults = Left $ "Validation failed: " ++ formatErrors validationResults
+    | otherwise = Right $ zipWith ($) transformers processedRow
+  where
+    processedRow = map trimWhitespace row
+    validationResults = zipWith ($) validators processedRow
+    formatErrors = intercalate ", " . map (\(i, r) -> "col" ++ show i ++ ": " ++ if r then "pass" else "fail") . zip [1..]
+
+safeReadDouble :: String -> Maybe Double
+safeReadDouble s = case reads s of
+    [(num, "")] -> Just num
+    _ -> Nothing
+
+normalizePhoneNumber :: Transformation
+normalizePhoneNumber = filter isDigit
