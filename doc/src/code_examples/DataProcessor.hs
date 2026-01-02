c23ae7d240accbaf90a0783cf77ac9085e4afae1
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-02 20:31:51 +0000

    Add function to filter and transform a list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -20,4 +20,17 @@ processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
 validateData :: [Int] -> Bool
-validateData = all (> 0)
+validateData = all (> 0)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let input = [1..10]
+    let result = processData input
+    putStrLn $ "Original list: " ++ show input
+    putStrLn $ "Processed list: " ++ show result
