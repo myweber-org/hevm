f911e2ceb1447fca5d48dcbb0e75500f77192c79
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-02 20:56:13 +0000

    Add data validation and transformation utilities for CSV processing

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -34,3 +34,51 @@ main = do
     let result = processData input
     putStrLn $ "Original list: " ++ show input
     putStrLn $ "Processed list: " ++ show result
+module DataProcessor where
+
+import Data.Char (isDigit, isSpace)
+import Data.List (intercalate)
+
+type ValidationRule = String -> Bool
+type Transformation = String -> String
+
+validateNumeric :: ValidationRule
+validateNumeric = all isDigit
+
+validateNonEmpty :: ValidationRule
+validateNonEmpty = not . all isSpace
+
+trimWhitespace :: Transformation
+trimWhitespace = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+
+toUpperCase :: Transformation
+toUpperCase = map toUpper
+  where
+    toUpper c
+      | c >= 'a' && c <= 'z' = toEnum (fromEnum c - 32)
+      | otherwise = c
+
+processCSVRow :: [ValidationRule] -> [Transformation] -> [String] -> Either String [String]
+processCSVRow validators transformers row
+  | length row /= length validators = Left "Row length doesn't match validator count"
+  | any (== False) validationResults = Left $ "Validation failed: " ++ show validationResults
+  | otherwise = Right transformedRow
+  where
+    validationResults = zipWith ($) validators row
+    transformedRow = zipWith ($) transformers row
+
+formatCSV :: [[String]] -> String
+formatCSV rows = intercalate "\n" $ map (intercalate ",") rows
+
+safeReadInt :: String -> Maybe Int
+safeReadInt s
+  | validateNumeric s = Just (read s)
+  | otherwise = Nothing
+
+data ProcessingResult = Success [String] | Failure String
+
+processData :: [ValidationRule] -> [Transformation] -> [String] -> ProcessingResult
+processData validators transformers row =
+  case processCSVRow validators transformers row of
+    Left err -> Failure err
+    Right result -> Success result
