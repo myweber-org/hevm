3f43f7acae261499f85662249058d05ed1c9080f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-02 17:08:38 +0000

    Add function to filter and transform a list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -115,4 +115,16 @@ exampleUsage = do
   
   case averageAge persons of
     Just avg -> putStrLn $ "\nAverage age: " ++ show avg
-    Nothing -> putStrLn "\nNo valid persons for average calculation"
+    Nothing -> putStrLn "\nNo valid persons for average calculation"module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
