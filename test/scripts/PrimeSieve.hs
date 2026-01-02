dde6349f1655d645f32ae8507f7b1f95e032125e
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-02 15:54:40 +0000

    Implement Sieve of Eratosthenes to generate primes up to a given limit

--- test/scripts/PrimeSieve.hs
+++ test/scripts/PrimeSieve.hs
@@ -1,55 +1,14 @@
-
 module PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-  where
-    go []     primes = reverse primes
-    go (x:xs) primes = go (filter (\n -> n `mod` x /= 0) xs) (x:primes)
 
 primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limit
-module PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
+primesUpTo n
+    | n < 2     = []
+    | otherwise = sieve [2..n]
     where
-        sieve' []     primes = reverse primes
-        sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
+        sieve [] = []
+        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]module PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+isPrime :: Int -> Bool
+isPrime n
+    | n < 2     = False
+    | otherwise = all (\p -> n `mod` p /= 0) (takeWhile (\p -> p * p <= n) (primesUpTo (floor (sqrt (fromIntegral n)) + 1)))
