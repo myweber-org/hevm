0b2663b776677602eee6b8464eb66b5fc537bfcc
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-03 16:16:07 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -5,11 +5,11 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+processNumbers = filterAndTransform even (\x -> x * x + 1)
 
 safeHead :: [Int] -> Maybe Int
 safeHead [] = Nothing
 safeHead (x:_) = Just x
+
+sumOfProcessed :: [Int] -> Int
+sumOfProcessed = sum . processNumbers
