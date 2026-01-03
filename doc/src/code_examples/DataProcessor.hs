c60e69dc376711febcac5cd4e6b9ef2345d72592
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-03 15:36:54 +0000

    Add function to filter and transform list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -13,3 +13,18 @@ validateInput xs
     | any (< -100) xs = Nothing
     | any (> 100) xs = Nothing
     | otherwise = Just xs
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 10) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
