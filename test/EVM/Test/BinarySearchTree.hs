0239331d62c644bffd26a68db163d1b20a5271c4
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-03 01:04:24 +0000

    Add binary search tree insertion and lookup functions

---
+++ test/EVM/Test/BinarySearchTree.hs
@@ -0,0 +1,21 @@
+module BinarySearchTree where
+
+data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
+
+insert :: Ord a => a -> BST a -> BST a
+insert x Empty = Node Empty x Empty
+insert x (Node left val right)
+    | x < val   = Node (insert x left) val right
+    | x > val   = Node left val (insert x right)
+    | otherwise = Node left val right
+
+lookup :: Ord a => a -> BST a -> Bool
+lookup _ Empty = False
+lookup x (Node left val right)
+    | x < val   = BinarySearchTree.lookup x left
+    | x > val   = BinarySearchTree.lookup x right
+    | otherwise = True
+
+inorder :: BST a -> [a]
+inorder Empty = []
+inorder (Node left val right) = inorder left ++ [val] ++ inorder right
