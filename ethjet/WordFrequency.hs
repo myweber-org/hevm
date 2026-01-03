c74a4b2054a7fc1c7c84ceacd471c90efc01b879
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-03 16:52:55 +0000

    Add word frequency counting utility with histogram output

--- ethjet/WordFrequency.hs
+++ ethjet/WordFrequency.hs
@@ -20,4 +20,26 @@ displayFrequency :: [(String, Int)] -> String
 displayFrequency = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
-analyzeText n = displayFrequency . topNWords n
+analyzeText n = displayFrequency . topNWords nmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let words' = filter (not . null) $ map (filter isAlpha . map toLower) $ words text
+        grouped = group $ sort words'
+    in sortOn (Down . snd) $ map (\ws -> (head ws, length ws)) grouped
+
+printHistogram :: [WordCount] -> IO ()
+printHistogram counts = 
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ replicate count '*') counts
+
+main :: IO ()
+main = do
+    input <- getContents
+    let frequencies = countWords input
+    printHistogram frequencies
