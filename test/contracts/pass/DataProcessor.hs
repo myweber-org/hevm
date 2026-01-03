1238662e60bf07ba0d02af4be3c6358c80871d7f
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-03 23:38:02 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -21,3 +21,17 @@ isWithinDateRange start end record =
 
 parseDate :: String -> Maybe Day
 parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumProcessedList :: [Int] -> Int
+sumProcessedList = sum . processEvenSquares
