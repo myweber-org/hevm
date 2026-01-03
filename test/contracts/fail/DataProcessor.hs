3a9a0f6bd0cea651f845adeb63146b377ce0e6ef
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-03 16:43:56 +0000

    Add function to parse CSV data and calculate average values

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -10,4 +10,20 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, 0, 5, -8]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+import Data.List.Split (splitOn)
+
+parseCSV :: String -> [[Double]]
+parseCSV content = map (map read . splitOn ",") (lines content)
+
+calculateAverages :: [[Double]] -> [Double]
+calculateAverages rows
+    | null rows = []
+    | otherwise = map avg (transpose rows)
+  where
+    avg xs = sum xs / fromIntegral (length xs)
+    transpose = foldr (zipWith (:)) (repeat [])
+
+processCSVData :: String -> [Double]
+processCSVData = calculateAverages . parseCSV
