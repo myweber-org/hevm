dcb2a7c04df25f200673ea73eef456ebc3981798
InkspillVerse <inkspill.verse@outlook.com>
2026-01-03 00:14:52 +0000

    Add function to filter and transform list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -2,59 +2,10 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just xmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transform = map transform . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (>= -100) xs && all (<= 100) xs
-
-safeProcess :: [Int] -> Maybe [Int]
-safeProcess xs
-    | validateInput xs = Just (processData xs)
-    | otherwise = Nothingmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles xs = 
-    let processed = processNumbers xs
-    in sum processed
-
-main :: IO ()
-main = do
-    let testData = [1, -2, 3, 0, 5, -8]
-    putStrLn $ "Original: " ++ show testData
-    putStrLn $ "Processed: " ++ show (processNumbers testData)
-    putStrLn $ "Sum: " ++ show (sumPositiveDoubles testData)
-    putStrLn $ "Head: " ++ show (safeHead testData)
-module DataProcessor where
-
-filterAndSquareEvens :: [Int] -> [Int]
-filterAndSquareEvens xs = map (^2) (filter even xs)
-
-processData :: [Int] -> [Int]
-processData = filterAndSquareEvens
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
