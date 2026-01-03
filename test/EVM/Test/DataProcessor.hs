8a0f580fe8bc83474888d83602ccea2166e3087a
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-03 15:36:56 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -234,3 +234,13 @@ formatReport stats =
 -- Example transformation pipeline
 processData :: [String] -> String
 processData = formatReport . computeStats
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubled :: [Int] -> Int
+sumPositiveDoubled = sum . processNumbers
