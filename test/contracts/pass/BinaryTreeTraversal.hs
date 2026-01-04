8725cc34b08a24cfe8f8a577179e8258b94ec719
InkspillVerse <inkspill.verse@outlook.com>
2026-01-04 16:02:14 +0000

    Add inorder traversal function for binary trees

---
+++ test/contracts/pass/BinaryTreeTraversal.hs
@@ -0,0 +1,14 @@
+
+data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a)
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left value right) = inorder left ++ [value] ++ inorder right
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left value right) = [value] ++ preorder left ++ preorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left value right) = postorder left ++ postorder right ++ [value]
