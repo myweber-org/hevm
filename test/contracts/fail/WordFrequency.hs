89925366f97a94d9fecda7cf57997355ef9e2f67
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-04 23:54:32 +0000

    Add word frequency counter for text analysis

--- test/contracts/fail/WordFrequency.hs
+++ test/contracts/fail/WordFrequency.hs
@@ -1,26 +1,23 @@
 module WordFrequency where
 
+import qualified Data.Map.Strict as Map
 import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
 
-type WordCount = (String, Int)
+type FrequencyMap = Map.Map String Int
 
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleaned = filter (all isAlpha) wordsList
-    in sortOn (Down . snd) $ foldl countWord [] cleaned
+countWords :: String -> FrequencyMap
+countWords = foldr incrementWord Map.empty . words
   where
-    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-    
-    countWord :: [WordCount] -> String -> [WordCount]
-    countWord [] word = [(word, 1)]
-    countWord ((w, c):rest) word
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord rest word
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
 
-displayTopWords :: Int -> String -> String
-displayTopWords n text = 
-    let counts = take n $ countWords text
-    in unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequencies . topNWords n
