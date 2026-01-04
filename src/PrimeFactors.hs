1b581e113329f1574aa531370e5c45ecaad3d376
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-04 15:38:28 +0000

    Add function to compute prime factors of an integer

---
+++ src/PrimeFactors.hs
@@ -0,0 +1,11 @@
+
+module PrimeFactors (primeFactors) where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = factorize n 2
+  where
+    factorize 1 _ = []
+    factorize m p
+      | m < p * p = [m]
+      | m `mod` p == 0 = p : factorize (m `div` p) p
+      | otherwise = factorize m (p + 1)
