b40cdca2420b3b2867b15ceba93d72b68c977ba8
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-04 00:35:10 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -1,44 +1,11 @@
-
 module DataProcessor where
-
-import Data.Time
-import Text.CSV
-
-filterCSVByDate :: String -> Day -> Day -> Either String [Record]
-filterCSVByDate csvContent startDate endDate = do
-    csv <- parseCSV "input" csvContent
-    let filtered = filter (isWithinDateRange startDate endDate) csv
-    return filtered
-
-isWithinDateRange :: Day -> Day -> Record -> Bool
-isWithinDateRange start end record =
-    case record of
-        (dateStr:_) -> 
-            case parseDate dateStr of
-                Just date -> date >= start && date <= end
-                Nothing -> False
-        _ -> False
-
-parseDate :: String -> Maybe Day
-parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-sumProcessedList :: [Int] -> Int
-sumProcessedList = sum . processEvenSquaresmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = 
     map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * 2 + 1)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
