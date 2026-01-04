10cff371ea18e6fbc7e31804e131ca96ba747de3
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-04 20:42:32 +0000

    Add function to filter and transform a list of integers, extracting even numbers and squaring them

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -17,4 +17,15 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processNumbers input
-    print result
+    print resultmodule DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter even
+
+sampleData :: [Int]
+sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+main :: IO ()
+main = do
+    let result = processData sampleData
+    putStrLn $ "Processed data: " ++ show result
