dd2f05948cfdce4d2ba318becd65f8dc765372f2
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-04 00:39:51 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -1,41 +1,13 @@
-
 module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transform = map transform . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processDatamodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
+    let input = [1, -2, 3, 0, 5, -7]
+    let result = processNumbers input
     print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (\x -> x >= -100 && x <= 100) xs
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5, -8]
-    if validateInput sampleData
-        then print $ processData sampleData
-        else putStrLn "Invalid input data"
