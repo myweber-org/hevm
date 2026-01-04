e1c75743ff0cf0b92055017e654f5478b164a891
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-04 20:22:24 +0000

    Add function to filter and transform a list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -15,4 +15,10 @@ main = do
     let testData = [1..10]
     putStrLn $ "Original data: " ++ show testData
     putStrLn $ "Processed data: " ++ show (processEvenSquares testData)
-    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData testData)
+    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData testData)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
