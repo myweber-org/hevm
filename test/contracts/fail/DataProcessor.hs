c4c8f30f095594b2c1917252e2a80330e80ad664
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-05 15:43:15 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -31,3 +31,27 @@ exampleUsage = do
   let inputData = ["hello123", "test456", "invalid!"]
   let processed = processData inputData
   putStrLn $ "Processed results: " ++ formatResults processed
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessed :: (Int -> Int) -> [Int] -> Int
+sumProcessed processor = sum . map processor
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    putStrLn "Original list:"
+    print numbers
+    
+    putStrLn "\nSquares of even numbers:"
+    let squares = processEvenSquares numbers
+    print squares
+    
+    putStrLn "\nSum of doubled numbers:"
+    let total = sumProcessed (*2) numbers
+    print total
