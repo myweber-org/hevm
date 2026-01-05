51d6301118622b830f94539d2a8c48bf600a165b
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-05 23:40:04 +0000

    Add function to filter and transform a list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -43,3 +43,16 @@ main = do
     case validateAndProcess input of
         Nothing -> putStrLn "Empty list provided"
         Just result -> print result
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 10) (* 2)
+
+main :: IO ()
+main = do
+    let input = [5, 12, 3, 20, 8, 15]
+    let result = processData input
+    print result
