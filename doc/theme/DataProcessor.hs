542bf8bd21bb166482f23ae15468a5ecaa222043
InkspillVerse <inkspill.verse@outlook.com>
2026-01-05 01:21:00 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -1,101 +1,14 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-import Data.Char (isDigit, isSpace)
-import Data.List (intercalate)
-
-type ValidationRule = String -> Bool
-type Transformation = String -> String
-
-validateNumeric :: ValidationRule
-validateNumeric = all (\c -> isDigit c || c == '.')
-
-validateNonEmpty :: ValidationRule
-validateNonEmpty = not . all isSpace
-
-trimWhitespace :: Transformation
-trimWhitespace = reverse . dropWhile isSpace . reverse . dropWhile isSpace
-
-toUpperCase :: Transformation
-toUpperCase = map toUpper
-
-processCSVRow :: [ValidationRule] -> [Transformation] -> [String] -> Either String [String]
-processCSVRow validators transformers row
-    | length row /= length validators = Left "Column count mismatch"
-    | any (== False) validationResults = Left $ "Validation failed: " ++ formatErrors validationResults
-    | otherwise = Right $ zipWith ($) transformers processedRow
-  where
-    processedRow = map trimWhitespace row
-    validationResults = zipWith ($) validators processedRow
-    formatErrors = intercalate ", " . map (\(i, r) -> "col" ++ show i ++ ": " ++ if r then "pass" else "fail") . zip [1..]
-
-safeReadDouble :: String -> Maybe Double
-safeReadDouble s = case reads s of
-    [(num, "")] -> Just num
-    _ -> Nothing
-
-normalizePhoneNumber :: Transformation
-normalizePhoneNumber = filter isDigitmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateData :: [Int] -> Bool
-validateData xs = all (> 0) xs && length xs > 0
+processNumbers = filterAndTransform even (*2)
 
 main :: IO ()
 main = do
-    let inputData = [1, -2, 3, 0, 5, -8]
-    let processed = processData inputData
-    putStrLn $ "Input: " ++ show inputData
-    putStrLn $ "Processed: " ++ show processed
-    putStrLn $ "Validation: " ++ show (validateData processed)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
+    let numbers = [1..10]
     let result = processNumbers numbers
-    print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = 
-    sum . filterAndTransform predicate transformer
-
-main :: IO ()
-main = do
-    let sampleData = [1..10]
-    putStrLn $ "Original data: " ++ show sampleData
-    putStrLn $ "Even squares: " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Sum of even squares: " ++ 
-        show (sumProcessedData even (\x -> x * x) sampleData)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 10) (* 2)
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show result
