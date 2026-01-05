7dd69f19ce278f697a6622ebe4cd2106b13d6718
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-05 14:20:10 +0000

    Add word frequency counter for text analysis

--- test/contracts/WordFrequency.hs
+++ test/contracts/WordFrequency.hs
@@ -5,40 +5,19 @@ import Data.List (sortOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-type WordCount = Map String Int
+type FrequencyMap = Map String Int
 
-countWords :: String -> WordCount
+countWords :: String -> FrequencyMap
 countWords = foldr incrementWord Map.empty . words
   where
     incrementWord word = Map.insertWith (+) (normalize word) 1
     normalize = map toLower . filter isAlpha
 
-topWords :: Int -> String -> [(String, Int)]
-topWords n text = take n $ sortOn (negate . snd) $ Map.toList (countWords text)
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (negate . snd) $ Map.toList $ countWords text
 
-displayFrequency :: String -> IO ()
-displayFrequency text = do
-  putStrLn "Top 10 most frequent words:"
-  mapM_ printWord (topWords 10 text)
-  where
-    printWord (word, count) = putStrLn $ word ++ ": " ++ show countmodule WordFrequency where
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
-import Data.Char (isAlpha, toLower)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type Frequency = [(String, Int)]
-
-countWords :: String -> Frequency
-countWords text =
-  let wordsList = filter (not . null) $ map (map toLower . filter isAlpha) $ words text
-      frequencyMap = foldr (\word acc -> case lookup word acc of
-                                          Just count -> (word, count + 1) : filter ((/= word) . fst) acc
-                                          Nothing    -> (word, 1) : acc) [] wordsList
-  in sortOn (Down . snd) frequencyMap
-
-mostFrequent :: Int -> String -> Frequency
-mostFrequent n = take n . countWords
-
-displayFrequency :: Frequency -> String
-displayFrequency freq = unlines $ map (\(word, count) -> word ++ ": " ++ show count) freq
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequencies . topNWords n
