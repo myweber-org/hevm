e7ccb63cc27c484148f74897e5f768630662a538
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-05 14:54:14 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -21,4 +21,16 @@ main = do
             print validData
             putStrLn "Processed data (even numbers squared plus one):"
             print $ processNumbers validData
-        Nothing -> putStrLn "Invalid input data"
+        Nothing -> putStrLn "Invalid input data"module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
