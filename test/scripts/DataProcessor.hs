e504ec318cfae22e51ff2fa6040104111ce06592
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-06 00:31:48 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -1,18 +1,10 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (>= -100) xs && all (<= 100) xs
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5, -10]
-    if validateInput sampleData
-        then print $ processData sampleData
-        else putStrLn "Invalid input data"
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
