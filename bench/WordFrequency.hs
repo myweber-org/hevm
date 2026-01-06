920a08011413c4e9ae4f9bcecda457e4ca48d5cb
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-06 20:23:08 +0000

    Add function to count word frequencies in a text file

--- bench/WordFrequency.hs
+++ bench/WordFrequency.hs
@@ -23,4 +23,26 @@ processTextFile filepath = do
         topWords = getTopWords 10 wordCounts
     
     putStrLn "Top 10 most frequent words:"
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWordsmodule WordFrequency where
+
+import qualified Data.Map as Map
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords = foldr insertWord Map.empty . words
+  where
+    insertWord w = Map.insertWith (+) (normalize w) 1
+    normalize = map toLower . filter isAlphaNum
+
+getTopWords :: Int -> String -> [(String, Int)]
+getTopWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+processFile :: FilePath -> IO ()
+processFile path = do
+  content <- readFile path
+  let topWords = getTopWords 10 content
+  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) topWords
