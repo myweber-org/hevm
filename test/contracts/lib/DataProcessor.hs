df1a264faafe9dc37f50e6862e84d17278edc2d4
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-06 20:15:05 +0000

    Add function to filter and transform a list of integers, keeping only even numbers and squaring them.

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -1,7 +1,12 @@
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processData = map (^2) . filter even
+
+sampleData :: [Int]
+sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+main :: IO ()
+main = do
+    let result = processData sampleData
+    print result
