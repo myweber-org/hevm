13dd781b16d63a96bc5bf07d29ea977e2a0a534e
InkspillVerse <inkspill.verse@outlook.com>
2026-01-06 23:20:57 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -15,3 +15,24 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | otherwise = Just xs
+
+main :: IO ()
+main = do
+    let sampleData = [-3, 0, 5, 2, -1, 8]
+    case validateInput sampleData of
+        Nothing -> putStrLn "Empty input list"
+        Just data' -> do
+            putStrLn $ "Original data: " ++ show data'
+            putStrLn $ "Processed data: " ++ show (processData data')
