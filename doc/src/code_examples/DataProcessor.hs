ef2d44e40119205fb1ab43abf896e71a1ff1bf5d
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-06 15:30:30 +0000

    Add function to filter and transform list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -35,3 +35,26 @@ smoothData = movingAverage 3
 
 processData :: [Double] -> (Double, Double, Double)
 processData xs = (minimum xs, maximum xs, sum xs / fromIntegral (length xs))
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [-5, 2, 0, 8, -1, 10]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original data: " ++ show validData
+            putStrLn $ "Processed data: " ++ show (processData validData)
+            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
+        Nothing -> putStrLn "Input validation failed: contains values <= -100"
