03ccd31bbc17aa868797b99ad5a6a8fc64671491
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-06 21:30:40 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -12,3 +12,18 @@ sumProcessedData predicate transformer = sum . filterAndTransform predicate tran
 validateInput :: [Int] -> Maybe [Int]
 validateInput [] = Nothing
 validateInput xs = Just xs
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 10) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (> 0) xs && length xs <= 1000
+
+safeProcess :: [Int] -> Maybe [Int]
+safeProcess xs
+  | validateInput xs = Just $ processData xs
+  | otherwise = Nothing
