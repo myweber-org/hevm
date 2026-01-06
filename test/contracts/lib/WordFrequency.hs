cdf121c88c48ff784b76a4d13673a9cbf59c04c6
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-06 20:35:59 +0000

    Add word frequency counting functionality

--- test/contracts/lib/WordFrequency.hs
+++ test/contracts/lib/WordFrequency.hs
@@ -26,4 +26,33 @@ displayFrequency counts =
 analyzeText :: String -> String
 analyzeText text = 
     let frequencies = countWords text
-    in "Top 10 most frequent words:\n" ++ displayFrequency frequencies
+    in "Top 10 most frequent words:\n" ++ displayFrequency frequenciesmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = foldr countWord [] cleanedWords
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    
+    countWord :: String -> [WordCount] -> [WordCount]
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+displayResults :: [WordCount] -> String
+displayResults counts =
+    "Top 10 most frequent words:\n" ++
+    unlines (map (\(w, c) -> w ++ ": " ++ show c) counts)
+
+processText :: String -> String
+processText = displayResults . countWords
