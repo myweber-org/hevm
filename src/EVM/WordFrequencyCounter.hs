bde8b79c15f45106493ded2d110112cc839f6d02
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-06 16:54:47 +0000

    Add word frequency counter with sorting and filtering capabilities

--- src/EVM/WordFrequencyCounter.hs
+++ src/EVM/WordFrequencyCounter.hs
@@ -28,4 +28,48 @@ analyzeText text = do
     let freqs = countWords text
     printHistogram $ take 20 freqs
     putStrLn $ "\nTotal unique words: " ++ show (length freqs)
-    putStrLn $ "Most frequent word: " ++ fst (head freqs) ++ " (" ++ show (snd (head freqs)) ++ " occurrences)"
+    putStrLn $ "Most frequent word: " ++ fst (head freqs) ++ " (" ++ show (snd (head freqs)) ++ " occurrences)"module WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        grouped = group $ sort cleanedWords
+    in map (\ws -> (head ws, length ws)) grouped
+
+cleanWord :: String -> String
+cleanWord = filter isAlpha
+
+sortByFrequency :: [WordCount] -> [WordCount]
+sortByFrequency = sortOn (Down . snd)
+
+sortAlphabetically :: [WordCount] -> [WordCount]
+sortAlphabetically = sortOn fst
+
+filterByMinFrequency :: Int -> [WordCount] -> [WordCount]
+filterByMinFrequency minFreq = filter ((>= minFreq) . snd)
+
+getTopNWords :: Int -> [WordCount] -> [WordCount]
+getTopNWords n = take n . sortByFrequency
+
+processText :: String -> Int -> Int -> [WordCount]
+processText text minFreq topN = 
+    let counted = countWords text
+        filtered = filterByMinFrequency minFreq counted
+    in getTopNWords topN filtered
+
+displayResults :: [WordCount] -> IO ()
+displayResults counts = 
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
+
+main :: IO ()
+main = do
+    let sampleText = "Hello world! Hello Haskell. Haskell is great. World says hello to Haskell."
+    putStrLn "Word frequency analysis:"
+    displayResults $ processText sampleText 1 5
