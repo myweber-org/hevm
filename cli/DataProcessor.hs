3da70d7f5c6451f0973030ae896feec4162ccbf8
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-07 23:03:37 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -1,16 +1,14 @@
+
 module DataProcessor where
 
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-  where
-    windows :: Int -> [a] -> [[a]]
-    windows m ys = take (length ys - m + 1) $ zipWith (const . take m) (tails ys) ys
-    
-    average :: Fractional a => [a] -> a
-    average zs = sum zs / fromIntegral (length zs)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
 
--- Example usage:
--- movingAverage 3 [1,2,3,4,5] == [2.0,3.0,4.0]
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processNumbers input
+    print result
