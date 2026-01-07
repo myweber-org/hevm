dd08a3a9c02a03251deb34c97c5238ddc5bc608f
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-07 15:37:26 +0000

    Add word frequency counter for text analysis

--- ethjet/WordFrequencyCounter.hs
+++ ethjet/WordFrequencyCounter.hs
@@ -30,4 +30,39 @@ main = do
     input <- getContents
     let frequencies = countWords input
     putStrLn "\nWord frequencies (sorted by count):"
-    putStrLn $ formatOutput frequencies
+    putStrLn $ formatOutput frequenciesmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = foldr countWord [] cleanedWords
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    
+    countWord :: String -> [WordCount] -> [WordCount]
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+analyzeText :: String -> IO ()
+analyzeText input = do
+    putStrLn "Top 10 most frequent words:"
+    mapM_ printWord (countWords input)
+  where
+    printWord (word, count) = 
+        putStrLn $ word ++ ": " ++ show count
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze (press Ctrl+D when done):"
+    content <- getContents
+    analyzeText content
