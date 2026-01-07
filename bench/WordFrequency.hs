3a4b3cd8132a937e714b54977ec142e7eff47a18
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-07 20:05:28 +0000

    Add word frequency counting utility

--- bench/WordFrequency.hs
+++ bench/WordFrequency.hs
@@ -45,4 +45,23 @@ processFile :: FilePath -> IO ()
 processFile path = do
   content <- readFile path
   let topWords = getTopWords 10 content
-  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) topWords
+  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) topWordsmodule WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
+
+topWords :: Int -> String -> [(String, Int)]
+topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
