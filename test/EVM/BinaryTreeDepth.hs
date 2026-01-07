e67020393a07e4facd20e1991296cf69f852a82a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-07 20:22:13 +0000

    Add function to calculate the maximum depth of a binary tree

--- test/EVM/BinaryTreeDepth.hs
+++ test/EVM/BinaryTreeDepth.hs
@@ -1,19 +1,5 @@
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
-
-treeDepth :: Tree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: Tree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty (Node 6 Empty Empty))data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Leaf | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Leaf = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: BinaryTree Int
-sampleTree = Node 1 (Node 2 (Node 4 Leaf Leaf) (Node 5 Leaf Leaf)) (Node 3 Leaf (Node 6 Leaf Leaf))
