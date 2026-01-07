f8e58a1053ad70ac508c2b6a56eadb24181d04a0
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-07 23:40:24 +0000

    Add a function to generate the nth Fibonacci number using a tail-recursive helper

--- ethjet/FibonacciSequence.hs
+++ ethjet/FibonacciSequence.hs
@@ -8,4 +8,12 @@ fibonacci n = fibs !! n
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 fibonacciMemoized :: Int -> Integer
-fibonacciMemoized = fix (\rec n -> if n < 2 then fromIntegral n else rec (n - 1) + rec (n - 2))
+fibonacciMemoized = fix (\rec n -> if n < 2 then fromIntegral n else rec (n - 1) + rec (n - 2))module FibonacciSequence where
+
+fibonacci :: Integer -> Integer
+fibonacci n
+    | n < 0 = error "Negative input not allowed"
+    | otherwise = fibHelper n 0 1
+  where
+    fibHelper 0 a _ = a
+    fibHelper n a b = fibHelper (n - 1) b (a + b)
