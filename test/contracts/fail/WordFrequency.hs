373aecdadf466f10db4c7668776c52ad01fcafce
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-08 16:41:53 +0000

    Add word frequency analysis function to count occurrences of each word in a given text

--- test/contracts/fail/WordFrequency.hs
+++ test/contracts/fail/WordFrequency.hs
@@ -50,4 +50,30 @@ main = do
     putStrLn "Enter file path:"
     path <- getLine
     counts <- readFileAndCount path
-    printWordCounts counts
+    printWordCounts countsmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        grouped = foldr countWord [] cleanedWords
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = filter isAlpha
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | word == w = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+formatResults :: [WordCount] -> String
+formatResults counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+analyzeText :: String -> String
+analyzeText = formatResults . countWords
