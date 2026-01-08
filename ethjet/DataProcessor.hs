a01882ce57b3f21801d776edbba26d2b4713a32f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-08 20:44:58 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -40,3 +40,16 @@ module DataProcessor where
 
 processData :: [Int] -> [Int]
 processData = map (^2) . filter (>0)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let inputData = [1, -2, 3, -4, 5, 0]
+    let result = processData inputData
+    print result
