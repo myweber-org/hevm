61d1f2dceaea45056280e777e9a4e88d03df4ab5
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-08 16:00:26 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -19,3 +19,20 @@ main = do
     putStrLn $ "Even numbers doubled: " ++ show (processEvenNumbers sampleData)
     putStrLn $ "Odd numbers incremented: " ++ show (processOddNumbers sampleData)
     putStrLn $ "Sum of processed even numbers: " ++ show (sumProcessedData sampleData)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+main :: IO ()
+main = do
+    let sampleData = [-3, 1, 0, 5, -2, 8]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Processed data: " ++ show (processData sampleData)
+    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData sampleData)
