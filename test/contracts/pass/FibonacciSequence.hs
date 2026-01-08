5687941c96635837769e88edb6b11c4f57492472
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-08 15:17:54 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/contracts/pass/FibonacciSequence.hs
+++ test/contracts/pass/FibonacciSequence.hs
@@ -18,4 +18,14 @@ main = do
     putStrLn "Fibonacci sequence (first 10 terms):"
     print $ fibonacci 10
     putStrLn "\nMemoized version (first 15 terms):"
-    print $ fibonacciMemoized 15
+    print $ fibonacciMemoized 15module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+  putStrLn "Fibonacci sequence up to 10 terms:"
+  print (fibonacci 10)
