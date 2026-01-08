0ed9631927237854748c7b756fb7718e7a9e15bb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-08 21:29:19 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -1,80 +1,19 @@
 module DataProcessor where
 
-processData :: [Int] -> [Int]
-processData = map (^2) . filter evenmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print result
-module DataProcessor where
-
-import Data.List.Split (splitOn)
-
-type Row = [String]
-type CSVData = [Row]
-
-parseCSV :: String -> CSVData
-parseCSV content = map (splitOn ",") (lines content)
-
-numericColumns :: CSVData -> [[Double]]
-numericColumns [] = []
-numericColumns (header:rows) = 
-    map (parseRow . map (!!) rows) [0..length header - 1]
-  where
-    parseRow col = map (read . trim) col
-    trim = dropWhile (== ' ') . reverse . dropWhile (== ' ') . reverse
-
-columnAverage :: [[Double]] -> [Double]
-columnAverage columns = map avg columns
-  where
-    avg col = if null col then 0 else sum col / fromIntegral (length col)
-
-processCSVData :: String -> [Double]
-processCSVData content = 
-    let parsed = parseCSV content
-        numeric = numericColumns parsed
-    in columnAverage numeric
-
-safeReadDouble :: String -> Maybe Double
-safeReadDouble s = case reads s of
-    [(n, "")] -> Just n
-    _ -> Nothing
-
-filterValidRows :: CSVData -> CSVData
-filterValidRows rows = 
-    filter (all (\cell -> not (null cell) && not (any (==',') cell))) rowsmodule DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = 
     map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
 
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just xmodule DataProcessor where
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | all (> -1000) xs = Just xs
+    | otherwise = Nothing
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processNumbers input
-    putStrLn $ "Input: " ++ show input
-    putStrLn $ "Result: " ++ show result
+processValidatedData :: [Int] -> Maybe Int
+processValidatedData = fmap sumProcessedData . validateInput
