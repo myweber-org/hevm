e96563b9b31e9d8482a597c9cfc146987119c05a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-08 20:22:30 +0000

    Implement a simple prime number sieve using the Sieve of Eratosthenes algorithm.

--- test/contracts/lib/PrimeSieve.hs
+++ test/contracts/lib/PrimeSieve.hs
@@ -1,35 +1,16 @@
 module PrimeSieve where
 
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
+primes :: [Int]
+primes = sieve [2..]
   where
-    sieve' [] primes = reverse primes
-    sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter a limit:"
-    input <- getLine
-    let limit = read input :: Int
-    let primes = primesUpTo limit
-    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primesmodule PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-    where
-        sieve [] = []
-        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
 
 main :: IO ()
 main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limit
+    putStrLn "First 20 prime numbers:"
+    print $ take 20 primes
+    putStrLn "\nThe 100th prime number is:"
+    print $ nthPrime 100
