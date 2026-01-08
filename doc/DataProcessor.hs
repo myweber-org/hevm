184889893ff905f9ab3c9017a6c5e70bb49d38dd
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-08 00:51:43 +0000

    Add function to filter and transform a list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -5,3 +5,10 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5, 0]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
