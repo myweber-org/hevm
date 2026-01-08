3007599a3255ff2b170823717f6d296ace01e201
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-08 16:24:02 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -1,76 +1,17 @@
+
 module DataProcessor where
 
-import Data.List.Split (splitOn)
+processNumbers :: [Int] -> [Int]
+processNumbers = map (^2) . filter (>0)
 
-type CSVRow = [String]
-type CSVData = [CSVRow]
+validateAndProcess :: [Int] -> Maybe [Int]
+validateAndProcess xs
+    | null xs = Nothing
+    | otherwise = Just (processNumbers xs)
 
-parseCSV :: String -> CSVData
-parseCSV content = map (splitOn ",") (lines content)
-
-numericColumns :: CSVData -> [[Double]]
-numericColumns [] = []
-numericColumns rows = 
-    let transposed = transpose rows
-        numericRows = map (filter (not . null) . map maybeRead) transposed
-    in map (map read) numericRows
-  where
-    transpose :: [[a]] -> [[a]]
-    transpose [] = repeat []
-    transpose ([]:xss) = transpose xss
-    transpose ((x:xs):xss) = 
-        (x : [h | (h:_) <- xss]) : transpose (xs : [t | (_:t) <- xss])
-    
-    maybeRead :: String -> String
-    maybeRead s = case reads s :: [(Double, String)] of
-        [(_, "")] -> s
-        _ -> ""
-
-calculateAverages :: CSVData -> [Double]
-calculateAverages csvData = 
-    let numericData = numericColumns csvData
-        sums = map sum numericData
-        counts = map fromIntegral (map length numericData)
-    in zipWith (/) sums counts
-
-processCSVFile :: FilePath -> IO [Double]
-processCSVFile filePath = do
-    content <- readFile filePath
-    let parsedData = parseCSV content
-    return $ calculateAverages parsedData
-
-safeHead :: CSVData -> CSVRow
-safeHead [] = []
-safeHead (x:_) = x
-
-validateCSV :: CSVData -> Bool
-validateCSV [] = True
-validateCSV (row:rows) = 
-    let rowLength = length row
-    in all (\r -> length r == rowLength) rowsmodule DataProcessor where
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage windowSize xs
-    | windowSize <= 0 = error "Window size must be positive"
-    | length xs < windowSize = []
-    | otherwise = map average $ windows windowSize xs
-  where
-    windows n = takeWhile (\w -> length w == n) . map (take n) . iterate tail
-    average = (/ fromIntegral windowSize) . summodule DataProcessor where
-
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-  where
-    windows :: Int -> [a] -> [[a]]
-    windows size = takeWhile ((== size) . length) . map (take size) . tails
-    
-    average :: Fractional a => [a] -> a
-    average lst = sum lst / fromIntegral (length lst)
-
--- Helper function from Data.List
-tails :: [a] -> [[a]]
-tails [] = [[]]
-tails xs@(_:ys) = xs : tails ys
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, -4, 5]
+    case validateAndProcess sampleData of
+        Nothing -> putStrLn "Empty input list"
+        Just result -> print result
