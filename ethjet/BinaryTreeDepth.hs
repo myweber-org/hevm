6373900b1d062db4747c9ac10ae82dfb3f52e02f
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-08 23:47:55 +0000

    Add function to calculate the depth of a binary tree

--- ethjet/BinaryTreeDepth.hs
+++ ethjet/BinaryTreeDepth.hs
@@ -18,4 +18,8 @@ main = do
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show, Eq)
+
+treeDepth :: BinaryTree a -> Int
+treeDepth Leaf = 0
+treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)
