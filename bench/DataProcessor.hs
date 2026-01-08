22f3e878189d3e2fd521667817d31e992e176122
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-08 23:14:21 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -19,4 +19,13 @@ validateInput xs =
 safeProcess :: [Int] -> Maybe Int
 safeProcess xs = do
     valid <- validateInput xs
-    return $ sumProcessed valid
+    return $ sumProcessed validmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
