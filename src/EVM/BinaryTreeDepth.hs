23d0e8cbaed8284c25fe6931dbce2fc114fa6141
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-08 01:28:31 +0000

    Add function to calculate the maximum depth of a binary tree

--- src/EVM/BinaryTreeDepth.hs
+++ src/EVM/BinaryTreeDepth.hs
@@ -1,28 +1,5 @@
-
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: BinaryTree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty (Node 6 Empty Empty))
-
-main :: IO ()
-main = do
-    let depth = treeDepth sampleTree
-    putStrLn $ "Maximum depth of the sample tree is: " ++ show depth
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: BinaryTree Int
-sampleTree = Node 1 
-                (Node 2 
-                    (Node 4 Empty Empty) 
-                    (Node 5 Empty Empty))
-                (Node 3 
-                    Empty 
-                    (Node 6 Empty Empty))
