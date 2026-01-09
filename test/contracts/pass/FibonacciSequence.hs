42bbf37404c34277fa7740a8bfb4843b1bc9a31a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-09 02:59:47 +0000

    Add Fibonacci sequence generator with memoization

--- test/contracts/pass/FibonacciSequence.hs
+++ test/contracts/pass/FibonacciSequence.hs
@@ -1,31 +1,6 @@
+module Fibonacci where
 
-module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n
-    | n <= 0    = []
-    | n == 1    = [0]
-    | n == 2    = [0, 1]
-    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-                  in take n fibs
-
-fibonacciMemoized :: Int -> [Integer]
-fibonacciMemoized n = take n fibs
-    where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-    putStrLn "Fibonacci sequence (first 10 terms):"
-    print $ fibonacci 10
-    putStrLn "\nMemoized version (first 15 terms):"
-    print $ fibonacciMemoized 15module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-  putStrLn "Fibonacci sequence up to 10 terms:"
-  print (fibonacci 10)
