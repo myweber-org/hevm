8501024c17e90ac9a769753944736008932b05a0
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-09 23:34:24 +0000

    Add a function to generate Fibonacci sequence up to a given number

--- cli/FibonacciSequence.hs
+++ cli/FibonacciSequence.hs
@@ -1,11 +1,6 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> Integer
-fibonacci n = fibs !! n
+fibonacci :: Integer -> [Integer]
+fibonacci n = takeWhile (<= n) fibs
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-    putStrLn "First 20 Fibonacci numbers:"
-    mapM_ (print . fibonacci) [0..19]
