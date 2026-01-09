ba00043f9d37cdaa6fa5ff02250b012886a20324
InkspillVerse <inkspill.verse@outlook.com>
2026-01-09 00:55:28 +0000

    Add a simple word frequency counter for text analysis

--- bench/WordFrequency.hs
+++ bench/WordFrequency.hs
@@ -1,67 +1,28 @@
 module WordFrequency where
 
-import qualified Data.Map.Strict as Map
-import Data.Char (toLower, isAlphaNum)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type WordCount = Map.Map String Int
-
-countWords :: String -> WordCount
-countWords = foldr insertWord Map.empty . words
-  where
-    insertWord w = Map.insertWith (+) (normalize w) 1
-    normalize = map toLower . filter isAlphaNum
-
-getTopWords :: Int -> WordCount -> [(String, Int)]
-getTopWords n = take n . sortOn (Down . snd) . Map.toList
-
-processTextFile :: FilePath -> IO ()
-processTextFile filepath = do
-    content <- readFile filepath
-    let wordCounts = countWords content
-        topWords = getTopWords 10 wordCounts
-    
-    putStrLn "Top 10 most frequent words:"
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWordsmodule WordFrequency where
-
-import qualified Data.Map as Map
-import Data.Char (toLower, isAlphaNum)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type FrequencyMap = Map.Map String Int
-
-countWords :: String -> FrequencyMap
-countWords = foldr insertWord Map.empty . words
-  where
-    insertWord w = Map.insertWith (+) (normalize w) 1
-    normalize = map toLower . filter isAlphaNum
-
-getTopWords :: Int -> String -> [(String, Int)]
-getTopWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
-
-processFile :: FilePath -> IO ()
-processFile path = do
-  content <- readFile path
-  let topWords = getTopWords 10 content
-  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) topWordsmodule WordFrequency where
-
-import qualified Data.Map.Strict as Map
 import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
 
-type FrequencyMap = Map.Map String Int
+type WordCount = [(String, Int)]
 
-countWords :: String -> FrequencyMap
-countWords = foldr incrementWord Map.empty . words
+countWords :: String -> WordCount
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map toLower <$> wordsList
+        frequencies = foldr countWord [] cleaned
+    in take 10 $ sortOn (Down . snd) frequencies
   where
-    incrementWord word = Map.insertWith (+) (normalize word) 1
-    normalize = map toLower . filter isAlpha
+    cleanWord = filter isAlpha
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
 
-topWords :: Int -> String -> [(String, Int)]
-topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+displayFrequency :: WordCount -> String
+displayFrequency freq = unlines $ map (\(w, c) -> w ++ ": " ++ show c) freq
 
-displayFrequencies :: [(String, Int)] -> String
-displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+analyzeText :: String -> String
+analyzeText text = 
+    let freq = countWords text
+    in "Top 10 most frequent words:\n" ++ displayFrequency freq
