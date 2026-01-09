044ff31144f53a9476461eb45efa7c1db51b3c9e
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-09 02:18:35 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -32,4 +32,27 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * 2 + 1)
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< 0) xs = Nothing
+    | otherwise = Just xs
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn "Original data:"
+            print validData
+            putStrLn "Processed data (even numbers doubled and incremented):"
+            print $ processData validData
+        Nothing -> putStrLn "Invalid input data"
