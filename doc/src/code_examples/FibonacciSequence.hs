3d21422ef55b804387eed4b6a9db073cc329ebda
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-09 00:57:13 +0000

    Add a function to generate the nth Fibonacci number using recursion

--- doc/src/code_examples/FibonacciSequence.hs
+++ doc/src/code_examples/FibonacciSequence.hs
@@ -1,11 +1,7 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fib :: Int -> Integer
-fib n = fibs !! n
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibonacci :: Integer -> Integer
+fibonacci n
+    | n <= 0    = 0
+    | n == 1    = 1
+    | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
