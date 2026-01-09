b0a7964a7090519d0883f067421d5507b3a35dcb
InkspillVerse <inkspill.verse@outlook.com>
2026-01-09 23:55:41 +0000

    Implement Sieve of Eratosthenes to generate prime numbers up to a given limit

--- src/PrimeSieve.hs
+++ src/PrimeSieve.hs
@@ -1,10 +1,19 @@
 module PrimeSieve where
 
+sieve :: Int -> [Int]
+sieve limit
+    | limit < 2 = []
+    | otherwise = sieve' [2..limit] []
+    where
+        sieve' [] primes = reverse primes
+        sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
+
 primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve :: [Int] -> [Int]
-    sieve []     = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+primesUpTo = sieve
+
+main :: IO ()
+main = do
+    putStrLn "Enter limit:"
+    input <- getLine
+    let limit = read input :: Int
+    print $ primesUpTo limit
