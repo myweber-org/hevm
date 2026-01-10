cec1082aab0424c9edba43235c7ce70a7756dfa1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-10 00:34:53 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -23,3 +23,21 @@ calculateTrend values
     where
         allIncreasing xs = and $ zipWith (<) xs (tail xs)
         allDecreasing xs = and $ zipWith (>) xs (tail xs)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Bool
+validateInput xs = not (null xs) && all (\x -> x >= -100 && x <= 100) xs
+
+safeProcess :: [Int] -> Maybe Int
+safeProcess xs
+    | validateInput xs = Just (sumProcessedData xs)
+    | otherwise = Nothing
