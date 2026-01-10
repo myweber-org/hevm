923c93c5f42feaa9bab83bc6b8bd97776c9900d7
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-10 23:40:50 +0000

    Add word frequency analysis function using Data.Map

--- doc/src/WordFrequency.hs
+++ doc/src/WordFrequency.hs
@@ -25,4 +25,26 @@ formatResults counts =
     unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
 
 processText :: String -> String
-processText = formatResults . countWords
+processText = formatResults . countWordsmodule WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlpha)
+
+countWords :: String -> Map.Map String Int
+countWords text = Map.fromListWith (+) [(word, 1) | word <- words text, not (null word)]
+  where
+    words = map (map toLower . filter isAlpha) . splitBySpace
+    splitBySpace = foldr splitter [[]]
+    splitter c acc@(current:rest)
+      | c == ' '  = []:acc
+      | otherwise = (c:current):rest
+
+mostFrequent :: String -> Maybe (String, Int)
+mostFrequent text
+  | Map.null freqMap = Nothing
+  | otherwise = Just (Map.foldrWithKey maxEntry ("", 0) freqMap)
+  where
+    freqMap = countWords text
+    maxEntry word count (maxWord, maxCount)
+      | count > maxCount = (word, count)
+      | otherwise = (maxWord, maxCount)
