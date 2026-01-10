a99b3c041f3193a10466b1e4512abb68e6aaf0f0
InkspillVerse <inkspill.verse@outlook.com>
2026-01-10 22:20:49 +0000

    Add word frequency counter for text analysis

--- doc/src/code_examples/WordFrequency.hs
+++ doc/src/code_examples/WordFrequency.hs
@@ -1,50 +1,24 @@
 module WordFrequency where
 
-import Data.Char (toLower, isAlpha)
-import Data.List (sortBy)
-import Data.Ord (comparing)
-
-type WordCount = (String, Int)
-
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        grouped = foldr countHelper [] wordsList
-    in sortBy (comparing snd) grouped
-  where
-    cleanWord = map toLower . filter isAlpha
-    countHelper word [] = [(word, 1)]
-    countHelper word ((w, c):rest)
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countHelper word rest
-
-printWordFrequencies :: [WordCount] -> IO ()
-printWordFrequencies counts = 
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
-
-analyzeText :: String -> IO ()
-analyzeText text = do
-    let frequencies = countWords text
-    putStrLn "Word frequencies (ascending order):"
-    printWordFrequencies frequenciesmodule WordFrequency where
-
 import qualified Data.Map.Strict as Map
-import Data.Char (toLower, isAlpha)
+import Data.Char (isAlpha, toLower)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
 
-type WordCount = Map.Map String Int
+type FrequencyMap = Map.Map String Int
 
-countWords :: String -> WordCount
+countWords :: String -> FrequencyMap
 countWords = foldr incrementWord Map.empty . words
   where
     incrementWord word = Map.insertWith (+) (normalize word) 1
     normalize = map toLower . filter isAlpha
 
-topWords :: Int -> String -> [(String, Int)]
-topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
 
-displayTopWords :: Int -> String -> IO ()
-displayTopWords n text = do
-  putStrLn $ "Top " ++ show n ++ " words:"
-  mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) (topWords n text)
+analyzeText :: String -> IO ()
+analyzeText text = do
+  putStrLn "Top 10 most frequent words:"
+  mapM_ printWord (topNWords 10 text)
+  where
+    printWord (word, count) = putStrLn $ word ++ ": " ++ show count
