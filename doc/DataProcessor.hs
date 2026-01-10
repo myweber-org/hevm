87167713833a566208558898a0629b0056c4a09d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-10 19:37:05 +0000

    Add function to filter and transform a list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -1,53 +1,13 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessed :: (Int -> Int) -> [Int] -> Int
-sumProcessed processor = sum . map processor
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
-    putStrLn $ "Sum of doubled odds: " ++ 
-        show (sumProcessed (\x -> x * 2) (filter odd numbers))
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [-5, 3, 0, 8, -2, 10]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = 
-    if all (> -100) xs && length xs <= 1000
-    then Just xs
-    else Nothing
-
-safeProcess :: [Int] -> Maybe Int
-safeProcess xs = do
-    valid <- validateInput xs
-    return $ sumProcessedData valid
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    putStrLn $ "Processed data: " ++ show result
