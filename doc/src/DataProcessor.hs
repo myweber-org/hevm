b4cf77fca9ee33fc115554b8407120461061c4fb
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-10 00:17:24 +0000

    Add function to calculate moving average of a list

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -33,4 +33,21 @@ main = do
     let numbers = [1..20]
     putStrLn $ "Original list: " ++ show numbers
     putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of processed numbers: " ++ show (sumProcessed numbers)
+    putStrLn $ "Sum of processed numbers: " ++ show (sumProcessed numbers)module DataProcessor where
+
+movingAverage :: (Fractional a) => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows size list = take (length list - size + 1) $ map (take size) (tails list)
+    
+    average :: (Fractional a) => [a] -> a
+    average lst = sum lst / fromIntegral (length lst)
+
+-- Helper function from Data.List
+tails :: [a] -> [[a]]
+tails [] = [[]]
+tails xs@(_:ys) = xs : tails ys
