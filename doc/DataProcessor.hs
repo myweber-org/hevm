4106f51aa1e627e2a92549c216d0d94bdd5f7421
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-10 22:40:25 +0000

    Add function to filter and transform list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -16,3 +16,19 @@ main = do
     putStrLn $ "Original list: " ++ show numbers
     putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
     putStrLn $ "Sum of even squares: " ++ show (sumProcessed (\x -> if even x then x*x else 0) numbers)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+safeProcess :: [Int] -> Maybe [Int]
+safeProcess xs
+    | validateInput xs = Just (processData xs)
+    | otherwise = Nothing
