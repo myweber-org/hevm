356dbfdaec9c7198d2908936e0f59052a1cf7174
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-10 01:14:52 +0000

    Add function to filter and transform a list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -15,4 +15,10 @@ main = do
     let sampleData = [1..10]
     putStrLn $ "Original: " ++ show sampleData
     putStrLn $ "Even squares: " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Odd cubes: " ++ show (processOddCubes sampleData)
+    putStrLn $ "Odd cubes: " ++ show (processOddCubes sampleData)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
