3d7f77e36e434897c7012aa72c9617977b6bb90e
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-10 15:41:36 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- ethjet/FibonacciSequence.hs
+++ ethjet/FibonacciSequence.hs
@@ -16,4 +16,26 @@ fibonacci n
     | otherwise = fibHelper n 0 1
   where
     fibHelper 0 a _ = a
-    fibHelper n a b = fibHelper (n - 1) b (a + b)
+    fibHelper n a b = fibHelper (n - 1) b (a + b)module Fibonacci where
+
+import Data.Function (fix)
+
+fibonacci :: Int -> Integer
+fibonacci = (map fib [0..] !!)
+  where
+    fib 0 = 0
+    fib 1 = 1
+    fib n = fibonacci (n - 1) + fibonacci (n - 2)
+
+-- Alternative implementation using fix for memoization
+fibonacciMemo :: Int -> Integer
+fibonacciMemo = fix memo
+  where
+    memo _ 0 = 0
+    memo _ 1 = 1
+    memo f n = f (n - 1) + f (n - 2)
+
+main :: IO ()
+main = do
+    putStrLn "Fibonacci numbers from 0 to 20:"
+    mapM_ (print . fibonacci) [0..20]
