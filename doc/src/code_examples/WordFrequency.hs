e0a18906c5c32d6c5e22bc3c888852f726d80ea8
InkspillVerse <inkspill.verse@outlook.com>
2026-01-10 16:09:50 +0000

    Add word frequency counting functionality using Data.Map

--- doc/src/code_examples/WordFrequency.hs
+++ doc/src/code_examples/WordFrequency.hs
@@ -26,4 +26,25 @@ analyzeText :: String -> IO ()
 analyzeText text = do
     let frequencies = countWords text
     putStrLn "Word frequencies (ascending order):"
-    printWordFrequencies frequencies
+    printWordFrequencies frequenciesmodule WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
+
+topWords :: Int -> String -> [(String, Int)]
+topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+displayTopWords :: Int -> String -> IO ()
+displayTopWords n text = do
+  putStrLn $ "Top " ++ show n ++ " words:"
+  mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) (topWords n text)
