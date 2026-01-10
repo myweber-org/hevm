7b29ab09715e5caf3e602fdde1fc289794726056
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-10 18:29:43 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -1,42 +1,20 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transform = map transform . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (\x -> x >= -100 && x <= 100) xs
+validateData :: [Int] -> Bool
+validateData = all (> 0)
 
 main :: IO ()
 main = do
-    let sampleData = [1, -2, 3, 0, 5, -8]
-    if validateInput sampleData
-        then print $ processData sampleData
-        else putStrLn "Invalid input data"
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 4, -1, 5, 9, -2, 6]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
+    let sampleData = [-3, 1, 0, 5, -2, 8]
+    putStrLn $ "Original data: " ++ show sampleData
+    
+    let processed = processData sampleData
+    putStrLn $ "Processed data: " ++ show processed
+    
+    putStrLn $ "Data validation: " ++ show (validateData processed)
