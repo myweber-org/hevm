4c9e90e8b4e178dac9717d3578b6fa577fb47f0c
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-11 14:48:03 +0000

    Add function to filter and transform a list of integers, extracting even numbers and computing their squares

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -1,16 +1,12 @@
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)module DataProcessor where
+processData = map (^2) . filter even
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+sampleData :: [Int]
+sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+main :: IO ()
+main = do
+    let result = processData sampleData
+    putStrLn $ "Processed data: " ++ show result
