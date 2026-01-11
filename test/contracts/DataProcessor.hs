3ae9018c39f0e2e2cb8a5cb0c4fbcb227af4c98f
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-11 00:27:17 +0000

    Add function to filter and transform a list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -1,46 +1,13 @@
 module DataProcessor where
 
-import Data.List (intercalate)
-import Data.Char (isDigit, isSpace)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
 
-type CSVRow = [String]
-type CSVData = [CSVRow]
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-parseCSV :: String -> Either String CSVData
-parseCSV input
-    | null (trim input) = Left "Empty input"
-    | otherwise = traverse validateRow $ map (splitOn ',') (lines input)
-  where
-    trim = dropWhile isSpace . reverse . dropWhile isSpace . reverse
-    
-    splitOn :: Char -> String -> [String]
-    splitOn delimiter = foldr splitHelper [""]
-      where
-        splitHelper ch (x:xs)
-            | ch == delimiter = "":x:xs
-            | otherwise = (ch:x):xs
-    
-    validateRow :: CSVRow -> Either String CSVRow
-    validateRow row
-        | length row < 2 = Left $ "Row has insufficient columns: " ++ show row
-        | any null (map trim row) = Left $ "Row contains empty fields: " ++ show row
-        | not (allDigits (trim (row !! 1))) = Left $ "Second column must be numeric: " ++ show row
-        | otherwise = Right (map trim row)
-    
-    allDigits :: String -> Bool
-    allDigits = all isDigit
-
-calculateTotal :: CSVData -> Either String Double
-calculateTotal rows = do
-    let values = map (read . (!! 1)) rows
-    return $ sum values
-
-formatOutput :: CSVData -> String
-formatOutput rows = intercalate "\n" $ map (intercalate " | ") rows
-
-processCSVData :: String -> Either String String
-processCSVData input = do
-    parsed <- parseCSV input
-    total <- calculateTotal parsed
-    let formatted = formatOutput parsed
-    return $ formatted ++ "\nTotal: " ++ show total
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -7]
+    let result = processNumbers input
+    print result
