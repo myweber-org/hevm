0368ce69bd273c34ecb3ff15e40b40454afb78cd
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-11 22:35:20 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -47,3 +47,16 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
+
+combineData :: [Int] -> [Int] -> [Int]
+combineData xs ys = zipWith (+) (processData xs) (processData ys)
