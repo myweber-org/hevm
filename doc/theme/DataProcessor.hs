22879d1145992c8b07a3dd1707177784f88ee8d6
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-11 18:29:03 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -4,29 +4,8 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedList :: (Int -> Int) -> [Int] -> Int
-sumProcessedList processor = sum . map processor
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processEvenSquares numbers
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Even numbers squared: " ++ show result
-    putStrLn $ "Sum of processed list: " ++ show (sumProcessedList (\x -> x * 2) result)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * x + 1)
+processNumbers = filterAndTransform (> 0) (* 2)
 
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show result
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
