cfc61d8e0d35e57f0a12f22c658c6d33bc4278fb
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-11 19:27:08 +0000

    Add function to calculate moving average of a list

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -156,4 +156,25 @@ main = do
     let sampleData = [1, -2, 3, 0, 5, -8]
     if validateInput sampleData
         then print $ processData sampleData
-        else putStrLn "Input validation failed"
+        else putStrLn "Input validation failed"module DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map avg $ filter (\w -> length w == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
+
+-- Example usage with test data
+testData :: [Double]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+
+main :: IO ()
+main = do
+    putStrLn "Testing moving average with window size 3:"
+    print $ movingAverage 3 testData
+    putStrLn "\nTesting moving average with window size 5:"
+    print $ movingAverage 5 testData
