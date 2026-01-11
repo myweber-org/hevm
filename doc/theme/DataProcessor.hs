8cbfd661108f33eb136df48b0c401806a599eb53
InkspillVerse <inkspill.verse@outlook.com>
2026-01-11 16:00:51 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -1,7 +1,19 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedList :: (Int -> Int) -> [Int] -> Int
+sumProcessedList processor = sum . map processor
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processEvenSquares numbers
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Even numbers squared: " ++ show result
+    putStrLn $ "Sum of processed list: " ++ show (sumProcessedList (\x -> x * 2) result)
