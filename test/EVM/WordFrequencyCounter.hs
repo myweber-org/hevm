b80ff9d47640e8cf09ca54b59d48bfc8d73ee4f7
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-11 17:15:26 +0000

    Add word frequency counter module with basic text processing functions

--- test/EVM/WordFrequencyCounter.hs
+++ test/EVM/WordFrequencyCounter.hs
@@ -56,4 +56,36 @@ main = do
     input <- getContents
     let frequencies = countWords input
     putStrLn "\nWord Frequency Histogram:"
-    putStrLn $ formatHistogram $ take 20 frequencies
+    putStrLn $ formatHistogram $ take 20 frequenciesmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = groupCount cleanedWords
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    
+    groupCount :: [String] -> [WordCount]
+    groupCount = foldr incrementCount []
+    
+    incrementCount :: String -> [WordCount] -> [WordCount]
+    incrementCount word [] = [(word, 1)]
+    incrementCount word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : incrementCount word rest
+
+displayResults :: [WordCount] -> String
+displayResults counts = 
+    "Top 10 most frequent words:\n" ++
+    unlines (map (\(word, count) -> word ++ ": " ++ show count) counts)
+
+processText :: String -> String
+processText = displayResults . countWords
