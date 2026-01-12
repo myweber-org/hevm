7071fdd80bb191d6e22a9793a1446df64e844d7e
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-12 00:17:45 +0000

    Add word frequency counter for text analysis

--- doc/WordFrequency.hs
+++ doc/WordFrequency.hs
@@ -75,4 +75,25 @@ displayFrequencies :: [(String, Int)] -> String
 displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
-analyzeText n = displayFrequencies . topNWords n
+analyzeText n = displayFrequencies . topNWords nmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+type WordCount = Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord w = Map.insertWith (+) (normalize w) 1
+    normalize = filter isAlpha . map toLower
+
+topWords :: Int -> String -> [(String, Int)]
+topWords n text = take n $ sortOn (negate . snd) $ Map.toList (countWords text)
+
+displayFrequency :: String -> IO ()
+displayFrequency text = do
+  putStrLn "Word Frequency Analysis:"
+  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (topWords 10 text)
