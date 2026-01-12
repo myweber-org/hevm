4402795732d7c5adbf6125c08800ad3a8fbf4280
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-12 15:08:22 +0000

    Add function to calculate depth of a binary tree

--- test/contracts/lib/BinaryTreeDepth.hs
+++ test/contracts/lib/BinaryTreeDepth.hs
@@ -1,13 +1,6 @@
-data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show)
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
 
 treeDepth :: BinaryTree a -> Int
-treeDepth Leaf = 0
-treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Leaf = 0
-treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show, Eq)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Leaf = 0
-treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)
+treeDepth Empty = 0
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
