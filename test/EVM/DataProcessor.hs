9357ac37c474f76be48d62fdd312966bc2dbb40b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-12 20:45:25 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -17,3 +17,17 @@ main = do
     let result = processEvenSquares input
     putStrLn $ "Input list: " ++ show input
     putStrLn $ "Even numbers squared: " ++ show result
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -8]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
