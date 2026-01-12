267079dfef9c7b2fd9411dc445b42e1304ad4b9b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-12 19:42:42 +0000

    Add function to filter and transform a list of integers, keeping only even numbers and squaring them

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -6,3 +6,14 @@ filterAndTransform predicate transformer =
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = map (^2) . filter even
+
+main :: IO ()
+main = do
+    let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    let result = processEvenSquares input
+    putStrLn $ "Input list: " ++ show input
+    putStrLn $ "Even numbers squared: " ++ show result
