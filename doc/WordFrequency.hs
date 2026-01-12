955aa7656ceaad9994d4eba2ed35d1d288659507
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-12 21:11:37 +0000

    Add word frequency analysis function to TextUtils module

--- doc/WordFrequency.hs
+++ doc/WordFrequency.hs
@@ -97,3 +97,30 @@ displayFrequency :: String -> IO ()
 displayFrequency text = do
   putStrLn "Word Frequency Analysis:"
   mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (topWords 10 text)
+module TextUtils.WordFrequency where
+
+import Data.Char (isAlpha, toLower)
+import Data.List (sortOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Ord (Down(..))
+
+type WordCount = Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord w m = Map.insertWith (+) (normalize w) 1 m
+    normalize = map toLower . filter isAlpha
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+wordFrequencyReport :: String -> String
+wordFrequencyReport text = unlines $
+  "Total unique words: " ++ show (Map.size counts) :
+  "Top 10 most frequent words:" :
+  map formatEntry (topNWords 10 text)
+  where
+    counts = countWords text
+    formatEntry (word, count) = "  " ++ word ++ ": " ++ show count
