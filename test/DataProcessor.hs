53d1a6d1448b47270b54fc5e5e3a87f0eebe3de3
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-12 16:54:18 +0000

    Add function to filter and transform a list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -178,3 +178,16 @@ main = do
     print $ movingAverage 3 testData
     putStrLn "\nTesting moving average with window size 5:"
     print $ movingAverage 5 testData
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processNumbers input
+    print result
