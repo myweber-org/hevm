f9d9e2e3837fda0729ee6c4db642d6ab35d93303
InkspillVerse <inkspill.verse@outlook.com>
2026-01-12 23:26:36 +0000

    Add function to filter and transform a list of integers, returning only even numbers squared

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -1,25 +1,13 @@
 
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-module DataProcessor where
+processData = map (^2) . filter even
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processNumbers
+sampleData :: [Int]
+sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 main :: IO ()
 main = do
-    let numbers = [-3, -1, 0, 2, 5, 8, -4]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of positive doubles: " ++ show (sumPositiveDoubles numbers)
+    let result = processData sampleData
+    putStrLn $ "Processed data: " ++ show result
