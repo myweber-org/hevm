54d298e7151d06f3a0b07f0d873bbeb934d7c48e
InkspillVerse <inkspill.verse@outlook.com>
2026-01-13 01:00:51 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -4,48 +4,18 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = 
     map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = map (^2) . filter even
-
-main :: IO ()
-main = do
-    let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let result = processEvenSquares input
-    putStrLn $ "Input list: " ++ show input
-    putStrLn $ "Even numbers squared: " ++ show result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, 0, 5, -8]
-    let result = processData input
-    putStrLn $ "Input: " ++ show input
-    putStrLn $ "Result: " ++ show resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
 processNumbers :: [Int] -> [Int]
-processNumbers = 
-    filterAndTransform (\x -> x > 0 && x `mod` 2 == 0) (\x -> x * 2 + 1)
+processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
 
-main :: IO ()
-main = do
-    let sampleData = [-5, 2, 3, 8, 10, -1, 7]
-    putStrLn $ "Original list: " ++ show sampleData
-    putStrLn $ "Processed list: " ++ show (processNumbers sampleData)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed sampleData)
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< -100) xs = Nothing
+    | any (> 1000) xs = Nothing
+    | otherwise = Just xs
+
+safeProcess :: [Int] -> Maybe Int
+safeProcess = fmap sumProcessed . validateInput
