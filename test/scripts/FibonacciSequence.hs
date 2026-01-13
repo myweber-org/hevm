ceae1941ca078f66c8d4a10b1f40037236e19ffc
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-13 20:05:06 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- test/scripts/FibonacciSequence.hs
+++ test/scripts/FibonacciSequence.hs
@@ -1,25 +1,9 @@
-
 module FibonacciSequence where
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
+fib :: Int -> Integer
+fib n = fibMemo !! n
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Integer -> [Integer]
-fibonacci n = takeWhile (<= n) fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-  putStrLn "Enter a number:"
-  input <- getLine
-  let n = read input :: Integer
-  putStrLn $ "Fibonacci sequence up to " ++ show n ++ ":"
-  print $ fibonacci n
+    fibMemo = map fib' [0..]
+    fib' 0 = 0
+    fib' 1 = 1
+    fib' i = fibMemo !! (i - 1) + fibMemo !! (i - 2)
