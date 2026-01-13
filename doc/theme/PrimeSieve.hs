2e25bda1b01a3ec02a995637ffee8f01b45af961
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-13 19:30:48 +0000

    Implement a simple prime number sieve using the Sieve of Eratosthenes algorithm.

--- doc/theme/PrimeSieve.hs
+++ doc/theme/PrimeSieve.hs
@@ -1,111 +1,13 @@
-
 module PrimeSieve where
 
-sieve :: Int -> [Int]
-sieve limit
-  | limit < 2 = []
-  | otherwise = sieve' [2..limit] []
+primes :: [Int]
+primes = sieve [2..]
   where
-    sieve' [] primes = reverse primes
-    sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
-main :: IO ()
-main = do
-  putStrLn "Enter limit:"
-  input <- getLine
-  let limit = read input :: Int
-  print $ sieve limitmodule PrimeSieve where
+-- Function to get the first n prime numbers
+firstNPrimes :: Int -> [Int]
+firstNPrimes n = take n primes
 
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-    where
-        go []     acc = reverse acc
-        go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
-    where
-        sieve' [] primes = reverse primes
-        sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit for prime numbers:"
-    input <- getLine
-    let limit = read input :: Int
-    let primes = sieve limit
-    putStrLn $ "Prime numbers up to " ++ show limit ++ ":"
-    print primesmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
-    where
-        sieve' [] primes = reverse primes
-        sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-    where
-        go []     acc = reverse acc
-        go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limit
-module PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-  where
-    go []     acc = reverse acc
-    go (p:xs) acc = go (filter (\x -> x `mod` p /= 0) xs) (p:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sievemodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-  where
-    go [] primes = reverse primes
-    go (x:xs) primes = go (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
+-- Example usage in GHCi:
+-- firstNPrimes 10 -> [2,3,5,7,11,13,17,19,23,29]
