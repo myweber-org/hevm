c8ec53c31c4797583cd289e4f2385db8968f05a7
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-13 14:32:10 +0000

    Add Fibonacci sequence generator with memoization

--- doc/src/FibonacciSequence.hs
+++ doc/src/FibonacciSequence.hs
@@ -1,18 +1,5 @@
 module FibonacciSequence where
 
-import Data.Function (fix)
-
-fibonacci :: Int -> Integer
-fibonacci = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibonacci (n - 1) + fibonacci (n - 2)
-
--- Alternative implementation using fix for memoization
-fibonacciMemoized :: Int -> Integer
-fibonacciMemoized = fix (\rec n -> if n < 2 then fromIntegral n else rec (n - 1) + rec (n - 2))module FibonacciSequence where
-
 fibonacci :: Int -> Integer
 fibonacci n = fibs !! n
   where
