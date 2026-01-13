65eff1993c9634916ecd96526d9ff34045855a37
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-13 14:58:18 +0000

    Add function to parse CSV data and calculate average values

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -4,4 +4,22 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processData = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+import Data.List.Split (splitOn)
+
+parseCSV :: String -> [[Double]]
+parseCSV content = map (map read . splitOn ",") (lines content)
+
+calculateAverages :: [[Double]] -> [Double]
+calculateAverages rows = 
+    if null rows then []
+    else map (\col -> sum col / fromIntegral (length col)) (transpose rows)
+  where
+    transpose :: [[Double]] -> [[Double]]
+    transpose [] = []
+    transpose ([]:_) = []
+    transpose x = map head x : transpose (map tail x)
+
+processCSVData :: String -> [Double]
+processCSVData = calculateAverages . parseCSV
