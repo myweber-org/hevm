ed57a5068222c094e97dfd20b577d9c9e4a78b75
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-14 23:47:56 +0000

    Add word frequency counting utility with sorting and filtering options

--- ethjet/WordFrequencyCounter.hs
+++ ethjet/WordFrequencyCounter.hs
@@ -19,4 +19,42 @@ main :: IO ()
 main = do
     let sampleText = "Hello world! Hello Haskell. Haskell is fun, isn't it?"
     let frequencies = countWords sampleText
-    printFrequencies frequencies
+    printFrequencies frequenciesmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        grouped = group $ sort cleanedWords
+    in map (\ws -> (head ws, length ws)) grouped
+
+cleanWord :: String -> String
+cleanWord = filter (\c -> isAlphaNum c || c == '\'')
+
+sortByFrequency :: [WordCount] -> [WordCount]
+sortByFrequency = sortOn (Down . snd)
+
+sortAlphabetically :: [WordCount] -> [WordCount]
+sortAlphabetically = sortOn fst
+
+filterByMinFrequency :: Int -> [WordCount] -> [WordCount]
+filterByMinFrequency minFreq = filter ((>= minFreq) . snd)
+
+getTopNWords :: Int -> [WordCount] -> [WordCount]
+getTopNWords n = take n . sortByFrequency
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+processText :: String -> Int -> Int -> String
+processText text minFreq topN = 
+    let counts = countWords text
+        filtered = filterByMinFrequency minFreq counts
+        topWords = getTopNWords topN filtered
+    in formatOutput topWords
