4989c26498fcb00e10333de55c333598109949b0
InkspillVerse <inkspill.verse@outlook.com>
2026-01-14 14:20:18 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -50,4 +50,20 @@ main = do
     let sampleData = [1, -2, 3, 4, -5, 6]
     putStrLn $ "Original data: " ++ show sampleData
     putStrLn $ "Processed data: " ++ show (processData sampleData)
-    putStrLn $ "Validation result: " ++ show (validateData sampleData)
+    putStrLn $ "Validation result: " ++ show (validateData sampleData)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 0
+
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, -4, 5]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Processed data: " ++ show (processData sampleData)
+    putStrLn $ "Data valid: " ++ show (validateData sampleData)
