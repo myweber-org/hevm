165070fb57547f3a718838e3e815f2ad324231d2
InkspillVerse <inkspill.verse@outlook.com>
2026-01-14 15:40:48 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,36 +1,14 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transform = map transform . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (* 2)
+processNumbers = filterAndTransform (> 0) (* 2)
 
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print result
-module DataProcessor where
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (^2)
-
-sumProcessed :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessed predicate transformer = sum . filterAndTransform predicate transformer
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
-    putStrLn $ "Sum of even squares: " ++ show (sumProcessed even (^2) numbers)
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -1000) xs then Just xs else Nothing
