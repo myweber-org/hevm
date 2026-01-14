02eb2b1816c06c940eb408e8f810b107bbd83722
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-14 19:14:24 +0000

    Add word frequency counting utility with command-line interface

--- test/contracts/fail/WordFrequencyCounter.hs
+++ test/contracts/fail/WordFrequencyCounter.hs
@@ -24,4 +24,32 @@ processText = formatOutput . countWordFrequency
 main :: IO ()
 main = do
     input <- getContents
-    putStr $ processText input
+    putStr $ processText inputmodule WordFrequencyCounter where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+import qualified System.Environment as Env
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map Char.toLower . filter Char.isAlphaNum
+
+formatResults :: WordCount -> String
+formatResults = unlines . map formatEntry . List.sortOn snd . Map.toList
+  where
+    formatEntry (word, count) = word ++ ": " ++ show count
+
+main :: IO ()
+main = do
+  args <- Env.getArgs
+  case args of
+    [] -> putStrLn "Usage: wordfreq <text>"
+    textPieces -> do
+      let text = unwords textPieces
+      putStrLn "Word frequencies:"
+      putStrLn . formatResults $ countWords text
