863b64094b0063a0424c60e8699aaea63850db7d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-14 16:24:48 +0000

    Add function to calculate the maximum depth of a binary tree

--- test/scripts/BinaryTreeDepth.hs
+++ test/scripts/BinaryTreeDepth.hs
@@ -1,15 +1,5 @@
-
-data BinaryTree a = EmptyTree | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+data BinaryTree a = Leaf | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 treeDepth :: BinaryTree a -> Int
-treeDepth EmptyTree = 0
+treeDepth Leaf = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: BinaryTree Int
-sampleTree = Node 1
-                (Node 2
-                    (Node 4 EmptyTree EmptyTree)
-                    (Node 5 EmptyTree EmptyTree))
-                (Node 3
-                    EmptyTree
-                    (Node 6 EmptyTree EmptyTree))
