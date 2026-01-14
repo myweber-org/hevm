4f1a068d56c056a282b504379f89e7f779ef0e24
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-14 21:49:12 +0000

    Implement Sieve of Eratosthenes to generate primes up to a given limit

--- test/contracts/PrimeSieve.hs
+++ test/contracts/PrimeSieve.hs
@@ -3,10 +3,10 @@ module PrimeSieve where
 sieve :: Int -> [Int]
 sieve limit
     | limit < 2 = []
-    | otherwise = go [2..limit] []
+    | otherwise = sieve' [2..limit] []
     where
-        go []     acc = reverse acc
-        go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
+        sieve' [] primes = reverse primes
+        sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
 
 primesUpTo :: Int -> [Int]
 primesUpTo = sieve
