90c27c239a57328a57e8887ee3371b1fcbb06319
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-15 20:21:17 +0000

    Add word frequency counting utility with command-line interface

--- bench/contracts/WordFrequency.hs
+++ bench/contracts/WordFrequency.hs
@@ -19,4 +19,34 @@ topNWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
 displayTopWords :: Int -> String -> String
 displayTopWords n text = unlines $ map format $ topNWords n text
   where
-    format (word, count) = word ++ ": " ++ show count
+    format (word, count) = word ++ ": " ++ show countmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map toLower <$> wordsList
+        grouped = foldr (\w m -> insertWord w m) [] cleaned
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = filter isAlpha
+    insertWord w [] = [(w, 1)]
+    insertWord w ((x, n):xs)
+        | w == x = (x, n+1):xs
+        | otherwise = (x, n):insertWord w xs
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+
+main :: IO ()
+main = do
+    putStrLn "Enter text (press Ctrl+D when finished):"
+    input <- getContents
+    let frequencies = countWords input
+    putStrLn "\nWord frequencies:"
+    putStrLn $ formatOutput frequencies
