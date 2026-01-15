f8099f70465e49063267e78022cfd01066371d69
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-15 21:01:42 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -1,43 +1,12 @@
+
 module DataProcessor where
 
-import Data.Char (isAlpha, isDigit, toLower)
-import Data.List (intercalate)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
 
-type Username = String
-type Email = String
-type UserData = (Username, Email)
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
 
-validateUsername :: Username -> Bool
-validateUsername name = 
-    let len = length name
-        validChars = all (\c -> isAlpha c || isDigit c || c `elem` "_-") name
-    in len >= 3 && len <= 20 && validChars
-
-validateEmail :: Email -> Bool
-validateEmail email =
-    let parts = splitOn '@' email
-        hasAt = length parts == 2
-        [local, domain] = if hasAt then parts else ["", ""]
-        validLocal = not (null local) && all (\c -> isAlpha c || isDigit c || c `elem` "._%+-") local
-        validDomain = '.' `elem` domain && all (\c -> isAlpha c || isDigit c || c `elem` ".-") domain
-    in hasAt && validLocal && validDomain
-
-normalizeUsername :: Username -> Username
-normalizeUsername = map toLower . filter (/= ' ')
-
-formatUserDisplay :: UserData -> String
-formatUserDisplay (name, email) =
-    intercalate " | " ["User: " ++ name, "Email: " ++ email]
-
-processUserInput :: Username -> Email -> Maybe String
-processUserInput rawName rawEmail
-    | not (validateUsername rawName) = Nothing
-    | not (validateEmail rawEmail) = Nothing
-    | otherwise = Just $ formatUserDisplay (normalizedName, rawEmail)
-    where normalizedName = normalizeUsername rawName
-
-splitOn :: Char -> String -> [String]
-splitOn delimiter = foldr (\c acc -> 
-    if c == delimiter 
-    then []:acc 
-    else (c:head acc):tail acc) [[]]
+sumProcessedList :: [Int] -> Int
+sumProcessedList = foldl' (+) 0 . processEvenSquares
+  where foldl' = foldl
