2b70b31268f9143ece1608ec27778c6398b63ab8
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-15 23:13:15 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/contracts/lib/FibonacciSequence.hs
+++ test/contracts/lib/FibonacciSequence.hs
@@ -13,4 +13,9 @@ main = do
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
