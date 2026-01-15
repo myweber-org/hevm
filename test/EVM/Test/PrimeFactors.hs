fb13ccdb5743915e0d2bfeb6da9d527811c2e3c6
InkspillVerse <inkspill.verse@outlook.com>
2026-01-15 20:26:25 +0000

    Add prime factorization function using trial division

--- test/EVM/Test/PrimeFactors.hs
+++ test/EVM/Test/PrimeFactors.hs
@@ -7,17 +7,6 @@ primeFactors n
   | otherwise = factorize n 2
   where
     factorize 1 _ = []
-    factorize m p
-      | m < p * p  = [m]
-      | m `mod` p == 0 = p : factorize (m `div` p) p
-      | otherwise      = factorize m (p + 1)
-module PrimeFactors where
-
-primeFactors :: Integer -> [Integer]
-primeFactors n = factors n 2
-  where
-    factors 1 _ = []
-    factors m p
-      | m < p * p = [m]
-      | m `mod` p == 0 = p : factors (m `div` p) p
-      | otherwise = factors m (p + 1)
+    factorize m d
+      | m `mod` d == 0 = d : factorize (m `div` d) d
+      | otherwise      = factorize m (d + 1)
