9069a7309157efa68eefadec87033b0d68ab3e25
InkspillVerse <inkspill.verse@outlook.com>
2026-01-16 00:41:35 +0000

    Add Fibonacci sequence generator with memoization for efficiency

--- bench/contracts/FibonacciSequence.hs
+++ bench/contracts/FibonacciSequence.hs
@@ -1,11 +1,24 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+import Data.Function (fix)
 
-printFibonacci :: Int -> IO ()
-printFibonacci n = do
-  putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
-  mapM_ (putStrLn . show) (fibonacci n)
+fibonacci :: Int -> Integer
+fibonacci = (map fib [0..] !!)
+  where
+    fib 0 = 0
+    fib 1 = 1
+    fib n = fibonacci (n - 1) + fibonacci (n - 2)
+
+memoizedFibonacci :: Int -> Integer
+memoizedFibonacci = fix (memoize . fib)
+  where
+    fib f 0 = 0
+    fib f 1 = 1
+    fib f n = f (n - 1) + f (n - 2)
+    
+    memoize f = (map f [0..] !!)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    mapM_ (print . memoizedFibonacci) [0..19]
