0fd5fb4722c02ac517e8857d8000e62987535e57
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-16 15:04:39 +0000

    Add function to filter and transform a list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -36,4 +36,17 @@ processData content =
   let records = parseCSV content
   in if null records 
      then Nothing 
-     else Just (calculateAverages records)
+     else Just (calculateAverages records)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let input = [1..10]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
