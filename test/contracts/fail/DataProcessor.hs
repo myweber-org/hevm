284d4b3abc3e885cfe7ec9f46c2ca4fc3d854972
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-16 22:49:57 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -1,3 +1,4 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
@@ -8,69 +9,3 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
-module DataProcessor where
-
-import Data.List.Split (splitOn)
-import Data.Maybe (mapMaybe)
-
-type Record = (String, Double, Double)
-
-parseCSV :: String -> [Record]
-parseCSV content = mapMaybe parseLine (lines content)
-  where
-    parseLine line = case splitOn "," line of
-      [name, val1Str, val2Str] -> 
-        case (reads val1Str, reads val2Str) of
-          ([(val1, "")], [(val2, "")]) -> Just (name, val1, val2)
-          _ -> Nothing
-      _ -> Nothing
-
-calculateAverages :: [Record] -> (Double, Double)
-calculateAverages records = (avg val1s, avg val2s)
-  where
-    (val1s, val2s) = unzip [(v1, v2) | (_, v1, v2) <- records]
-    avg xs = sum xs / fromIntegral (length xs)
-
-processData :: String -> Maybe (Double, Double)
-processData content = 
-  let records = parseCSV content
-  in if null records 
-     then Nothing 
-     else Just (calculateAverages records)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform even (\x -> x * x + 1)
-
-main :: IO ()
-main = do
-    let input = [1..10]
-    let result = processData input
-    putStrLn $ "Input: " ++ show input
-    putStrLn $ "Result: " ++ show result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessed :: (Int -> Int) -> [Int] -> Int
-sumProcessed processor = sum . map processor
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn "Original list:"
-    print numbers
-    
-    putStrLn "\nSquares of even numbers:"
-    let processed = processEvenSquares numbers
-    print processed
-    
-    putStrLn "\nSum of squares of even numbers:"
-    print $ sumProcessed (\x -> x * x) (filter even numbers)
