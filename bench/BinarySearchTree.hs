645c0e4ff0c4ad015e5eddc52cd043cc462388dd
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-16 18:54:05 +0000

    Implement a binary search tree with insertion and in-order traversal functions

--- bench/BinarySearchTree.hs
+++ bench/BinarySearchTree.hs
@@ -19,3 +19,20 @@ fromList = foldr insert Empty
 
 toSortedList :: BST a -> [a]
 toSortedList = inOrder
+module BinarySearchTree where
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert x Empty = Node x Empty Empty
+insert x (Node y left right)
+    | x < y     = Node y (insert x left) right
+    | x > y     = Node y left (insert x right)
+    | otherwise = Node y left right
+
+inOrder :: Tree a -> [a]
+inOrder Empty = []
+inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
+
+fromList :: Ord a => [a] -> Tree a
+fromList = foldr insert Empty
