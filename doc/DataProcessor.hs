09a941cd4b3c79b18d083a0bcaded52038695ca8
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-17 23:52:23 +0000

    Add function to filter and transform list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -16,3 +16,20 @@ validateInput xs
     | any (< -100) xs = Nothing
     | any (> 100) xs = Nothing
     | otherwise = Just xs
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transform = map transform . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (>0) (*2)
+
+validateData :: [Int] -> Bool
+validateData = all (>0)
+
+main :: IO ()
+main = do
+    let sampleData = [-3, 5, 0, 8, -1, 12]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Processed data: " ++ show (processData sampleData)
+    putStrLn $ "Data valid: " ++ show (validateData sampleData)
