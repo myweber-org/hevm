0528e8d461956aa109c826da4e9102d4c846b289
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-17 23:27:30 +0000

    Add function to calculate the maximum depth of a binary tree

--- src/EVM/BinaryTreeDepth.hs
+++ src/EVM/BinaryTreeDepth.hs
@@ -7,3 +7,8 @@ treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data B
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+
+treeDepth :: Tree a -> Int
+treeDepth Empty = 0
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
