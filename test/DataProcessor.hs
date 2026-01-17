3e3c0145bb95c32de7b96ee37ece2d6272002f96
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-17 15:50:35 +0000

    Add data validation and transformation module for CSV processing

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -1,20 +1,49 @@
 
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+import Data.List (intercalate)
+import Data.Char (isDigit, isAlpha)
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)module DataProcessor where
+type ValidationRule = String -> Bool
+type Transformation = String -> String
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+validateNumeric :: ValidationRule
+validateNumeric = all isDigit
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+validateAlpha :: ValidationRule
+validateAlpha = all isAlpha
 
-validateData :: [Int] -> Bool
-validateData xs = all (> 0) xs && length xs > 3
+validateLength :: Int -> ValidationRule
+validateLength n s = length s == n
 
-combineProcessors :: [Int] -> [Int]
-combineProcessors xs = if validateData xs then processData xs else []
+transformUppercase :: Transformation
+transformUppercase = map toUpper
+
+transformPadLeft :: Int -> Char -> Transformation
+transformPadLeft n c s = replicate (n - length s) c ++ s
+
+processCSVRow :: [ValidationRule] -> [Transformation] -> [String] -> Either String [String]
+processCSVRow validators transformers row
+    | length row /= length validators = Left "Column count mismatch"
+    | not (all id $ zipWith ($) validators row) = Left "Validation failed"
+    | otherwise = Right $ zipWith ($) transformers row
+
+formatCSV :: [String] -> String
+formatCSV = intercalate ","
+
+safeReadInt :: String -> Maybe Int
+safeReadInt s = case reads s of
+    [(n, "")] -> Just n
+    _ -> Nothing
+
+processDataFile :: [String] -> Either String String
+processDataFile rows = do
+    processed <- mapM (processCSVRow [validateNumeric, validateAlpha] [transformPadLeft 5 '0', transformUppercase]) rows
+    return $ unlines $ map formatCSV processed
+
+main :: IO ()
+main = do
+    let testData = [["123", "abc"], ["456", "def"]]
+    case processDataFile testData of
+        Left err -> putStrLn $ "Error: " ++ err
+        Right result -> putStrLn result
