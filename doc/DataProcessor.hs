feb68b09a23cd71a51795ab181a146474c4a3046
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-17 14:16:06 +0000

    Add function to filter and transform list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -1,39 +1,18 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print resultmodule DataProcessor where
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
 
-import Data.List (tails)
-
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map avg $ filter (\w -> length w == n) $ tails xs
-  where
-    avg window = sum window / fromIntegral n
-
-smoothData :: Fractional a => Int -> [a] -> [a]
-smoothData windowSize dataPoints =
-    movingAverage windowSize dataPoints
-
-calculateTrend :: Fractional a => [a] -> a
-calculateTrend [] = 0
-calculateTrend values =
-    let n = fromIntegral $ length values
-        indices = [0..n-1]
-        sumX = sum indices
-        sumY = sum values
-        sumXY = sum $ zipWith (*) indices values
-        sumX2 = sum $ map (^2) indices
-        slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX)
-    in slope
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< -100) xs = Nothing
+    | any (> 100) xs = Nothing
+    | otherwise = Just xs
