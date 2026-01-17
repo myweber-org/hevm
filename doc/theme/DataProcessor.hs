4ca668c6a3b5f1e526e07857c69eaf023e523513
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-17 15:43:55 +0000

    Add function to filter and transform a list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -17,3 +17,10 @@ main = do
     putStrLn $ "Original list: " ++ show numbers
     putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
     putStrLn $ "Sum of doubled values: " ++ show (sumProcessed (*2) numbers)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
