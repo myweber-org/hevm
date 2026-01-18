a82c0c6043d5fb8b33f1ba5eb6ba487767488283
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-18 14:27:41 +0000

    Add function to filter and transform a list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -13,4 +13,17 @@ sumProcessed processor = foldl' (+) 0 . map processor
         foldl' f acc (x:xs) = foldl' f (f acc x) xs
 
 validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (>0) xs then Just xs else Nothing
+validateInput xs = if all (>0) xs then Just xs else Nothingmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let input = [1..10]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
