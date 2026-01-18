90252cbdfe6786ec14a4e91d14e6fcba0f0b46d2
InkspillVerse <inkspill.verse@outlook.com>
2026-01-18 20:07:53 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -35,3 +35,20 @@ main = do
     putStrLn $ "Original data: " ++ show sampleData
     putStrLn $ "Processed data: " ++ show (processData sampleData)
     putStrLn $ "Sum of processed data: " ++ show (sumProcessedData sampleData)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5, 0, 7]
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Processed: " ++ show (processData input)
+    putStrLn $ "Sum: " ++ show (sumProcessedData input)
