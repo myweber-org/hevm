b55a298617cc0f9ffa5c2579125d99af4a753b65
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-18 00:14:13 +0000

    Add word frequency analysis utility with histogram output

--- test/EVM/Test/WordFrequency.hs
+++ test/EVM/Test/WordFrequency.hs
@@ -21,3 +21,44 @@ displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
 analyzeText n = displayFrequencies . topNWords n
+module WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+histogramBar :: Int -> Int -> String
+histogramBar count maxWidth
+    | maxWidth <= 0 = ""
+    | otherwise = replicate barLength '█' ++ padding
+  where
+    barLength = round (fromIntegral count / fromIntegral maxWidth * 30)
+    padding = replicate (30 - barLength) ' '
+
+analyzeText :: String -> [WordCount]
+analyzeText text = 
+    take 10 $ 
+    sortOn (Down . snd) $
+    map (\ws -> (head ws, length ws)) $
+    group $
+    sort $
+    filter (not . null) $
+    map (filter isAlpha . map toLower) $
+    words text
+
+displayHistogram :: [WordCount] -> IO ()
+displayHistogram counts = do
+    putStrLn "\nTop 10 Most Frequent Words:"
+    putStrLn "============================="
+    let maxCount = maximum (map snd counts)
+    mapM_ (\(word, count) -> 
+        putStrLn $ word ++ " " ++ histogramBar count maxCount ++ " " ++ show count) counts
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze (press Ctrl+D when finished):"
+    content <- getContents
+    let frequencies = analyzeText content
+    displayHistogram frequencies
