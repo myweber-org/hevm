1f68ddc609311e9bb51bd08be20ac426ee95b958
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-18 15:48:03 +0000

    Add function to filter and transform list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -13,3 +13,15 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 10) (* 2)
+
+calculateAverage :: [Int] -> Double
+calculateAverage [] = 0.0
+calculateAverage xs = fromIntegral (sum xs) / fromIntegral (length xs)
