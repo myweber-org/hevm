a9ec05a10592f1f112dc79c2ea6c964e7f2881c4
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-18 20:51:04 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- test/contracts/FibonacciSequence.hs
+++ test/contracts/FibonacciSequence.hs
@@ -1,13 +1,15 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> Integer
-fibonacci = (map fib [0..] !!)
+import Data.Function.Memoize
+
+fib :: Integer -> Integer
+fib = memoize fib'
   where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibonacci (n - 1) + fibonacci (n - 2)
+    fib' 0 = 0
+    fib' 1 = 1
+    fib' n = fib (n - 1) + fib (n - 2)
 
 main :: IO ()
 main = do
-  putStrLn "First 20 Fibonacci numbers:"
-  mapM_ (print . fibonacci) [0..19]
+    putStrLn "Fibonacci numbers from 0 to 10:"
+    mapM_ (print . fib) [0..10]
