1ce71cc51a1640c511b2428107da9789d95c5b95
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-19 15:51:26 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -1,25 +1,3 @@
-
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (> -100) xs then Just xs else Nothing
-
-main :: IO ()
-main = do
-    let sampleData = [1, -5, 3, 0, 8, -2]
-    case validateInput sampleData of
-        Just validData -> do
-            putStrLn "Original data:"
-            print sampleData
-            putStrLn "Processed data (positive numbers doubled):"
-            print $ processData validData
-        Nothing -> putStrLn "Input contains values less than -100"
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
@@ -27,45 +5,6 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
-module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (\x -> x >= -100 && x <= 100) xs 
-                   then Just xs 
-                   else Nothing
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5, -8]
-    case validateInput sampleData of
-        Just validData -> do
-            let result = sumProcessedData validData
-            putStrLn $ "Sum of processed data: " ++ show result
-        Nothing -> putStrLn "Input validation failed"module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateData :: [Int] -> Bool
-validateData xs = all (> 0) xs && length xs > 3
-
-combineResults :: [Int] -> [Int] -> [Int]
-combineResults xs ys = 
-    let processedXs = processData xs
-        processedYs = processData ys
-    in if validateData processedXs && validateData processedYs
-       then processedXs ++ processedYs
-       else []
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
