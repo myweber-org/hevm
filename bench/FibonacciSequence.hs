f078c8be534a730e8cb367740a2be35b294771eb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-19 20:37:12 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- bench/FibonacciSequence.hs
+++ bench/FibonacciSequence.hs
@@ -12,4 +12,18 @@ main = do
 fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
-fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)module FibonacciSequence where
+
+import Data.Function.Memoize (memoize)
+
+fib :: Int -> Integer
+fib = memoize fib'
+  where
+    fib' 0 = 0
+    fib' 1 = 1
+    fib' n = fib (n - 1) + fib (n - 2)
+
+main :: IO ()
+main = do
+    putStrLn "Fibonacci numbers from 0 to 20:"
+    mapM_ (print . fib) [0..20]
