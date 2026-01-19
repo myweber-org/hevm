5fc42d4994f0b46ea50045969878239e940753b8
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-19 16:31:33 +0000

    Add word frequency analysis functionality

--- test/contracts/WordFrequency.hs
+++ test/contracts/WordFrequency.hs
@@ -20,4 +20,32 @@ displayFrequencies :: [(String, Int)] -> String
 displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
-analyzeText n = displayFrequencies . topNWords n
+analyzeText n = displayFrequencies . topNWords nmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map toLower wordsList
+        grouped = foldr countHelper [] cleaned
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = filter isAlpha
+    countHelper word [] = [(word, 1)]
+    countHelper word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countHelper word rest
+
+analyzeText :: String -> IO ()
+analyzeText input = do
+    let frequencies = countWords input
+    putStrLn "Top 10 most frequent words:"
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) frequencies
+
+sampleText :: String
+sampleText = "This is a sample text. This text contains words. Some words repeat. This is intentional."
