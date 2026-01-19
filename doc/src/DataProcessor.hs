b368b4effff026553d11326ed75a9909b7b3b337
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-19 00:44:35 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -1,27 +1,15 @@
 module DataProcessor where
 
-processData :: [Int] -> [Int]
-processData = map (^2) . filter (>0)
-
-validateData :: [Int] -> Bool
-validateData xs = all (>=0) (processData xs)
-
-sampleData :: [Int]
-sampleData = [1, -5, 3, 0, 8, -2]module DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processEvenNumbers :: [Int] -> [Int]
+processEvenNumbers = filterAndTransform even (*2)
 
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+processOddNumbers :: [Int] -> [Int]
+processOddNumbers = filterAndTransform odd (+1)
 
-validateInput :: [Int] -> Bool
-validateInput = all (\x -> x >= -100 && x <= 100)
-
-safeProcessData :: [Int] -> Maybe [Int]
-safeProcessData xs
-    | validateInput xs = Just (processData xs)
-    | otherwise = Nothing
+sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
+sumProcessedData predicate transformer = 
+    sum . filterAndTransform predicate transformer
