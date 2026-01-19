55e6ce133bc76e064d50d2334a835c1cf2908bee
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-19 01:06:02 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -1,15 +1,23 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
+filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenNumbers :: [Int] -> [Int]
-processEvenNumbers = filterAndTransform even (*2)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-processOddNumbers :: [Int] -> [Int]
-processOddNumbers = filterAndTransform odd (+1)
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
 
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = 
-    sum . filterAndTransform predicate transformer
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [1, -5, 3, 0, 8, -2]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original data: " ++ show validData
+            putStrLn $ "Processed data: " ++ show (processData validData)
+            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
+        Nothing -> putStrLn "Input contains invalid numbers below -100"
