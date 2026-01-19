7737fc8b0a4ad6e73552f2c155a595823db5b8a0
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-19 20:49:32 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -1,37 +1,10 @@
-
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-main :: IO ()
-main = do
-    let numbers = [-3, -1, 0, 2, 5, 8, 10]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbersmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processData = map (^2) . filter (>0)
 
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+validateData :: [Int] -> Bool
+validateData xs = all (>=0) (processData xs)
+
+sampleData :: [Int]
+sampleData = [1, -2, 3, -4, 5]
