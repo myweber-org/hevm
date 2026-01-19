a8e9183c03168102adfa1cd97cd3d9c3469d5014
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-19 21:07:56 +0000

    Implement a simple prime number sieve using the Sieve of Eratosthenes algorithm.

--- bench/contracts/PrimeSieve.hs
+++ bench/contracts/PrimeSieve.hs
@@ -19,4 +19,19 @@ main = do
     input <- getLine
     let limit = read input :: Int
     let result = primesUpTo limit
-    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show result
+    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show resultmodule PrimeSieve where
+
+primes :: [Int]
+primes = sieve [2..]
+  where
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 prime numbers:"
+    print $ take 20 primes
+    putStrLn "The 100th prime number is:"
+    print $ nthPrime 100
