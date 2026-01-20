8428e7fb3c35772b43a412fcd85e16e283d74fd4
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-20 16:45:09 +0000

    Add word frequency counter with sorting and output formatting

--- test/EVM/WordFrequencyCounter.hs
+++ test/EVM/WordFrequencyCounter.hs
@@ -123,4 +123,35 @@ processText text minFreq topN =
 main :: IO ()
 main = do
     let sampleText = "Hello world hello haskell world programming haskell functional programming"
-    putStrLn $ processText sampleText 1 5
+    putStrLn $ processText sampleText 1 5module WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map toLower wordsList
+        counts = foldl incrementCount [] cleaned
+    in sortOn (Down . snd) counts
+  where
+    cleanWord = filter isAlpha
+    incrementCount [] word = [(word, 1)]
+    incrementCount ((w, c):rest) word
+        | w == word = (w, c+1) : rest
+        | otherwise = (w, c) : incrementCount rest word
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze word frequency:"
+    input <- getContents
+    let frequencies = countWords input
+    putStrLn "\nWord frequencies (sorted):"
+    putStrLn $ formatOutput frequencies
