2cf53c9b00b9f8a6a7f16b5de443d016872fab52
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-20 23:21:48 +0000

    Add function to filter and transform list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -4,10 +4,7 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (*2)
+processNumbers = filterAndTransform (> 0) (* 2)
 
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print result
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
