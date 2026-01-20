0d3176694f822c22427b457c1ba3b75e1acb4bcb
InkspillVerse <inkspill.verse@outlook.com>
2026-01-20 22:23:50 +0000

    Add function to parse CSV data and compute average values

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -34,4 +34,28 @@ main = do
     let sampleData = [1, -2, 3, 0, 5, -10]
     if validateInput sampleData
         then print $ processData sampleData
-        else putStrLn "Invalid input data"
+        else putStrLn "Invalid input data"module DataProcessor where
+
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
+
+type Record = (String, Double)
+
+parseCSV :: String -> [Record]
+parseCSV content = mapMaybe parseLine (lines content)
+  where
+    parseLine line = case splitOn "," line of
+      [name, valueStr] -> case reads valueStr of
+        [(value, "")] -> Just (name, value)
+        _ -> Nothing
+      _ -> Nothing
+
+computeAverage :: [Record] -> Double
+computeAverage records
+  | null records = 0.0
+  | otherwise = total / fromIntegral count
+  where
+    (total, count) = foldr (\(_, val) (sum, cnt) -> (sum + val, cnt + 1)) (0.0, 0) records
+
+processData :: String -> Double
+processData = computeAverage . parseCSV
