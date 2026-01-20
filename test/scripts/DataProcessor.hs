f963dfd3dee58309574809430f5bf56594fd9294
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-20 15:44:01 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -1,55 +1,16 @@
+
 module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbersmodule DataProcessor where
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map avg $ windows n xs
-  where
-    windows :: Int -> [a] -> [[a]]
-    windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
-    
-    avg :: [Double] -> Double
-    avg ys = sum ys / fromIntegral (length ys)
-
-smoothData :: [Double] -> [Double]
-smoothData = movingAverage 3
-
-calculateTrend :: [Double] -> Maybe Double
-calculateTrend [] = Nothing
-calculateTrend xs = Just (last xs - head xs)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print resultmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = 
     map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processNumbers
+processNumbers = filterAndTransform (> 10) (* 2)
 
 safeHead :: [Int] -> Maybe Int
 safeHead [] = Nothing
 safeHead (x:_) = Just x
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
