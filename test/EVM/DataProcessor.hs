3bd1533ab2c605de2a83201598667032394a7fc2
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-20 00:45:55 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -1,18 +1,3 @@
-
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processNumbers
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
@@ -23,3 +8,16 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [1, -5, 3, 0, 8, -2]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original: " ++ show validData
+            putStrLn $ "Processed: " ++ show (processNumbers validData)
+            putStrLn $ "Sum: " ++ show (sumProcessed validData)
+        Nothing -> putStrLn "Invalid input detected"
