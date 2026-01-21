b1a9cc0bd9b8bf54b5347e401f013e479f9dc085
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-21 00:21:10 +0000

    Add function to calculate moving average of a list

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -21,4 +21,26 @@ main = do
         Just data' -> do
             putStrLn $ "Original data: " ++ show data'
             putStrLn $ "Even squares: " ++ show (processEvenSquares data')
-            putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) data')
+            putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) data')module DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | n > length xs = error "Window size exceeds list length"
+    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
+  where
+    average :: [Double] -> Double
+    average ws = sum ws / fromIntegral (length ws)
+
+-- Example usage function
+exampleUsage :: IO ()
+exampleUsage = do
+    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    putStrLn "Original data:"
+    print dataSeries
+    putStrLn "\n3-period moving average:"
+    print $ movingAverage 3 dataSeries
+    putStrLn "\n5-period moving average:"
+    print $ movingAverage 5 dataSeries
