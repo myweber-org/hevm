feb1977c899ff91387d7235d9aeb12b76155cc97
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-21 16:02:03 +0000

    Add function to filter and transform a list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -1,21 +1,14 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Bool
-validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (* 2)
 
 main :: IO ()
 main = do
-    let sampleData = [-5, 2, 0, 8, -3, 10]
-    if validateInput sampleData
-        then do
-            putStrLn "Processing valid data..."
-            let result = processData sampleData
-            putStrLn $ "Input: " ++ show sampleData
-            putStrLn $ "Result: " ++ show result
-        else putStrLn "Invalid input detected"
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    print result
