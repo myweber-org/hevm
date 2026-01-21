c0dfc0739f26fad6991d1a8e974680930d1eaddc
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-21 21:38:36 +0000

    Implement binary search tree insertion and in-order traversal

--- src/BinarySearchTree.hs
+++ src/BinarySearchTree.hs
@@ -18,4 +18,25 @@ fromList :: Ord a => [a] -> Tree a
 fromList = foldr insert Empty
 
 toSortedList :: Tree a -> [a]
-toSortedList = inorder
+toSortedList = inordermodule BinarySearchTree where
+
+data Tree a = Empty | Node (Tree a) a (Tree a) deriving (Show, Eq)
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert x Empty = Node Empty x Empty
+insert x (Node left val right)
+    | x < val   = Node (insert x left) val right
+    | x > val   = Node left val (insert x right)
+    | otherwise = Node left val right
+
+inorder :: Tree a -> [a]
+inorder Empty = []
+inorder (Node left val right) = inorder left ++ [val] ++ inorder right
+
+fromList :: Ord a => [a] -> Tree a
+fromList = foldr insert Empty
+
+main :: IO ()
+main = do
+    let tree = fromList [5, 3, 8, 1, 4, 7, 9]
+    print $ inorder tree
