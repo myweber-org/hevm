ea54f4aba65d209b0804bddd9fe3b0f7f033cebc
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-21 20:50:07 +0000

    Add function to parse CSV data and calculate average values

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -48,4 +48,42 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processNumbers = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
+
+type Row = [String]
+type CSVData = [Row]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
+
+safeReadDouble :: String -> Maybe Double
+safeReadDouble s = case reads s of
+    [(val, "")] -> Just val
+    _ -> Nothing
+
+calculateColumnAverage :: CSVData -> Int -> Maybe Double
+calculateColumnAverage rows colIndex
+    | null validValues = Nothing
+    | otherwise = Just (sum validValues / fromIntegral (length validValues))
+  where
+    columnValues = mapMaybe (safeGetColumn colIndex) rows
+    validValues = mapMaybe safeReadDouble columnValues
+    safeGetColumn idx row
+        | idx < length row = Just (row !! idx)
+        | otherwise = Nothing
+
+processCSVFile :: String -> IO ()
+processCSVFile filename = do
+    content <- readFile filename
+    let parsed = parseCSV content
+    putStrLn "Column averages:"
+    mapM_ (printColumnAverage parsed) [0..4]
+  where
+    printColumnAverage data' idx = do
+        putStrLn $ "Column " ++ show idx ++ ": " ++ 
+            case calculateColumnAverage data' idx of
+                Just avg -> show avg
+                Nothing -> "N/A"
