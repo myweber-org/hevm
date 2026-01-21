fba25f57332eb3e8f5db12b0ca9ab3d9645d482a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-21 15:19:37 +0000

    Add function to calculate moving average of a list

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -64,4 +64,14 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processNumbers = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
