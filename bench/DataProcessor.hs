114b36be41efc9f3c2bf05615e8a088e9b89be6a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-21 16:34:30 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -1,10 +1,18 @@
+
 module DataProcessor where
 
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-  where
-    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
-    average zs = sum zs / fromIntegral (length zs)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = not (null xs) && all (>= -100) xs && all (<= 100) xs
+
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, 0, 5, -8]
+    if validateInput sampleData
+        then print $ processData sampleData
+        else putStrLn "Invalid input data"
