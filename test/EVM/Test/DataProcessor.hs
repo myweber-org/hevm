8cedba5354ad19f08cd2343448be5942b9dfadb5
InkspillVerse <inkspill.verse@outlook.com>
2026-01-21 16:20:40 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -44,3 +44,26 @@ exampleUsage = do
     print $ movingAverage 3 dataSeries
     putStrLn "\n5-period moving average:"
     print $ movingAverage 5 dataSeries
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processEvenSquares
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (>0) xs then Just xs else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original data: " ++ show validData
+            putStrLn $ "Processed data: " ++ show (processEvenSquares validData)
+            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
+        Nothing -> putStrLn "Invalid input: all numbers must be positive"
