1c798597cbdc6a9650a153aee22f29650154e1e0
InkspillVerse <inkspill.verse@outlook.com>
2026-01-22 01:42:02 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -3,53 +3,8 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform even (\x -> x * x + 1)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-main :: IO ()
-main = do
-    let input = [1..10]
-    let result = processData input
-    putStrLn $ "Input: " ++ show input
-    putStrLn $ "Result: " ++ show resultmodule DataProcessor where
-
-movingAverage :: (Fractional a) => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-    where
-        windows :: Int -> [a] -> [[a]]
-        windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
-        
-        average :: (Fractional a) => [a] -> a
-        average ys = sum ys / fromIntegral (length ys)
-
--- Example usage:
--- movingAverage 3 [1,2,3,4,5,6] -> [2.0,3.0,4.0,5.0]
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = 
-    filterAndTransform even (\x -> x * x)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = 
-    sum . processEvenSquares
-
-main :: IO ()
-main = do
-    let testData = [1..10]
-    putStrLn $ "Original data: " ++ show testData
-    putStrLn $ "Processed data: " ++ show (processEvenSquares testData)
-    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData testData)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
