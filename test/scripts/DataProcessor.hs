8a9cd69f4cb23c044195c52d75f63cacb3741519
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-22 15:30:37 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -47,3 +47,28 @@ filterAboveAverage records =
 
 processCSVData :: String -> [Record]
 processCSVData = filterAboveAverage . parseCSV
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
+sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput [] = Nothing
+validateInput xs = Just xs
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    case validateInput sampleData of
+        Nothing -> putStrLn "Empty input list"
+        Just dataList -> do
+            putStrLn $ "Original data: " ++ show dataList
+            putStrLn $ "Even squares: " ++ show (processEvenSquares dataList)
+            putStrLn $ "Sum of even squares: " ++ 
+                show (sumProcessedData even (\x -> x * x) dataList)
