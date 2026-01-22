7cf84cb748bce7a45078c0cb629d576d925fe729
InkspillVerse <inkspill.verse@outlook.com>
2026-01-22 16:14:15 +0000

    Implement a simple prime number sieve using the Sieve of Eratosthenes algorithm

--- bench/PrimeSieve.hs
+++ bench/PrimeSieve.hs
@@ -67,4 +67,17 @@ main = do
     input <- getLine
     let limit = read input :: Int
     let primes = primesUpTo limit
-    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primes
+    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primesmodule PrimeSieve where
+
+primes :: [Int]
+primes = sieve [2..]
+  where
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
+
+isPrime :: Int -> Bool
+isPrime n
+    | n < 2     = False
+    | otherwise = all (\p -> n `mod` p /= 0) (takeWhile (\p -> p * p <= n) primes)
