35f52de3acf0a083b369f65d493bb445cb1633f9
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-22 23:38:42 +0000

    Add word frequency analysis function

--- ethjet/WordFrequency.hs
+++ ethjet/WordFrequency.hs
@@ -24,4 +24,33 @@ displayFrequency counts =
     unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
 
 analyzeText :: String -> String
-analyzeText = displayFrequency . countWords
+analyzeText = displayFrequency . countWordsmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        grouped = foldr countWord [] cleanedWords
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = filter isAlpha
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | word == w = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+displayFrequency :: [WordCount] -> String
+displayFrequency counts = unlines $ map showEntry counts
+  where
+    showEntry (word, count) = word ++ ": " ++ show count
+
+analyzeText :: String -> String
+analyzeText text = 
+    let frequencies = countWords text
+    in "Top 10 most frequent words:\n" ++ displayFrequency frequencies
