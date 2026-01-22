c68c473db114220564f3cb052b780c9db4d2efae
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-22 20:01:46 +0000

    Add function to calculate the depth of a binary tree

--- test/contracts/pass/BinaryTreeDepth.hs
+++ test/contracts/pass/BinaryTreeDepth.hs
@@ -1,27 +1,5 @@
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
-
-treeDepth :: Tree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: Tree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty (Node 6 Empty Empty))
-
-main :: IO ()
-main = do
-    let depth = treeDepth sampleTree
-    putStrLn $ "Maximum depth of the tree: " ++ show depth
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
+data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a)
 
 treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
+treeDepth Leaf = 0
+treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)
