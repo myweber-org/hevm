b015092f8b373dd9fe2d9fb457e70231b97781e9
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-22 15:52:52 +0000

    Add function to filter and transform list of integers

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -12,4 +12,20 @@ movingAverage n xs
 -- Helper function to get all tails of a list
 tails :: [a] -> [[a]]
 tails [] = [[]]
-tails (x:xs) = (x:xs) : tails xs
+tails (x:xs) = (x:xs) : tails xsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
+
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, 4, -5, 6]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Processed data: " ++ show (processData sampleData)
+    putStrLn $ "Validation result: " ++ show (validateData sampleData)
