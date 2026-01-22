9b7d0f08fb12ed44ffc487694c0577e5b642bb6b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-22 19:19:13 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- cli/FibonacciSequence.hs
+++ cli/FibonacciSequence.hs
@@ -1,10 +1,14 @@
 module FibonacciSequence where
 
-import Data.Function.Memoize
+fibonacci :: Int -> [Integer]
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
 
-fib :: Integer -> Integer
-fib = memoize fib'
-  where
-    fib' 0 = 0
-    fib' 1 = 1
-    fib' n = fib (n - 1) + fib (n - 2)
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+    putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
+    mapM_ (putStrLn . show) (fibonacci n)
