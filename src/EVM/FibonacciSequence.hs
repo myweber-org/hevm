44738d75f24ab21c5e995b84572aedf207c4f8e0
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-22 00:41:30 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- src/EVM/FibonacciSequence.hs
+++ src/EVM/FibonacciSequence.hs
@@ -3,14 +3,4 @@ module FibonacciSequence where
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-  putStrLn "Fibonacci sequence up to 10 terms:"
-  print $ fibonacci 10
