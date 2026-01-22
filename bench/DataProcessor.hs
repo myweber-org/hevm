e363226da76c899167306415d4c8fc6c2957ddbe
InkspillVerse <inkspill.verse@outlook.com>
2026-01-22 23:07:58 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -18,3 +18,24 @@ main = do
     putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
     putStrLn $ "Sum of even squares: " ++ 
         show (sumProcessed (\x -> x * x) (filter even numbers))
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transform = map transform . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+main :: IO ()
+main = do
+    let sampleData = [-5, 2, 0, 8, -3, 10]
+    if validateInput sampleData
+        then do
+            putStrLn "Original data:"
+            print sampleData
+            putStrLn "Processed data (positive numbers doubled):"
+            print $ processData sampleData
+        else putStrLn "Input validation failed: values must be between -100 and 100"
