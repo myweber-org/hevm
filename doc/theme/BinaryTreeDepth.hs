c85c82fd1659229971d7317fa4f12ffe5cda84bf
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-22 18:17:33 +0000

    Add function to calculate the maximum depth of a binary tree

--- doc/theme/BinaryTreeDepth.hs
+++ doc/theme/BinaryTreeDepth.hs
@@ -1,18 +1,13 @@
-module BinaryTreeDepth where
-
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
 
 sampleTree :: BinaryTree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty Empty)data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty (Node 6 Empty Empty))
 
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
-
-treeDepth :: Tree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
+main :: IO ()
+main = do
+    let depth = treeDepth sampleTree
+    putStrLn $ "Maximum depth of the sample tree is: " ++ show depth
