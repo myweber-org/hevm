052eafede59b23b40e15971f53d89cca8723d28d
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-23 16:21:08 +0000

    Add function to filter and transform a list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -36,3 +36,16 @@ formatResults rows = intercalate "\n" $ map formatRow (zip [1..] rows)
 
 processCSVData :: String -> String
 processCSVData = formatResults . parseCSV
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    print result
