dafd7855d1a3f08fbe7e44a056e4ad68f5765cdc
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-23 16:04:18 +0000

    Add function to calculate the height of a binary tree

--- doc/src/BinaryTreeHeight.hs
+++ doc/src/BinaryTreeHeight.hs
@@ -4,4 +4,10 @@ data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show,
 
 treeHeight :: BinaryTree a -> Int
 treeHeight Empty = 0
+treeHeight (Node _ left right) = 1 + max (treeHeight left) (treeHeight right)module BinaryTreeHeight where
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
+
+treeHeight :: Tree a -> Int
+treeHeight Empty = 0
 treeHeight (Node _ left right) = 1 + max (treeHeight left) (treeHeight right)
