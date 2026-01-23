21a55a48427d30c8ad53292c0a126acece17a83d
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-23 20:13:50 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -37,3 +37,17 @@ processNumericCSV content = do
     parsed <- parseCSV content
     validated <- validateCSV parsed
     mapM (convertColumn readMaybe) validated
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
