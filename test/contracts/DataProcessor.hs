831cd81f8b75ec7fac6576587778e1bc1b42f2c6
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-23 16:59:38 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -72,3 +72,22 @@ mapMaybe _ [] = []
 mapMaybe f (x:xs) = case f x of
     Just y -> y : mapMaybe f xs
     Nothing -> mapMaybe f xs
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< -100) xs = Nothing
+    | any (> 100) xs = Nothing
+    | otherwise = Just xs
+
+safeDataProcessing :: [Int] -> Maybe [Int]
+safeDataProcessing input = do
+    validated <- validateInput input
+    return $ processData validated
