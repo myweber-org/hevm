ea07db8816014d4b56d16219f9099c006197a8ed
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-24 19:10:02 +0000

    Add word frequency counter with sorting and output formatting

--- doc/src/WordFrequencyCounter.hs
+++ doc/src/WordFrequencyCounter.hs
@@ -132,4 +132,37 @@ main = do
             case reads nStr of
                 [(n, "")] | n > 0 -> processFile filePath n
                 _ -> putStrLn "Second argument must be positive integer"
-        _ -> putStrLn "Usage: WordFrequencyCounter <file> <top-n>"
+        _ -> putStrLn "Usage: WordFrequencyCounter <file> <top-n>"module WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        grouped = foldr countHelper [] cleanedWords
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = filter isAlpha
+    countHelper word [] = [(word, 1)]
+    countHelper word ((w, c):rest)
+        | word == w = (w, c + 1) : rest
+        | otherwise = (w, c) : countHelper word rest
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+processText :: String -> String
+processText = formatOutput . countWords
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze word frequency:"
+    input <- getContents
+    putStrLn "\nWord frequencies:"
+    putStrLn $ processText input
