0f3444bace457ec6ad7a543dbf9d53752aa1401d
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-24 15:37:14 +0000

    Add word frequency counting functionality with command line argument support

--- test/contracts/fail/WordFrequencyCounter.hs
+++ test/contracts/fail/WordFrequencyCounter.hs
@@ -52,4 +52,43 @@ main = do
     textPieces -> do
       let text = unwords textPieces
       putStrLn "Word frequencies:"
-      putStrLn . formatResults $ countWords text
+      putStrLn . formatResults $ countWords textmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+import System.Environment (getArgs)
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        grouped = foldr (\w m -> insertWord w m) [] cleanedWords
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = filter (\c -> isAlphaNum c || c == '\'')
+    
+    insertWord :: String -> [WordCount] -> [WordCount]
+    insertWord w [] = [(w, 1)]
+    insertWord w ((word, count):rest)
+        | w == word = (word, count + 1) : rest
+        | otherwise = (word, count) : insertWord w rest
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = 
+    unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+
+processFile :: FilePath -> IO ()
+processFile filename = do
+    contents <- readFile filename
+    let frequencies = countWords contents
+    putStrLn $ formatOutput frequencies
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [] -> putStrLn "Usage: wordfreq <filename>"
+        (filename:_) -> processFile filename
