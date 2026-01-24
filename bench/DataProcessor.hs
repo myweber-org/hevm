f19233bf0bb3b1ac5d675674f37b67cbe439cd24
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-24 23:53:31 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -1,49 +1,20 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
 
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbersmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (\x -> x >= -100 && x <= 100) xs
+sumProcessed :: (Int -> Int) -> [Int] -> Int
+sumProcessed processor = sum . map processor
 
 main :: IO ()
 main = do
-    let sampleData = [1, -2, 3, 0, 5, -10]
-    if validateInput sampleData
-        then do
-            putStrLn "Original data:"
-            print sampleData
-            putStrLn "Processed data (positive numbers doubled):"
-            print $ processData sampleData
-        else putStrLn "Invalid input data"module DataProcessor where
-
-import Data.List.Split (splitOn)
-
-parseCSV :: String -> [[Double]]
-parseCSV content = map (map read . splitOn ",") (lines content)
-
-computeAverages :: [[Double]] -> [Double]
-computeAverages rows = 
-    if null rows 
-    then []
-    else map (\col -> sum col / fromIntegral (length col)) (transpose rows)
-  where
-    transpose :: [[Double]] -> [[Double]]
-    transpose [] = []
-    transpose ([]:_) = []
-    transpose x = map head x : transpose (map tail x)
-
-processCSVData :: String -> [Double]
-processCSVData = computeAverages . parseCSV
+    let numbers = [1..10]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
+    putStrLn $ "Sum of even squares: " ++ 
+        show (sumProcessed (\x -> x * x) (filter even numbers))
