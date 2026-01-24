dca9e504522c0ef0eaceb1e1fe0b4f7957808c9c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-24 01:08:02 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -41,4 +41,16 @@ filterAndTransform predicate transformer =
     map transformer . filter predicate
 
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processData = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenNumbers :: [Int] -> [Int]
+processEvenNumbers = filterAndTransform even (* 2)
+
+processOddNumbers :: [Int] -> [Int]
+processOddNumbers = filterAndTransform odd (+ 1)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processEvenNumbers
