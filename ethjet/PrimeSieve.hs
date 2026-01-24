a88d8056bb5a0a786b69b19fa9a3d5e98d3ad7ac
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-24 20:51:23 +0000

    Implement Sieve of Eratosthenes to generate prime numbers up to a given limit

--- ethjet/PrimeSieve.hs
+++ ethjet/PrimeSieve.hs
@@ -12,3 +12,12 @@ isPrime :: Int -> Bool
 isPrime n
     | n < 2     = False
     | otherwise = all (\p -> n `mod` p /= 0) (takeWhile (\p -> p * p <= n) primes)
+module PrimeSieve where
+
+primesUpTo :: Int -> [Int]
+primesUpTo n
+    | n < 2     = []
+    | otherwise = sieve [2..n]
+    where
+        sieve []     = []
+        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
