9435d11dcbacfab63a87ff2c72cd9421cc1b8d74
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-24 19:55:16 +0000

    Add word frequency analysis function to count occurrences in a list of strings

--- src/WordFrequency.hs
+++ src/WordFrequency.hs
@@ -1,58 +1,23 @@
+
 module WordFrequency where
 
-import Data.Char (toLower, isAlpha)
+import Data.Char (toLower)
 import Data.List (sortOn)
-import Data.Ord (Down(..))
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-type WordCount = (String, Int)
+type WordCount = Map String Int
 
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleaned = map toLower <$> wordsList
-        grouped = foldr countWord [] cleaned
-    in sortOn (Down . snd) grouped
+countWords :: [String] -> WordCount
+countWords = foldr incrementWord Map.empty . concatMap words
   where
-    cleanWord = filter isAlpha
-    countWord w [] = [(w, 1)]
-    countWord w ((x, n):xs)
-        | w == x = (x, n+1) : xs
-        | otherwise = (x, n) : countWord w xs
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower
 
-formatOutput :: [WordCount] -> String
-formatOutput counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+topWords :: Int -> [String] -> [(String, Int)]
+topWords n texts = take n $ sortOn (negate . snd) $ Map.toList $ countWords texts
 
-main :: IO ()
-main = do
-    putStrLn "Enter text to analyze (press Ctrl+D when finished):"
-    input <- getContents
-    let frequencies = countWords input
-    putStrLn "\nWord frequencies (sorted by count):"
-    putStrLn $ formatOutput frequenciesmodule WordFrequency where
-
-import Data.Char (toLower, isAlphaNum)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type WordCount = (String, Int)
-
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        frequencyMap = foldr countWord [] wordsList
-    in take 10 $ sortOn (Down . snd) frequencyMap
+displayFrequency :: [(String, Int)] -> String
+displayFrequency counts = unlines $ map format counts
   where
-    cleanWord = map toLower . filter isAlphaNum
-    countWord word [] = [(word, 1)]
-    countWord word ((w, c):rest)
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord word rest
-
-displayCounts :: [WordCount] -> String
-displayCounts counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
-
-main :: IO ()
-main = do
-    let sampleText = "Hello world! This is a test. Hello again, world!"
-    putStrLn "Top 10 most frequent words:"
-    putStrLn $ displayCounts $ countWords sampleText
+    format (word, count) = word ++ ": " ++ show count
