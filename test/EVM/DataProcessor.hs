8b45dab19d2af38f14de9ecc2e6b42d85b91f4d9
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-24 22:11:10 +0000

    Add function to filter and transform a list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -3,5 +3,12 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    let result = processData sampleData
+    putStrLn $ "Original: " ++ show sampleData
+    putStrLn $ "Processed: " ++ show result
