6aa8ec7625a078541e885b110275d37bb5dcf4e4
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-25 15:36:39 +0000

    Add a function to generate the nth Fibonacci number using memoization for efficiency.

--- test/EVM/FibonacciSequence.hs
+++ test/EVM/FibonacciSequence.hs
@@ -1,31 +1,6 @@
 module FibonacciSequence where
 
-fibonacci :: Integer -> [Integer]
-fibonacci n
-    | n <= 0    = []
-    | n == 1    = [0]
-    | n == 2    = [0, 1]
-    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-                  in take (fromIntegral n) fibs
-
-fibonacciUpTo :: Integer -> [Integer]
-fibonacciUpTo limit = takeWhile (<= limit) (fibonacci (limit + 1))module FibonacciSequence where
-
-fibonacci :: Int -> [Int]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Int -> [Int]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
 fib :: Int -> Integer
 fib n = fibs !! n
-  where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-    putStrLn "First 20 Fibonacci numbers:"
-    mapM_ (print . fib) [0..19]
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
