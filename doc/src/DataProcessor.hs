d11f7233e185e9feb06e3f5b9847440eeb888914
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-25 23:10:27 +0000

    Add function to filter and transform a list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -12,4 +12,10 @@ sumProcessed f = foldl' (\acc x -> acc + f x) 0
 
 safeHead :: [Int] -> Maybe Int
 safeHead [] = Nothing
-safeHead (x:_) = Just x
+safeHead (x:_) = Just xmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
