9474623949617a25c9a1ec0fb8a9a7fedc3ecbc4
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-25 16:37:18 +0000

    Add function to filter and transform a list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -38,3 +38,10 @@ main = do
     putStrLn "Filtered records:"
     mapM_ (\(name, val) -> putStrLn $ name ++ ": " ++ show val) filtered
     putStrLn $ "Average: " ++ show (calculateAverage filtered)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
