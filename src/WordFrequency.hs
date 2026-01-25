ea6ca2ef9d186006d7fc09736b45b66da26d4acb
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-25 15:58:20 +0000

    Add word frequency counting utility with command-line interface

--- src/WordFrequency.hs
+++ src/WordFrequency.hs
@@ -20,4 +20,47 @@ topWords n texts = take n $ sortOn (negate . snd) $ Map.toList $ countWords text
 displayFrequency :: [(String, Int)] -> String
 displayFrequency counts = unlines $ map format counts
   where
-    format (word, count) = word ++ ": " ++ show count
+    format (word, count) = word ++ ": " ++ show countmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+import System.Environment (getArgs)
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = filter (all isAlpha) wordsList
+        grouped = groupCount cleaned
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    groupCount [] = []
+    groupCount (x:xs) = 
+        let (matches, rest) = partition (== x) (x:xs)
+        in (x, length matches) : groupCount rest
+
+    partition _ [] = ([], [])
+    partition p (y:ys)
+        | p y       = let (as, bs) = partition p ys in (y:as, bs)
+        | otherwise = let (as, bs) = partition p ys in (as, y:bs)
+
+printResults :: [WordCount] -> IO ()
+printResults counts = do
+    putStrLn "Top 10 most frequent words:"
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [] -> do
+            putStrLn "Reading from stdin..."
+            content <- getContents
+            printResults $ countWords content
+        [filename] -> do
+            content <- readFile filename
+            printResults $ countWords content
+        _ -> putStrLn "Usage: wordfreq [filename] (reads from stdin if no filename)"
