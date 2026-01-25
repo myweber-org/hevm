9100e8b3470a59bc290d190a60b99390f5c7fb26
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-25 21:56:31 +0000

    Add word frequency counting utility with histogram output

--- test/EVM/WordFrequency.hs
+++ test/EVM/WordFrequency.hs
@@ -1,31 +1,32 @@
-module TextUtils.WordFrequency where
 
-import Data.Char (isAlpha, toLower)
-import Data.List (sortOn)
+module WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
 import Data.Ord (Down(..))
 
-type WordCount = (String, Int)
+type Histogram = [(String, Int)]
 
--- | Count frequency of words in a text string
---   Words are normalized to lowercase and non-alphabetic characters are ignored
-countWords :: String -> [WordCount]
+countWords :: String -> Histogram
 countWords text = 
-    let wordsList = filter (not . null) $ map normalize $ words text
-        normalized = map toLower <$> wordsList
-        grouped = foldr countWord [] normalized
-    in sortOn (Down . snd) grouped
+    let wordsList = filter (not . null) $ map clean $ words text
+        cleaned = map (map toLower) wordsList
+        grouped = group $ sort cleaned
+    in sortOn (Down . snd) $ map (\ws -> (head ws, length ws)) grouped
   where
-    normalize = filter isAlpha
-    countWord word [] = [(word, 1)]
-    countWord word ((w, c):rest)
-        | word == w = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord word rest
+    clean = filter isAlpha
 
--- | Get top N most frequent words
-topWords :: Int -> String -> [WordCount]
-topWords n text = take n $ countWords text
+printHistogram :: Histogram -> IO ()
+printHistogram hist = do
+    putStrLn "Word Frequency Histogram:"
+    putStrLn "=========================="
+    mapM_ (\(word, count) -> 
+        putStrLn $ word ++ ": " ++ replicate count '*') hist
+    putStrLn $ "Total unique words: " ++ show (length hist)
 
--- | Pretty print word frequencies
-printFrequencies :: [WordCount] -> IO ()
-printFrequencies counts = 
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze (press Ctrl+D when done):"
+    input <- getContents
+    let freq = countWords input
+    printHistogram freq
