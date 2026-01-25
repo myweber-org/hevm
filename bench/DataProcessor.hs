dcf54a9ea39a93f672a154a46a61755a36e9c52f
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-25 16:37:36 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -4,18 +4,8 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-processOddCubes :: [Int] -> [Int]
-processOddCubes = filterAndTransform odd (\x -> x * x * x)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn "Original list:"
-    print numbers
-    putStrLn "\nEven numbers squared:"
-    print $ processEvenSquares numbers
-    putStrLn "\nOdd numbers cubed:"
-    print $ processOddCubes numbers
+sumPositiveDoubled :: [Int] -> Int
+sumPositiveDoubled = sum . processNumbers
