6d6115e5da1891eb0ec026bea20a82bf10998ae3
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-26 19:11:35 +0000

    Add function to calculate maximum depth of a binary tree

--- test/contracts/lib/BinaryTreeDepth.hs
+++ test/contracts/lib/BinaryTreeDepth.hs
@@ -1,14 +1,6 @@
+module BinaryTreeDepth where
 
 data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
-
-treeDepth :: Tree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
