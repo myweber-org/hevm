6485e4ec01cc62b7dc204fc2cadc380991c8fb17
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-26 14:30:43 +0000

    Add a word frequency counter for text analysis

--- test/contracts/lib/WordFrequencyCounter.hs
+++ test/contracts/lib/WordFrequencyCounter.hs
@@ -1,55 +1,33 @@
 module WordFrequencyCounter where
 
-import qualified Data.Char as Char
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
-import qualified System.Environment as Env
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
 
-type WordCount = Map.Map String Int
+type WordCount = (String, Int)
 
-countWords :: String -> WordCount
-countWords = foldr incrementWord Map.empty . words . normalize
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        wordMap = foldr (\word acc -> 
+            case lookup word acc of
+                Just count -> (word, count + 1) : filter ((/= word) . fst) acc
+                Nothing -> (word, 1) : acc
+            ) [] wordsList
+    in sortOn (Down . snd) wordMap
   where
-    normalize = map Char.toLower . filter (\c -> Char.isAlpha c || Char.isSpace c)
-    incrementWord word = Map.insertWith (+) word 1
+    cleanWord = map toLower . filter isAlpha
 
-formatResults :: WordCount -> String
-formatResults = unlines . map formatItem . List.sortOn (negate . snd) . Map.toList
-  where
-    formatItem (word, count) = word ++ ": " ++ show count
+formatResults :: [WordCount] -> String
+formatResults counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+analyzeText :: String -> String
+analyzeText = formatResults . countWords
 
 main :: IO ()
 main = do
-    args <- Env.getArgs
-    case args of
-        [] -> putStrLn "Usage: wordfreq <text>"
-        textPieces -> do
-            let text = unwords textPieces
-            putStrLn "Word frequencies:"
-            putStrLn $ formatResults $ countWords textmodule WordFrequencyCounter where
-
-import qualified Data.Char as Char
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
-
-type WordCount = Map.Map String Int
-
-countWords :: String -> WordCount
-countWords = foldr incrementWord Map.empty . words . normalize
-  where
-    normalize = map Char.toLower . filter (\c -> Char.isAlpha c || Char.isSpace c)
-    incrementWord word = Map.insertWith (+) word 1
-
-sortByFrequency :: WordCount -> [(String, Int)]
-sortByFrequency = List.sortOn (\(_, count) -> negate count) . Map.toList
-
-formatResults :: [(String, Int)] -> String
-formatResults = unlines . map (\(word, count) -> word ++ ": " ++ show count)
-
-processText :: String -> String
-processText = formatResults . sortByFrequency . countWords
-
-main :: IO ()
-main = do
+    putStrLn "Enter text to analyze (press Ctrl+D when done):"
     input <- getContents
-    putStrLn $ processText input
+    putStrLn "\nWord frequencies:"
+    putStrLn $ analyzeText input
