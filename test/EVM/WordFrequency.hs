885edfc92d679520e26bb3de8fb47ebce19e6f3c
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-26 21:28:31 +0000

    Add word frequency analysis utility with histogram output

--- test/EVM/WordFrequency.hs
+++ test/EVM/WordFrequency.hs
@@ -22,4 +22,45 @@ readFileAndCount path = do
     return $ getTopWords 10 content
 
 displayResults :: [(String, Int)] -> IO ()
-displayResults = mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count)
+displayResults = mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count)module WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+histogramBar :: Int -> Int -> String
+histogramBar count maxCount = replicate (scale count) '█'
+  where
+    scale c = round (fromIntegral c / fromIntegral maxCount * 50)
+
+analyzeText :: String -> [WordCount]
+analyzeText text = take 20 $ sortOn (Down . snd) wordCounts
+  where
+    words' = map normalize $ filter (not . null) $ splitText text
+    wordCounts = map (\ws -> (head ws, length ws)) $ group $ sort words'
+    
+    normalize = map toLower . filter isAlpha
+    splitText = words
+
+printHistogram :: [WordCount] -> IO ()
+printHistogram counts = do
+    let maxCount = maximum $ map snd counts
+    putStrLn "\nTop 20 Most Frequent Words:"
+    putStrLn $ replicate 60 '─'
+    mapM_ (\(word, count) -> 
+        putStrLn $ padRight 15 word ++ " " ++ 
+                 padLeft 4 (show count) ++ " " ++ 
+                 histogramBar count maxCount) counts
+    putStrLn $ replicate 60 '─'
+  where
+    padRight n str = str ++ replicate (n - length str) ' '
+    padLeft n str = replicate (n - length str) ' ' ++ str
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze (press Ctrl+D when finished):"
+    content <- getContents
+    let frequencies = analyzeText content
+    printHistogram frequencies
