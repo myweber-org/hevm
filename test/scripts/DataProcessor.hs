6054d528e86aed3b1e9356f8f56493d939625b2f
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-26 00:26:20 +0000

    Add function to filter and transform a list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -1,48 +1,5 @@
 module DataProcessor where
 
-import Data.List (tails)
-
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map avg $ filter (\w -> length w == n) $ tails xs
-  where
-    avg ws = sum ws / fromIntegral n
-
-smoothData :: Fractional a => Int -> [a] -> [a]
-smoothData window dataList = movingAverage window dataList
-
-validateData :: (Ord a, Num a) => [a] -> Maybe String
-validateData [] = Just "Empty dataset"
-validateData xs
-    | any (< 0) xs = Just "Negative values detected"
-    | any isInfinite xs = Just "Infinite values detected"
-    | otherwise = Nothing
-  where
-    isInfinite x = x == 1/0 || x == -1/0
-
-processDataset :: Fractional a => Int -> [a] -> Either String [a]
-processDataset window dataset =
-    case validateData dataset of
-        Just err -> Left err
-        Nothing -> Right $ smoothData window datasetmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-module DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
@@ -51,46 +8,6 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let numbers = [1, -2, 3, -4, 5]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
+    let input = [1, -2, 3, 0, 5, -7]
+    let result = processNumbers input
     print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processEvenSquares
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processEvenSquares
-module DataProcessor where
-
-processData :: [Int] -> [Int]
-processData = map (*2) . filter (>0)
