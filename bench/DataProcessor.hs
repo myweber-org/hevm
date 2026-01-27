cb537668be3ab17ca8106661d508ea7a6d39b8d2
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-27 14:58:11 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -35,4 +35,18 @@ processDataSet :: Fractional a => Int -> [a] -> Either String [a]
 processDataSet windowSize dataPoints =
     case validateData dataPoints of
         Just err -> Left err
-        Nothing -> Right $ smoothData windowSize dataPoints
+        Nothing -> Right $ smoothData windowSize dataPointsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (>= -100) xs && all (<= 100) xs
+                   then Just xs
+                   else Nothing
