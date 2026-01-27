700eeb196d0371307a644f48c18aef11ce2b75af
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-27 16:22:06 +0000

    Add inorder, preorder, and postorder traversal functions for binary trees

--- doc/theme/BinaryTreeTraversal.hs
+++ doc/theme/BinaryTreeTraversal.hs
@@ -12,4 +12,18 @@ preorder (Node left val right) = [val] ++ preorder left ++ preorder right
 
 postorder :: BinaryTree a -> [a]
 postorder Leaf = []
-postorder (Node left val right) = postorder left ++ postorder right ++ [val]
+postorder (Node left val right) = postorder left ++ postorder right ++ [val]module BinaryTreeTraversal where
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+
+preorder :: BinaryTree a -> [a]
+preorder Empty = []
+preorder (Node val left right) = [val] ++ preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Empty = []
+inorder (Node val left right) = inorder left ++ [val] ++ inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Empty = []
+postorder (Node val left right) = postorder left ++ postorder right ++ [val]
