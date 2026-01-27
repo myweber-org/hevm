ad435ae5bd83307bef4498d6b4272ea9e925c5b6
InkspillVerse <inkspill.verse@outlook.com>
2026-01-27 19:46:30 +0000

    Add function to filter and transform a list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -1,41 +1,4 @@
-
 module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)module DataProcessor where
-
-import Data.List (tails)
-
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | n > length xs = error "Window size exceeds list length"
-    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
-  where
-    avg window = sum window / fromIntegral n
-
-smoothData :: Fractional a => Int -> [a] -> [a]
-smoothData windowSize dataPoints =
-    movingAverage windowSize dataPoints
-
-validateData :: (Ord a, Num a) => [a] -> Maybe String
-validateData [] = Just "Empty data set"
-validateData xs
-    | any (< 0) xs = Just "Negative values found"
-    | any isInfinite xs = Just "Infinite values found"
-    | any isNaN xs = Just "NaN values found"
-    | otherwise = Nothing
-  where
-    isInfinite x = x > 1e100 || x < -1e100
-
-processDataSet :: Fractional a => Int -> [a] -> Either String [a]
-processDataSet windowSize dataPoints =
-    case validateData dataPoints of
-        Just err -> Left err
-        Nothing -> Right $ smoothData windowSize dataPointsmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
@@ -43,10 +6,8 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (>= -100) xs && all (<= 100) xs
-                   then Just xs
-                   else Nothing
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -8]
+    let result = processNumbers input
+    print result
