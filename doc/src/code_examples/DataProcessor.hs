bd0dc754a578ea4728a7aa9cb712ef7aaaefba4c
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-27 19:21:15 +0000

    Add function to filter and transform a list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -1,39 +1,13 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-sumOfProcessed :: [Int] -> Int
-sumOfProcessed = sum . processEvenSquares
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (* 2)
 
 main :: IO ()
 main = do
-    let sampleData = [1..10]
-    putStrLn $ "Original list: " ++ show sampleData
-    putStrLn $ "Processed list: " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Sum of processed: " ++ show (sumOfProcessed sampleData)
-    putStrLn $ "Head of empty list: " ++ show (safeHead [])
-    putStrLn $ "Head of sample: " ++ show (safeHead sampleData)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processNumbers
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    print result
