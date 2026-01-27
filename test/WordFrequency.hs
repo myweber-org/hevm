221e2dbcc73449c42a8ecaa3a9c9fc8de9a4ddf3
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-27 15:50:32 +0000

    Add word frequency counter for text analysis

--- test/WordFrequency.hs
+++ test/WordFrequency.hs
@@ -62,4 +62,31 @@ analyzeText :: String -> IO ()
 analyzeText text = do
     putStrLn "Word frequencies:"
     printWordFrequencies $ countWords text
-    putStrLn $ "\nTotal unique words: " ++ show (length $ countWords text)
+    putStrLn $ "\nTotal unique words: " ++ show (length $ countWords text)module WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (isAlpha, toLower)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
+
+topWords :: Int -> String -> [(String, Int)]
+topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+displayTopWords :: Int -> String -> String
+displayTopWords n text = unlines $ map format $ topWords n text
+  where
+    format (word, count) = word ++ ": " ++ show count
+
+analyzeText :: String -> IO ()
+analyzeText text = do
+  putStrLn "Top 10 most frequent words:"
+  putStrLn $ displayTopWords 10 text
+  putStrLn $ "Total unique words: " ++ show (Map.size $ countWords text)
