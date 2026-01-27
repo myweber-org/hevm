5971cec955a4e171e3a60b31033312f57045e720
InkspillVerse <inkspill.verse@outlook.com>
2026-01-27 01:41:03 +0000

    Add prime factorization function using trial division

--- doc/theme/PrimeFactors.hs
+++ doc/theme/PrimeFactors.hs
@@ -10,3 +10,13 @@ primeFactors n
     factorize m d
       | m `mod` d == 0 = d : factorize (m `div` d) d
       | otherwise      = factorize m (d + 1)
+module PrimeFactors where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = factors n 2
+  where
+    factors 1 _ = []
+    factors m p
+      | m < p * p = [m]
+      | m `mod` p == 0 = p : factors (m `div` p) p
+      | otherwise = factors m (p + 1)
