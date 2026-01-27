714967d4473e637d46231296f1f8bb7982a7b081
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-27 19:15:32 +0000

    Add function to filter and transform list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -22,3 +22,18 @@ main = do
     putStrLn $ "Sum of processed: " ++ show (sumOfProcessed sampleData)
     putStrLn $ "Head of empty list: " ++ show (safeHead [])
     putStrLn $ "Head of sample: " ++ show (safeHead sampleData)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
