c9215007bca980321aafaa39c22c7debc55e6cad
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-27 18:47:56 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -21,3 +21,15 @@ sumProcessed = sum . processNumbersmodule DataProcessor where
 
 processNumbers :: [Int] -> [Int]
 processNumbers = map (^2) . filter (>0)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = 
+    filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
