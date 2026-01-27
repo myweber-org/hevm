8ff24da2e7c024a26a094dae9068f7576f7e4e6d
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-27 23:23:37 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -16,3 +16,17 @@ main = do
     putStrLn $ "Original data: " ++ show testData
     putStrLn $ "Processed data: " ++ show (processEvenSquares testData)
     putStrLn $ "Sum of processed data: " ++ show (sumProcessedData testData)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processData
