f574df4e4366486fdd3fd2526cebed31549ebc7e
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-27 22:52:35 +0000

    Add function to parse CSV data and compute basic statistics

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -10,4 +10,61 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, 0, 5, -8]
     let result = processNumbers input
-    print result
+    print resultmodule DataProcessor where
+
+import Data.List (foldl')
+import Text.Read (readMaybe)
+
+type Row = [String]
+type CSVData = [Row]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ',') (lines content)
+  where
+    splitOn :: Char -> String -> [String]
+    splitOn delimiter = foldr splitHelper [""]
+      where
+        splitHelper :: Char -> [String] -> [String]
+        splitHelper c (x:xs)
+          | c == delimiter = "":x:xs
+          | otherwise = (c:x):xs
+
+safeReadDouble :: String -> Maybe Double
+safeReadDouble = readMaybe
+
+computeColumnStats :: CSVData -> Int -> Maybe (Double, Double, Double)
+computeColumnStats rows columnIndex
+  | null rows = Nothing
+  | otherwise = do
+      let values = mapMaybe (safeReadDouble . (!! columnIndex)) rows
+      if null values
+        then Nothing
+        else let sumVal = foldl' (+) 0 values
+                 count = fromIntegral (length values)
+                 mean = sumVal / count
+                 variance = foldl' (\acc x -> acc + (x - mean)^2) 0 values / count
+             in Just (mean, variance, sqrt variance)
+
+mapMaybe :: (a -> Maybe b) -> [a] -> [b]
+mapMaybe f = foldr (\x acc -> case f x of
+                                Just val -> val:acc
+                                Nothing -> acc) []
+
+processCSVFile :: String -> String -> IO ()
+processCSVFile inputFile outputFile = do
+  content <- readFile inputFile
+  let csvData = parseCSV content
+  case csvData of
+    [] -> putStrLn "Empty CSV file"
+    (header:rows) -> do
+      let stats = map (\i -> computeColumnStats rows i) [0..length header - 1]
+      let results = zipWith (\h s -> (h, s)) header stats
+      writeFile outputFile (formatResults results)
+      putStrLn $ "Statistics written to " ++ outputFile
+
+formatResults :: [(String, Maybe (Double, Double, Double))] -> String
+formatResults = unlines . map formatRow
+  where
+    formatRow (colName, Nothing) = colName ++ ": No numeric data"
+    formatRow (colName, Just (mean, variance, stdDev)) =
+      colName ++ ": Mean=" ++ show mean ++ ", Variance=" ++ show variance ++ ", StdDev=" ++ show stdDev
