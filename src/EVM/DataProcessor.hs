60d19ba476c97d7c87eb02a525d0927f4239072c
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-28 14:35:51 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives.

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -1,17 +1,5 @@
+
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateData :: [Int] -> Bool
-validateData xs = all (> 0) xs && length xs > 0
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5]
-    putStrLn $ "Original data: " ++ show sampleData
-    putStrLn $ "Processed data: " ++ show (processData sampleData)
-    putStrLn $ "Data valid: " ++ show (validateData sampleData)
+processData = map (^2) . filter (>0)
