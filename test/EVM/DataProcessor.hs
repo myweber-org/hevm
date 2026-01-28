b2bc8afe126289167d0bbd0aadb75479e9964e65
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-28 22:33:17 +0000

    Add function to filter and transform a list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -60,4 +60,16 @@ main = do
     let csvContent = "Name,Value\nAlice,25\nBob,30\nCharlie,xyz"
     case parseCSV csvContent >>= processCSVData of
         Left err -> putStrLn $ "Error: " ++ err
-        Right results -> putStrLn $ formatResults results
+        Right results -> putStrLn $ formatResults resultsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [-3, 1, 0, 5, -2, 8]
+    let result = processNumbers numbers
+    print result
