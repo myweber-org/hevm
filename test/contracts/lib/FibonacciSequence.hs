aa4fcb06f8047f70ce203b40190c06cf0f77eddd
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-28 01:48:58 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- test/contracts/lib/FibonacciSequence.hs
+++ test/contracts/lib/FibonacciSequence.hs
@@ -23,4 +23,18 @@ fib = memoize fib'
   where
     fib' 0 = 0
     fib' 1 = 1
-    fib' n = fib (n - 1) + fib (n - 2)
+    fib' n = fib (n - 1) + fib (n - 2)module FibonacciSequence where
+
+import Data.Function (fix)
+
+fibMemo :: Int -> Integer
+fibMemo = fix memoize
+  where
+    memoize _ 0 = 0
+    memoize _ 1 = 1
+    memoize f n = f (n - 1) + f (n - 2)
+
+main :: IO ()
+main = do
+    putStrLn "Fibonacci numbers from 0 to 20:"
+    mapM_ (print . fibMemo) [0..20]
