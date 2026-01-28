b7dc6928e6fcd50f658c5374437e2a739e775654
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-28 23:13:18 +0000

    Add function to filter and transform list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -1,29 +1,10 @@
-
-module DataProcessor where
-
-processData :: [Int] -> [Int]
-processData = map (^2) . filter (>0)module DataProcessor where
-
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-  where
-    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
-    average zs = sum zs / fromIntegral (length zs)
-
--- Helper function similar to Data.List.tails
-tails :: [a] -> [[a]]
-tails [] = [[]]
-tails (x:xs) = (x:xs) : tails xs
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-validateData :: [Int] -> Bool
-validateData = all (> 0) . processData
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
