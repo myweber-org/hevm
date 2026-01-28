ef493069cf890b9124bde52ec9df1d1822427864
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-28 20:46:42 +0000

    Implement a binary search tree with insertion and in-order traversal

--- bench/BinarySearchTree.hs
+++ bench/BinarySearchTree.hs
@@ -5,34 +5,17 @@ data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
 
 insert :: Ord a => a -> BST a -> BST a
 insert x Empty = Node Empty x Empty
-insert x (Node left y right)
-    | x < y     = Node (insert x left) y right
-    | x > y     = Node left y (insert x right)
-    | otherwise = Node left y right
+insert x (Node left val right)
+    | x < val   = Node (insert x left) val right
+    | x > val   = Node left val (insert x right)
+    | otherwise = Node left val right
 
 inOrder :: BST a -> [a]
 inOrder Empty = []
-inOrder (Node left x right) = inOrder left ++ [x] ++ inOrder right
+inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
 
 fromList :: Ord a => [a] -> BST a
 fromList = foldr insert Empty
 
-toSortedList :: BST a -> [a]
-toSortedList = inOrder
-module BinarySearchTree where
-
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
-
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Empty = Node x Empty Empty
-insert x (Node y left right)
-    | x < y     = Node y (insert x left) right
-    | x > y     = Node y left (insert x right)
-    | otherwise = Node y left right
-
-inOrder :: Tree a -> [a]
-inOrder Empty = []
-inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
-
-fromList :: Ord a => [a] -> Tree a
-fromList = foldr insert Empty
+toList :: BST a -> [a]
+toList = inOrder
