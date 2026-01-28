7aaa29437222091553fddaafb632340783f953f8
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-28 01:18:59 +0000

    Add Fibonacci sequence generator with memoization

--- doc/FibonacciSequence.hs
+++ doc/FibonacciSequence.hs
@@ -1,14 +1,10 @@
 module FibonacciSequence where
 
-fibonacci :: Integer -> [Integer]
-fibonacci n
-    | n <= 0    = []
-    | otherwise = takeWhile (<= n) fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-module FibonacciSequence where
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
+  where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+main :: IO ()
+main = do
+  putStrLn "First 20 Fibonacci numbers:"
+  mapM_ (putStrLn . show . fibonacci) [0..19]
