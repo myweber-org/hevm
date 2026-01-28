6dd25c936d1bd70e30e4d8abcb0ecf7b316748b6
InkspillVerse <inkspill.verse@outlook.com>
2026-01-28 22:36:42 +0000

    Add function to filter and transform a list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -1,59 +1,15 @@
 
 module DataProcessor where
 
-import Data.List (intercalate)
-import Text.Read (readMaybe)
-
-type Row = [String]
-type CSVData = [Row]
-
-parseCSV :: String -> CSVData
-parseCSV content = map (splitOn ',') (lines content)
-  where
-    splitOn :: Char -> String -> [String]
-    splitOn delimiter = foldr f [[]]
-      where
-        f c (x:xs) | c == delimiter = []:x:xs
-                   | otherwise = (c:x):xs
-
-safeReadDouble :: String -> Maybe Double
-safeReadDouble = readMaybe
-
-columnStats :: CSVData -> Int -> Maybe (Double, Double, Double)
-columnStats rows colIndex
-  | null rows = Nothing
-  | otherwise = case mapM (safeReadDouble . (!! colIndex)) rows of
-      Just values -> let avg = sum values / fromIntegral (length values)
-                         mx = maximum values
-                         mn = minimum values
-                     in Just (avg, mx, mn)
-      Nothing -> Nothing
-
-formatStats :: (Double, Double, Double) -> String
-formatStats (avg, mx, mn) =
-  intercalate ", " [ "Average: " ++ show avg
-                   , "Maximum: " ++ show mx
-                   , "Minimum: " ++ show mn
-                   ]
-
-processCSVFile :: String -> String
-processCSVFile content =
-  case parseCSV content of
-    [] -> "No data found"
-    rows@(header:_) ->
-      let statsResults = map (\i -> (header !! i, columnStats rows i)) [0..length header - 1]
-          formatted = map (\(colName, mStats) ->
-            case mStats of
-              Just stats -> colName ++ ": " ++ formatStats stats
-              Nothing -> colName ++ ": Non-numeric data") statsResults
-      in unlines formatted
-module DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * x + 1)
 
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show result
