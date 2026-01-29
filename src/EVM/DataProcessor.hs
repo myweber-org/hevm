605a6bafd807a82160e1541c3ef18d478b9ff783
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-29 23:50:09 +0000

    Add function to filter and transform a list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -2,23 +2,13 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
+filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = 
-    if all (\x -> x >= -100 && x <= 100) xs
-        then Just xs
-        else Nothing
-
 main :: IO ()
 main = do
-    let sampleData = [-5, 2, 0, 8, -3, 10]
-    case validateInput sampleData of
-        Just validData -> 
-            print $ processData validData
-        Nothing -> 
-            putStrLn "Input validation failed"
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
