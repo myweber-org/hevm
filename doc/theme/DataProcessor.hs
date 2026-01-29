d32f7dd926020b7aae7ea3e58166a455ed906038
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-29 00:22:43 +0000

    Add function to calculate moving average of a list

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -10,4 +10,29 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processNumbers input
-    print result
+    print resultmodule DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | n > length xs = error "Window size exceeds list length"
+    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
+  where
+    avg :: [Double] -> Double
+    avg window = sum window / fromIntegral n
+
+smoothData :: Int -> [Double] -> [Double]
+smoothData windowSize dataPoints =
+    movingAverage windowSize dataPoints
+
+validateData :: [Double] -> Maybe [Double]
+validateData [] = Nothing
+validateData xs
+    | any isInfinite xs = Nothing
+    | any isNaN xs = Nothing
+    | otherwise = Just xs
+  where
+    isInfinite x = x == 1/0 || x == -1/0
+    isNaN x = x /= x
