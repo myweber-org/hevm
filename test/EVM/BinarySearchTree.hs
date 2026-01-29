55b7df21ba02eb508bebea1c39b960949276a1e7
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-29 18:53:36 +0000

    Implement binary search tree with insertion and search operations

--- test/EVM/BinarySearchTree.hs
+++ test/EVM/BinarySearchTree.hs
@@ -6,19 +6,17 @@ data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
 insert :: Ord a => a -> BST a -> BST a
 insert x Empty = Node Empty x Empty
 insert x (Node left val right)
-    | x < val  = Node (insert x left) val right
-    | x > val  = Node left val (insert x right)
+    | x < val   = Node (insert x left) val right
+    | x > val   = Node left val (insert x right)
     | otherwise = Node left val right
+
+search :: Ord a => a -> BST a -> Bool
+search _ Empty = False
+search x (Node left val right)
+    | x < val   = search x left
+    | x > val   = search x right
+    | otherwise = True
 
 inorder :: BST a -> [a]
 inorder Empty = []
 inorder (Node left val right) = inorder left ++ [val] ++ inorder right
-
-fromList :: Ord a => [a] -> BST a
-fromList = foldr insert Empty
-
-main :: IO ()
-main = do
-    let tree = fromList [5, 3, 7, 1, 4, 6, 8]
-    putStrLn "Inorder traversal:"
-    print $ inorder tree
