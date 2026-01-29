1921c5ca5f9bd44ccc9e42cc11e351a67c8615c3
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-29 15:16:52 +0000

    Add word frequency counting functionality with a simple command-line interface

--- src/WordFrequencyCounter.hs
+++ src/WordFrequencyCounter.hs
@@ -26,4 +26,33 @@ main = do
   args <- getArgs
   case args of
     [] -> putStrLn "Usage: WordFrequencyCounter <text>"
-    text -> putStrLn $ processText $ unwords text
+    text -> putStrLn $ processText $ unwords textmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortBy, group, sort)
+import Data.Ord (comparing)
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map (filter isAlpha . map toLower) $ words text
+        grouped = group $ sort wordsList
+    in map (\ws -> (head ws, length ws)) grouped
+
+sortByFrequency :: [WordCount] -> [WordCount]
+sortByFrequency = sortBy (flip $ comparing snd)
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+processText :: String -> String
+processText = formatOutput . sortByFrequency . countWords
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze word frequency:"
+    input <- getLine
+    putStrLn "\nWord frequencies:"
+    putStrLn $ processText input
