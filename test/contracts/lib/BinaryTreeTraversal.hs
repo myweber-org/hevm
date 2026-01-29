2e0e9bd8230ed662ee03249a4edea0dbaeac8fd3
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-29 14:32:20 +0000

    Add in-order traversal for binary trees using continuation passing style

--- test/contracts/lib/BinaryTreeTraversal.hs
+++ test/contracts/lib/BinaryTreeTraversal.hs
@@ -1,14 +1,37 @@
+module BinaryTreeTraversal where
 
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
+data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show, Eq)
+
+inorderCPS :: BinaryTree a -> ([a] -> r) -> r
+inorderCPS Leaf cont = cont []
+inorderCPS (Node left value right) cont =
+    inorderCPS left $ \leftResult ->
+    inorderCPS right $ \rightResult ->
+    cont (leftResult ++ [value] ++ rightResult)
 
 inorder :: BinaryTree a -> [a]
-inorder Empty = []
-inorder (Node val left right) = inorder left ++ [val] ++ inorder right
+inorder tree = inorderCPS tree id
+
+preorderCPS :: BinaryTree a -> ([a] -> r) -> r
+preorderCPS Leaf cont = cont []
+preorderCPS (Node left value right) cont =
+    cont ([value] ++) $ \prefixFunc ->
+    preorderCPS left $ \leftResult ->
+    preorderCPS right $ \rightResult ->
+    prefixFunc (leftResult ++ rightResult)
 
 preorder :: BinaryTree a -> [a]
-preorder Empty = []
-preorder (Node val left right) = [val] ++ preorder left ++ preorder right
+preorder tree = preorderCPS tree id
+
+postorderCPS :: BinaryTree a -> ([a] -> r) -> r
+postorderCPS Leaf cont = cont []
+postorderCPS (Node left value right) cont =
+    postorderCPS left $ \leftResult ->
+    postorderCPS right $ \rightResult ->
+    cont (leftResult ++ rightResult ++ [value])
 
 postorder :: BinaryTree a -> [a]
-postorder Empty = []
-postorder (Node val left right) = postorder left ++ postorder right ++ [val]
+postorder tree = postorderCPS tree id
+
+sampleTree :: BinaryTree Int
+sampleTree = Node (Node Leaf 2 Leaf) 1 (Node (Node Leaf 4 Leaf) 3 Leaf)
