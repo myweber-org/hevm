cd189c236e4adc39736c7ee94733d860c82a4b16
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-29 00:29:14 +0000

    Add word frequency counter module with basic text processing

--- test/scripts/WordFrequencyCounter.hs
+++ test/scripts/WordFrequencyCounter.hs
@@ -29,4 +29,28 @@ displayResults freqs = unlines $ map formatEntry freqs
     formatEntry (word, count) = word ++ ": " ++ show count
 
 analyzeText :: String -> String
-analyzeText = displayResults . processText
+analyzeText = displayResults . processTextmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordFreq = (String, Int)
+
+countWords :: String -> [WordFreq]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        freqMap = foldl updateFreq [] wordsList
+    in take 10 $ sortOn (Down . snd) freqMap
+  where
+    cleanWord = filter isAlpha . map toLower
+    updateFreq [] word = [(word, 1)]
+    updateFreq ((w, c):rest) word
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : updateFreq rest word
+
+formatResults :: [WordFreq] -> String
+formatResults freqs = unlines $ map (\(w, c) -> w ++ ": " ++ show c) freqs
+
+processText :: String -> String
+processText = formatResults . countWords
