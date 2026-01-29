53d14db78878ea7ce8e488c830b85d47e288d538
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-29 00:47:31 +0000

    Add word frequency counter module with basic text processing functions

--- bench/WordFrequencyCounter.hs
+++ bench/WordFrequencyCounter.hs
@@ -25,4 +25,30 @@ processText = formatOutput . sortByFrequency . countWords
 main :: IO ()
 main = do
     input <- TIO.getContents
-    TIO.putStrLn $ processText input
+    TIO.putStrLn $ processText inputmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        grouped = foldr countWord [] wordsList
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter isAlpha
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+formatResults :: [WordCount] -> String
+formatResults counts = unlines $ map formatLine counts
+  where
+    formatLine (word, count) = word ++ ": " ++ show count
+
+processText :: String -> String
+processText = formatResults . countWords
