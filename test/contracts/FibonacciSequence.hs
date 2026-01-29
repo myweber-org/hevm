bb8324e0183bed5d75293ba96908b0efb54d573a
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-29 14:52:43 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/contracts/FibonacciSequence.hs
+++ test/contracts/FibonacciSequence.hs
@@ -1,25 +1,5 @@
 module FibonacciSequence where
 
-import Data.Function.Memoize
-
-fib :: Integer -> Integer
-fib = memoize fib'
-  where
-    fib' 0 = 0
-    fib' 1 = 1
-    fib' n = fib (n - 1) + fib (n - 2)
-
-main :: IO ()
-main = do
-    putStrLn "Fibonacci numbers from 0 to 10:"
-    mapM_ (print . fib) [0..10]module FibonacciSequence where
-
-fibonacci :: Integer -> [Integer]
-fibonacci n = takeWhile (<= n) fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-module FibonacciSequence where
-
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
