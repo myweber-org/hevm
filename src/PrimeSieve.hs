beb6f4137282496b3884393ee0b75468a949385c
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-29 01:10:53 +0000

    Implement Sieve of Eratosthenes to generate primes up to a given limit

--- src/PrimeSieve.hs
+++ src/PrimeSieve.hs
@@ -1,14 +1,19 @@
 module PrimeSieve where
 
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
+sieve :: Int -> [Int]
+sieve limit
+    | limit < 2 = []
+    | otherwise = sieve' [2..limit] []
     where
-        sieve [] = []
-        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+        sieve' [] primes = reverse primes
+        sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
 
-isPrime :: Int -> Bool
-isPrime n
-    | n < 2     = False
-    | otherwise = n `elem` primesUpTo n
+primesUpTo :: Int -> [Int]
+primesUpTo = sieve
+
+main :: IO ()
+main = do
+    putStrLn "Enter limit:"
+    input <- getLine
+    let limit = read input :: Int
+    print $ primesUpTo limit
