ca8af7663afa51525834b464691d2f5078eef8fe
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-29 19:59:29 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -99,3 +99,13 @@ main = do
         Right data -> do
             putStrLn "Processed successfully:"
             putStrLn $ formatCSVOutput data
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
