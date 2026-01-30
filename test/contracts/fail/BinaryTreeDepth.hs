f8092edba2459747ef6693debc42bbfd4d7fc9f5
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-30 19:50:58 +0000

    Add function to calculate the depth of a binary tree

--- test/contracts/fail/BinaryTreeDepth.hs
+++ test/contracts/fail/BinaryTreeDepth.hs
@@ -1,5 +1,5 @@
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+data BinaryTree a = Leaf | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
+treeDepth Leaf = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
