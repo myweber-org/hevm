60c9b3c92eae3d9002de81723c0fa8a07099d467
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-30 19:36:51 +0000

    Implement Sieve of Eratosthenes to generate primes up to a given limit

--- doc/src/code_examples/PrimeSieve.hs
+++ doc/src/code_examples/PrimeSieve.hs
@@ -1,82 +1,13 @@
+
 module PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]module PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
-
--- Example usage (commented out):
--- main = print $ primesUpTo 100module PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]module PrimeSieve where
 
 sieve :: Int -> [Int]
 sieve limit
     | limit < 2 = []
     | otherwise = go [2..limit] []
-  where
-    go [] primes = reverse primes
-    go (x:xs) primes = go (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
     where
-        sieve' [] primes = reverse primes
-        sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
+        go []     primes = reverse primes
+        go (x:xs) primes = go (filter (\n -> n `mod` x /= 0) xs) (x:primes)
 
 primesUpTo :: Int -> [Int]
 primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter a limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve :: [Int] -> [Int]
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
-module PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve []     = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
