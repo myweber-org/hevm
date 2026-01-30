fa14dfa186879eb7f695a48227f5eb75fb911cf3
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-30 21:42:33 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -1,32 +1,5 @@
+
 module DataProcessor where
-
-import Data.List.Split (splitOn)
-
-parseCSV :: String -> [[Double]]
-parseCSV content = map (map read . splitOn ",") (lines content)
-
-computeAverages :: [[Double]] -> [Double]
-computeAverages rows
-    | null rows = []
-    | otherwise = map avg (transpose rows)
-  where
-    avg xs = sum xs / fromIntegral (length xs)
-    transpose = foldr (zipWith (:)) (repeat [])
-
-processCSVData :: String -> [Double]
-processCSVData = computeAverages . parseCSVmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print resultmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
@@ -36,20 +9,3 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
-
-main :: IO ()
-main = do
-    let dataSet = [1..10]
-    putStrLn $ "Original data: " ++ show dataSet
-    putStrLn $ "Even squares: " ++ show (processEvenSquares dataSet)
-    putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) dataSet)
