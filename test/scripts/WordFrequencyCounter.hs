127dcfd4024f980d09d0b751ae9622425b4b01e1
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-30 14:57:08 +0000

    Add word frequency counter with command-line interface

--- test/scripts/WordFrequencyCounter.hs
+++ test/scripts/WordFrequencyCounter.hs
@@ -1,56 +1,29 @@
 module WordFrequencyCounter where
 
-import Data.Char (toLower, isAlpha)
-import Data.List (sortOn)
-import Data.Map (Map)
-import qualified Data.Map as Map
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+import System.Environment (getArgs)
 
-type WordFreq = Map String Int
+type WordCount = Map.Map String Int
 
-normalizeWord :: String -> String
-normalizeWord = map toLower . filter isAlpha
-
-countWords :: String -> WordFreq
-countWords text = foldr incrementWord Map.empty words
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
   where
-    words' = words text
-    words = map normalizeWord words'
-    incrementWord word freqMap = Map.insertWith (+) word 1 freqMap
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = filter Char.isAlpha . map Char.toLower
 
-getSortedFrequencies :: WordFreq -> [(String, Int)]
-getSortedFrequencies = sortOn (negate . snd) . Map.toList
-
-processText :: String -> [(String, Int)]
-processText = getSortedFrequencies . countWords
-
-displayResults :: [(String, Int)] -> String
-displayResults freqs = unlines $ map formatEntry freqs
+formatResults :: WordCount -> String
+formatResults = unlines . map formatEntry . List.sortOn snd . Map.toList
   where
     formatEntry (word, count) = word ++ ": " ++ show count
 
-analyzeText :: String -> String
-analyzeText = displayResults . processTextmodule WordFrequencyCounter where
-
-import Data.Char (toLower, isAlpha)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type WordFreq = (String, Int)
-
-countWords :: String -> [WordFreq]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        freqMap = foldl updateFreq [] wordsList
-    in take 10 $ sortOn (Down . snd) freqMap
-  where
-    cleanWord = filter isAlpha . map toLower
-    updateFreq [] word = [(word, 1)]
-    updateFreq ((w, c):rest) word
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : updateFreq rest word
-
-formatResults :: [WordFreq] -> String
-formatResults freqs = unlines $ map (\(w, c) -> w ++ ": " ++ show c) freqs
-
-processText :: String -> String
-processText = formatResults . countWords
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> putStrLn "Usage: WordFrequencyCounter <text>"
+    textPieces -> do
+      let text = unwords textPieces
+      let frequencyMap = countWords text
+      putStrLn $ formatResults frequencyMap
