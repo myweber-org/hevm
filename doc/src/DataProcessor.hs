a6133d660407502a31ba300e04b8e0c85081437f
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-31 16:13:49 +0000

    Add function to filter and transform a list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -21,3 +21,16 @@ main = do
     putStrLn $ "Processed list: " ++ show (processNumbers numbers)
     putStrLn $ "Sum of positive doubles: " ++ show (sumPositiveDoubles numbers)
     putStrLn $ "First element: " ++ show (safeHead numbers)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processNumbers input
+    print result
