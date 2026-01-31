ce128e4b9c2fa1e7eccb4cbed0febcdaf6a00763
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-31 22:49:12 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -59,3 +59,13 @@ sumProcessedData = sum . processData
 
 validateInput :: [Int] -> Maybe [Int]
 validateInput xs = if all (/= 0) xs then Just xs else Nothing
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedList :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
+sumProcessedList predicate transformer = sum . filterAndTransform predicate transformer
