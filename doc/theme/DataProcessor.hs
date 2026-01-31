89ac2f54cd815a34d4ff65ad0b1bd96a027eba20
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-31 20:40:37 +0000

    Add function to calculate moving average of a list

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -33,4 +33,27 @@ sumProcessedData :: [Int] -> Int
 sumProcessedData = sum . processData
 
 validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (> -100) xs then Just xs else Nothing
+validateInput xs = if all (> -100) xs then Just xs else Nothingmodule DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+smoothData :: [Double] -> [Double]
+smoothData = movingAverage 3
+
+main :: IO ()
+main = do
+    let testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
+    putStrLn "Original data:"
+    print testData
+    putStrLn "\nSmoothed data (3-point moving average):"
+    print $ smoothData testData
