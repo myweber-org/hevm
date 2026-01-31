7eaab0dc85634a516d69233fd491744259f817dc
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-31 23:28:13 +0000

    Add function to parse CSV data and compute basic statistics

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -16,4 +16,44 @@ movingAverage n xs
     average zs = sum zs / fromIntegral (length zs)module DataProcessor where
 
 processNumbers :: [Int] -> [Int]
-processNumbers = map (*2) . filter (>0)
+processNumbers = map (*2) . filter (>0)module DataProcessor where
+
+import Data.List (foldl')
+import Text.Read (readMaybe)
+
+data StatRecord = StatRecord
+    { recordId :: Int
+    , value    :: Double
+    } deriving (Show, Eq)
+
+parseCSVLine :: String -> Maybe StatRecord
+parseCSVLine line = case words line of
+    [idStr, valStr] -> do
+        idVal <- readMaybe idStr
+        dVal  <- readMaybe valStr
+        return $ StatRecord idVal dVal
+    _ -> Nothing
+
+parseCSVData :: String -> [StatRecord]
+parseCSVData = mapMaybe parseCSVLine . lines
+  where
+    mapMaybe f = foldr (\x acc -> case f x of
+        Just val -> val : acc
+        Nothing  -> acc) []
+
+computeStats :: [StatRecord] -> (Double, Double, Double)
+computeStats records =
+    let values = map value records
+        sumVal = foldl' (+) 0 values
+        len = fromIntegral $ length values
+        mean = sumVal / len
+        variance = foldl' (\acc v -> acc + (v - mean) ** 2) 0 values / len
+        stdDev = sqrt variance
+    in (mean, variance, stdDev)
+
+processCSV :: String -> Maybe (Double, Double, Double)
+processCSV input =
+    let records = parseCSVData input
+    in if null records
+        then Nothing
+        else Just $ computeStats records
