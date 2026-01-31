a70f1cec486ba198a1cc9cfe74b0bf929b5af565
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-31 18:57:55 +0000

    Add function to parse and filter CSV data by date range

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -23,3 +23,39 @@ main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
     print result
+module DataProcessor where
+
+import Data.Time
+import Data.Time.Format
+import Data.List
+import Text.Read (readMaybe)
+
+type CSVRow = [String]
+
+parseCSV :: String -> [CSVRow]
+parseCSV = map (splitOn ',') . lines
+  where splitOn _ [] = []
+        splitOn delimiter str =
+          let (token, rest) = break (== delimiter) str
+          in token : case rest of
+                      [] -> []
+                      (_:xs) -> splitOn delimiter xs
+
+filterByDateRange :: Day -> Day -> [CSVRow] -> [CSVRow]
+filterByDateRange startDate endDate rows =
+  filter (isWithinRange . parseDate . head) rows
+  where
+    parseDate dateStr = parseTimeOrError True defaultTimeLocale "%Y-%m-%d" dateStr :: Day
+    isWithinRange date = date >= startDate && date <= endDate
+
+calculateDailyAverage :: [CSVRow] -> Maybe Double
+calculateDailyAverage rows =
+  if null validValues
+    then Nothing
+    else Just (sum validValues / fromIntegral (length validValues))
+  where
+    validValues = mapMaybe (readMaybe . (!!1)) rows
+
+processData :: String -> Day -> Day -> Maybe Double
+processData csvData start end =
+  calculateDailyAverage $ filterByDateRange start end $ parseCSV csvData
