74cd51d285045b0c23d4e759ab131958847b2d93
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-31 22:09:55 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- ethjet/FibonacciSequence.hs
+++ ethjet/FibonacciSequence.hs
@@ -13,4 +13,17 @@ fibonacci n = take n fibs
 main :: IO ()
 main = do
   putStrLn "Fibonacci sequence up to 10 terms:"
-  print $ fibonacci 10
+  print $ fibonacci 10module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
+
+main :: IO ()
+main = do
+    putStrLn "Fibonacci sequence up to 10 terms:"
+    print $ fibonacci 10
