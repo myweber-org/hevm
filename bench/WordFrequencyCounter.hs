96642c13fdb9defbb0f62bd4b71d81d3fc6c4c1b
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-31 23:21:14 +0000

    Add word frequency counter module with basic text processing functions

--- bench/WordFrequencyCounter.hs
+++ bench/WordFrequencyCounter.hs
@@ -1,32 +1,5 @@
 module WordFrequencyCounter where
 
-import qualified Data.Char as Char
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-
-type WordCount = Map.Map T.Text Int
-
-countWords :: T.Text -> WordCount
-countWords = Map.fromListWith (+) . map (\w -> (w, 1)) . filter (not . T.null) . map normalize . T.words
-  where
-    normalize = T.toLower . T.filter Char.isLetter
-
-sortByFrequency :: WordCount -> [(T.Text, Int)]
-sortByFrequency = List.sortBy (\(_, c1) (_, c2) -> compare c2 c1) . Map.toList
-
-formatOutput :: [(T.Text, Int)] -> T.Text
-formatOutput = T.unlines . map (\(word, count) -> T.concat [word, T.pack ": ", T.pack (show count)])
-
-processText :: T.Text -> T.Text
-processText = formatOutput . sortByFrequency . countWords
-
-main :: IO ()
-main = do
-    input <- TIO.getContents
-    TIO.putStrLn $ processText inputmodule WordFrequencyCounter where
-
 import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
@@ -36,63 +9,20 @@ type WordCount = (String, Int)
 countWords :: String -> [WordCount]
 countWords text = 
     let wordsList = filter (not . null) $ map cleanWord $ words text
-        grouped = foldr countWord [] wordsList
-    in sortOn (Down . snd) grouped
+        wordMap = foldr incrementWord [] wordsList
+    in take 10 $ sortOn (Down . snd) wordMap
   where
     cleanWord = map toLower . filter isAlpha
-    countWord word [] = [(word, 1)]
-    countWord word ((w, c):rest)
+    incrementWord word [] = [(word, 1)]
+    incrementWord word ((w, c):rest)
         | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord word rest
+        | otherwise = (w, c) : incrementWord word rest
 
 formatResults :: [WordCount] -> String
-formatResults counts = unlines $ map formatLine counts
+formatResults counts = 
+    unlines $ "Top 10 most frequent words:" : map formatEntry counts
   where
-    formatLine (word, count) = word ++ ": " ++ show count
+    formatEntry (word, count) = word ++ ": " ++ show count
 
 processText :: String -> String
-processText = formatResults . countWordsmodule WordFrequencyCounter where
-
-import Data.Char (toLower, isAlpha)
-import Data.List (sortOn, group, sort)
-import Data.Ord (Down(..))
-
-type WordCount = (String, Int)
-
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleanedWords = filter (all isAlpha) wordsList
-        grouped = group $ sort cleanedWords
-    in sortOn (Down . snd) $ map (\ws -> (head ws, length ws)) grouped
-  where
-    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-
-filterByFrequency :: Int -> [WordCount] -> [WordCount]
-filterByFrequency minCount = filter (\(_, count) -> count >= minCount)
-
-getTopNWords :: Int -> [WordCount] -> [WordCount]
-getTopNWords n = take n
-
-printWordCounts :: [WordCount] -> IO ()
-printWordCounts counts = 
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
-
-processText :: String -> Int -> Int -> IO ()
-processText text minFreq topN = do
-    let counts = countWords text
-    let filtered = filterByFrequency minFreq counts
-    let topWords = getTopNWords topN filtered
-    putStrLn "Top words by frequency:"
-    printWordCounts topWords
-
--- Example usage
-sampleText :: String
-sampleText = "Hello world! Hello Haskell. Haskell is great. World says hello to Haskell."
-
-main :: IO ()
-main = do
-    putStrLn "Processing sample text..."
-    processText sampleText 1 5
-    putStrLn "\nProcessing with minimum frequency 2..."
-    processText sampleText 2 3
+processText = formatResults . countWords
