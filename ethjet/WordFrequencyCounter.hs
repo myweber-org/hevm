799cd315d388368cc06c69a792296266e65a48f3
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-01 23:15:07 +0000

    Add word frequency counter with sorting and output formatting

--- ethjet/WordFrequencyCounter.hs
+++ ethjet/WordFrequencyCounter.hs
@@ -1,32 +1,28 @@
 module WordFrequencyCounter where
 
-import Data.Char (toLower, isAlpha)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
-type WordCount = (String, Int)
+type WordCount = Map.Map T.Text Int
 
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleanedWords = map (map toLower) wordsList
-        grouped = foldr countWord [] cleanedWords
-    in sortOn (Down . snd) grouped
+countWords :: T.Text -> WordCount
+countWords = Map.fromListWith (+) . map (\w -> (w, 1)) . filter (not . T.null) . map normalize . T.words
   where
-    cleanWord = filter isAlpha
-    countWord word [] = [(word, 1)]
-    countWord word ((w, c):rest)
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord word rest
+    normalize = T.toLower . T.filter Char.isLetter
 
-formatOutput :: [WordCount] -> String
-formatOutput counts = 
-    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+sortByFrequency :: WordCount -> [(T.Text, Int)]
+sortByFrequency = List.sortBy (\(_, c1) (_, c2) -> compare c2 c1) . Map.toList
+
+formatOutput :: [(T.Text, Int)] -> T.Text
+formatOutput = T.unlines . map (\(word, count) -> T.pack (show count) <> " " <> word)
+
+processText :: T.Text -> T.Text
+processText = formatOutput . sortByFrequency . countWords
 
 main :: IO ()
 main = do
-    putStrLn "Enter text to analyze word frequency:"
-    input <- getLine
-    let frequencies = countWords input
-    putStrLn "\nWord frequencies (sorted by count):"
-    putStrLn $ formatOutput frequencies
+    input <- TIO.getContents
+    TIO.putStr $ processText input
