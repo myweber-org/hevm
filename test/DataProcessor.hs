851c0a301d6df7db819cb9f407604aff83468887
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-01 00:21:33 +0000

    Add function to filter and transform a list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -2,40 +2,13 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (^2)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer =
-    sum . filterAndTransform predicate transformer
-
-main :: IO ()
-main = do
-    let sampleData = [1..10]
-    putStrLn $ "Original data: " ++ show sampleData
-    putStrLn $ "Even squares: " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Sum of even squares: " ++ 
-        show (sumProcessedData even (^2) sampleData)
-module DataProcessor where
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = map (^2) . filter even
-
-main :: IO ()
-main = do
-    let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let result = processEvenSquares input
-    print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-sumPositiveDoubled :: [Int] -> Int
-sumPositiveDoubled = sum . processNumbers
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
