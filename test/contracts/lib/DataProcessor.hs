8e6f4a124cd2822f8712920ff67cc2687b35387d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-01 15:45:27 +0000

    Add function to filter and transform a list of integers

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -16,3 +16,10 @@ main = do
     if validateInput sampleData
         then print $ processData sampleData
         else putStrLn "Invalid input data"
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
