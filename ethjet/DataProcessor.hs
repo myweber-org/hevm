4569cae52bf9be1f78111a0c8119a11f77d751bd
ElaraWrites <elara.writes@outlook.com>
2026-02-01 23:32:51 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -111,4 +111,16 @@ exampleUsage = do
     putStrLn "Original data:"
     print testData
     putStrLn "\nProcessed data (numeric values converted to uppercase):"
-    print $ processData testData
+    print $ processData testDatamodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (*2)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    print result
