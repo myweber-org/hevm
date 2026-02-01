0935317ff99b11e74c7501edf431bc84d9ab45f0
VioletInquiry <violet.inquiry@outlook.com>
2026-02-01 14:18:36 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- bench/contracts/FibonacciSequence.hs
+++ bench/contracts/FibonacciSequence.hs
@@ -17,3 +17,9 @@ fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
