0485a3dacc672e0c88e4d8b2f86753f456eee0f0
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-02 20:40:48 +0000

    Add function to calculate moving average of a list

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -20,4 +20,23 @@ movingAverage n xs
     | otherwise = map average $ windows n xs
   where
     windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
-    average zs = sum zs / fromIntegral (length zs)
+    average zs = sum zs / fromIntegral (length zs)module DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | n > length xs = error "Window size exceeds list length"
+    | otherwise = map avg $ filter (\w -> length w == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
+
+safeMovingAverage :: Fractional a => Int -> [a] -> Maybe [a]
+safeMovingAverage n xs
+    | n <= 0 = Nothing
+    | n > length xs = Nothing
+    | otherwise = Just $ movingAverage n xs
+
+testData :: [Double]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
