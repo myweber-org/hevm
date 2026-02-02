fc194b13c543e68da67f4b70edc3a9378896cd13
heyQuim <hey.quim@hotmail.com>
2026-02-02 00:59:44 +0000

    Add function to filter and transform a list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -101,4 +101,10 @@ movingAverage windowSize xs
     average list = sum list / fromIntegral (length list)module DataProcessor where
 
 processData :: [Int] -> [Int]
-processData = map (^2) . filter even
+processData = map (^2) . filter evenmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (* 2)
