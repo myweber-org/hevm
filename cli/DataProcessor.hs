6b31f49a002e9f0c40850424211a83d6390c5f0d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-02 16:40:25 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -24,4 +24,16 @@ main = do
             let result = sumProcessed validNumbers
             putStrLn $ "Sum of processed numbers: " ++ show result
         Nothing -> 
-            putStrLn "Invalid input: all numbers must be positive"
+            putStrLn "Invalid input: all numbers must be positive"module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
