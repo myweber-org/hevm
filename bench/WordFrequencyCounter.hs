b71b7063eb12fe9aa618eb6b5e0dd39962308456
FelixMakes <felix.makes@hotmail.com>
2026-02-02 02:09:04 +0000

    Add word frequency counter module with basic text processing functions

--- bench/WordFrequencyCounter.hs
+++ bench/WordFrequencyCounter.hs
@@ -25,4 +25,27 @@ formatResults counts =
     formatEntry (word, count) = word ++ ": " ++ show count
 
 processText :: String -> String
-processText = formatResults . countWords
+processText = formatResults . countWordsmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import qualified Data.Map.Strict as Map
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+    in foldr (\word -> Map.insertWith (+) word 1) Map.empty wordsList
+    where
+        cleanWord = filter isAlpha . map toLower
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = 
+    take n $ sortOn (\(_, count) -> negate count) $ Map.toList $ countWords text
+
+printWordFrequencies :: String -> IO ()
+printWordFrequencies text = do
+    let frequencies = topNWords 10 text
+    putStrLn "Top 10 most frequent words:"
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) frequencies
