43966d49ed3685fc261c827388fb6e3409a7faa4
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-02 20:25:42 +0000

    Add function to filter and transform a list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -41,4 +41,10 @@ validateInput :: [Int] -> Maybe [Int]
 validateInput xs = 
     if all (> -1000) xs && all (< 1000) xs
     then Just xs
-    else Nothing
+    else Nothingmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
