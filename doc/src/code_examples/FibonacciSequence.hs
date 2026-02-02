444daf87bdb12310142d27183d8775d4e4f65cf8
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-02 19:19:13 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/src/code_examples/FibonacciSequence.hs
+++ doc/src/code_examples/FibonacciSequence.hs
@@ -51,4 +51,14 @@ fibMemo = (map fib [0..] !!)
 main :: IO ()
 main = do
     putStrLn "Fibonacci numbers from 0 to 20:"
-    mapM_ (print . fibMemo) [0..20]
+    mapM_ (print . fibMemo) [0..20]module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+  putStrLn "Fibonacci sequence up to 10 terms:"
+  print (fibonacci 10)
