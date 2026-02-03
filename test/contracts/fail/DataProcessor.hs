8ca6cb978c42c6d756b3ca0a604c522c4d3e6fea
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-03 01:42:52 +0000

    Add function to filter and transform a list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -67,3 +67,16 @@ safeMovingAverage n xs
 
 testData :: [Double]
 testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    print result
