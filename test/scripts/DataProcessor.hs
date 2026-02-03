c558bb1b16c19aaafd61e4f07c95deb7602ec16a
InkspillVerse <inkspill.verse@outlook.com>
2026-02-03 18:47:59 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -28,3 +28,13 @@ calculateAverages rows
 
 processCSVData :: String -> [Double]
 processCSVData = calculateAverages . parseCSV
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
