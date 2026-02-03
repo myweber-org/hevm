80d6c2832960732ed0a8ceaa0058dc2be7459ee9
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-03 02:28:39 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -57,4 +57,18 @@ calculateColumnAverage rows colIndex = do
     return (sum numericValues / fromIntegral (length numericValues))
 
 formatCSVOutput :: CSVData -> String
-formatCSVOutput = intercalate "\n" . map (intercalate ",")
+formatCSVOutput = intercalate "\n" . map (intercalate ",")module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
