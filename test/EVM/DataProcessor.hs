2bbec31372a31c9b521a01265f7e5ad6205b36ee
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-03 03:51:40 +0000

    Add function to parse CSV data and compute average values

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -11,3 +11,25 @@ main = do
     let numbers = [1..10]
     let result = processNumbers numbers
     print result
+module DataProcessor where
+
+import Data.List.Split (splitOn)
+
+parseCSV :: String -> [[Double]]
+parseCSV content = map (map read . splitOn ",") $ lines content
+
+computeAverages :: [[Double]] -> [Double]
+computeAverages rows = 
+    if null rows 
+    then []
+    else map (\col -> sum col / fromIntegral (length col)) $ transpose rows
+  where
+    transpose [] = []
+    transpose ([]:_) = []
+    transpose x = map head x : transpose (map tail x)
+
+processCSVFile :: String -> IO [Double]
+processCSVFile filename = do
+    content <- readFile filename
+    let parsed = parseCSV content
+    return $ computeAverages parsed
