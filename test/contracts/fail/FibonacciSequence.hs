c41bd1878258930145dda6bfa737d2d6d6b961ee
InkspillVerse <inkspill.verse@outlook.com>
2026-02-03 14:54:49 +0000

    Add Fibonacci sequence generator with memoization

--- test/contracts/fail/FibonacciSequence.hs
+++ test/contracts/fail/FibonacciSequence.hs
@@ -15,4 +15,14 @@ main = do
     putStrLn "Enter the number of Fibonacci terms to generate:"
     input <- getLine
     let n = read input :: Int
-    print (fibonacci n)
+    print (fibonacci n)module FibonacciSequence where
+
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    mapM_ (print . fibonacci) [0..19]
