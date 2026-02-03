b132e348af2d90c4a89bcddf9f62f3b8cc3cde1a
ElaraWrites <elara.writes@outlook.com>
2026-02-03 01:24:18 +0000

    Add function to filter and transform a list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -123,4 +123,16 @@ main = do
     result <- processCSVFile "data.csv"
     case result of
         Just avg -> putStrLn $ "Average: " ++ show avg
-        Nothing -> putStrLn "Could not calculate average"
+        Nothing -> putStrLn "Could not calculate average"module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -7]
+    let result = processNumbers input
+    print result
