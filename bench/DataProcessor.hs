78e480c0509f87f3441a3e16ff60cd699f14699b
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-03 18:33:59 +0000

    Add function to filter and transform a list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -173,3 +173,10 @@ personToString p =
 -- Batch process list of persons
 processPersonList :: [Person] -> [String]
 processPersonList = map personToString . filter (maybe False (const True) . processPerson)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
