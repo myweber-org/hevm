a1e154b8086b0a6d3477000f55c82036e3e84f34
heyQuim <hey.quim@hotmail.com>
2026-02-03 23:51:58 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -29,3 +29,20 @@ smoothData windowSize dataPoints =
     let avg = movingAverage windowSize dataPoints
         padding = replicate (windowSize `div` 2) (head avg)
     in padding ++ avg ++ padding
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+main :: IO ()
+main = do
+    let numbers = [-3, -1, 0, 2, 5, 8]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
+    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
