85073d39fa10ef43d6934c2007eacb654b7cd3cf
ElaraWrites <elara.writes@outlook.com>
2026-02-03 00:09:34 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -1,53 +1,14 @@
+
 module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (> -100) xs
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 4, -5, 6]
-    if validateInput sampleData
-        then do
-            putStrLn $ "Original data: " ++ show sampleData
-            putStrLn $ "Processed data: " ++ show (processData sampleData)
-            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData sampleData)
-        else putStrLn "Invalid input data"module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * x + 1)
+processNumbers = filterAndTransform (> 0) (* 2)
 
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
 
-import Data.List.Split (splitOn)
-
-parseCSV :: String -> [[Double]]
-parseCSV csv = map (map read . splitOn ",") $ lines csv
-
-calculateAverages :: [[Double]] -> [Double]
-calculateAverages rows = 
-    if null rows 
-    then []
-    else map avg $ transpose rows
-  where
-    avg xs = sum xs / fromIntegral (length xs)
-    transpose = foldr (zipWith (:)) (repeat [])
-
-processCSVData :: String -> [Double]
-processCSVData = calculateAverages . parseCSV
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
