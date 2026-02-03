41facb9d5b317f536271ec789d00be3f909d0608
ElaraWrites <elara.writes@outlook.com>
2026-02-03 16:29:51 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -1,116 +1,18 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * x + 1)
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessed :: (Int -> Int) -> [Int] -> Int
+sumProcessed f = sum . map f
 
 main :: IO ()
 main = do
     let numbers = [1..10]
-    let result = processNumbers numbers
     putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, 0, -5, 6]
-    let result = processNumbers input
-    print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processEvenSquares
-
-main :: IO ()
-main = do
-    let sampleData = [1..10]
-    putStrLn $ "Original: " ++ show sampleData
-    putStrLn $ "Processed: " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Sum: " ++ show (sumProcessed sampleData)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Bool
-validateInput xs = all (\x -> x >= -100 && x <= 100) xs
-
-safeProcess :: [Int] -> Maybe [Int]
-safeProcess xs
-  | validateInput xs = Just (processData xs)
-  | otherwise = Nothing
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedList :: [Int] -> Int
-sumProcessedList = sum . processEvenSquares
-
-main :: IO ()
-main = do
-    let sampleData = [1..10]
-    putStrLn $ "Original list: " ++ show sampleData
-    putStrLn $ "Processed list (even numbers squared): " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Sum of processed list: " ++ show (sumProcessedList sampleData)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (*2)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-import Data.List.Split (splitOn)
-
-parseCSV :: String -> [[Double]]
-parseCSV content = map (map read . splitOn ",") (lines content)
-
-calculateAverages :: [[Double]] -> [Double]
-calculateAverages rows = 
-    if null rows then []
-    else map (\col -> sum col / fromIntegral (length col)) (transpose rows)
-  where
-    transpose :: [[Double]] -> [[Double]]
-    transpose [] = []
-    transpose ([]:_) = []
-    transpose xss = map head xss : transpose (map tail xss)
-
-processCSVData :: String -> [Double]
-processCSVData = calculateAverages . parseCSV
-module DataProcessor where
-
-processNumbers :: [Int] -> [Int]
-processNumbers = map (*2) . filter (>0)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-isAllPositive :: [Int] -> Bool
-isAllPositive = all (>0)
+    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
+    putStrLn $ "Sum of even squares: " ++ show (sumProcessed (\x -> x * x) (filter even numbers))
