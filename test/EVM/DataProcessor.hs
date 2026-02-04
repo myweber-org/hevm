1252bce2d39d5e33248e0aab1eec73715ee76c13
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-04 15:37:15 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -33,3 +33,21 @@ processCSVFile filename = do
     content <- readFile filename
     let parsed = parseCSV content
     return $ computeAverages parsed
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (>= -100) xs && all (<= 100) xs
+                   then Just xs
+                   else Nothing
+
+safeProcess :: [Int] -> Maybe Int
+safeProcess = fmap sumProcessedData . validateInput
