e6781d464abcde4323fd2da6ba4a7fd646e50be1
heyQuim <hey.quim@hotmail.com>
2026-02-04 16:23:15 +0000

    Add function to parse CSV data and calculate average values

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -23,3 +23,52 @@ sumProcessed = sum . processNumbers
 
 validateInput :: [Int] -> Maybe [Int]
 validateInput xs = if all (> -100) xs then Just xs else Nothing
+module DataProcessor where
+
+import Data.List.Split (splitOn)
+
+type Row = [String]
+type CSVData = [Row]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
+
+numericColumns :: CSVData -> [Int]
+numericColumns [] = []
+numericColumns (header:rows) = 
+    [i | (i, cell) <- zip [0..] header, all isNumeric (map (!! i) rows)]
+  where
+    isNumeric str = case reads str :: [(Double, String)] of
+        [(_, "")] -> True
+        _ -> False
+
+columnAverage :: CSVData -> Int -> Maybe Double
+columnAverage rows colIndex
+    | null numericValues = Nothing
+    | otherwise = Just (sum numericValues / fromIntegral (length numericValues))
+  where
+    numericValues = [read cell :: Double | row <- rows, 
+                    colIndex < length row,
+                    let cell = row !! colIndex,
+                    case reads cell :: [(Double, String)] of
+                        [(_, "")] -> True
+                        _ -> False]
+
+processCSVFile :: String -> IO ()
+processCSVFile filename = do
+    content <- readFile filename
+    let parsed = parseCSV content
+    case parsed of
+        [] -> putStrLn "Empty CSV file"
+        (header:dataRows) -> do
+            putStrLn $ "Header: " ++ show header
+            let numericCols = numericColumns parsed
+            putStrLn $ "Numeric columns: " ++ show numericCols
+            mapM_ (\col -> 
+                case columnAverage dataRows col of
+                    Just avg -> putStrLn $ "Column " ++ show col ++ 
+                                " (" ++ (header !! col) ++ 
+                                ") average: " ++ show avg
+                    Nothing -> putStrLn $ "Column " ++ show col ++ 
+                               " has no numeric data"
+                ) numericCols
