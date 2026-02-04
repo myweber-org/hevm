16709b702622190ef128df8a4a0553671effad11
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-04 00:15:49 +0000

    Add word frequency counter with sorting and filtering capabilities

--- test/scripts/WordFrequencyCounter.hs
+++ test/scripts/WordFrequencyCounter.hs
@@ -26,4 +26,41 @@ main = do
     textPieces -> do
       let text = unwords textPieces
       let frequencyMap = countWords text
-      putStrLn $ formatResults frequencyMap
+      putStrLn $ formatResults frequencyMapmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = group $ sort cleanedWords
+    in map (\ws -> (head ws, length ws)) grouped
+
+cleanWord :: String -> String
+cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+
+sortByFrequency :: [WordCount] -> [WordCount]
+sortByFrequency = sortOn (Down . snd)
+
+filterByMinFrequency :: Int -> [WordCount] -> [WordCount]
+filterByMinFrequency minFreq = filter ((>= minFreq) . snd)
+
+getTopNWords :: Int -> [WordCount] -> [WordCount]
+getTopNWords n = take n . sortByFrequency
+
+wordFrequencyReport :: String -> Int -> Int -> [WordCount]
+wordFrequencyReport text minFreq topN =
+    let counts = countWords text
+        filtered = filterByMinFrequency minFreq counts
+    in getTopNWords topN filtered
+
+exampleUsage :: IO ()
+exampleUsage = do
+    let sampleText = "Hello world! Hello Haskell. Haskell is great. World says hello to Haskell."
+    let report = wordFrequencyReport sampleText 1 5
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) report
