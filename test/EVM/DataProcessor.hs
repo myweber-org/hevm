7f10640abd1644f2a10527f6861c2bc318229b37
InkspillVerse <inkspill.verse@outlook.com>
2026-02-04 19:37:00 +0000

    Add function to filter and transform a list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -50,4 +50,17 @@ validateInput xs = if all (>= -100) xs && all (<= 100) xs
                    else Nothing
 
 safeProcess :: [Int] -> Maybe Int
-safeProcess = fmap sumProcessedData . validateInput
+safeProcess = fmap sumProcessedData . validateInputmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let input = [1..10]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
