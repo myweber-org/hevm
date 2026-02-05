4f0dc9f83ce0a4cc17dea16f20c3ecfaad37ec4e
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-05 19:11:58 +0000

    Add function to count word frequencies in a text file

--- doc/WordFrequency.hs
+++ doc/WordFrequency.hs
@@ -347,4 +347,27 @@ main = do
     content <- getContents
     let histogram = analyzeText content
     putStrLn "\nTop 10 most frequent words:"
-    putStrLn $ displayHistogram histogram
+    putStrLn $ displayHistogram histogrammodule WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlphaNum
+
+getTopWords :: Int -> String -> [(String, Int)]
+getTopWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+processFile :: FilePath -> IO ()
+processFile path = do
+  content <- readFile path
+  let topWords = getTopWords 10 content
+  putStrLn "Top 10 most frequent words:"
+  mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
