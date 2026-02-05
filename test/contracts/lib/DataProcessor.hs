43f57150aed622d54a4471e1b535a223bdcdbcde
InkspillVerse <inkspill.verse@outlook.com>
2026-02-05 03:05:04 +0000

    Add function to filter and transform list of integers

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -1,160 +1,18 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (>= -100) xs && all (<= 100) xs
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5, -8]
-    if validateInput sampleData
-        then print $ processData sampleData
-        else putStrLn "Invalid input data"
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessed :: (Int -> Int) -> [Int] -> Int
-sumProcessed f = sum . map f
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original: " ++ show numbers
-    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
-    putStrLn $ "Sum of even squares: " ++ show (sumProcessed (\x -> x * x) (filter even numbers))
-module DataProcessor where
-
-import Data.Time
-import Data.Time.Format
-import Data.List
-import Data.Maybe
-import Text.Read
-
-type CSVRow = [String]
-type DateRange = (Day, Day)
-
-parseDate :: String -> Maybe Day
-parseDate str = parseTimeM True defaultTimeLocale "%Y-%m-%d" str
-
-filterByDateRange :: DateRange -> CSVRow -> Bool
-filterByDateRange (startDate, endDate) row
-    | length row < 3 = False
-    | otherwise = case parseDate (row !! 2) of
-        Just date -> date >= startDate && date <= endDate
-        Nothing -> False
-
-processCSVData :: DateRange -> [CSVRow] -> [CSVRow]
-processCSVData dateRange = filter (filterByDateRange dateRange)
-
-calculateDailyAverage :: [CSVRow] -> Maybe Double
-calculateDailyAverage rows
-    | null validValues = Nothing
-    | otherwise = Just (sum validValues / fromIntegral (length validValues))
-    where
-        validValues = catMaybes $ map (readMaybe . head) rows
-
-groupByMonth :: [CSVRow] -> [[CSVRow]]
-groupByMonth rows = groupBy sameMonth sortedRows
-    where
-        sortedRows = sortOn (parseDate . (!! 2)) rows
-        sameMonth a b = case (parseDate (a !! 2), parseDate (b !! 2)) of
-            (Just d1, Just d2) -> yearMonth d1 == yearMonth d2
-            _ -> False
-        yearMonth day = (fromIntegral $ year (toGregorian day), month (toGregorian day))module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processData
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = 
-    if all (\x -> x >= -100 && x <= 100) xs
-        then Just xs
-        else Nothingmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * x + 1)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processEvenSquares
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show (processEvenSquares numbers)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+sumProcessed = sum . processNumbers
 
 validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (> -100) xs then Just xs else Nothing
-
-main :: IO ()
-main = do
-    let sampleData = [1, -5, 3, 0, 8, -2]
-    case validateInput sampleData of
-        Just validData -> do
-            putStrLn $ "Original data: " ++ show validData
-            putStrLn $ "Processed data: " ++ show (processData validData)
-            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
-        Nothing -> putStrLn "Input validation failed: values must be greater than -100"
+validateInput xs
+    | null xs = Nothing
+    | any (< -100) xs = Nothing
+    | any (> 100) xs = Nothing
+    | otherwise = Just xs
