de0d56f69e922ae84677893ff7146eafd94f73c7
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-05 19:35:42 +0000

    Add function to filter and transform a list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -61,4 +61,16 @@ safeReadInt s = case reads s of
     _ -> Nothing
 
 filterValidRows :: [[String]] -> [[String]]
-filterValidRows = filter (not . any null)
+filterValidRows = filter (not . any null)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [-3, 1, 0, 5, -2, 8]
+    let result = processNumbers numbers
+    print result
