8be9d1a8370fda26afa7c956ec542b8c4a60ef08
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-05 19:55:44 +0000

    Add function to filter and transform a list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -1,52 +1,8 @@
 
 module DataProcessor where
 
-import Data.List (foldl')
-import Text.Read (readMaybe)
-
-type Row = [String]
-type CSVData = [Row]
-
-parseCSV :: String -> CSVData
-parseCSV content = map (splitOn ',') (lines content)
-  where
-    splitOn :: Char -> String -> [String]
-    splitOn delimiter = foldr splitter [[]]
-      where
-        splitter c (x:xs)
-          | c == delimiter = []:x:xs
-          | otherwise = (c:x):xs
-
-safeReadDouble :: String -> Maybe Double
-safeReadDouble = readMaybe
-
-calculateColumnAverage :: CSVData -> Int -> Maybe Double
-calculateColumnAverage rows columnIndex
-  | null validValues = Nothing
-  | otherwise = Just (sum validValues / fromIntegral (length validValues))
-  where
-    extractValue :: Row -> Maybe Double
-    extractValue row
-      | columnIndex < length row = safeReadDouble (row !! columnIndex)
-      | otherwise = Nothing
-    
-    validValues = [val | Just val <- map extractValue rows]
-
-processCSVFile :: String -> Int -> IO (Maybe Double)
-processCSVFile filePath columnIndex = do
-  content <- readFile filePath
-  let parsedData = parseCSV content
-  return $ calculateColumnAverage parsedData columnIndex
-
-validateCSV :: CSVData -> Bool
-validateCSV [] = True
-validateCSV (row:rows) = all (== length row) (map length rows)module DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
