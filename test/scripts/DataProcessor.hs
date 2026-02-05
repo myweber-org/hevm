3e9046ecb445dbf5e1e9a18275a64577921f3776
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-05 21:11:43 +0000

    Add function to filter and transform a list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -1,36 +1,13 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedList :: [Int] -> Int
-sumProcessedList = sum . processEvenSquares
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let sampleData = [1..10]
-    putStrLn $ "Original list: " ++ show sampleData
-    putStrLn $ "Processed list: " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Sum of processed list: " ++ show (sumProcessedList sampleData)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processEvenSquares
-
-main :: IO ()
-main = do
-    let sampleData = [1..10]
-    putStrLn $ "Original data: " ++ show sampleData
-    putStrLn $ "Processed data: " ++ show (processEvenSquares sampleData)
-    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData sampleData)
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
