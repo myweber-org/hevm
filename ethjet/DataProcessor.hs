6688922419d6397e658982ec4ebe66de567e5c9e
InkspillVerse <inkspill.verse@outlook.com>
2026-02-05 15:10:24 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -309,3 +309,10 @@ validateInput xs =
     if all (\x -> x >= -100 && x <= 100) xs
         then Just xs
         else Nothing
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
