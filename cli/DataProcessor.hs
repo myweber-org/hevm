850921de4c36cf2d3a728b0b3e293c6698622560
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-06 18:48:45 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -234,4 +234,16 @@ processData = uniqueSorted . squareAll . filterOdds
 isPrime :: Int -> Bool
 isPrime n
     | n <= 1 = False
-    | otherwise = null [x | x <- [2..floor (sqrt (fromIntegral n))], n `mod` x == 0]
+    | otherwise = null [x | x <- [2..floor (sqrt (fromIntegral n))], n `mod` x == 0]module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
