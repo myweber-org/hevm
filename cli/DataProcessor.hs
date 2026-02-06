8a749c23905699fedcb85eb3661dcf2d64c215bb
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-06 01:42:53 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -172,4 +172,17 @@ main = do
     putStrLn "Odd numbers incremented:"
     print $ processOddNumbers numbers
     putStrLn "Sum of processed odd numbers:"
-    print $ sumProcessedData odd (+1) numbers
+    print $ sumProcessedData odd (+1) numbersmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show result
