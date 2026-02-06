7844f0187832236a9dc097f7568d65b96b6ac759
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-06 21:41:55 +0000

    Add prime factorization function using trial division

---
+++ test/contracts/fail/PrimeFactors.hs
@@ -0,0 +1,13 @@
+
+module PrimeFactors (primeFactors) where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n
+  | n <= 1    = []
+  | otherwise = factorize n 2
+  where
+    factorize 1 _ = []
+    factorize m p
+      | m < p * p = [m]
+      | m `mod` p == 0 = p : factorize (m `div` p) p
+      | otherwise = factorize m (p + 1)
