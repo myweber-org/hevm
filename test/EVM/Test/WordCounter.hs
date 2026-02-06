75eebe2e2a0f64d86d528f1500855e135149b770
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-06 00:39:44 +0000

    Add a simple word counter function with basic text processing

--- test/EVM/Test/WordCounter.hs
+++ test/EVM/Test/WordCounter.hs
@@ -1,13 +1,30 @@
 module WordCounter where
 
-import Data.Char (isSpace)
-import Data.List (words)
+import Data.Char (isSpace, isPunctuation)
+import Data.List (group, sort)
 
-countWords :: String -> Int
-countWords = length . words
+cleanText :: String -> String
+cleanText = map toLower . filter (not . isPunctuation)
 
-main :: IO ()
-main = do
-    input <- getContents
-    let wordCount = countWords input
-    putStrLn $ "Word count: " ++ show wordCount
+countWords :: String -> [(String, Int)]
+countWords text = 
+    let wordsList = words $ cleanText text
+        sortedWords = sort wordsList
+        groupedWords = group sortedWords
+    in map (\ws -> (head ws, length ws)) groupedWords
+
+mostFrequentWords :: Int -> String -> [(String, Int)]
+mostFrequentWords n text = 
+    take n $ reverse $ sortOn snd (countWords text)
+
+wordFrequency :: String -> String -> Maybe Int
+wordFrequency word text = 
+    lookup (map toLower word) (countWords text)
+
+processText :: String -> IO ()
+processText inputText = do
+    putStrLn "Word frequencies:"
+    mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (countWords inputText)
+    
+    putStrLn "\nTop 5 most frequent words:"
+    mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (mostFrequentWords 5 inputText)
