ef8aaf0fc5fbcedf22d131c3fbf4b5a39cbe7fe6
InkspillVerse <inkspill.verse@outlook.com>
2026-02-07 16:56:14 +0000

    Add function to calculate moving average of a list

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -41,4 +41,27 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+-- Example usage function
+exampleUsage :: IO ()
+exampleUsage = do
+    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    putStrLn "Original data:"
+    print dataSeries
+    putStrLn "\n3-period moving average:"
+    print $ movingAverage 3 dataSeries
+    putStrLn "\n5-period moving average:"
+    print $ movingAverage 5 dataSeries
