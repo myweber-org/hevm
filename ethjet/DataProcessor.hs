3d4f24c1ed5bd59271fda75cecc860ae32a826c9
ElaraWrites <elara.writes@outlook.com>
2026-02-07 00:24:40 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -11,3 +11,20 @@ validateData xs = all (> 0) xs && length xs > 3
 
 combineResults :: [Int] -> [Int] -> [Int]
 combineResults xs ys = zipWith (+) (processData xs) (processData ys)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = 
+    if all (\x -> x >= -100 && x <= 100) xs
+    then Just xs
+    else Nothing
