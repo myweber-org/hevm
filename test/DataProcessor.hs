9cf7de67d88db466597a32210587ac2b316ce2dc
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-07 23:19:19 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -2,39 +2,17 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
 
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbersmodule DataProcessor where
+sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
+sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processDatamodule DataProcessor where
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage windowSize xs
-    | windowSize <= 0 = error "Window size must be positive"
-    | windowSize > length xs = error "Window size exceeds list length"
-    | otherwise = map average $ windows windowSize xs
-  where
-    windows n list = take (length list - n + 1) $ map (take n) (tails list)
-    average vals = sum vals / fromIntegral (length vals)
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Even squares: " ++ show (processEvenSquares sampleData)
+    putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) sampleData)
