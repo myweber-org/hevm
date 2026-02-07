effa261dbea24a1d311420c9b283d8904f37a174
InkspillVerse <inkspill.verse@outlook.com>
2026-02-07 19:27:55 +0000

    Implement Sieve of Eratosthenes for prime number generation up to a given limit

--- src/PrimeSieve.hs
+++ src/PrimeSieve.hs
@@ -3,43 +3,17 @@ module PrimeSieve where
 sieve :: Int -> [Int]
 sieve limit
     | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
+    | otherwise = go [2..limit] []
     where
-        sieve' [] primes = reverse primes
-        sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
+        go []     primes = reverse primes
+        go (x:xs) primes = go (filter (\n -> n `mod` x /= 0) xs) (x:primes)
 
 primesUpTo :: Int -> [Int]
 primesUpTo = sieve
 
 main :: IO ()
 main = do
-    putStrLn "Enter limit:"
+    putStrLn "Enter limit for prime generation:"
     input <- getLine
     let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]module PrimeSieve where
-
-sieve :: [Int] -> [Int]
-sieve [] = []
-sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
-
-primes :: [Int]
-primes = sieve [2..]
-
-getPrimesUpTo :: Int -> [Int]
-getPrimesUpTo n = takeWhile (<= n) primesmodule PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+    print $ primesUpTo limit
