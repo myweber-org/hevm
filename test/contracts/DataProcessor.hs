fa573e943a9bf1535da24934c67c4c9df174a7f6
FelixMakes <felix.makes@hotmail.com>
2026-02-07 15:46:21 +0000

    Add function to filter and transform a list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -54,3 +54,17 @@ processOddCubes = filterAndTransform odd (\x -> x * x * x)
 
 sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
 sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let input = [1..10]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
