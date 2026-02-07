9879d93108a3cb4c1771e61363a32767bcf1daaf
heyQuim <hey.quim@hotmail.com>
2026-02-07 22:32:02 +0000

    Add word frequency counter with sorting and output formatting

--- src/WordFrequency.hs
+++ src/WordFrequency.hs
@@ -105,4 +105,30 @@ analyzeText text minFreq topN =
 -- | Pretty print word frequencies
 printFrequencies :: [WordCount] -> IO ()
 printFrequencies counts = 
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) countsmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map (filter isAlpha . map toLower) $ words text
+        grouped = group $ sort wordsList
+    in map (\ws -> (head ws, length ws)) grouped
+
+sortByFrequency :: [WordCount] -> [WordCount]
+sortByFrequency = sortOn (Down . snd)
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+processText :: String -> String
+processText = formatOutput . sortByFrequency . countWords
+
+main :: IO ()
+main = do
+    input <- getContents
+    putStr $ processText input
