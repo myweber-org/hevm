4cd7d0a565b5da1645465f822ad573bb8bea1c9e
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-07 19:41:48 +0000

    Add function to calculate moving average of a list

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -126,4 +126,24 @@ processCSVData csvContent start end = do
         Right records -> 
             if null records 
             then Left "Empty CSV data"
-            else Right $ filterByDateRange start end (tail records)
+            else Right $ filterByDateRange start end (tail records)module DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+safeMovingAverage :: Int -> [Double] -> Maybe [Double]
+safeMovingAverage n xs
+    | n <= 0 = Nothing
+    | otherwise = Just $ movingAverage n xs
+
+testData :: [Double]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
