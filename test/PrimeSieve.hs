395209bf4f983352578148d42a8e2e7cbfcae836
InkspillVerse <inkspill.verse@outlook.com>
2026-02-07 15:26:42 +0000

    Implement a simple prime number sieve using the Sieve of Eratosthenes algorithm.

--- test/PrimeSieve.hs
+++ test/PrimeSieve.hs
@@ -1,36 +1,16 @@
 module PrimeSieve where
 
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieveHelper [2..limit] []
-
-sieveHelper :: [Int] -> [Int] -> [Int]
-sieveHelper [] primes = reverse primes
-sieveHelper (x:xs) primes = sieveHelper (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit for prime generation:"
-    input <- getLine
-    let limit = read input :: Int
-    let primes = sieve limit
-    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primesmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
+primes :: [Int]
+primes = sieve [2..]
   where
-    sieve' [] primes = reverse primes
-    sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
 
 main :: IO ()
 main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limit
+    putStrLn "First 20 prime numbers:"
+    print $ take 20 primes
+    putStrLn "\nThe 100th prime number is:"
+    print $ nthPrime 100
