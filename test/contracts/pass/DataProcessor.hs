26a1f4d5cc5e0606126e0ad7ca01cd196c20541a
FelixMakes <felix.makes@hotmail.com>
2026-02-07 16:32:43 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -4,33 +4,13 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessed :: (Int -> Int) -> [Int] -> Int
-sumProcessed processor = sum . map processor
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Even squares: " ++ show (processEvenSquares numbers)
-    putStrLn $ "Sum of doubled values: " ++ show (sumProcessed (*2) numbers)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
 
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (>= -1000) xs && all (<= 1000) xs
-                   then Just xs
-                   else Nothing
-
-processValidatedData :: [Int] -> Maybe Int
-processValidatedData = fmap sumProcessedData . validateInput
+combineProcessors :: [Int] -> [Int]
+combineProcessors xs
+    | validateData xs = processData xs
+    | otherwise = []
