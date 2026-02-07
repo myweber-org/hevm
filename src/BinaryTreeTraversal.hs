eaae4b917ac5ffc2af3fd85ad4c88ec60aec5324
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-07 16:36:01 +0000

    Add in-order traversal for binary trees

--- src/BinaryTreeTraversal.hs
+++ src/BinaryTreeTraversal.hs
@@ -69,4 +69,27 @@ sampleTree = Node 1
                     (Node 5 Empty Empty))
                 (Node 3
                     (Node 6 Empty Empty)
+                    Empty)module BinaryTreeTraversal where
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
+
+inorder :: BinaryTree a -> [a]
+inorder Empty = []
+inorder (Node val left right) = inorder left ++ [val] ++ inorder right
+
+preorder :: BinaryTree a -> [a]
+preorder Empty = []
+preorder (Node val left right) = [val] ++ preorder left ++ preorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Empty = []
+postorder (Node val left right) = postorder left ++ postorder right ++ [val]
+
+sampleTree :: BinaryTree Int
+sampleTree = Node 1
+                (Node 2
+                    (Node 4 Empty Empty)
+                    (Node 5 Empty Empty))
+                (Node 3
+                    (Node 6 Empty Empty)
                     Empty)
