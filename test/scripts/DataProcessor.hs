8adb0f63654c458e9fa7cb36fb0170bc35f0836e
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-07 20:37:53 +0000

    Add function to filter and transform a list of integers, extracting even numbers and computing their squares

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -150,4 +150,7 @@ safeMovingAverage n xs
     | otherwise = Just $ movingAverage n xs
 
 testData :: [Double]
-testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]module DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter even
