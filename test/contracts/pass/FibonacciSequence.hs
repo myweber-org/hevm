60094c3f5b282809f43214b756960638677b2ffb
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-08 22:37:47 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/contracts/pass/FibonacciSequence.hs
+++ test/contracts/pass/FibonacciSequence.hs
@@ -1,34 +1,6 @@
-module Fibonacci where
+module FibonacciSequence where
 
-fibonacci :: Int -> Integer
-fibonacci n = fibs !! n
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module Fibonacci where
-
-fibonacci :: Int -> Integer
-fibonacci = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibonacci (n-1) + fibonacci (n-2)
-
-main :: IO ()
-main = do
-    putStrLn "First 20 Fibonacci numbers:"
-    mapM_ (print . fibonacci) [0..19]module FibonacciSequence where
-
-import Data.Function (fix)
-
-fibonacci :: Int -> Integer
-fibonacci n = fibList !! n
-  where
-    fibList = 0 : 1 : zipWith (+) fibList (tail fibList)
-
--- Alternative implementation using fix for memoization
-fibonacci' :: Int -> Integer
-fibonacci' = fix (\rec n -> if n < 2 then fromIntegral n else rec (n-1) + rec (n-2))
-
-main :: IO ()
-main = do
-    putStrLn "First 20 Fibonacci numbers:"
-    mapM_ (print . fibonacci) [0..19]
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
