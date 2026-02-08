5573fefc8c4e92c7989fac498803d81541be22e1
heyQuim <hey.quim@hotmail.com>
2026-02-08 22:08:34 +0000

    Add function to calculate moving average of a list

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -46,4 +46,29 @@ smoothData :: [Double] -> [Double]
 smoothData = movingAverage 3
 
 processDataset :: [Double] -> (Double, Double, Double)
-processDataset dataset = (minimum dataset, maximum dataset, sum dataset / fromIntegral (length dataset))
+processDataset dataset = (minimum dataset, maximum dataset, sum dataset / fromIntegral (length dataset))module DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+-- Example usage
+sampleData :: [Double]
+sampleData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+
+main :: IO ()
+main = do
+    putStrLn "Original data:"
+    print sampleData
+    putStrLn "\n3-point moving average:"
+    print $ movingAverage 3 sampleData
+    putStrLn "\n5-point moving average:"
+    print $ movingAverage 5 sampleData
