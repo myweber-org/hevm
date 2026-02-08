ee643fc37a3b53c918d39d56ffc84abf3bb4f204
ElaraWrites <elara.writes@outlook.com>
2026-02-08 22:50:16 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -43,4 +43,15 @@ exampleUsage = do
   mapM_ print processed
   
   putStrLn "\nValid transformed entries:"
-  mapM_ putStrLn validEntries
+  mapM_ putStrLn validEntriesmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
