0115d3d4e275ef188500b7a352c375470f166454
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-08 15:09:08 +0000

    Add function to filter and transform a list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -21,3 +21,16 @@ main = do
     print testData
     putStrLn "\nSmoothed data (3-point moving average):"
     print $ smoothData testData
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -7]
+    let result = processNumbers input
+    print result
