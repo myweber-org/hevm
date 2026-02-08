179b167903df944b0531d545e56b4ffa531d2d2d
FelixMakes <felix.makes@hotmail.com>
2026-02-08 00:06:55 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -27,4 +27,22 @@ validateInput :: [Int] -> Maybe [Int]
 validateInput xs = 
     if all (\x -> x >= -100 && x <= 100) xs
     then Just xs
-    else Nothing
+    else Nothingmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+main :: IO ()
+main = do
+    let inputData = [1, -2, 3, 0, 5, -10]
+    if validateInput inputData
+        then do
+            let result = processData inputData
+            putStrLn $ "Processed data: " ++ show result
+        else putStrLn "Invalid input data detected"
