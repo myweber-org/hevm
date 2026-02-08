b75dcfd445613b96c3fe00380687c0aeadc701a0
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-08 01:39:54 +0000

    Implement Sieve of Eratosthenes to generate prime numbers up to a given limit

--- doc/PrimeSieve.hs
+++ doc/PrimeSieve.hs
@@ -3,45 +3,15 @@ module PrimeSieve where
 sieve :: Int -> [Int]
 sieve limit
     | limit < 2 = []
-    | otherwise = go [2..limit] []
-  where
-    go []     acc = reverse acc
-    go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-primes :: [Int]
-primes = sieve [2..]
-  where
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
-
-nthPrime :: Int -> Int
-nthPrime n = primes !! (n - 1)
-
-isPrime :: Int -> Bool
-isPrime n
-    | n < 2     = False
-    | otherwise = n == head (dropWhile (< n) primes)module PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
     | otherwise = sieve' [2..limit] []
     where
         sieve' [] primes = reverse primes
-        sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
+        sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
 
 main :: IO ()
 main = do
     putStrLn "Enter limit:"
     input <- getLine
     let limit = read input :: Int
-    print $ sieve limit
+    let primes = sieve limit
+    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primes
