6c164dfe3b6c66999e4ec0228fe3d67fcf986d7a
FelixMakes <felix.makes@hotmail.com>
2026-02-08 23:06:32 +0000

    Add a simple word counting utility with command-line interface

--- test/EVM/Test/WordCounter.hs
+++ test/EVM/Test/WordCounter.hs
@@ -27,4 +27,42 @@ processText inputText = do
     mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (countWords inputText)
     
     putStrLn "\nTop 5 most frequent words:"
-    mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (mostFrequentWords 5 inputText)
+    mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (mostFrequentWords 5 inputText)module WordCounter where
+
+import System.Environment (getArgs)
+import Data.Char (isSpace)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ splitWords text
+        grouped = foldr countWord [] wordsList
+    in sortOn (Down . snd) grouped
+  where
+    splitWords = words . map normalize
+    normalize c = if isSpace c then ' ' else c
+    countWord w [] = [(w, 1)]
+    countWord w ((x, n):xs)
+        | w == x = (x, n+1):xs
+        | otherwise = (x, n):countWord w xs
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = unlines $ map formatLine counts
+  where
+    formatLine (word, count) = word ++ ": " ++ show count
+
+processFile :: FilePath -> IO ()
+processFile filename = do
+    content <- readFile filename
+    let counts = countWords content
+    putStrLn $ formatOutput counts
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [filename] -> processFile filename
+        _ -> putStrLn "Usage: wordcounter <filename>"
