fcdfc95cda5b3ae4efce9b144dfce39743b016f2
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-08 23:43:05 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -1,12 +1,23 @@
+
 module DataProcessor where
 
-processData :: [Int] -> [Int]
-processData = map (^2) . filter even
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
 
-sampleData :: [Int]
-sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+processOddCubes :: [Int] -> [Int]
+processOddCubes = filterAndTransform odd (\x -> x * x * x)
+
+sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
+sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
 
 main :: IO ()
 main = do
-    let result = processData sampleData
-    putStrLn $ "Processed data: " ++ show result
+    let testData = [1..10]
+    putStrLn $ "Original data: " ++ show testData
+    putStrLn $ "Even squares: " ++ show (processEvenSquares testData)
+    putStrLn $ "Odd cubes: " ++ show (processOddCubes testData)
+    putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) testData)
+    putStrLn $ "Sum of odd cubes: " ++ show (sumProcessedData odd (\x -> x * x * x) testData)
