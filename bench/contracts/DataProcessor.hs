df4001049694c77bf646794545a0d8f92f91ba78
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-08 00:46:19 +0000

    Add function to filter and transform a list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -3,107 +3,11 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
-    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
-module DataProcessor where
-
-import Data.List (intercalate)
-import Data.Char (isDigit, isAlpha)
-
-type ValidationRule = String -> Bool
-type Transformation = String -> String
-
-validateNumeric :: ValidationRule
-validateNumeric = all isDigit
-
-validateAlpha :: ValidationRule
-validateAlpha = all isAlpha
-
-validateLength :: Int -> ValidationRule
-validateLength n s = length s == n
-
-transformUppercase :: Transformation
-transformUppercase = map toUpper
-
-transformPadLeft :: Int -> Char -> Transformation
-transformPadLeft n c s = replicate (n - length s) c ++ s
-
-processCSVRow :: [ValidationRule] -> [Transformation] -> [String] -> Either String [String]
-processCSVRow validators transformers row
-    | length row /= length validators = Left "Column count mismatch"
-    | any (== False) validResults = Left $ "Validation failed: " ++ intercalate ", " errors
-    | otherwise = Right transformed
-  where
-    validResults = zipWith ($) validators row
-    errors = [show i ++ ":" ++ r | (i, (r, False)) <- zip [1..] (zip row validResults)]
-    transformed = zipWith ($) transformers row
-
-safeReadInt :: String -> Maybe Int
-safeReadInt s = case reads s of
-    [(n, "")] -> Just n
-    _ -> Nothing
-
-filterValidRows :: [[String]] -> [[String]]
-filterValidRows = filter (not . any null)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
+    let numbers = [-5, 3, 0, 8, -2, 10]
     let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processNumbers input
     print result
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transform = map transform . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * 2 + 1)
