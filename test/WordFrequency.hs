a84ce76b8234556f06c328b07836d791144d5bb1
heyQuim <hey.quim@hotmail.com>
2026-02-09 16:13:04 +0000

    Add word frequency counting utility with histogram output

--- test/WordFrequency.hs
+++ test/WordFrequency.hs
@@ -21,4 +21,21 @@ analyzeText text = do
   putStrLn "Top 10 most frequent words:"
   mapM_ printWord $ topWords 10 text
   where
-    printWord (word, count) = putStrLn $ word ++ ": " ++ show count
+    printWord (word, count) = putStrLn $ word ++ ": " ++ show countmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type Histogram = [(String, Int)]
+
+countWords :: String -> Histogram
+countWords text = 
+    let wordsList = filter (not . null) $ map (filter isAlpha . map toLower) $ words text
+        grouped = group $ sort wordsList
+    in sortOn (Down . snd) $ map (\ws -> (head ws, length ws)) grouped
+
+printHistogram :: Histogram -> IO ()
+printHistogram hist = mapM_ printEntry hist
+  where
+    printEntry (word, count) = putStrLn $ word ++ ": " ++ replicate count '*'
