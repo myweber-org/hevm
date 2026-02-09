80d92b146e0b8fae5b9ed9bf8799bfb313ad165d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-09 20:48:10 +0000

    Add function to count word frequencies in a text

--- doc/theme/WordFrequency.hs
+++ doc/theme/WordFrequency.hs
@@ -21,4 +21,34 @@ analyzeText text = do
   putStrLn "Top 10 most frequent words:"
   mapM_ printWord (topWords 10 text)
   where
-    printWord (word, count) = putStrLn $ word ++ ": " ++ show count
+    printWord (word, count) = putStrLn $ word ++ ": " ++ show countmodule WordFrequency where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map toLower <$> wordsList
+        freqMap = foldr (\w m -> insertWord w m) [] cleaned
+    in take 10 $ sortOn (Down . snd) freqMap
+  where
+    cleanWord = filter (\c -> isAlphaNum c || c == '\'')
+    
+    insertWord :: String -> [WordCount] -> [WordCount]
+    insertWord w [] = [(w, 1)]
+    insertWord w ((word, count):rest)
+        | w == word = (word, count + 1) : rest
+        | otherwise = (word, count) : insertWord w rest
+
+displayCounts :: [WordCount] -> String
+displayCounts counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+
+main :: IO ()
+main = do
+    let sampleText = "Hello world! This is a test. Hello again, world."
+    putStrLn "Top 10 most frequent words:"
+    putStrLn $ displayCounts $ countWords sampleText
