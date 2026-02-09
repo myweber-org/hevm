9e64356caf6dec7e621c718b2319a18b16fdb096
ElaraWrites <elara.writes@outlook.com>
2026-02-09 14:32:04 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -1,23 +1,11 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
-processOddCubes :: [Int] -> [Int]
-processOddCubes = filterAndTransform odd (\x -> x * x * x)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
-
-main :: IO ()
-main = do
-    let testData = [1..10]
-    putStrLn $ "Original data: " ++ show testData
-    putStrLn $ "Even squares: " ++ show (processEvenSquares testData)
-    putStrLn $ "Odd cubes: " ++ show (processOddCubes testData)
-    putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) testData)
-    putStrLn $ "Sum of odd cubes: " ++ show (sumProcessedData odd (\x -> x * x * x) testData)
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
