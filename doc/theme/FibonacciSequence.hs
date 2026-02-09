77256da9d90a45412e3d365affbd95528442e3cb
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-09 19:42:09 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -1,12 +1,10 @@
+
 module FibonacciSequence where
 
 fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
