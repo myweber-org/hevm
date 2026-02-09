df6b422409bc75f383e976bc85a2f88d609d6474
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-09 15:15:27 +0000

    Add word frequency counter module with basic text processing functions

--- src/EVM/WordFrequencyCounter.hs
+++ src/EVM/WordFrequencyCounter.hs
@@ -98,4 +98,38 @@ formatResults counts =
     unlines (map (\(word, count) -> word ++ ": " ++ show count) counts)
 
 processText :: String -> String
-processText = formatResults . countWords
+processText = formatResults . countWordsmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = foldr countWord [] cleanedWords
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    
+    countWord :: String -> [WordCount] -> [WordCount]
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+topNWords :: Int -> String -> [WordCount]
+topNWords n text = take n $ countWords text
+
+totalWords :: String -> Int
+totalWords text = length $ filter (not . null) $ map cleanWord $ words text
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+
+wordFrequency :: String -> String -> Maybe Int
+wordFrequency target text = 
+    let counts = countWords text
+    in lookup (map toLower target) counts
