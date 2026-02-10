1377ac25c4a267df2ac915209e1d397b5c162e35
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-10 16:12:08 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -148,3 +148,17 @@ main = do
     putStrLn $ "Processed list: " ++ show (processNumbers numbers)
     putStrLn $ "Sum of positive doubles: " ++ show (sumPositiveDoubles numbers)
     putStrLn $ "First element: " ++ show (safeHead numbers)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
