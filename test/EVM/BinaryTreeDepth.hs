79e41781ca859b624c3faebba43ce9079f6de1bb
heyQuim <hey.quim@hotmail.com>
2026-02-10 22:25:58 +0000

    Add function to calculate depth of a binary tree

--- test/EVM/BinaryTreeDepth.hs
+++ test/EVM/BinaryTreeDepth.hs
@@ -19,4 +19,13 @@ treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data B
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Leaf = 0
-treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)
+treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)module BinaryTreeDepth where
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
+
+treeDepth :: BinaryTree a -> Int
+treeDepth Empty = 0
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
+
+sampleTree :: BinaryTree Int
+sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty Empty)
