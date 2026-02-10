9a32ccd63156cc8463c88f47b7ac6dfbd9a65567
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-10 21:15:08 +0000

    Add function to filter and transform a list of integers, extracting even numbers and computing their squares

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -107,4 +107,7 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processNumbers = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter even
