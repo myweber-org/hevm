0a747ad11a6dc6b5712bbfdeea8fc97a3382041e
FelixMakes <felix.makes@hotmail.com>
2026-02-10 00:47:05 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -17,4 +17,21 @@ calculateAverages rows =
     transpose xs = map head xs : transpose (map tail xs)
 
 processCSVData :: String -> [Double]
-processCSVData = calculateAverages . parseCSV
+processCSVData = calculateAverages . parseCSVmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (\x -> x >= -100 && x <= 100) xs
+                   then Just xs
+                   else Nothing
+
+processValidated :: [Int] -> Maybe Int
+processValidated xs = sumProcessed <$> validateInput xs
