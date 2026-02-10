5e3966d58439f70a79dd3381e99c3e32f2211505
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-10 19:51:28 +0000

    Add a simple word counting utility for text analysis

--- doc/src/code_examples/WordCounter.hs
+++ doc/src/code_examples/WordCounter.hs
@@ -1,13 +1,27 @@
+
 module WordCounter where
 
 import Data.Char (isSpace)
 import Data.List (group, sort)
 
 countWords :: String -> [(String, Int)]
-countWords = map (\xs -> (head xs, length xs)) . group . sort . words . filter (not . isSpace)
+countWords = map (\xs -> (head xs, length xs)) 
+           . group 
+           . sort 
+           . words 
+           . map toLower
+           . filter (\c -> not (isSpace c) || c == ' ')
 
-displayCounts :: [(String, Int)] -> String
-displayCounts = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+toLower :: Char -> Char
+toLower c
+    | c >= 'A' && c <= 'Z' = toEnum (fromEnum c + 32)
+    | otherwise = c
 
-processText :: String -> String
-processText = displayCounts . countWords
+mostFrequent :: String -> (String, Int)
+mostFrequent text = 
+    case countWords text of
+        [] -> ("", 0)
+        ws -> maximumBy (compare `on` snd) ws
+
+uniqueWords :: String -> Int
+uniqueWords = length . countWords
