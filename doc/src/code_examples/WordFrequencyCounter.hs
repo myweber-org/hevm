e72630284cb389a61c3e8e5cda52f2f62d3fd0e2
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-10 14:42:43 +0000

    Add word frequency counter with sorting and output formatting

--- doc/src/code_examples/WordFrequencyCounter.hs
+++ doc/src/code_examples/WordFrequencyCounter.hs
@@ -26,4 +26,39 @@ main = do
   args <- getArgs
   case args of
     [] -> putStrLn "Please provide text as argument"
-    textArgs -> putStrLn $ processText $ unwords textArgs
+    textArgs -> putStrLn $ processText $ unwords textArgsmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = map (map toLower) wordsList
+        wordMap = foldl (\acc word -> insertWord word acc) [] cleanedWords
+    in sortOn (Down . snd) wordMap
+  where
+    cleanWord = filter isAlpha
+    
+    insertWord :: String -> [WordCount] -> [WordCount]
+    insertWord word [] = [(word, 1)]
+    insertWord word ((w, c):rest)
+        | word == w = (w, c + 1) : rest
+        | otherwise = (w, c) : insertWord word rest
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+processText :: String -> String
+processText = formatOutput . countWords
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze word frequency:"
+    input <- getContents
+    putStrLn "\nWord frequencies:"
+    putStrLn $ processText input
