68af76c2be090949bbd08c1a5412553d46b33c62
FelixMakes <felix.makes@hotmail.com>
2026-02-10 15:57:01 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -123,3 +123,19 @@ main = do
     putStrLn $ "Original list: " ++ show numbers
     putStrLn $ "Processed list: " ++ show (processNumbers numbers)
     putStrLn $ "Sum of positive doubled numbers: " ++ show (sumPositiveDoubled numbers)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = 
+    filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+safeHeadProcessed :: [Int] -> Maybe Int
+safeHeadProcessed [] = Nothing
+safeHeadProcessed xs = Just (head (processNumbers xs))
