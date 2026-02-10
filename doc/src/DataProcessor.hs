4e8bd02b099a5795e44d310c350298481bbeb5f5
InkspillVerse <inkspill.verse@outlook.com>
2026-02-10 18:54:26 +0000

    Add function to filter and transform a list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -1,23 +1,4 @@
-
 module DataProcessor where
-
-import Data.List.Split (splitOn)
-
-parseCSV :: String -> [[Double]]
-parseCSV content = map (map read . splitOn ",") (lines content)
-
-calculateAverages :: [[Double]] -> [Double]
-calculateAverages rows = 
-    if null rows then []
-    else map (\col -> sum col / fromIntegral (length col)) (transpose rows)
-  where
-    transpose :: [[Double]] -> [[Double]]
-    transpose [] = []
-    transpose ([]:_) = []
-    transpose xs = map head xs : transpose (map tail xs)
-
-processCSVData :: String -> [Double]
-processCSVData = calculateAverages . parseCSVmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
@@ -27,23 +8,3 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (\x -> x >= -100 && x <= 100) xs
-                   then Just xs
-                   else Nothing
-
-processValidated :: [Int] -> Maybe Int
-processValidated xs = sumProcessed <$> validateInput xsmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print result
