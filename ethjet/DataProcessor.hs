9224670e9d199cc2005a23b12f4f0d0c399077fc
InkspillVerse <inkspill.verse@outlook.com>
2026-02-11 23:13:23 +0000

    Add function to calculate moving average of a list

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,24 +1,24 @@
-
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+import Data.List (tails)
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * x + 1)
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map avg $ filter (\w -> length w == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
+
+-- Example usage with test data
+testData :: [Double]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
 
 main :: IO ()
 main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+    putStrLn "Original data:"
+    print testData
+    putStrLn "\n3-period moving average:"
+    print $ movingAverage 3 testData
+    putStrLn "\n5-period moving average:"
+    print $ movingAverage 5 testData
