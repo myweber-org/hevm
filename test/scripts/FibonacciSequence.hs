fc9589aea2a8c3d7e6fd6bae6fb8aa329274ce20
FelixMakes <felix.makes@hotmail.com>
2026-02-11 00:31:56 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/scripts/FibonacciSequence.hs
+++ test/scripts/FibonacciSequence.hs
@@ -1,34 +1,20 @@
 module FibonacciSequence where
 
-fib :: Int -> Integer
-fib n = fibMemo !! n
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
   where
-    fibMemo = map fib' [0..]
-    fib' 0 = 0
-    fib' 1 = 1
-    fib' i = fibMemo !! (i - 1) + fibMemo !! (i - 2)module Fibonacci where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-import Data.Map (Map)
-import qualified Data.Map as Map
-
-fibonacci :: Int -> Integer
-fibonacci n = fst (fibMemo n)
-
-fibMemo :: Int -> (Integer, Map Int Integer)
-fibMemo n = go n Map.empty
-  where
-    go 0 m = (0, Map.insert 0 0 m)
-    go 1 m = (1, Map.insert 1 1 m)
-    go k m =
-      case Map.lookup k m of
-        Just val -> (val, m)
-        Nothing ->
-          let (a, m1) = go (k - 1) m
-              (b, m2) = go (k - 2) m1
-              sumVal = a + b
-          in (sumVal, Map.insert k sumVal m2)
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+    putStrLn $ "First " ++ show n ++ " Fibonacci numbers:"
+    mapM_ (putStrLn . show) (fibonacci n)
 
 main :: IO ()
 main = do
-  putStrLn "First 20 Fibonacci numbers:"
-  mapM_ (putStrLn . show . fibonacci) [0..19]
+    putStr "Enter number of terms: "
+    input <- getLine
+    let n = read input :: Int
+    if n > 0
+        then printFibonacci n
+        else putStrLn "Please enter a positive integer."
