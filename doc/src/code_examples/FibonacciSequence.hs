e70fc3696696dc67fa2ce8e933b42aec6c910946
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-11 22:02:00 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- doc/src/code_examples/FibonacciSequence.hs
+++ doc/src/code_examples/FibonacciSequence.hs
@@ -1,81 +1,13 @@
 module FibonacciSequence where
 
-import Data.Function (fix)
-
 fibonacci :: Int -> Integer
 fibonacci = (map fib [0..] !!)
   where
     fib 0 = 0
     fib 1 = 1
     fib n = fibonacci (n - 1) + fibonacci (n - 2)
-
--- More efficient version using memoization with fixpoint combinator
-fibonacciMemoized :: Int -> Integer
-fibonacciMemoized = fix memoFib
-  where
-    memoFib _ 0 = 0
-    memoFib _ 1 = 1
-    memoFib f n = f (n - 1) + f (n - 2)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-import Data.Function (fix)
-
-fibonacci :: Int -> Integer
-fibonacci = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibonacci (n - 1) + fibonacci (n - 2)
-
--- More efficient version using memoization with fixpoint combinator
-fibonacciMemoized :: Int -> Integer
-fibonacciMemoized = fix memoFib
-  where
-    memoFib _ 0 = 0
-    memoFib _ 1 = 1
-    memoFib f n = f (n - 1) + f (n - 2)module FibonacciSequence where
-
-import Data.Function (fix)
-
-fibMemo :: Int -> Integer
-fibMemo = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibMemo (n - 1) + fibMemo (n - 2)
 
 main :: IO ()
 main = do
-    putStrLn "Fibonacci numbers from 0 to 20:"
-    mapM_ (print . fibMemo) [0..20]module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-  putStrLn "Fibonacci sequence up to 10 terms:"
-  print (fibonacci 10)module Fibonacci where
-
-import Data.Function (fix)
-
-fibonacci :: Int -> Integer
-fibonacci = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibonacci (n - 1) + fibonacci (n - 2)
-
--- More efficient version using memoization with fixpoint combinator
-fibonacciMemoized :: Int -> Integer
-fibonacciMemoized = fix memoFib
-  where
-    memoFib _ 0 = 0
-    memoFib _ 1 = 1
-    memoFib f n = f (n - 1) + f (n - 2)
+    putStrLn "Fibonacci sequence up to 10:"
+    mapM_ (print . fibonacci) [0..10]
