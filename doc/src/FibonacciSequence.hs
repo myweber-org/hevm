0d693b5cb95ece5bdc1c9a5e3ff4dbe8286113ae
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-11 19:57:43 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/src/FibonacciSequence.hs
+++ doc/src/FibonacciSequence.hs
@@ -13,3 +13,9 @@ main :: IO ()
 main = do
     putStrLn "Fibonacci numbers from 0 to 10:"
     mapM_ (print . fib) [0..10]
+module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
