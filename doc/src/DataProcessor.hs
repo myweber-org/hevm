f8725ab3fccc47b66a5d2913e0a31f564bb1e3bb
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-11 23:22:24 +0000

    Add data processing module with validation and transformation functions

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -7,4 +7,31 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+import Data.Char (toUpper)
+
+data ValidationError = InvalidLength | InvalidFormat | EmptyInput
+    deriving (Show, Eq)
+
+validateInput :: String -> Either ValidationError String
+validateInput "" = Left EmptyInput
+validateInput s
+    | length s < 3 = Left InvalidLength
+    | not (all isAlpha s) = Left InvalidFormat
+    | otherwise = Right s
+    where
+        isAlpha c = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+
+transformData :: String -> String
+transformData = map toUpper
+
+processData :: String -> Either ValidationError String
+processData input = do
+    validated <- validateInput input
+    return $ transformData validated
+
+safeProcess :: String -> String
+safeProcess input = case processData input of
+    Left err -> "Error: " ++ show err
+    Right result -> "Processed: " ++ result
