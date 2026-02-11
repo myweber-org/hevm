acab54a46b6a38d41230faec56ba69d28814c857
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-11 15:01:24 +0000

    Implement binary search tree insertion and in-order traversal

--- test/BinarySearchTree.hs
+++ test/BinarySearchTree.hs
@@ -1,41 +1,17 @@
-
 module BinarySearchTree where
 
-data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
 
-insert :: Ord a => a -> BST a -> BST a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
+insert :: Ord a => a -> Tree a -> Tree a
+insert x Empty = Node x Empty Empty
+insert x (Node y left right)
+    | x < y     = Node y (insert x left) right
+    | x > y     = Node y left (insert x right)
+    | otherwise = Node y left right
 
-inOrder :: BST a -> [a]
+inOrder :: Tree a -> [a]
 inOrder Empty = []
-inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
+inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
 
-fromList :: Ord a => [a] -> BST a
+fromList :: Ord a => [a] -> Tree a
 fromList = foldr insert Empty
-
-toSortedList :: BST a -> [a]
-toSortedList = inOrder
-module BinarySearchTree where
-
-data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
-
-insert :: Ord a => a -> BST a -> BST a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
-
-inorder :: BST a -> [a]
-inorder Empty = []
-inorder (Node left val right) = inorder left ++ [val] ++ inorder right
-
-fromList :: Ord a => [a] -> BST a
-fromList = foldr insert Empty
-
-toSortedList :: BST a -> [a]
-toSortedList = inorder
