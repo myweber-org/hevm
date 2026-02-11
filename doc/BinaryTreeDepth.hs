8f8bb6fce639d8807c6a67aa87b74f539334fbea
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-11 20:16:09 +0000

    Add function to calculate depth of a binary tree

--- doc/BinaryTreeDepth.hs
+++ doc/BinaryTreeDepth.hs
@@ -47,4 +47,10 @@ main = do
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)module BinaryTreeDepth where
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
+
+treeDepth :: BinaryTree a -> Int
+treeDepth Empty = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
