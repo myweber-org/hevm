47e1961ed9a326944138a7dedddf57e5522f729c
FelixMakes <felix.makes@hotmail.com>
2026-02-12 15:46:57 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -49,3 +49,26 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -1000) xs then Just xs else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, 0, 5, -8]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original data: " ++ show validData
+            putStrLn $ "Processed data: " ++ show (processData validData)
+            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
+        Nothing -> putStrLn "Input validation failed: values must be greater than -1000"
