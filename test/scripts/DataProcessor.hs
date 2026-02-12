824551c63bdd3911fae2499bdb7766b532a7ead1
VioletInquiry <violet.inquiry@outlook.com>
2026-02-12 23:22:00 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -161,3 +161,13 @@ sumProcessedData predicate transformer =
 validateInput :: [Int] -> Maybe [Int]
 validateInput [] = Nothing
 validateInput xs = Just xs
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
