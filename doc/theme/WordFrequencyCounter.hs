fbec6c90206364053456f321902ed1163bf1aca1
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-12 20:31:33 +0000

    Add word frequency counter with sorting and output formatting

--- doc/theme/WordFrequencyCounter.hs
+++ doc/theme/WordFrequencyCounter.hs
@@ -328,4 +328,35 @@ sampleText = "Hello world! Hello Haskell. Haskell is great. World says hello to 
 main :: IO ()
 main = do
     putStrLn "Word Frequency Analysis:"
-    putStrLn $ wordFrequencyReport sampleText 2 5
+    putStrLn $ wordFrequencyReport sampleText 2 5module WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map toLower <$> wordsList
+        counts = foldl incrementCount [] cleaned
+    in sortOn (Down . snd) counts
+  where
+    cleanWord = filter isAlpha
+    incrementCount [] word = [(word, 1)]
+    incrementCount ((w, c):rest) word
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : incrementCount rest word
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze word frequency:"
+    input <- getContents
+    let frequencies = countWords input
+    putStrLn "\nWord frequencies (sorted by count):"
+    putStrLn $ formatOutput frequencies
