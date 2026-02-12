59145d17d8a24d3906cb55d21a84aee4ccf9fe35
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-12 22:20:42 +0000

    Add a simple word counter module with a function to count words in a string

--- ethjet/WordCounter.hs
+++ ethjet/WordCounter.hs
@@ -14,4 +14,22 @@ testCountWords = and
     , countWords "  hello   world  " == 2
     , countWords "multiple   spaces   between" == 3
     , countWords "line1\nline2\nline3" == 3
-    ]
+    ]module WordCounter where
+
+import Data.Char (isSpace)
+
+-- | Counts the number of words in a given string.
+-- Words are defined as sequences of characters separated by whitespace.
+countWords :: String -> Int
+countWords = length . words
+
+-- | A more explicit version of countWords for demonstration.
+countWordsExplicit :: String -> Int
+countWordsExplicit str = go str False 0
+  where
+    go :: String -> Bool -> Int -> Int
+    go [] inWord count = if inWord then count + 1 else count
+    go (c:cs) inWord count
+        | isSpace c = go cs False count
+        | not inWord = go cs True (count + 1)
+        | otherwise = go cs inWord count
