88bd67ca9cdd7b83d0cb5c32c9690e6d99ec377f
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-12 19:49:07 +0000

    Add function to parse CSV data and calculate average values

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -19,3 +19,48 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+import Data.List (foldl')
+import Text.Read (readMaybe)
+
+type Row = [String]
+type CSVData = [Row]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ',') (lines content)
+  where
+    splitOn :: Char -> String -> [String]
+    splitOn delimiter = foldr splitHelper [""]
+      where
+        splitHelper :: Char -> [String] -> [String]
+        splitHelper c (x:xs)
+          | c == delimiter = "":x:xs
+          | otherwise = (c:x):xs
+        splitHelper _ [] = []
+
+calculateColumnAverage :: CSVData -> Int -> Maybe Double
+calculateColumnAverage rows columnIndex
+  | null validNumbers = Nothing
+  | otherwise = Just (sum validNumbers / fromIntegral (length validNumbers))
+  where
+    extractNumber :: Row -> Maybe Double
+    extractNumber row
+      | columnIndex < length row = readMaybe (row !! columnIndex)
+      | otherwise = Nothing
+    
+    validNumbers = [x | Just x <- map extractNumber rows]
+
+processCSVFile :: String -> Int -> IO (Maybe Double)
+processCSVFile filePath columnIndex = do
+  content <- readFile filePath
+  let parsedData = parseCSV content
+  return $ calculateColumnAverage parsedData columnIndex
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+validateCSV :: CSVData -> Bool
+validateCSV [] = True
+validateCSV (row:rows) = all (\r -> length r == length row) rows
