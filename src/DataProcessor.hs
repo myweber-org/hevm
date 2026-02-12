0eeea39f45b39c323639587e91d3ecfbdd38ad73
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-12 20:48:34 +0000

    Add function to filter and transform a list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -63,4 +63,17 @@ safeHead (x:_) = Just x
 
 validateCSV :: CSVData -> Bool
 validateCSV [] = True
-validateCSV (row:rows) = all (\r -> length r == length row) rows
+validateCSV (row:rows) = all (\r -> length r == length row) rowsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let input = [1..10]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
