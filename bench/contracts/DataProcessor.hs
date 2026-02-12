23f20b152cb749d32026165d4e7a52312e338f2c
heyQuim <hey.quim@hotmail.com>
2026-02-12 22:40:42 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -60,3 +60,20 @@ calculateColumnAverage (header:rows) colIndex = do
 
 formatCSVOutput :: CSVData -> String
 formatCSVOutput = intercalate "\n" . map (intercalate ",")
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processEvenSquares
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Processed data: " ++ show (processEvenSquares sampleData)
+    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData sampleData)
