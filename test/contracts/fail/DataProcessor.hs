c60e0dcbea9dd5c293fa715ede8f7d30b0933968
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-12 19:08:44 +0000

    Add function to filter and transform a list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -3,43 +3,5 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbersmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print result
-module DataProcessor where
-
-import Data.Time
-import Text.CSV
-
-filterCSVByDate :: String -> Day -> Day -> Either String [Record]
-filterCSVByDate csvContent startDate endDate = do
-    csv <- parseCSV "input" csvContent
-    let filtered = filter (isWithinDateRange startDate endDate) csv
-    return filtered
-
-isWithinDateRange :: Day -> Day -> Record -> Bool
-isWithinDateRange start end record =
-    case record of
-        (dateStr:_) -> 
-            case parseTimeM True defaultTimeLocale "%Y-%m-%d" dateStr of
-                Just date -> date >= start && date <= end
-                Nothing -> False
-        _ -> False
-
-processData :: [Record] -> [String]
-processData records = 
-    map (\(date:value:_) -> date ++ "," ++ value) records
