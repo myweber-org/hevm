3efd60ae615f595efc366060fff0f65d6d57e54a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-12 23:04:18 +0000

    Add function to filter and transform a list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -138,4 +138,10 @@ processCSVData header rows = (validRows, errorReport)
                   else "Validation errors:\n" ++ formatErrors errorsmodule DataProcessor where
 
 processData :: [Int] -> [Int]
-processData = map (^2) . filter even
+processData = map (^2) . filter evenmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
