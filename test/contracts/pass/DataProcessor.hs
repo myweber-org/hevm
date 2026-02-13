0cd99a39c37577fcc2b0ab696febba6cf0e7366a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-13 01:05:04 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -74,3 +74,18 @@ main = do
     case parseCSV csvContent >>= processCSVData of
         Left err -> putStrLn $ "Error: " ++ err
         Right results -> putStrLn $ formatResults results
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles xs = sum (processNumbers xs)
