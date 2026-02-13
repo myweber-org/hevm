042f99a8d3c7855847b49c205b1e0ddde043c9f2
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-13 20:40:59 +0000

    Implement a simple prime number sieve using the Sieve of Eratosthenes algorithm.

--- doc/theme/PrimeSieve.hs
+++ doc/theme/PrimeSieve.hs
@@ -51,4 +51,19 @@ main = do
     putStrLn "Enter limit:"
     input <- getLine
     let limit = read input :: Int
-    print $ primesUpTo limit
+    print $ primesUpTo limitmodule PrimeSieve where
+
+primes :: [Int]
+primes = sieve [2..]
+  where
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 prime numbers:"
+    print $ take 20 primes
+    putStrLn "\nThe 100th prime number is:"
+    print $ nthPrime 100
