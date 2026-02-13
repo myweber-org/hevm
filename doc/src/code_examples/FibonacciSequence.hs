ef5d5aa15884e368268b8c956f1aa13c5cd6c22c
FelixMakes <felix.makes@hotmail.com>
2026-02-13 22:25:31 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/src/code_examples/FibonacciSequence.hs
+++ doc/src/code_examples/FibonacciSequence.hs
@@ -12,4 +12,17 @@ fib = memoize fib'
 main :: IO ()
 main = do
     putStrLn "Fibonacci numbers from 0 to 20:"
-    mapM_ (print . fib) [0..20]
+    mapM_ (print . fib) [0..20]module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
+
+main :: IO ()
+main = do
+    putStrLn "Fibonacci sequence up to 10 terms:"
+    print $ fibonacci 10
