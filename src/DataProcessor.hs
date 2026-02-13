86df6aca45cfcbfe7d374a28a1d87f984be74d0b
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-13 14:26:39 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives.

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -12,3 +12,7 @@ main = do
     let numbers = [-5, 3, 0, 8, -2, 10]
     let result = processNumbers numbers
     print result
+module DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter (>0)
