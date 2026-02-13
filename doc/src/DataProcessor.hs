276852f7838726791aa25f28d34b527d2c1b7033
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-13 16:49:33 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -52,3 +52,13 @@ movingAverage n xs
 tails :: [a] -> [[a]]
 tails [] = [[]]
 tails xs@(_:ys) = xs : tails ys
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
