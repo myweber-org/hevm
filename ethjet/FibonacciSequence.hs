cad5aff9e07f800a8dd5fd4c3992b67b8a996373
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-13 16:06:41 +0000

    Add a function to generate the nth Fibonacci number using a fast doubling algorithm

--- ethjet/FibonacciSequence.hs
+++ ethjet/FibonacciSequence.hs
@@ -1,39 +1,17 @@
 module Fibonacci where
 
-fib :: Int -> Integer
-fib n = fibs !! n
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-  putStrLn "Fibonacci sequence up to 10 terms:"
-  print $ fibonacci 10module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
+fibonacci :: Integer -> Integer
 fibonacci n
-    | n <= 0    = []
-    | n == 1    = [0]
-    | n == 2    = [0, 1]
-    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-                  in take n fibs
-
-main :: IO ()
-main = do
-    putStrLn "Fibonacci sequence up to 10 terms:"
-    print $ fibonacci 10module FibonacciSequence where
-
-fibonacci :: Int -> [Int]
-fibonacci n = take n fibs
+    | n < 0 = error "Negative input not allowed"
+    | otherwise = fst (fibPair n)
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+    fibPair :: Integer -> (Integer, Integer)
+    fibPair 0 = (0, 1)
+    fibPair k
+        | even k = (a, b)
+        | otherwise = (b, a + b)
+      where
+        (a, b) = fibPair (k `div` 2)
+        c = a * (b * 2 - a)
+        d = a * a + b * b
+        (a, b) = if even k then (c, d) else (d, c + d)
