45faae22f1278f030625c50eac35675eaf9600eb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-13 18:47:35 +0000

    Add function to filter and transform a list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -59,3 +59,13 @@ tails (x:xs) = (x:xs) : tails xsmodule DataProcessor where
 
 processData :: [Int] -> [Int]
 processData = map (^2) . filter even
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
