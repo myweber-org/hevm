0ca28f0a3df471ed042051194f1de4496e852143
ElaraWrites <elara.writes@outlook.com>
2026-02-13 15:36:24 +0000

    Add function to calculate moving average of a list

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -34,4 +34,21 @@ processData input = do
 safeProcess :: String -> String
 safeProcess input = case processData input of
     Left err -> "Error: " ++ show err
-    Right result -> "Processed: " ++ result
+    Right result -> "Processed: " ++ resultmodule DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . tails
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+-- Helper function from Data.List
+tails :: [a] -> [[a]]
+tails [] = [[]]
+tails xs@(_:ys) = xs : tails ys
