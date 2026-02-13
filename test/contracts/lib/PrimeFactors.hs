f51ea6bf49dc10286f23b6b5cbd92cbb839d64cb
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-13 15:30:32 +0000

    Add function to compute prime factors of an integer

--- test/contracts/lib/PrimeFactors.hs
+++ test/contracts/lib/PrimeFactors.hs
@@ -1,13 +1,3 @@
-module PrimeFactors where
-
-primeFactors :: Integer -> [Integer]
-primeFactors n = factors n 2
-  where
-    factors 1 _ = []
-    factors m p
-      | m < p * p = [m]
-      | m `mod` p == 0 = p : factors (m `div` p) p
-      | otherwise = factors m (p + 1)
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
@@ -18,23 +8,3 @@ primeFactors n = factorize n 2
       | m < p * p = [m]
       | m `mod` p == 0 = p : factorize (m `div` p) p
       | otherwise = factorize m (p + 1)
-module PrimeFactors where
-
-primeFactors :: Integer -> [Integer]
-primeFactors n = factors n 2
-  where
-    factors 1 _ = []
-    factors m p
-      | m < p * p = [m]
-      | m `mod` p == 0 = p : factors (m `div` p) p
-      | otherwise = factors m (p + 1)module PrimeFactors (primeFactors) where
-
-primeFactors :: Integer -> [Integer]
-primeFactors n
-  | n <= 1    = []
-  | otherwise = factor n 2
-  where
-    factor m p
-      | m == 1         = []
-      | m `mod` p == 0 = p : factor (m `div` p) p
-      | otherwise      = factor m (p + 1)
