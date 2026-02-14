7f3285214981a491834e5a5f8efa7abf3cb1b759
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-14 15:05:00 +0000

    Add a function to generate Fibonacci sequence up to a given number

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -12,4 +12,9 @@ fibonacci n
 fibonacci :: Integer -> [Integer]
 fibonacci n = takeWhile (<= n) fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Integer -> [Integer]
+fibonacci n = takeWhile (<= n) fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
