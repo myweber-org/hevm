2e29fd209888293d309d8e3bf07cf3c19f9e584c
FelixMakes <felix.makes@hotmail.com>
2026-02-14 15:41:28 +0000

    Add word frequency counter module with histogram output

--- test/WordFrequencyCounter.hs
+++ test/WordFrequencyCounter.hs
@@ -1,36 +1,27 @@
 module WordFrequencyCounter where
 
 import Data.Char (toLower, isAlpha)
-import Data.List (sortOn)
+import Data.List (sortOn, group, sort)
 import Data.Ord (Down(..))
-import System.Environment (getArgs)
 
-type WordCount = (String, Int)
+type WordFreq = (String, Int)
 
-countWords :: String -> [WordCount]
+countWords :: String -> [WordFreq]
 countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleaned = filter (all isAlpha) wordsList
-        grouped = foldr countHelper [] cleaned
-    in sortOn (Down . snd) grouped
-  where
-    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-    countHelper word [] = [(word, 1)]
-    countHelper word ((w, c):rest)
-        | word == w = (w, c+1):rest
-        | otherwise = (w, c):countHelper word rest
+    let wordsList = filter (not . null) $ map (filter isAlpha . map toLower) $ words text
+        grouped = group $ sort wordsList
+    in sortOn (Down . snd) $ map (\ws -> (head ws, length ws)) grouped
 
-formatOutput :: [WordCount] -> String
-formatOutput counts = unlines $ map formatLine counts
+printHistogram :: [WordFreq] -> IO ()
+printHistogram freqs = mapM_ printBar freqs
   where
-    formatLine (word, count) = word ++ ": " ++ show count
+    printBar (word, count) = 
+        putStrLn $ word ++ ": " ++ replicate count '*'
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        [] -> putStrLn "Usage: wordfreq <text>"
-        textParts -> do
-            let text = unwords textParts
-            let frequencies = countWords text
-            putStrLn $ formatOutput frequencies
+    putStrLn "Enter text to analyze word frequency:"
+    input <- getContents
+    let frequencies = countWords input
+    putStrLn "\nWord Frequency Histogram:"
+    printHistogram frequencies
