5efbb6ebd75ea0acb9dcdaef5d6df7f4b526e05d
heyQuim <hey.quim@hotmail.com>
2026-02-14 21:29:58 +0000

    Add inorder and preorder traversal functions for binary trees

--- test/contracts/fail/BinaryTreeTraversal.hs
+++ test/contracts/fail/BinaryTreeTraversal.hs
@@ -12,3 +12,25 @@ postorder (Node val left right) = postorder left ++ postorder right ++ [val]
 preorder :: BinaryTree a -> [a]
 preorder Empty = []
 preorder (Node val left right) = [val] ++ preorder left ++ preorder right
+module BinaryTreeTraversal where
+
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
+
+preorder :: BinaryTree a -> [a]
+preorder Empty = []
+preorder (Node value left right) = [value] ++ preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Empty = []
+inorder (Node value left right) = inorder left ++ [value] ++ inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Empty = []
+postorder (Node value left right) = postorder left ++ postorder right ++ [value]
+
+levelOrder :: BinaryTree a -> [a]
+levelOrder tree = levelOrderHelper [tree]
+  where
+    levelOrderHelper [] = []
+    levelOrderHelper (Empty : xs) = levelOrderHelper xs
+    levelOrderHelper (Node value left right : xs) = value : levelOrderHelper (xs ++ [left, right])
