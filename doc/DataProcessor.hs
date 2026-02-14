0f8096dc2e2d5a65dd245d398554a8b10e881ee1
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-14 01:09:17 +0000

    Add function to filter and transform list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -23,3 +23,25 @@ main = do
     let input = [1..10]
     let result = processData input
     print result
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
+
+main :: IO ()
+main = do
+    let inputData = [1, -2, 3, 4, -5, 6]
+    case validateInput inputData of
+        Just validData -> do
+            let result = sumProcessedData validData
+            putStrLn $ "Sum of processed data: " ++ show result
+        Nothing -> putStrLn "Invalid input data detected"
