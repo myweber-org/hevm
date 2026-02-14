ebc49bc047aed07f1c368e1b27e839f5ac5782ac
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-14 15:40:55 +0000

    Add in-order traversal for binary trees using continuation passing style

--- test/EVM/BinaryTreeTraversal.hs
+++ test/EVM/BinaryTreeTraversal.hs
@@ -1,31 +1,37 @@
 module BinaryTreeTraversal where
 
-data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show)
+data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show, Eq)
 
-inorderTraversal :: BinaryTree a -> [a]
-inorderTraversal tree = go tree []
-  where
-    go Leaf stack = stack
-    go (Node left val right) stack = go left (val : go right stack)
+inorderCPS :: BinaryTree a -> ([a] -> r) -> r
+inorderCPS Leaf k = k []
+inorderCPS (Node left val right) k =
+    inorderCPS left $ \leftList ->
+    inorderCPS right $ \rightList ->
+    k (leftList ++ [val] ++ rightList)
 
-preorderTraversal :: BinaryTree a -> [a]
-preorderTraversal tree = go tree []
-  where
-    go Leaf stack = stack
-    go (Node left val right) stack = val : go left (go right stack)
+inorder :: BinaryTree a -> [a]
+inorder tree = inorderCPS tree id
 
-postorderTraversal :: BinaryTree a -> [a]
-postorderTraversal tree = go tree []
-  where
-    go Leaf stack = stack
-    go (Node left val right) stack = go left (go right (val : stack))
+preorderCPS :: BinaryTree a -> ([a] -> r) -> r
+preorderCPS Leaf k = k []
+preorderCPS (Node left val right) k =
+    k ([val] ++) $ \prefix ->
+    preorderCPS left $ \leftList ->
+    preorderCPS right $ \rightList ->
+    prefix (leftList ++ rightList)
 
--- Example usage
-exampleTree :: BinaryTree Int
-exampleTree = Node (Node Leaf 2 Leaf) 1 (Node Leaf 3 Leaf)
+preorder :: BinaryTree a -> [a]
+preorder tree = preorderCPS tree id
 
-main :: IO ()
-main = do
-  putStrLn $ "In-order: " ++ show (inorderTraversal exampleTree)
-  putStrLn $ "Pre-order: " ++ show (preorderTraversal exampleTree)
-  putStrLn $ "Post-order: " ++ show (postorderTraversal exampleTree)
+postorderCPS :: BinaryTree a -> ([a] -> r) -> r
+postorderCPS Leaf k = k []
+postorderCPS (Node left val right) k =
+    postorderCPS left $ \leftList ->
+    postorderCPS right $ \rightList ->
+    k (leftList ++ rightList ++ [val])
+
+postorder :: BinaryTree a -> [a]
+postorder tree = postorderCPS tree id
+
+sampleTree :: BinaryTree Int
+sampleTree = Node (Node Leaf 2 Leaf) 1 (Node (Node Leaf 4 Leaf) 3 Leaf)
