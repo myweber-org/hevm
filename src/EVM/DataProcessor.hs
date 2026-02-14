9074201079b5764ad5e38bae0c485eec5e9499b8
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-14 15:21:03 +0000

    Add function to filter and transform list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -3,49 +3,16 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
 validateInput :: [Int] -> Bool
-validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+validateInput = all (\x -> x >= -100 && x <= 100)
 
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5, -10]
-    if validateInput sampleData
-        then print $ processData sampleData
-        else putStrLn "Input validation failed"module DataProcessor where
-
-import Data.List (tails)
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | n > length xs = []
-    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
-  where
-    avg window = sum window / fromIntegral n
-
-smoothData :: Int -> [Double] -> [Double]
-smoothData windowSize = movingAverage windowSize
-
-calculateTrend :: [Double] -> Double
-calculateTrend values = (last values - head values) / fromIntegral (length values - 1)
-
-processDataset :: Int -> [Double] -> (Double, [Double])
-processDataset windowSize dataset =
-    let smoothed = smoothData windowSize dataset
-        trend = calculateTrend smoothed
-    in (trend, smoothed)
+safeProcessData :: [Int] -> Maybe [Int]
+safeProcessData xs
+    | validateInput xs = Just (processData xs)
+    | otherwise = Nothing
