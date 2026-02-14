30987482ea7a1cfb0cbcaf0dd003de00fe3ecb3c
FelixMakes <felix.makes@hotmail.com>
2026-02-14 22:44:29 +0000

    Add a simple word counting function for text analysis

--- src/WordCounter.hs
+++ src/WordCounter.hs
@@ -80,3 +80,20 @@ main = do
   putStrLn "Enter text to analyze (end with Ctrl+D on empty line):"
   content <- getContents
   putStrLn $ wordFrequencyReport content 10
+module WordCounter where
+
+import Data.Char (isSpace)
+import Data.List (group, sort)
+
+countWords :: String -> [(String, Int)]
+countWords text = 
+    let wordsList = filter (not . all isSpace) $ splitWords text
+        lowerWords = map (map toLower) wordsList
+        sortedWords = sort lowerWords
+        groupedWords = group sortedWords
+    in map (\ws -> (head ws, length ws)) groupedWords
+    where
+        splitWords = words . map (\c -> if isSpace c then ' ' else c)
+        toLower c
+            | c >= 'A' && c <= 'Z' = toEnum (fromEnum c + 32)
+            | otherwise = c
