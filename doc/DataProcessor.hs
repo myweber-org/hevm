63d1d2ba16d5c1199b311880195e5254f1f8f663
InkspillVerse <inkspill.verse@outlook.com>
2026-02-14 18:18:03 +0000

    Add function to filter and transform list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -101,4 +101,17 @@ processDataPipeline inputs
     | otherwise = Just stats
     where
         validNumbers = processNumbers inputs
-        stats = calculateStats validNumbers
+        stats = calculateStats validNumbersmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processData
