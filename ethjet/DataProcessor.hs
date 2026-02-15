e024566f4b44a03bec36fe009788d508b4e19d36
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-15 20:21:16 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -27,3 +27,21 @@ main = do
     let result = processData input
     putStrLn $ "Input: " ++ show input
     putStrLn $ "Result: " ++ show result
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData = all (> 0)
+
+main :: IO ()
+main = do
+    let inputData = [1, -2, 3, 0, 5, -8]
+    let processed = processData inputData
+    putStrLn $ "Original data: " ++ show inputData
+    putStrLn $ "Processed data: " ++ show processed
+    putStrLn $ "Validation result: " ++ show (validateData processed)
