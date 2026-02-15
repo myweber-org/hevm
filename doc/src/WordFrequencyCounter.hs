676381bf298932a9b99aeb390daa9e308fe65d3e
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-15 00:14:03 +0000

    Add word frequency counter with case-insensitive sorting and pretty printing

--- doc/src/WordFrequencyCounter.hs
+++ doc/src/WordFrequencyCounter.hs
@@ -21,4 +21,46 @@ processFile filepath = do
   content <- readFile filepath
   let topWords = getTopWords 10 content
   putStrLn "Top 10 most frequent words:"
-  mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
+  mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWordsmodule WordFrequencyCounter where
+
+import Data.Char (toLower)
+import Data.List (sortOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Text.Printf (printf)
+
+type WordCount = Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower
+
+sortByFrequency :: WordCount -> [(String, Int)]
+sortByFrequency = sortOn (negate . snd) . Map.toList
+
+prettyPrint :: [(String, Int)] -> String
+prettyPrint counts = unlines $ header : map formatRow counts
+  where
+    header = "Word                | Frequency"
+    separator = replicate 22 '-' ++ "+" ++ replicate 11 '-'
+    formatRow (word, freq) = printf "%-20s | %10d" word freq
+
+analyzeText :: String -> String
+analyzeText text = unlines
+  [ "Text Analysis Results:"
+  , ""
+  , prettyPrint sortedCounts
+  , printf "Total unique words: %d" (length sortedCounts)
+  , printf "Total words: %d" (sum $ map snd sortedCounts)
+  ]
+  where
+    counts = countWords text
+    sortedCounts = sortByFrequency counts
+
+-- Example usage
+main :: IO ()
+main = do
+  let sampleText = "Hello world! Hello Haskell. World of functional programming."
+  putStrLn $ analyzeText sampleText
