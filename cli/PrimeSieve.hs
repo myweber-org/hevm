d92e8d445ca7a3f117d5ef76d3ed543296e1aaa0
InkspillVerse <inkspill.verse@outlook.com>
2026-02-15 14:50:48 +0000

    Implement Sieve of Eratosthenes for prime number generation up to a given limit

--- cli/PrimeSieve.hs
+++ cli/PrimeSieve.hs
@@ -1,30 +1,12 @@
 module PrimeSieve where
 
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
-  where
-    sieve' []     primes = reverse primes
-    sieve' (p:xs) primes = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:primes)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
 primesUpTo :: Int -> [Int]
 primesUpTo n
     | n < 2     = []
     | otherwise = sieve [2..n]
-    where
-        sieve []     = []
-        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+  where
+    sieve []     = []
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
 isPrime :: Int -> Bool
 isPrime n
@@ -37,80 +19,5 @@ main = do
     input <- getLine
     let limit = read input :: Int
     let primes = primesUpTo limit
-    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primesmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-  where
-    go []     acc = reverse acc
-    go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sievemodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-    where
-        go []     acc = reverse acc
-        go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sievemodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-    where
-        go []     acc = reverse acc
-        go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
-    where
-        sieve' [] primes = reverse primes
-        sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter a limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
-    where
-        sieve' [] primes = reverse primes
-        sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limit
+    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primes
+    putStrLn $ "Total primes found: " ++ show (length primes)
