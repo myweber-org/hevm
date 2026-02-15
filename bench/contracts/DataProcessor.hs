c85c179d7f32c52f2455127ca5b806ad67185c03
heyQuim <hey.quim@hotmail.com>
2026-02-15 14:35:28 +0000

    Add function to filter and transform a list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -44,4 +44,16 @@ extractNumeric = map (\(orig, _, _) -> orig) . filter (\(_, isNum, _) -> isNum)
 
 -- Main processing pipeline
 pipeline :: [String] -> [String]
-pipeline = extractNumeric . processData
+pipeline = extractNumeric . processDatamodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
