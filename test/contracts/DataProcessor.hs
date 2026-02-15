91b13f2e0007989262009a8ab3926d0c42fd07f7
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-15 00:46:39 +0000

    Add function to parse CSV data and calculate average values

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -31,4 +31,42 @@ main :: IO ()
 main = do
     let numbers = [-3, 1, 0, 5, -2, 8]
     let result = processNumbers numbers
-    print result
+    print resultmodule DataProcessor where
+
+import Data.List.Split (splitOn)
+
+type Row = [String]
+type CSVData = [Row]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
+
+numericColumns :: CSVData -> [Int]
+numericColumns [] = []
+numericColumns (header:_) = 
+    [i | (i, cell) <- zip [0..] header, all isNumericChar cell]
+  where
+    isNumericChar c = c `elem` ['0'..'9'] || c == '.'
+
+columnAverage :: CSVData -> Int -> Maybe Double
+columnAverage rows colIndex
+    | null numericValues = Nothing
+    | otherwise = Just (sum numericValues / fromIntegral (length numericValues))
+  where
+    numericValues = [read cell | row <- rows, 
+                   length row > colIndex,
+                   let cell = row !! colIndex,
+                   all (\c -> c `elem` ['0'..'9'] || c == '.') cell]
+
+processCSVFile :: String -> IO ()
+processCSVFile filename = do
+    content <- readFile filename
+    let parsed = parseCSV content
+    let numericCols = numericColumns parsed
+    
+    putStrLn "Column averages:"
+    mapM_ (\col -> 
+        case columnAverage (tail parsed) col of
+            Just avg -> putStrLn $ "Column " ++ show col ++ ": " ++ show avg
+            Nothing -> putStrLn $ "Column " ++ show col ++ ": No numeric data"
+        ) numericCols
