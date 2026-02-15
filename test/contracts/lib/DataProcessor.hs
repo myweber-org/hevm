359ac6786e66aac954fc3e7cadd817f554761ffa
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-15 20:43:10 +0000

    Add data processing module with validation and transformation functions

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -10,4 +10,28 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+import Data.Char (isDigit, toUpper)
+
+-- Validate if a string contains only digits
+validateDigits :: String -> Bool
+validateDigits = all isDigit
+
+-- Transform a string to uppercase
+transformToUpper :: String -> String
+transformToUpper = map toUpper
+
+-- Process data: validate digits and transform to uppercase if valid
+processData :: String -> Maybe String
+processData input
+    | validateDigits input = Just (transformToUpper input)
+    | otherwise = Nothing
+
+-- Example usage function
+exampleUsage :: IO ()
+exampleUsage = do
+    let testData1 = "12345"
+        testData2 = "12a45"
+    print $ processData testData1
+    print $ processData testData2
