9868197dfe54cc56c3ab1fd1a033862334d083de
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-15 21:37:00 +0000

    Add function to filter and transform a list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -1,4 +1,3 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
@@ -6,47 +5,3 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
-
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (>= -1000) xs && all (<= 1000) xs
-
-safeProcess :: [Int] -> Maybe [Int]
-safeProcess xs
-  | validateInput xs = Just (processData xs)
-  | otherwise = Nothing
-
-exampleUsage :: IO ()
-exampleUsage = do
-  let input = [1, -2, 3, 0, 5]
-  case safeProcess input of
-    Just result -> putStrLn $ "Processed result: " ++ show result
-    Nothing -> putStrLn "Invalid input detected"module DataProcessor where
-
-import Data.List (tails)
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | n > length xs = error "Window size exceeds list length"
-    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
-  where
-    avg window = sum window / fromIntegral n
-
-smoothData :: [Double] -> [Double]
-smoothData = movingAverage 3
-
-processData :: [Double] -> (Double, Double, Double)
-processData xs = (minimum xs, maximum xs, sum xs / fromIntegral (length xs))
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-processOddCubes :: [Int] -> [Int]
-processOddCubes = filterAndTransform odd (\x -> x * x * x)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
