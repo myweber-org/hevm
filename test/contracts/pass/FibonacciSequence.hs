7a2b0d0991baa7aedf286001eeaeca48049b33ae
heyQuim <hey.quim@hotmail.com>
2026-02-16 15:44:43 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/contracts/pass/FibonacciSequence.hs
+++ test/contracts/pass/FibonacciSequence.hs
@@ -1,11 +1,20 @@
 module FibonacciSequence where
 
 fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibonacciSum :: Int -> Integer
+fibonacciSum n = sum (fibonacci n)
+
+main :: IO ()
+main = do
+    putStrLn "Enter number of Fibonacci terms:"
+    input <- getLine
+    let n = read input :: Int
+    putStrLn $ "Fibonacci sequence: " ++ show (fibonacci n)
+    putStrLn $ "Sum of first " ++ show n ++ " terms: " ++ show (fibonacciSum n)
