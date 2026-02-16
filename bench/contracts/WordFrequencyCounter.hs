7938f828c241aa3348833bdec122b9bcc7406c0d
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-16 19:11:29 +0000

    Add word frequency counter with command line argument support

--- bench/contracts/WordFrequencyCounter.hs
+++ bench/contracts/WordFrequencyCounter.hs
@@ -25,4 +25,40 @@ processText = formatOutput . sortByFrequency . countWords
 main :: IO ()
 main = do
     input <- TIO.getContents
-    TIO.putStr $ processText input
+    TIO.putStr $ processText inputmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+import System.Environment (getArgs)
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = filter (all isAlpha) wordsList
+        grouped = foldr countHelper [] cleaned
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    countHelper word [] = [(word, 1)]
+    countHelper word ((w, c):rest)
+        | word == w = (w, c+1) : rest
+        | otherwise = (w, c) : countHelper word rest
+
+formatOutput :: [WordCount] -> String
+formatOutput counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+
+processFile :: FilePath -> IO ()
+processFile filename = do
+    content <- readFile filename
+    let counts = countWords content
+    putStrLn $ formatOutput counts
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [filename] -> processFile filename
+        _ -> putStrLn "Usage: wordfreq <filename>"
