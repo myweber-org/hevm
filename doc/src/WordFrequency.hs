c1464a51f5a0cc520133b854ee5239e54572d984
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-16 20:03:01 +0000

    Add word frequency counter for text analysis

--- doc/src/WordFrequency.hs
+++ doc/src/WordFrequency.hs
@@ -249,4 +249,32 @@ main = do
         (filename:_) -> do
             content <- readFile filename
             let frequencies = countWords content
-            putStrLn $ formatOutput frequencies
+            putStrLn $ formatOutput frequenciesmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = [(String, Int)]
+
+countWords :: String -> WordCount
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = map toLower wordsList
+        grouped = foldr countHelper [] cleaned
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = filter isAlpha
+    countHelper word [] = [(word, 1)]
+    countHelper word ((w, c):rest)
+        | word == w = (w, c + 1) : rest
+        | otherwise = (w, c) : countHelper word rest
+
+displayFrequency :: WordCount -> String
+displayFrequency counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+analyzeText :: String -> String
+analyzeText text = 
+    let frequencies = countWords text
+    in "Top 10 most frequent words:\n" ++ displayFrequency frequencies
