3915b17e8d6f6df8d08d5a3b6df3cc9f09dfbe2b
heyQuim <hey.quim@hotmail.com>
2026-02-16 23:25:39 +0000

    Add function to filter and transform list of integers

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -1,49 +1,15 @@
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (*2)
 
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print resultmodule DataProcessor where
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
 
-import Data.Char (isDigit, toUpper)
-
--- Validate if a string contains only digits
-validateDigits :: String -> Bool
-validateDigits = all isDigit
-
--- Transform a string to uppercase
-transformToUpper :: String -> String
-transformToUpper = map toUpper
-
--- Process data: validate digits and transform to uppercase if valid
-processData :: String -> Maybe String
-processData input
-    | validateDigits input = Just (transformToUpper input)
-    | otherwise = Nothing
-
--- Example usage function
-exampleUsage :: IO ()
-exampleUsage = do
-    let testData1 = "12345"
-        testData2 = "12a45"
-    print $ processData testData1
-    print $ processData testData2module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, 0, 5, -8]
-    let result = processData input
-    print result
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
