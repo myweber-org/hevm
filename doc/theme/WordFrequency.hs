3d99ae34c71c8e2d9adf440504523ee457a6d052
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-17 20:38:00 +0000

    Add word frequency counter for text analysis

--- doc/theme/WordFrequency.hs
+++ doc/theme/WordFrequency.hs
@@ -140,4 +140,26 @@ main = do
     putStrLn "Enter text to analyze (press Ctrl+D when done):"
     input <- getContents
     let histogram = countWords input
-    printHistogram histogram
+    printHistogram histogrammodule WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (isAlpha, toLower)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (Down . snd) $ Map.toList $ countWords text
+
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies = unlines . map (\(w,c) -> w ++ ": " ++ show c)
+
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequencies . topNWords n
