9ac79995e17388036b2584bd438acf7ec03f9006
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-17 14:58:29 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- bench/contracts/FibonacciSequence.hs
+++ bench/contracts/FibonacciSequence.hs
@@ -1,25 +1,14 @@
 module FibonacciSequence where
 
-import Data.Function (fix)
-
-fibMemo :: Int -> Integer
-fibMemo = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibMemo (n - 1) + fibMemo (n - 2)
-
-fibonacci :: Int -> Integer
-fibonacci = fix (\f n -> if n < 2 then fromIntegral n else f (n - 1) + f (n - 2))
-module FibonacciSequence where
-
 fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-module FibonacciSequence where
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+    putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
+    mapM_ (putStrLn . show) (fibonacci n)
