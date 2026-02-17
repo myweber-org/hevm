c55f03b45a03232a7317530a6dbcd5c35a321468
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-17 23:25:19 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -57,4 +57,21 @@ processDataset :: Int -> [Double] -> (Double, [Double])
 processDataset windowSize dataset =
     let smoothed = smoothData windowSize dataset
         trend = calculateTrend smoothed
-    in (trend, smoothed)
+    in (trend, smoothed)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< -100) xs = Nothing
+    | any (> 100) xs = Nothing
+    | otherwise = Just xs
