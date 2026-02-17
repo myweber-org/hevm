af350eb3f68dc89265ea0a8d8ad7e5370364906c
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-17 20:23:54 +0000

    Add word frequency counter for text analysis

--- test/WordFrequency.hs
+++ test/WordFrequency.hs
@@ -131,4 +131,31 @@ main :: IO ()
 main = do
     let sampleText = "Hello world! Hello Haskell. World of functional programming."
     let frequencies = countWords sampleText
-    printWordFrequencies frequencies
+    printWordFrequencies frequenciesmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleaned = filter (all isAlpha) wordsList
+        grouped = foldr countWord [] cleaned
+    in take 10 $ sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    
+    countWord :: String -> [WordCount] -> [WordCount]
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
+
+analyzeText :: String -> IO ()
+analyzeText input = do
+    let topWords = countWords input
+    putStrLn "Top 10 most frequent words:"
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
