df788b809d7f86383c79adc35bebfe603e375f17
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-17 23:44:30 +0000

    Add function to calculate moving average of a list

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -7,4 +7,25 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
+  where
+    average window = sum window / fromIntegral n
+
+smoothData :: [Double] -> [Double]
+smoothData = movingAverage 3
+
+main :: IO ()
+main = do
+    let sampleData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    putStrLn "Original data:"
+    print sampleData
+    putStrLn "\nSmoothed data (3-point moving average):"
+    print $ smoothData sampleData
