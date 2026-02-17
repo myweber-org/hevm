e5c17941f7f39df0873d781646481f46c05357f7
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-17 19:29:21 +0000

    Add function to filter and transform list of integers

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -25,3 +25,17 @@ calculateTrend values =
         numerator = n * sumXY - sumX * sumY
         denominator = n * sumX2 - sumX * sumX
     in if denominator == 0 then 0 else numerator / denominator
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processData
