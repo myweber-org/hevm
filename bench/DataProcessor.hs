c8ddf0501fa7211ed9566329ab605b863b48bd45
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-17 23:11:35 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -10,4 +10,25 @@ main :: IO ()
 main = do
     let numbers = [-5, 3, 0, 8, -2, 10]
     let result = processNumbers numbers
-    print result
+    print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+main :: IO ()
+main = do
+    let numbers = [-3, 2, 0, 7, -1, 4]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
+    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
+    putStrLn $ "First element: " ++ show (safeHead numbers)
