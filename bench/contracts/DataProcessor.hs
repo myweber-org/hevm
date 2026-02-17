dbba6ffe31d7fb2614abff421492011cd879412a
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-17 00:01:57 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -88,4 +88,16 @@ processCSVData :: String -> Day -> Day -> Either String [Record]
 processCSVData csvContent start end =
     case parseCSV "input" csvContent of
         Left err -> Left $ "CSV parse error: " ++ err
-        Right csv -> Right $ filterByDateRange start end csv
+        Right csv -> Right $ filterByDateRange start end csvmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
+
+combineResults :: [Int] -> [Int] -> [Int]
+combineResults xs ys = zipWith (+) (processData xs) (processData ys)
