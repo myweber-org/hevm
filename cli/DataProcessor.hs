2e7c0eb5a2f4482f76368c9b4d535a37c7f4dc77
InkspillVerse <inkspill.verse@outlook.com>
2026-02-17 00:02:30 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -76,4 +76,16 @@ exampleProcessing = do
   where
     processRow data idx = case processCSVRow validators transformers (data !! idx) of
       Left err -> putStrLn $ "Row " ++ show idx ++ " error: " ++ err
-      Right result -> putStrLn $ "Row " ++ show idx ++ " processed: " ++ show result
+      Right result -> putStrLn $ "Row " ++ show idx ++ " processed: " ++ show resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    print result
