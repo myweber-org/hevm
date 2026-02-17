63a763d352b96dc52d9dbdafa9f475e9dd36454a
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-17 01:14:08 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -25,4 +25,13 @@ processDataset :: Fractional a => Int -> [a] -> ([a], String)
 processDataset windowSize dataset =
     let smoothed = smoothData windowSize dataset
         trend = calculateTrend smoothed
-    in (smoothed, trend)
+    in (smoothed, trend)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
