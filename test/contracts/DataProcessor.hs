23d76ec879673a01431113aa9dc257b5e590e1e1
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-18 00:57:00 +0000

    Add function to filter and transform a list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -35,4 +35,16 @@ processDataset windowSize dataset =
     in (trend, smoothed)module DataProcessor where
 
 processData :: [Int] -> [Int]
-processData = map (^2) . filter (>0)
+processData = map (^2) . filter (>0)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
