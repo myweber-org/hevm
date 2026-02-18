a18628e76dd9b747b9ee008fdc36c5837d75ca18
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-18 18:49:47 +0000

    Add function to calculate moving average of a list

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -57,4 +57,18 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
+    average zs = sum zs / fromIntegral (length zs)
+
+safeMovingAverage :: Fractional a => Int -> [a] -> Maybe [a]
+safeMovingAverage n xs
+    | n <= 0 = Nothing
+    | otherwise = Just $ movingAverage n xs
