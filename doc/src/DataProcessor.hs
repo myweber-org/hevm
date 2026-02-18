2569d3f1954494f0f9ed334e81ed8f241e0214a5
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-18 14:26:35 +0000

    Add data processing module with CSV parsing and filtering functions

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -13,4 +13,44 @@ movingAverage n xs
     | otherwise = map average $ windows n xs
   where
     windows m ys = take (length ys - m + 1) $ map (take m) (tails ys)
-    average zs = sum zs / fromIntegral (length zs)
+    average zs = sum zs / fromIntegral (length zs)module DataProcessor where
+
+import Data.List (intercalate)
+import Data.Maybe (catMaybes)
+
+type Row = [String]
+type CSV = [Row]
+
+parseCSV :: String -> CSV
+parseCSV content = map (splitOn ',') (lines content)
+  where
+    splitOn :: Char -> String -> [String]
+    splitOn delimiter = foldr splitter [[]]
+      where
+        splitter char acc@(current:rest)
+          | char == delimiter = []:acc
+          | otherwise = (char:current):rest
+
+filterRows :: (Row -> Bool) -> CSV -> CSV
+filterRows predicate = filter predicate
+
+selectColumns :: [Int] -> CSV -> CSV
+selectColumns indices = map (mapMaybeByIndex indices)
+  where
+    mapMaybeByIndex :: [Int] -> Row -> Row
+    mapMaybeByIndex idxs row = catMaybes [safeIndex row i | i <- idxs]
+    
+    safeIndex :: Row -> Int -> Maybe String
+    safeIndex row idx
+      | idx >= 0 && idx < length row = Just (row !! idx)
+      | otherwise = Nothing
+
+toCSVString :: CSV -> String
+toCSVString = intercalate "\n" . map (intercalate ",")
+
+processData :: String -> String
+processData input = toCSVString processed
+  where
+    csv = parseCSV input
+    filtered = filterRows (\row -> length row > 2) csv
+    processed = selectColumns [0, 2, 4] filtered
