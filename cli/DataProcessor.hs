c6f368ec1a7a255bcfedad1198d14748760c6246
InkspillVerse <inkspill.verse@outlook.com>
2026-02-18 21:05:39 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -39,4 +39,16 @@ filterAboveAverage records =
     avg = calculateAverage records
 
 processCSVData :: String -> [Record]
-processCSVData = filterAboveAverage . parseCSV
+processCSVData = filterAboveAverage . parseCSVmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processNumbers input
+    print result
