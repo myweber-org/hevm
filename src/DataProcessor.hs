a06c4e25d7e8addf174daee5e0cd5132cfdb53d5
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-19 16:17:11 +0000

    Add function to filter and transform a list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -10,4 +10,16 @@ processData = filterAndTransform (> 0) (* 2)
 validateAndProcess :: [Int] -> Maybe [Int]
 validateAndProcess xs
     | null xs = Nothing
-    | otherwise = Just $ processData xs
+    | otherwise = Just $ processData xsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -8]
+    let result = processData input
+    print result
