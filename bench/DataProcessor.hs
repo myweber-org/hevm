52092221e307340921da8400b06db8b2edd63ac4
VioletInquiry <violet.inquiry@outlook.com>
2026-02-19 00:29:16 +0000

    Add function to filter and transform a list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -65,4 +65,16 @@ filterEven = filter even
 
 -- Main processing pipeline example
 processData :: [String] -> [Maybe String]
-processData = map processName
+processData = map processNamemodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -7]
+    let result = processNumbers input
+    print result
