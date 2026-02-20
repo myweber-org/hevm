331f76dd6af7deb500f56c401a31813c64a8c946
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-20 00:54:08 +0000

    Add word frequency counter for text analysis

--- bench/contracts/WordFrequency.hs
+++ bench/contracts/WordFrequency.hs
@@ -228,4 +228,26 @@ displayFrequencies :: [(String, Int)] -> String
 displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
-analyzeText n = displayFrequencies . topNWords n
+analyzeText n = displayFrequencies . topNWords nmodule WordFrequency where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+type WordCount = Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (\(_, count) -> negate count) $ Map.toList $ countWords text
+
+displayFrequency :: [(String, Int)] -> String
+displayFrequency = unlines . map (\(word, count) -> word ++ ": " ++ show count)
+
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequency . topNWords n
