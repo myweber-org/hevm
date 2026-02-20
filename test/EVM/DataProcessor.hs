1c2d438ffc5ebe7238f7dc4a932c6b6c322d9d71
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-20 20:51:59 +0000

    Add function to calculate moving average of a list

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -1,108 +1,32 @@
-
 module DataProcessor where
 
-import Data.Char (isDigit, isSpace)
-import Data.List (intercalate)
-import Data.Maybe (catMaybes, fromMaybe)
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | length xs < n = []
+    | otherwise = avg : movingAverage n (tail xs)
+  where
+    window = take n xs
+    avg = sum window / fromIntegral n
 
-newtype ValidatedString = ValidatedString String
-    deriving (Show, Eq)
+smoothData :: Fractional a => Int -> [a] -> [a]
+smoothData windowSize dataPoints =
+    let prefix = replicate (windowSize `div` 2) (head dataPoints)
+        suffix = replicate (windowSize `div` 2) (last dataPoints)
+        extendedData = prefix ++ dataPoints ++ suffix
+    in movingAverage windowSize extendedData
 
-data ValidationError = 
-      EmptyString
-    | ContainsSpaces
-    | InvalidCharacters
-    | LengthExceeded Int
-    deriving (Show, Eq)
+calculateTrend :: (Fractional a, Ord a) => [a] -> String
+calculateTrend values
+    | allIncreasing = "Increasing trend"
+    | allDecreasing = "Decreasing trend"
+    | otherwise = "No clear trend"
+  where
+    pairs = zip values (tail values)
+    allIncreasing = all (uncurry (<)) pairs
+    allDecreasing = all (uncurry (>)) pairs
 
-validateNonEmpty :: String -> Either ValidationError ValidatedString
-validateNonEmpty "" = Left EmptyString
-validateNonEmpty str = Right (ValidatedString str)
-
-validateNoSpaces :: String -> Either ValidationError ValidatedString
-validateNoSpaces str
-    | any isSpace str = Left ContainsSpaces
-    | otherwise = Right (ValidatedString str)
-
-validateNumeric :: String -> Either ValidationError ValidatedString
-validateNumeric str
-    | all isDigit str = Right (ValidatedString str)
-    | otherwise = Left InvalidCharacters
-
-validateMaxLength :: Int -> String -> Either ValidationError ValidatedString
-validateMaxLength maxLen str
-    | length str > maxLen = Left (LengthExceeded maxLen)
-    | otherwise = Right (ValidatedString str)
-
-composeValidators :: [String -> Either ValidationError ValidatedString] -> String -> Either [ValidationError] ValidatedString
-composeValidators validators input =
-    case partitionEithers results of
-        ([], [ValidatedString _]) -> Right (ValidatedString input)
-        (errors, _) -> Left errors
-    where
-        results = map ($ input) validators
-        partitionEithers = foldr select ([], [])
-        select (Left err) (errs, vals) = (err:errs, vals)
-        select (Right val) (errs, vals) = (errs, val:vals)
-
-transformToUpper :: ValidatedString -> String
-transformToUpper (ValidatedString str) = map toUpper str
-
-transformToLower :: ValidatedString -> String
-transformToLower (ValidatedString str) = map toLower str
-
-padString :: Int -> Char -> ValidatedString -> String
-padString width padChar (ValidatedString str) =
-    if length str >= width
-        then str
-        else str ++ replicate (width - length str) padChar
-
-parseCommaSeparated :: String -> [String]
-parseCommaSeparated = filter (not . null) . map (dropWhile isSpace) . splitBy ','
-
-splitBy :: Char -> String -> [String]
-splitBy delimiter = foldr splitHelper [""]
-    where
-        splitHelper ch (current:rest)
-            | ch == delimiter = "":current:rest
-            | otherwise = (ch:current):rest
-
-safeReadInt :: String -> Maybe Int
-safeReadInt str = 
-    case reads str of
-        [(n, "")] -> Just n
-        _ -> Nothing
-
-batchProcess :: (a -> Maybe b) -> [a] -> [b]
-batchProcess processor = catMaybes . map processor
-
-formatErrors :: [ValidationError] -> String
-formatErrors errors = 
-    "Validation failed: " ++ intercalate ", " (map describeError errors)
-    where
-        describeError EmptyString = "string cannot be empty"
-        describeError ContainsSpaces = "string contains spaces"
-        describeError InvalidCharacters = "invalid characters detected"
-        describeError (LengthExceeded n) = "exceeds maximum length of " ++ show n
-
-processInput :: String -> Either String String
-processInput input = 
-    case composeValidators [validateNonEmpty, validateNoSpaces, validateMaxLength 50] input of
-        Left errs -> Left (formatErrors errs)
-        Right validated -> Right (transformToUpper validated)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 10) (* 2)
-
-validateData :: [Int] -> Bool
-validateData = all (\x -> x `mod` 2 == 0) . processData
+processDataset :: Fractional a => Int -> [a] -> ([a], String)
+processDataset windowSize dataset =
+    let smoothed = smoothData windowSize dataset
+        trend = calculateTrend smoothed
+    in (smoothed, trend)
