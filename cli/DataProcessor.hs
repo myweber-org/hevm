5a3d73c9aef275512c40a365139eed9bb063c05a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-20 20:13:08 +0000

    Add function to filter and transform list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -157,4 +157,16 @@ validateInput xs
     | null xs = Nothing
     | any (< -100) xs = Nothing
     | any (> 100) xs = Nothing
-    | otherwise = Just xs
+    | otherwise = Just xsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
