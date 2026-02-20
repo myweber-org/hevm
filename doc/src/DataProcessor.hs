33162ee0451b0a81bf377749860ce3b23787d3a7
VioletInquiry <violet.inquiry@outlook.com>
2026-02-20 00:13:40 +0000

    Add function to filter and transform list of integers

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -4,36 +4,16 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-processOddCubes :: [Int] -> [Int]
-processOddCubes = filterAndTransform odd (\x -> x * x * x)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = sum . filterAndTransform predicate transformermodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs
-    | all (> -1000) xs = Just xs
-    | otherwise = Nothing
+validateData :: [Int] -> Bool
+validateData = all (> 0)
 
 main :: IO ()
 main = do
-    let sampleData = [1, -5, 3, 0, 8, -2]
-    case validateInput sampleData of
-        Just validData -> do
-            putStrLn $ "Original data: " ++ show validData
-            putStrLn $ "Processed data: " ++ show (processData validData)
-            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
-        Nothing -> putStrLn "Invalid input data detected"
+    let inputData = [1, -2, 3, 0, 5, -8]
+    let processed = processData inputData
+    putStrLn $ "Original data: " ++ show inputData
+    putStrLn $ "Processed data: " ++ show processed
+    putStrLn $ "Data validation: " ++ show (validateData processed)
