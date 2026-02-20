cd867ac4b92a9c32b9ce256ae7d7a79efa28355b
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-20 20:09:03 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -96,3 +96,13 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 10) (* 2)
+
+validateData :: [Int] -> Bool
+validateData = all (\x -> x `mod` 2 == 0) . processData
