b6050dfca3b974844f79516a0e92453d97c9c4d0
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-20 16:19:32 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -71,4 +71,17 @@ movingAverage n xs
                 else []
         
         average :: Fractional a => [a] -> a
-        average vals = sum vals / fromIntegral (length vals)
+        average vals = sum vals / fromIntegral (length vals)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let input = [1..10]
+    let result = processData input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
