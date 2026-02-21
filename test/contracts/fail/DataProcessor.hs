d39e6c1e96fff15f67798a3bd65de4250f8717ce
InkspillVerse <inkspill.verse@outlook.com>
2026-02-21 21:17:01 +0000

    Add function to parse CSV data and calculate average values

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -22,3 +22,32 @@ main = do
             putStrLn $ "Processed data: " ++ show (processData validData)
             putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
         Nothing -> putStrLn "Input contains values less than -1000"
+module DataProcessor where
+
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
+
+type Record = (String, Double, Double)
+
+parseCSV :: String -> [Record]
+parseCSV content = mapMaybe parseLine (lines content)
+  where
+    parseLine line = case splitOn "," line of
+      [name, val1Str, val2Str] -> 
+        case (reads val1Str, reads val2Str) of
+          ([(val1, "")], [(val2, "")]) -> Just (name, val1, val2)
+          _ -> Nothing
+      _ -> Nothing
+
+calculateAverages :: [Record] -> (Double, Double)
+calculateAverages records = (avg val1s, avg val2s)
+  where
+    (val1s, val2s) = unzip [(v1, v2) | (_, v1, v2) <- records]
+    avg xs = sum xs / fromIntegral (length xs)
+
+processData :: String -> Maybe (Double, Double)
+processData content = 
+  let records = parseCSV content
+  in if null records 
+     then Nothing 
+     else Just (calculateAverages records)
