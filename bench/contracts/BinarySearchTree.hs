5f6b48b15b171aba697f4822c50fe8e0eacfbe59
ElaraWrites <elara.writes@outlook.com>
2026-02-21 22:19:03 +0000

    Implement binary search tree insertion and in-order traversal

--- bench/contracts/BinarySearchTree.hs
+++ bench/contracts/BinarySearchTree.hs
@@ -15,17 +15,11 @@ inOrder Empty = []
 inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
 
 fromList :: Ord a => [a] -> Tree a
-fromList = foldr insert Emptymodule BinarySearchTree where
+fromList = foldr insert Empty
 
-data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
-
-insert :: Ord a => a -> BST a -> BST a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
-
-inOrder :: BST a -> [a]
-inOrder Empty = []
-inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
+main :: IO ()
+main = do
+    let tree = fromList [5, 3, 7, 1, 4, 6, 8]
+    print tree
+    putStrLn "In-order traversal:"
+    print $ inOrder tree
