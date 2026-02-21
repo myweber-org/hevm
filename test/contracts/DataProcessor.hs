eee185b8fb24dbe759ab2b9bf767d3d1859d12a4
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-21 23:36:55 +0000

    Add function to parse CSV data and calculate average values

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -211,3 +211,31 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Csv as Csv
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
+type Row = (String, Double, Double, Double)
+
+parseCSV :: BL.ByteString -> Either String (Vector Row)
+parseCSV input = case Csv.decode Csv.NoHeader input of
+    Left err -> Left $ "Parse error: " ++ err
+    Right rows -> Right $ V.fromList rows
+
+calculateAverages :: Vector Row -> (Double, Double, Double)
+calculateAverages rows
+    | V.null rows = (0, 0, 0)
+    | otherwise = (avg col1, avg col2, avg col3)
+  where
+    col1 = V.map (\(_, x, _, _) -> x) rows
+    col2 = V.map (\(_, _, x, _) -> x) rows
+    col3 = V.map (\(_, _, _, x) -> x) rows
+    avg xs = V.sum xs / fromIntegral (V.length xs)
+
+processData :: BL.ByteString -> Either String (Double, Double, Double)
+processData input = do
+    parsed <- parseCSV input
+    return $ calculateAverages parsed
