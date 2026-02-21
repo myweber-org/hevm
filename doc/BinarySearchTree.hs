d7ae9a360828e3e942eb71d73dc409c2a49f74c6
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-21 20:38:10 +0000

    Implement binary search tree insertion and in-order traversal

--- doc/BinarySearchTree.hs
+++ doc/BinarySearchTree.hs
@@ -13,3 +13,20 @@ insert x (Node y left right)
 inOrder :: Tree a -> [a]
 inOrder Empty = []
 inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
+module BinarySearchTree where
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert x Empty = Node x Empty Empty
+insert x (Node y left right)
+    | x < y     = Node y (insert x left) right
+    | x > y     = Node y left (insert x right)
+    | otherwise = Node y left right
+
+inorder :: Tree a -> [a]
+inorder Empty = []
+inorder (Node x left right) = inorder left ++ [x] ++ inorder right
+
+fromList :: Ord a => [a] -> Tree a
+fromList = foldr insert Empty
