1dba9857143a7ec85494c329a369fac9fd00e206
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-21 21:54:09 +0000

    Add function to calculate depth of a binary tree

--- test/EVM/BinaryTreeDepth.hs
+++ test/EVM/BinaryTreeDepth.hs
@@ -50,3 +50,8 @@ main :: IO ()
 main = do
     let depth = treeDepth sampleTree
     putStrLn $ "Maximum depth of the sample tree is: " ++ show depth
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
+
+treeDepth :: BinaryTree a -> Int
+treeDepth Empty = 0
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
