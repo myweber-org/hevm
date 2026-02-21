3d7ca9d17dbf7ff5be01593c68b240a24e4b3831
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-21 18:23:17 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -128,4 +128,16 @@ validateInput xs
     | null xs = Nothing
     | any (< -100) xs = Nothing
     | any (> 100) xs = Nothing
-    | otherwise = Just xs
+    | otherwise = Just xsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
+
+combineResults :: [Int] -> [Int] -> [Int]
+combineResults xs ys = zipWith (+) (processData xs) (processData ys)
