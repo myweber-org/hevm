ee3170185790163a0b8022e53452865f67ff7d53
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-21 19:40:28 +0000

    Add function to count word frequencies in a text

--- src/EVM/WordFrequency.hs
+++ src/EVM/WordFrequency.hs
@@ -19,4 +19,35 @@ topWords n text = take n $ sortOn (negate . snd) $ Map.toList (countWords text)
 displayFrequency :: String -> IO ()
 displayFrequency text = do
   putStrLn "Top 10 most frequent words:"
-  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (topWords 10 text)
+  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) (topWords 10 text)module WordFrequency where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        freqMap = foldr (\word acc -> insertWord word acc) [] wordsList
+    in take 10 $ sortOn (Down . snd) freqMap
+  where
+    cleanWord = map toLower . filter isAlphaNum
+    
+    insertWord :: String -> [WordCount] -> [WordCount]
+    insertWord word [] = [(word, 1)]
+    insertWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : insertWord word rest
+
+displayResults :: [WordCount] -> String
+displayResults counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+
+main :: IO ()
+main = do
+    putStrLn "Enter text to analyze:"
+    input <- getContents
+    let topWords = countWords input
+    putStrLn "\nTop 10 most frequent words:"
+    putStrLn $ displayResults topWords
