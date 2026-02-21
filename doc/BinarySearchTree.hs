d36469b1f915b355479d92e9de99b19340c09652
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-21 23:01:59 +0000

    Implement binary search tree insertion and in-order traversal

--- doc/BinarySearchTree.hs
+++ doc/BinarySearchTree.hs
@@ -29,4 +29,29 @@ inorder Empty = []
 inorder (Node x left right) = inorder left ++ [x] ++ inorder right
 
 fromList :: Ord a => [a] -> Tree a
+fromList = foldr insert Emptymodule BinarySearchTree where
+
+data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
+
+insert :: Ord a => a -> BST a -> BST a
+insert x Empty = Node Empty x Empty
+insert x (Node left val right)
+    | x < val   = Node (insert x left) val right
+    | x > val   = Node left val (insert x right)
+    | otherwise = Node left val right
+
+inOrder :: BST a -> [a]
+inOrder Empty = []
+inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
+
+fromList :: Ord a => [a] -> BST a
 fromList = foldr insert Empty
+
+toList :: BST a -> [a]
+toList = inOrder
+
+isBST :: Ord a => BST a -> Bool
+isBST tree = isSorted (inOrder tree)
+    where isSorted [] = True
+          isSorted [_] = True
+          isSorted (x:y:xs) = x <= y && isSorted (y:xs)
