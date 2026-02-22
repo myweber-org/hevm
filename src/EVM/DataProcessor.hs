ce9798c6d77a7e3ef2fe16c5b42dbf72289a3ac8
InkspillVerse <inkspill.verse@outlook.com>
2026-02-22 21:49:25 +0000

    Add function to filter and transform list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -1,22 +1,15 @@
+
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
+filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = 
-    if all (> -1000) xs && all (< 1000) xs
-    then Just xs
-    else Nothing
-
-safeProcess :: [Int] -> Maybe Int
-safeProcess xs = do
-    valid <- validateInput xs
-    return $ sumProcessed valid
+sumProcessed = sum . processEvenSquares
