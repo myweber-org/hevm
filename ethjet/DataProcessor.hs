ae8383e3636a412a09cd6a277cba37d453d9075c
InkspillVerse <inkspill.verse@outlook.com>
2026-02-22 19:04:08 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -44,4 +44,21 @@ fullPipeline names =
 -- main :: IO ()
 -- main = do
 --   let sampleData = [("John", "Doe"), ("123-456-7890", ""), ("test123", "")]
---   putStrLn $ fullPipeline sampleData
+--   putStrLn $ fullPipeline sampleDatamodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< -100) xs = Nothing
+    | any (> 100) xs = Nothing
+    | otherwise = Just xs
