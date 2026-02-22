87072d98bfc72a7716c239172a506bba6e0a7d3b
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-22 14:29:34 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -238,4 +238,16 @@ calculateAverages rows
 processData :: BL.ByteString -> Either String (Double, Double, Double)
 processData input = do
     parsed <- parseCSV input
-    return $ calculateAverages parsed
+    return $ calculateAverages parsedmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
+
+combineResults :: [Int] -> [Int] -> [Int]
+combineResults xs ys = zipWith (+) (processData xs) (processData ys)
