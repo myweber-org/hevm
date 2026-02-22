0d224b6ad146664bb6028aa2e65eb31b556278ac
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-22 23:21:06 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -8,3 +8,23 @@ processData = filterAndTransform (> 0) (* 2)
 
 validateData :: [Int] -> Bool
 validateData = all (> 0)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData = all (> 0)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -8]
+    let processed = processData input
+    let isValid = validateData processed
+    
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Processed: " ++ show processed
+    putStrLn $ "Validation: " ++ show isValid
