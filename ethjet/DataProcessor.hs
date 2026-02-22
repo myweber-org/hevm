98d80d4b61c8e503752564af1a05a988d9a869af
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-22 19:43:52 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -67,4 +67,20 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (* 2)
+processNumbers = filterAndTransform even (* 2)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, 0, 5, -10]
+    if validateInput sampleData
+        then print $ processData sampleData
+        else putStrLn "Invalid input data"
