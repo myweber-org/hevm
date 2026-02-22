cce4d59071142287964b45f8260cc5c0cc9217bf
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-22 00:59:32 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/scripts/FibonacciSequence.hs
+++ test/scripts/FibonacciSequence.hs
@@ -1,30 +1,14 @@
 module FibonacciSequence where
 
 fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-printFibonacci :: Int -> IO ()
-printFibonacci n = do
-    putStrLn $ "First " ++ show n ++ " Fibonacci numbers:"
-    mapM_ (putStrLn . show) (fibonacci n)
+fibonacci n
+    | n <= 0    = []
+    | n == 1    = [0]
+    | n == 2    = [0, 1]
+    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+                  in take n fibs
 
 main :: IO ()
 main = do
-    putStr "Enter number of terms: "
-    input <- getLine
-    let n = read input :: Int
-    if n > 0
-        then printFibonacci n
-        else putStrLn "Please enter a positive integer."module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-  putStrLn "Fibonacci sequence up to 10 terms:"
-  print $ fibonacci 10
+    putStrLn "Fibonacci sequence up to 10 terms:"
+    print $ fibonacci 10
