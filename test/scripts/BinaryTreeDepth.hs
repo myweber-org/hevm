7190d1759928095fc3cba57ae3e46959c59ee932
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-23 20:02:35 +0000

    Add function to calculate depth of a binary tree

--- test/scripts/BinaryTreeDepth.hs
+++ test/scripts/BinaryTreeDepth.hs
@@ -1,13 +1,7 @@
+module BinaryTreeDepth where
+
 data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: BinaryTree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty (Node 6 Empty Empty))
-
-main :: IO ()
-main = do
-    let depth = treeDepth sampleTree
-    putStrLn $ "Maximum depth of the sample tree is: " ++ show depth
