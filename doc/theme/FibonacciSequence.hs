fe6fd110ea191b8c7967ac7a3e9be0984418389a
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-23 15:23:53 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -18,3 +18,9 @@ fibonacci :: Integer -> [Integer]
 fibonacci n = takeWhile (<= n) fibs
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
