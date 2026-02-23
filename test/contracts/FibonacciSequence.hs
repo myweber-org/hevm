228bf0fb91f5db216e73574b0eb19fa9ca369c7a
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-23 18:59:20 +0000

    Add Fibonacci sequence generator with memoization

--- test/contracts/FibonacciSequence.hs
+++ test/contracts/FibonacciSequence.hs
@@ -1,65 +1,11 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-import Data.Function (fix)
-
-fibMemo :: Int -> Integer
-fibMemo = (map fib [0..] !!)
-    where
-        fib 0 = 0
-        fib 1 = 1
-        fib n = fibMemo (n-1) + fibMemo (n-2)
-
-fibonacci :: Int -> Integer
-fibonacci = fix fibMemomodule FibonacciSequence where
-
-fib :: Int -> Integer
-fib n = fibs !! n
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n
-    | n <= 0    = []
-    | n == 1    = [0]
-    | n == 2    = [0, 1]
-    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-                  in take n fibs
-
-fibonacciWithIndex :: Int -> [(Int, Integer)]
-fibonacciWithIndex n = zip [0..] (fibonacci n)
-
-printFibonacciSequence :: Int -> IO ()
-printFibonacciSequence n = do
-    putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
-    mapM_ (\(i, val) -> putStrLn $ "F(" ++ show i ++ ") = " ++ show val) 
-          (fibonacciWithIndex n)module FibonacciSequence where
-
-import Data.Function (fix)
-
 fibonacci :: Int -> Integer
 fibonacci n = fibs !! n
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
--- Alternative implementation using explicit memoization with a list
-fibonacciMemoized :: Int -> Integer
-fibonacciMemoized = (map fib [0..] !!)
-  where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = fibonacciMemoized (n - 1) + fibonacciMemoized (n - 2)
-
--- Using fix for a point-free style infinite list definition
-fibonacciInfinite :: [Integer]
-fibonacciInfinite = fix $ \f -> 0 : 1 : zipWith (+) f (tail f)
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    mapM_ (print . fibonacci) [0..19]
