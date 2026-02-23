52b92820424f9c5bfee911e4d6b0062f7b5ab13b
heyQuim <hey.quim@hotmail.com>
2026-02-23 16:41:01 +0000

    Add a function to generate Fibonacci sequence up to a given number

--- test/scripts/FibonacciSequence.hs
+++ test/scripts/FibonacciSequence.hs
@@ -1,14 +1,6 @@
 module FibonacciSequence where
 
-fibonacci :: Int -> [Integer]
-fibonacci n
-    | n <= 0    = []
-    | n == 1    = [0]
-    | n == 2    = [0, 1]
-    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-                  in take n fibs
-
-main :: IO ()
-main = do
-    putStrLn "Fibonacci sequence up to 10 terms:"
-    print $ fibonacci 10
+fibonacci :: Integer -> [Integer]
+fibonacci n = takeWhile (<= n) fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
