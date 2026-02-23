b1053ec6f3c8337e340933ce9b6efcc634465faa
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-23 23:24:23 +0000

    Add function to filter and transform a list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -126,4 +126,10 @@ validateInput = all (\x -> x >= -100 && x <= 100)
 safeProcessData :: [Int] -> Maybe [Int]
 safeProcessData xs
     | validateInput xs = Just (processData xs)
-    | otherwise = Nothing
+    | otherwise = Nothingmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
