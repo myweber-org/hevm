c9d467242e30e482555632ffebfe32ec3891913f
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-23 23:11:40 +0000

    Add function to filter and transform list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -22,4 +22,27 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processEvenNumbers :: [Int] -> [Int]
+processEvenNumbers = filterAndTransform even (*2)
+
+processOddNumbers :: [Int] -> [Int]
+processOddNumbers = filterAndTransform odd (+1)
+
+calculateStatistics :: [Int] -> (Int, Int, Double)
+calculateStatistics xs = (minimum xs, maximum xs, average)
+  where
+    average = fromIntegral (sum xs) / fromIntegral (length xs)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    putStrLn $ "Original: " ++ show numbers
+    putStrLn $ "Even doubled: " ++ show (processEvenNumbers numbers)
+    putStrLn $ "Odd incremented: " ++ show (processOddNumbers numbers)
+    putStrLn $ "Stats (min, max, avg): " ++ show (calculateStatistics numbers)
