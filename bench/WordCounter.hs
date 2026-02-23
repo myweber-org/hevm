56d8edd4959bad021397c4a2d171e13fd063e9c5
InkspillVerse <inkspill.verse@outlook.com>
2026-02-23 15:21:34 +0000

    Add a simple word counting function for text analysis

--- bench/WordCounter.hs
+++ bench/WordCounter.hs
@@ -21,3 +21,21 @@ displayCounts = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 processText :: String -> String
 processText = displayCounts . countWords
+module WordCounter where
+
+import Data.Char (isSpace)
+import Data.List (group, sort)
+
+countWords :: String -> [(String, Int)]
+countWords text = 
+    let wordsList = filter (not . null) $ splitWords text
+        sortedWords = sort $ map normalize wordsList
+        grouped = group sortedWords
+    in map (\ws -> (head ws, length ws)) grouped
+    where
+        splitWords = words . map (\c -> if isSpace c then ' ' else c)
+        normalize = map toLower . filter (not . isPunctuation)
+        isPunctuation c = c `elem` ".,!?;:\"'()[]{}"
+        toLower c
+            | c >= 'A' && c <= 'Z' = toEnum (fromEnum c + 32)
+            | otherwise = c
