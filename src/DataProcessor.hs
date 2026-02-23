4099511aaafcec3bd8eaa170015c2e964666d933
InkspillVerse <inkspill.verse@outlook.com>
2026-02-23 16:04:24 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -4,33 +4,16 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-processOddCubes :: [Int] -> [Int]
-processOddCubes = filterAndTransform odd (\x -> x * x * x)
-
-sumProcessedData :: (Int -> Bool) -> (Int -> Int) -> [Int] -> Int
-sumProcessedData predicate transformer = sum . filterAndTransform predicate transformer
+validateData :: [Int] -> Bool
+validateData = all (> 0)
 
 main :: IO ()
 main = do
-    let testData = [1..10]
-    putStrLn $ "Original data: " ++ show testData
-    putStrLn $ "Even squares: " ++ show (processEvenSquares testData)
-    putStrLn $ "Odd cubes: " ++ show (processOddCubes testData)
-    putStrLn $ "Sum of even squares: " ++ show (sumProcessedData even (\x -> x * x) testData)
-    putStrLn $ "Sum of odd cubes: " ++ show (sumProcessedData odd (\x -> x * x * x) testData)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show result
+    let inputData = [1, -2, 3, 0, 5, -8]
+    let processed = processData inputData
+    putStrLn $ "Original data: " ++ show inputData
+    putStrLn $ "Processed data: " ++ show processed
+    putStrLn $ "Data validation: " ++ show (validateData processed)
