ad3f4994031163a32b92703cf1ef309e2f7aeb41
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-23 15:57:08 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -15,3 +15,20 @@ exampleUsage = do
     let result = movingAverage windowSize dataSeries
     putStrLn $ "Data series: " ++ show dataSeries
     putStrLn $ "Moving average (window=" ++ show windowSize ++ "): " ++ show result
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processEvenSquares
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Processed data: " ++ show (processEvenSquares sampleData)
+    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData sampleData)
