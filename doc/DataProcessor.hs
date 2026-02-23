c0fb81955ddeab95d26c8700ebc93a1e33e589ff
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-23 21:16:08 +0000

    Add function to calculate moving average of a list

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -60,4 +60,13 @@ validateData :: [Int] -> Bool
 validateData xs = all (> 0) xs && length xs > 3
 
 combineResults :: [Int] -> [Int] -> [Int]
-combineResults xs ys = zipWith (+) (processData xs) (processData ys)
+combineResults xs ys = zipWith (+) (processData xs) (processData ys)module DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage windowSize xs
+    | windowSize <= 0 = error "Window size must be positive"
+    | length xs < windowSize = []
+    | otherwise = map average $ windows windowSize xs
+  where
+    average ys = sum ys / fromIntegral (length ys)
+    windows n = takeWhile (\w -> length w == n) . map (take n) . iterate tail
