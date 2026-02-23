3a1565b3387a2e8afebdad2da6fd9e4f2b39c0c0
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-23 14:47:34 +0000

    Add function to calculate moving average of a list

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -92,4 +92,21 @@ main = do
             putStrLn $ "Original: " ++ show validData
             putStrLn $ "Processed: " ++ show (processNumbers validData)
             putStrLn $ "Sum: " ++ show (sumProcessed validData)
-        Nothing -> putStrLn "Input contains invalid numbers"
+        Nothing -> putStrLn "Input contains invalid numbers"module DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . tails
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+-- Helper function from Data.List
+tails :: [a] -> [[a]]
+tails [] = [[]]
+tails xs@(_:ys) = xs : tails ys
