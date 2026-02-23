c0d393e903b9df01cf1af461303e8b0a41843234
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-23 20:53:00 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- ethjet/FibonacciSequence.hs
+++ ethjet/FibonacciSequence.hs
@@ -19,4 +19,14 @@ fibonacci n
 fibonacci :: Int -> [Int]
 fibonacci n = takeWhile (<= n) fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+  putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
+  mapM_ (putStrLn . show) (fibonacci n)
