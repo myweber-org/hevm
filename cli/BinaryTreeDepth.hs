3ae10dfa3c2a3b4be483504b98cf29ef0bfb8d0a
VioletInquiry <violet.inquiry@outlook.com>
2026-02-24 16:10:42 +0000

    Add function to calculate depth of a binary tree

--- cli/BinaryTreeDepth.hs
+++ cli/BinaryTreeDepth.hs
@@ -20,3 +20,21 @@ treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data B
 treeDepth :: BinaryTree a -> Int
 treeDepth Empty = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
+data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
+
+treeDepth :: BinaryTree a -> Int
+treeDepth Empty = 0
+treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
+
+sampleTree :: BinaryTree Int
+sampleTree = Node 1 
+                (Node 2 
+                    (Node 4 Empty Empty) 
+                    (Node 5 Empty Empty))
+                (Node 3 
+                    Empty 
+                    (Node 6 Empty Empty))
+
+main :: IO ()
+main = do
+    putStrLn $ "Depth of sample tree: " ++ show (treeDepth sampleTree)
