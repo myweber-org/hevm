eb9afbccf26128c1c3e5a50dbdb45e628fbcb6b8
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-24 18:19:41 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- doc/FibonacciSequence.hs
+++ doc/FibonacciSequence.hs
@@ -1,51 +1,32 @@
 module FibonacciSequence where
 
+import Data.Function (fix)
+
 fibonacci :: Int -> Integer
-fibonacci n = fibs !! n
-  where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-  putStrLn "First 20 Fibonacci numbers:"
-  mapM_ (putStrLn . show . fibonacci) [0..19]module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n
-    | n <= 0    = []
-    | n == 1    = [0]
-    | n == 2    = [0, 1]
-    | otherwise = let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-                  in take n fibs
-
-main :: IO ()
-main = do
-    putStrLn "Fibonacci sequence up to 10 terms:"
-    print $ fibonacci 10module FibonacciSequence where
-
-fibonacci :: Int -> [Int]
-fibonacci n = takeWhile (<= n) fibs
+fibonacci n = fibList !! n
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+    fibList = map fib [0..]
+    fib 0 = 0
+    fib 1 = 1
+    fib x = fibList !! (x - 1) + fibList !! (x - 2)
 
-main :: IO ()
-main = do
-    putStrLn "Enter a number:"
-    input <- getLine
-    let n = read input :: Int
-    putStrLn $ "Fibonacci numbers up to " ++ show n ++ ":"
-    print $ fibonacci nmodule FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
+-- Alternative implementation using fix for memoization
+fibonacci' :: Int -> Integer
+fibonacci' = (map fib [0..] !!)
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+    fib 0 = 0
+    fib 1 = 1
+    fib n = fibonacci' (n - 1) + fibonacci' (n - 2)
 
-main :: IO ()
-main = do
-  putStrLn "Fibonacci sequence up to 10 terms:"
-  print $ fibonacci 10module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
+-- Fast doubling algorithm for O(log n) Fibonacci
+fastFibonacci :: Int -> Integer
+fastFibonacci n
+    | n >= 0 = fst (fibPair n)
+    | otherwise = error "Negative input not supported"
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+    fibPair 0 = (0, 1)
+    fibPair n
+        | even n = (a * (2 * b - a), a * a + b * b)
+        | otherwise = (a * a + b * b, b * (2 * a + b))
+      where
+        (a, b) = fibPair (n `div` 2)
