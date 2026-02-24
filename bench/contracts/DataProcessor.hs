3009a9301cb2a5095d6db21b3546ab1d11999f81
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-24 00:35:28 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -107,4 +107,17 @@ calculateTrend values =
         xySum = sum $ zipWith (*) values [0..]
         x2Sum = sum $ map (^2) [0..n-1]
         slope = (n * xySum - xSum * ySum) / (n * x2Sum - xSum * xSum)
-    in Just slope
+    in Just slopemodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
