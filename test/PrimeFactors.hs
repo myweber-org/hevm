9a25f25c2fa72c0c78733d434a6261e55425aa41
ElaraWrites <elara.writes@outlook.com>
2026-02-24 15:31:49 +0000

    Add prime factorization function using trial division

--- test/PrimeFactors.hs
+++ test/PrimeFactors.hs
@@ -56,3 +56,13 @@ primeFactors n
     
     nextDivisor 2 = 3
     nextDivisor d = d + 2
+module PrimeFactors where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = factors n 2
+  where
+    factors 1 _ = []
+    factors m p
+      | m < p * p = [m]
+      | m `mod` p == 0 = p : factors (m `div` p) p
+      | otherwise = factors m (p + 1)
