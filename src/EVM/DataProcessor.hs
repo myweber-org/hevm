22dbb8c1a0e7bf43ca88d3d234bb5006a0d320bb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-24 22:32:41 +0000

    Add data processing module with validation and transformation functions

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -97,3 +97,45 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+import Data.Char (isDigit, isAlpha, toUpper)
+
+-- Validate if a string contains only digits
+validateNumeric :: String -> Bool
+validateNumeric = all isDigit
+
+-- Validate if a string contains only alphabetic characters
+validateAlpha :: String -> Bool
+validateAlpha = all isAlpha
+
+-- Transform string to uppercase
+transformToUpper :: String -> String
+transformToUpper = map toUpper
+
+-- Process a list of strings with validation and transformation
+processData :: [String] -> [(String, Bool, String)]
+processData = map processSingle
+  where
+    processSingle str = (str, validateNumeric str, transformToUpper str)
+
+-- Filter numeric strings from a list
+filterNumeric :: [String] -> [String]
+filterNumeric = filter validateNumeric
+
+-- Filter alphabetic strings from a list
+filterAlpha :: [String] -> [String]
+filterAlpha = filter validateAlpha
+
+-- Example usage function
+exampleUsage :: IO ()
+exampleUsage = do
+  let dataList = ["abc123", "456", "test", "789", "hello"]
+  putStrLn "Original data:"
+  print dataList
+  putStrLn "\nProcessed data (original, isNumeric, uppercase):"
+  print $ processData dataList
+  putStrLn "\nNumeric items:"
+  print $ filterNumeric dataList
+  putStrLn "\nAlphabetic items:"
+  print $ filterAlpha dataList
