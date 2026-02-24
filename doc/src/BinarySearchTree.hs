ac7f45812818aac7468b27b5089bceba77d49a16
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-24 23:18:17 +0000

    Add binary search tree insertion and search functions

--- doc/src/BinarySearchTree.hs
+++ doc/src/BinarySearchTree.hs
@@ -1,4 +1,3 @@
-
 module BinarySearchTree where
 
 data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
@@ -10,38 +9,13 @@ insert x (Node left val right)
     | x > val   = Node left val (insert x right)
     | otherwise = Node left val right
 
-inorder :: BST a -> [a]
-inorder Empty = []
-inorder (Node left val right) = inorder left ++ [val] ++ inorder right
-
-fromList :: Ord a => [a] -> BST a
-fromList = foldr insert Empty
-
-toSortedList :: BST a -> [a]
-toSortedList = inorder
-
-contains :: Ord a => a -> BST a -> Bool
-contains _ Empty = False
-contains x (Node left val right)
-    | x == val  = True
-    | x < val   = contains x left
-    | otherwise = contains x rightmodule BinarySearchTree where
-
-data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
-
-insert :: Ord a => a -> BST a -> BST a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
+search :: Ord a => a -> BST a -> Bool
+search _ Empty = False
+search x (Node left val right)
+    | x < val   = search x left
+    | x > val   = search x right
+    | otherwise = True
 
 inOrder :: BST a -> [a]
 inOrder Empty = []
 inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
-
-fromList :: Ord a => [a] -> BST a
-fromList = foldr insert Empty
-
-toSortedList :: BST a -> [a]
-toSortedList = inOrder
