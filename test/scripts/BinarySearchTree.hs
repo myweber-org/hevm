9096213bccf54ed7825890f43256c81fd2b3d46f
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-24 19:27:58 +0000

    Implement binary search tree insertion and in-order traversal

--- test/scripts/BinarySearchTree.hs
+++ test/scripts/BinarySearchTree.hs
@@ -1,30 +1,7 @@
 
 module BinarySearchTree where
 
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
-
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Empty = Node x Empty Empty
-insert x (Node y left right)
-    | x < y     = Node y (insert x left) right
-    | x > y     = Node y left (insert x right)
-    | otherwise = Node y left right
-
-inorder :: Tree a -> [a]
-inorder Empty = []
-inorder (Node x left right) = inorder left ++ [x] ++ inorder right
-
-fromList :: Ord a => [a] -> Tree a
-fromList = foldr insert Empty
-
-main :: IO ()
-main = do
-    let tree = fromList [5, 3, 7, 1, 4, 6, 8]
-    putStrLn "In-order traversal:"
-    print $ inorder treemodule BinarySearchTree where
-
-data BST a = Empty | Node (BST a) a (BST a)
-    deriving (Show, Eq)
+data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
 
 insert :: Ord a => a -> BST a -> BST a
 insert x Empty = Node Empty x Empty
@@ -36,3 +13,9 @@ insert x (Node left val right)
 inOrder :: BST a -> [a]
 inOrder Empty = []
 inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
+
+fromList :: Ord a => [a] -> BST a
+fromList = foldr insert Empty
+
+toSortedList :: BST a -> [a]
+toSortedList = inOrder
