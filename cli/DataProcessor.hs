5890f625aa915d4aa989e5a464351c0dc672760c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-24 15:52:42 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -3,46 +3,12 @@ module DataProcessor where
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (> -100) xs then Just xs else Nothingmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processData = filterAndTransform even (\x -> x * x + 1)
 
 main :: IO ()
 main = do
-    let input = [1, -2, 3, -4, 5]
+    let input = [1..10]
     let result = processData input
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processEvenNumbers :: [Int] -> [Int]
-processEvenNumbers = filterAndTransform even (*2)
-
-processOddNumbers :: [Int] -> [Int]
-processOddNumbers = filterAndTransform odd (+1)
-
-calculateStatistics :: [Int] -> (Int, Int, Double)
-calculateStatistics xs = (minimum xs, maximum xs, average)
-  where
-    average = fromIntegral (sum xs) / fromIntegral (length xs)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    putStrLn $ "Original: " ++ show numbers
-    putStrLn $ "Even doubled: " ++ show (processEvenNumbers numbers)
-    putStrLn $ "Odd incremented: " ++ show (processOddNumbers numbers)
-    putStrLn $ "Stats (min, max, avg): " ++ show (calculateStatistics numbers)
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
