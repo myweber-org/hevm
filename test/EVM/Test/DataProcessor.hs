c6a46e12d9b01ab912ade66c267b80649a63492f
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-24 14:21:14 +0000

    Add function to filter and transform a list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -82,4 +82,16 @@ exampleUsage = do
   putStrLn $ "\nCombined: " ++ combineWithSeparator " | " processedmodule DataProcessor where
 
 processNumbers :: [Int] -> [Int]
-processNumbers = map (^2) . filter (>0)
+processNumbers = map (^2) . filter (>0)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [1, -2, 3, -4, 5]
+    let result = processNumbers numbers
+    print result
