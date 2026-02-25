8eb2c1c55db6ede08340cea122f4c2cd403aa1a2
ElaraWrites <elara.writes@outlook.com>
2026-02-25 23:09:29 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -66,4 +66,16 @@ processData filepath threshold = do
             putStrLn $ "Median value: " ++ show (calculateMedian values)
             
             mapM_ (\(cat, avg) -> 
-                putStrLn $ "Category " ++ cat ++ " average: " ++ show avg) categories
+                putStrLn $ "Category " ++ cat ++ " average: " ++ show avg) categoriesmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
+
+combineResults :: [Int] -> [Int] -> [Int]
+combineResults xs ys = zipWith (+) (processData xs) (processData ys)
