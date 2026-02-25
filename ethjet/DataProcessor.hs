28b70c7151997e3555758feed0d265c5e06e87cf
FelixMakes <felix.makes@hotmail.com>
2026-02-25 22:36:55 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,19 +1,5 @@
 module DataProcessor where
 
-import Data.List (tails)
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | n > length xs = error "Window size exceeds list length"
-    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
-  where
-    average :: [Double] -> Double
-    average ws = sum ws / fromIntegral (length ws)
-
--- Example usage:
--- movingAverage 3 [1.0, 2.0, 3.0, 4.0, 5.0] -> [2.0, 3.0, 4.0]module DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
