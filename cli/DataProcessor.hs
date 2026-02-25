653c35bd3bb3bcc316715ed3b815cabd7d95a298
InkspillVerse <inkspill.verse@outlook.com>
2026-02-25 21:46:05 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -30,4 +30,16 @@ safeHead [] = Nothing
 safeHead (x:_) = Just x
 
 sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processData
+sumPositiveDoubles = sum . processDatamodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * x + 1)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    print result
