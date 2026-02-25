c0cf57619031bd2e93147151b47a9f905656cf03
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-25 15:12:12 +0000

    Add a simple word counting utility for text analysis

--- test/contracts/WordCounter.hs
+++ test/contracts/WordCounter.hs
@@ -20,3 +20,46 @@ displayResults = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
 analyzeText n = displayResults . topWords n
+module WordCounter where
+
+import Data.Char (isSpace)
+import qualified Data.Map.Strict as Map
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter (not . isPunctuation)
+    toLower c
+      | c >= 'A' && c <= 'Z' = toEnum (fromEnum c + 32)
+      | otherwise = c
+    isPunctuation c = c `elem` ".,!?;:\"\'()[]{}"
+
+topWords :: Int -> String -> [(String, Int)]
+topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+wordFrequency :: String -> [(String, Double)]
+wordFrequency text =
+  let counts = countWords text
+      total = fromIntegral $ sum $ Map.elems counts
+  in map (\(w, c) -> (w, fromIntegral c / total)) $ Map.toList counts
+
+uniqueWords :: String -> Int
+uniqueWords = Map.size . countWords
+
+totalWords :: String -> Int
+totalWords = length . words
+
+analyzeText :: String -> IO ()
+analyzeText text = do
+  putStrLn $ "Total words: " ++ show (totalWords text)
+  putStrLn $ "Unique words: " ++ show (uniqueWords text)
+  putStrLn "\nTop 10 words:"
+  mapM_ (\(w, c) -> putStrLn $ "  " ++ w ++ ": " ++ show c) $ topWords 10 text
+  putStrLn "\nWord frequencies:"
+  mapM_ (\(w, f) -> putStrLn $ "  " ++ w ++ ": " ++ show (round (f * 10000) / 100) ++ "%") 
+        $ take 5 $ wordFrequency text
