a59af5f1d21d6ac659647d82ca29988bfbe006f1
VioletInquiry <violet.inquiry@outlook.com>
2026-02-25 14:26:26 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -52,4 +52,30 @@ main = do
     let testData = [1..10]
     putStrLn $ "Original data: " ++ show testData
     putStrLn $ "Processed data: " ++ show (processEvenSquares testData)
-    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData testData)
+    putStrLn $ "Sum of processed data: " ++ show (sumProcessedData testData)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 10) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 0
+
+main :: IO ()
+main = do
+    let testData = [5, 12, 8, 15, 3, 20]
+    if validateData testData
+        then do
+            putStrLn "Original data:"
+            print testData
+            putStrLn "Processed data:"
+            print $ processData testData
+            putStrLn "Sum of processed data:"
+            print $ sumProcessedData testData
+        else putStrLn "Invalid input data"
