774eb6d39cd7f00bd88058b138792a9402e8c459
ElaraWrites <elara.writes@outlook.com>
2026-02-25 22:04:29 +0000

    Add a function to generate Fibonacci sequence up to a given number

--- doc/src/FibonacciSequence.hs
+++ doc/src/FibonacciSequence.hs
@@ -1,47 +1,6 @@
 module FibonacciSequence where
 
-import Data.Function.Memoize (memoize)
-
-fib :: Integer -> Integer
-fib = memoize fib'
-  where
-    fib' 0 = 0
-    fib' 1 = 1
-    fib' n = fib (n - 1) + fib (n - 2)
-
-main :: IO ()
-main = do
-    putStrLn "Fibonacci numbers from 0 to 10:"
-    mapM_ (print . fib) [0..10]
-module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
+fibonacci :: Integer -> [Integer]
+fibonacci n = takeWhile (<= n) fibs
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-    putStrLn "Enter the number of Fibonacci terms to generate:"
-    input <- getLine
-    let n = read input :: Int
-    if n <= 0
-        then putStrLn "Please enter a positive integer."
-        else print (fibonacci n)module FibonacciSequence where
-
-fibonacci :: Int -> [Integer]
-fibonacci n = take n fibs
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-    putStrLn "Enter the number of Fibonacci terms to generate:"
-    input <- getLine
-    let n = read input :: Int
-    print (fibonacci n)
