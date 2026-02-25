b62291ed06d9f0c2b27ae3e4c7626c3b04c39823
VioletInquiry <violet.inquiry@outlook.com>
2026-02-25 16:33:50 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -10,4 +10,7 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processData = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter (>0)
