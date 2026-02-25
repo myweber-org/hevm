02da33e31f7a1da4f86b61330cf9d2976d54835b
InkspillVerse <inkspill.verse@outlook.com>
2026-02-25 19:18:44 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -130,4 +130,20 @@ movingAverage n xs
 
 smoothData :: Int -> [Double] -> [Double]
 smoothData windowSize dataPoints =
-    movingAverage windowSize dataPoints ++ replicate (windowSize - 1) 0.0
+    movingAverage windowSize dataPoints ++ replicate (windowSize - 1) 0.0module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, 0, 5, -8]
+    if validateInput sampleData
+        then print $ processData sampleData
+        else putStrLn "Input validation failed"
