fb8aef02852684bfa045b650130e94a1c6dbf1b9
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-25 21:48:49 +0000

    Add function to filter and transform a list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -42,4 +42,16 @@ main :: IO ()
 main = do
     let numbers = [1..10]
     let result = processNumbers numbers
+    print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [-3, 1, 0, 5, -2, 8]
+    let result = processNumbers numbers
     print result
