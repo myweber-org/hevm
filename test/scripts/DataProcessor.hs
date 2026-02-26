04857e5794d71bfd8361626d7277cff2466f441a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-26 00:03:03 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -75,3 +75,13 @@ processCSVFile content columnIndex = do
       "Maximum: " ++ show maxVal,
       "Average: " ++ show avgVal
     ]
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData = all (> 0) . processData
