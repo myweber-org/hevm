f8a9a7bdbc5441cf5000f604a0bef65d87e7cbb5
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-26 18:45:49 +0000

    Add function to filter and transform list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -49,3 +49,20 @@ calculateTrend values =
         sumX2 = sum $ map (^2) indices
         slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX)
     in slope
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< -100) xs = Nothing
+    | any (> 100) xs = Nothing
+    | otherwise = Just xs
+
+safeProcess :: [Int] -> Maybe [Int]
+safeProcess xs = processData <$> validateInput xs
