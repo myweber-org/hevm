86ad3941a4538bc5f07c9240b3628c9ebfa8b4d8
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-26 16:41:07 +0000

    Implement Sieve of Eratosthenes to generate prime numbers up to a given limit

--- doc/src/code_examples/PrimeSieve.hs
+++ doc/src/code_examples/PrimeSieve.hs
@@ -1,60 +1,4 @@
-
 module PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-    where
-        go []     primes = reverse primes
-        go (x:xs) primes = go (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sievemodule PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve :: [Int] -> [Int]
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]module PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = go [2..limit] []
-  where
-    go []     acc = reverse acc
-    go (p:xs) acc = go [x | x <- xs, x `mod` p /= 0] (p:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieveHelper [2..limit] []
-
-sieveHelper :: [Int] -> [Int] -> [Int]
-sieveHelper [] primes = reverse primes
-sieveHelper (x:xs) primes = sieveHelper (filter (\n -> n `mod` x /= 0) xs) (x:primes)
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    let primes = sieve limit
-    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primesmodule PrimeSieve where
 
 sieve :: Int -> [Int]
 sieve limit
@@ -64,51 +8,10 @@ sieve limit
         sieve' [] primes = reverse primes
         sieve' (x:xs) primes = sieve' (filter (\n -> n `mod` x /= 0) xs) (x:primes)
 
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
 main :: IO ()
 main = do
-    putStrLn "Enter limit:"
+    putStrLn "Enter limit for prime generation:"
     input <- getLine
     let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-primes :: [Integer]
-primes = sieve [2..]
-  where
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
-
-nthPrime :: Int -> Integer
-nthPrime n = primes !! (n - 1)
-
-isPrime :: Integer -> Bool
-isPrime n
-    | n < 2     = False
-    | otherwise = all (\p -> n `mod` p /= 0) (takeWhile (\p -> p * p <= n) primes)module PrimeSieve where
-
-sieve :: Int -> [Int]
-sieve limit
-    | limit < 2 = []
-    | otherwise = sieve' [2..limit] []
-  where
-    sieve' []     acc = reverse acc
-    sieve' (p:xs) acc = sieve' (filter (\x -> x `mod` p /= 0) xs) (p:acc)
-
-primesUpTo :: Int -> [Int]
-primesUpTo = sieve
-
-main :: IO ()
-main = do
-    putStrLn "Enter limit:"
-    input <- getLine
-    let limit = read input :: Int
-    print $ primesUpTo limitmodule PrimeSieve where
-
-primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-    where
-        sieve [] = []
-        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+    let primes = sieve limit
+    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primes
