bf08ca0d08d71039790e2db3c626d859be3d32ea
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-26 19:46:01 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/contracts/fail/FibonacciSequence.hs
+++ test/contracts/fail/FibonacciSequence.hs
@@ -36,4 +36,14 @@ module FibonacciSequence where
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+    putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
+    mapM_ (putStrLn . show) (fibonacci n)
