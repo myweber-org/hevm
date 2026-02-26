3d87dfb750b7562a7dca40c8b2cca3d55d4c4eb8
InkspillVerse <inkspill.verse@outlook.com>
2026-02-26 22:48:27 +0000

    Add function to filter and transform a list of integers, extracting even numbers and computing their squares

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -53,4 +53,7 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processData = filterAndTransform (> 0) (* 2)module DataProcessor where
+
+processData :: [Int] -> [Int]
+processData xs = map (^2) (filter even xs)
