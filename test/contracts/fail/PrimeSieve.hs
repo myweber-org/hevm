87b188d9237f398e29b96b157cf29d698f0dda7e
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-26 22:42:13 +0000

    Implement Sieve of Eratosthenes to generate primes up to a given limit

--- test/contracts/fail/PrimeSieve.hs
+++ test/contracts/fail/PrimeSieve.hs
@@ -44,4 +44,22 @@ main = do
     input <- getLine
     let limit = read input :: Int
     let primes = primesUpTo limit
-    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primes
+    putStrLn $ "Primes up to " ++ show limit ++ ": " ++ show primesmodule PrimeSieve where
+
+sieve :: Int -> [Int]
+sieve limit
+    | limit < 2 = []
+    | otherwise = go [2..limit] []
+    where
+        go []     acc = reverse acc
+        go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
+
+primesUpTo :: Int -> [Int]
+primesUpTo = sieve
+
+main :: IO ()
+main = do
+    putStrLn "Enter limit:"
+    input <- getLine
+    let limit = read input :: Int
+    print $ primesUpTo limit
