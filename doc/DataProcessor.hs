d34ea06fab0d58e7e7c37e08a26f1483e2192921
heyQuim <hey.quim@hotmail.com>
2026-02-26 15:58:51 +0000

    Add function to calculate moving average of a list

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -23,4 +23,29 @@ main = do
     let sampleData = [1..10]
     let result = processData sampleData
     putStrLn $ "Original: " ++ show sampleData
-    putStrLn $ "Processed: " ++ show result
+    putStrLn $ "Processed: " ++ show resultmodule DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map avg $ filter (\window -> length window == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
+
+smoothData :: Fractional a => Int -> [a] -> [a]
+smoothData windowSize dataPoints =
+    movingAverage windowSize dataPoints
+
+calculateTrend :: Fractional a => [a] -> a
+calculateTrend values =
+    let n = fromIntegral $ length values
+        indices = [0..n-1]
+        sumX = sum indices
+        sumY = sum values
+        sumXY = sum $ zipWith (*) indices values
+        sumX2 = sum $ map (^2) indices
+        slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX)
+    in slope
