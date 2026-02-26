9c7ceb5219ea62c83168d35e220147e02b082ef4
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-26 19:13:32 +0000

    Add word frequency counter module with basic text processing functions

--- bench/contracts/WordFrequencyCounter.hs
+++ bench/contracts/WordFrequencyCounter.hs
@@ -1,64 +1,32 @@
+
 module WordFrequencyCounter where
-
-import qualified Data.Char as Char
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-
-type WordCount = Map.Map T.Text Int
-
-countWords :: T.Text -> WordCount
-countWords = Map.fromListWith (+) . map (\w -> (w, 1)) . filter (not . T.null) . map normalize . T.words
-  where
-    normalize = T.toLower . T.filter Char.isLetter
-
-sortByFrequency :: WordCount -> [(T.Text, Int)]
-sortByFrequency = List.sortBy (\(_, c1) (_, c2) -> compare c2 c1) . Map.toList
-
-formatOutput :: [(T.Text, Int)] -> T.Text
-formatOutput = T.unlines . map (\(word, count) -> T.pack (show count) <> " " <> word)
-
-processText :: T.Text -> T.Text
-processText = formatOutput . sortByFrequency . countWords
-
-main :: IO ()
-main = do
-    input <- TIO.getContents
-    TIO.putStr $ processText inputmodule WordFrequencyCounter where
 
 import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
-import System.Environment (getArgs)
 
 type WordCount = (String, Int)
 
 countWords :: String -> [WordCount]
 countWords text = 
     let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleaned = filter (all isAlpha) wordsList
-        grouped = foldr countHelper [] cleaned
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = foldr countWord [] cleanedWords
     in sortOn (Down . snd) grouped
   where
     cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-    countHelper word [] = [(word, 1)]
-    countHelper word ((w, c):rest)
-        | word == w = (w, c+1) : rest
-        | otherwise = (w, c) : countHelper word rest
+    
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
 
-formatOutput :: [WordCount] -> String
-formatOutput counts = unlines $ map (\(w, c) -> w ++ ": " ++ show c) counts
+topNWords :: Int -> String -> [WordCount]
+topNWords n text = take n $ countWords text
 
-processFile :: FilePath -> IO ()
-processFile filename = do
-    content <- readFile filename
-    let counts = countWords content
-    putStrLn $ formatOutput counts
+wordFrequency :: String -> String -> Maybe Int
+wordFrequency targetWord text = 
+    lookup (map toLower targetWord) $ countWords text
 
-main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [filename] -> processFile filename
-        _ -> putStrLn "Usage: wordfreq <filename>"
+totalUniqueWords :: String -> Int
+totalUniqueWords = length . countWords
