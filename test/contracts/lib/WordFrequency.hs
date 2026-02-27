307dcfddce3141428d54195d61d2e5b86f740e6d
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-27 15:32:26 +0000

    Add word frequency counting functionality

--- test/contracts/lib/WordFrequency.hs
+++ test/contracts/lib/WordFrequency.hs
@@ -1,33 +1,11 @@
 module WordFrequency where
 
-import qualified Data.Map as Map
-import Data.Char (isAlpha, toLower)
+import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
 
-type FrequencyMap = Map.Map String Int
-
-countWords :: String -> FrequencyMap
-countWords = foldr incrementWord Map.empty . words
-  where
-    incrementWord word = Map.insertWith (+) (normalize word) 1
-    normalize = map toLower . filter isAlpha
-
-getTopWords :: Int -> String -> [(String, Int)]
-getTopWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
-
-main :: IO ()
-main = do
-    content <- readFile "sample.txt"
-    let topWords = getTopWords 10 content
-    mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) topWordsmodule WordFrequency where
-
-import Data.Char (toLower, isAlpha)
-import Data.List (sortOn)
-import Data.Map (Map)
-import qualified Data.Map as Map
-
-type WordCount = Map String Int
+type WordCount = Map.Map String Int
 
 countWords :: String -> WordCount
 countWords = foldr incrementWord Map.empty . words
@@ -35,12 +13,8 @@ countWords = foldr incrementWord Map.empty . words
     incrementWord word = Map.insertWith (+) (normalize word) 1
     normalize = map toLower . filter isAlpha
 
-topWords :: Int -> String -> [(String, Int)]
-topWords n text = take n $ sortOn (negate . snd) $ Map.toList (countWords text)
+topWords :: Int -> WordCount -> [(String, Int)]
+topWords n = take n . sortOn (Down . snd) . Map.toList
 
-displayFrequency :: String -> IO ()
-displayFrequency text = do
-  putStrLn "Top 10 most frequent words:"
-  mapM_ printWord (topWords 10 text)
-  where
-    printWord (word, count) = putStrLn $ word ++ ": " ++ show count
+analyzeText :: Int -> String -> [(String, Int)]
+analyzeText n = topWords n . countWords
