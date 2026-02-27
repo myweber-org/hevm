5c4d743be47d05b5a1bd9e7dc40404a5605c64fc
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-27 22:30:34 +0000

    Add function to filter and transform a list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -43,4 +43,16 @@ movingAverage n xs
     windows m ys = take (length ys - m + 1) $ zipWith (const . take m) (tails ys) ys
     
     average :: Fractional a => [a] -> a
-    average zs = sum zs / fromIntegral (length zs)
+    average zs = sum zs / fromIntegral (length zs)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
