3d6450fc4a9ca9ee963f902d86da74c778c7b293
InkspillVerse <inkspill.verse@outlook.com>
2026-02-27 14:56:11 +0000

    Add function to filter and transform a list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -4,52 +4,11 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Bool
-validateInput = all (\x -> x >= -100 && x <= 100)
-
-safeProcess :: [Int] -> Maybe Int
-safeProcess xs
-    | validateInput xs = Just (sumProcessedData xs)
-    | otherwise = Nothing
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (>= -100) xs then Just xs else Nothing
+    let input = [1, -2, 3, 0, 5, -8]
+    let result = processNumbers input
+    putStrLn $ "Input: " ++ show input
+    putStrLn $ "Result: " ++ show result
