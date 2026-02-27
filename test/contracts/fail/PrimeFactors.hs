555abc2f5dab09c35d419f0f1adfa54ff3ce1818
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-27 16:45:37 +0000

    Add prime factorization function using trial division

--- test/contracts/fail/PrimeFactors.hs
+++ test/contracts/fail/PrimeFactors.hs
@@ -23,3 +23,18 @@ primeFactors n
       | m < p * p  = [m]
       | m `mod` p == 0 = p : factorize (m `div` p) p
       | otherwise      = factorize m (p + 1)
+module PrimeFactors where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n
+    | n <= 1    = []
+    | otherwise = factorize n 2
+    where
+        factorize 1 _ = []
+        factorize m p
+            | m `mod` p == 0 = p : factorize (m `div` p) p
+            | otherwise      = factorize m (nextPrime p)
+        
+        nextPrime p
+            | p == 2    = 3
+            | otherwise = p + 2
