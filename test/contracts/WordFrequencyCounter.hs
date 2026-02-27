4401f4848279ae191d257e8d5ef0f402880341e1
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-27 23:08:19 +0000

    Add word frequency counter module with basic text processing functions

--- test/contracts/WordFrequencyCounter.hs
+++ test/contracts/WordFrequencyCounter.hs
@@ -29,4 +29,36 @@ main = do
   mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) frequencies
   
   putStrLn "\nWords appearing at least 2 times:"
-  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) filtered
+  mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) filteredmodule WordFrequencyCounter where
+
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+-- | Count frequency of each word in a string
+countWords :: String -> [WordCount]
+countWords text = 
+    let words' = filter (not . null) $ map cleanWord $ words text
+        grouped = foldr countHelper [] words'
+    in sortOn (Down . snd) grouped
+  where
+    cleanWord = map toLower . filter isAlpha
+    countHelper word [] = [(word, 1)]
+    countHelper word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countHelper word rest
+
+-- | Get top N most frequent words
+topNWords :: Int -> String -> [WordCount]
+topNWords n text = take n $ countWords text
+
+-- | Pretty print word frequencies
+printFrequencies :: [WordCount] -> IO ()
+printFrequencies counts = 
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
+
+-- | Example usage
+exampleText :: String
+exampleText = "Hello world! Hello Haskell. Haskell is functional. World is imperative."
