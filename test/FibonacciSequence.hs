b1689a001bcb7a0278567e1a17422e205072a8cc
InkspillVerse <inkspill.verse@outlook.com>
2026-02-28 16:02:05 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/FibonacciSequence.hs
+++ test/FibonacciSequence.hs
@@ -16,3 +16,9 @@ fibonacciMemoized = fix memoFib
     memoFib _ 0 = 0
     memoFib _ 1 = 1
     memoFib f n = f (n - 1) + f (n - 2)
+module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
