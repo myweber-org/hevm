d0e9bdd24f6d954e78f325c471d39d7b3df1232b
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-28 19:36:56 +0000

    Add function to filter and transform a list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -165,4 +165,16 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 10) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, 0, 5, -7]
+    let result = processNumbers input
+    print result
