8ea058deed15e388af1189105723c417ef8cefa5
heyQuim <hey.quim@hotmail.com>
2026-02-28 16:40:42 +0000

    Add function to calculate moving average of a list

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -18,4 +18,35 @@ processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
 sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+sumProcessedData = sum . processDatamodule DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | length xs < n = []
+    | otherwise = avg : movingAverage n (tail xs)
+  where
+    window = take n xs
+    avg = sum window / fromIntegral n
+
+smoothData :: Fractional a => Int -> [a] -> [a]
+smoothData windowSize dataPoints =
+    let prefix = replicate (windowSize `div` 2) (head dataPoints)
+        suffix = replicate (windowSize `div` 2) (last dataPoints)
+        extendedData = prefix ++ dataPoints ++ suffix
+    in movingAverage windowSize extendedData
+
+calculateTrend :: (Fractional a, Ord a) => [a] -> String
+calculateTrend values
+    | allIncreasing = "Increasing trend"
+    | allDecreasing = "Decreasing trend"
+    | otherwise = "No clear trend"
+  where
+    pairs = zip values (tail values)
+    allIncreasing = all (uncurry (<)) pairs
+    allDecreasing = all (uncurry (>)) pairs
+
+processDataset :: Fractional a => Int -> [a] -> ([a], String)
+processDataset windowSize dataset =
+    let smoothed = smoothData windowSize dataset
+        trend = calculateTrend smoothed
+    in (smoothed, trend)
