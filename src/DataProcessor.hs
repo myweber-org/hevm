f6e4a0a00b5f9e4840d9e68ca2459c6f72fa7495
InkspillVerse <inkspill.verse@outlook.com>
2026-02-28 00:59:58 +0000

    Add function to filter and transform a list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -66,4 +66,16 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (* 2)
+processNumbers = filterAndTransform even (* 2)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
