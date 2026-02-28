f0a7bc916ffc59582c2de64e47ac04b723153cb1
heyQuim <hey.quim@hotmail.com>
2026-02-28 14:28:18 +0000

    Add function to calculate moving average of a list

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -12,4 +12,24 @@ sumProcessed = sum . processNumbers
 
 safeHead :: [Int] -> Maybe Int
 safeHead [] = Nothing
-safeHead (x:_) = Just x
+safeHead (x:_) = Just xmodule DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | length xs < n = []
+    | otherwise = avg : movingAverage n (tail xs)
+    where
+        window = take n xs
+        avg = sum window / fromIntegral n
+
+smoothData :: Fractional a => Int -> [a] -> [a]
+smoothData windowSize dataPoints =
+    let padSize = windowSize `div` 2
+        padded = replicate padSize (head dataPoints) ++ dataPoints ++ replicate padSize (last dataPoints)
+    in movingAverage windowSize padded
+
+processNumericStream :: Fractional a => Int -> [a] -> [a]
+processNumericStream windowSize stream =
+    if null stream
+        then []
+        else smoothData windowSize stream
