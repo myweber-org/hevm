19888c9b2806c729925f126a5777a555c9cc57b9
ElaraWrites <elara.writes@outlook.com>
2026-02-28 01:10:49 +0000

    Add word frequency counter with sorting and output formatting

--- test/contracts/WordFrequencyCounter.hs
+++ test/contracts/WordFrequencyCounter.hs
@@ -61,4 +61,31 @@ printFrequencies counts =
 
 -- | Example usage
 exampleText :: String
-exampleText = "Hello world! Hello Haskell. Haskell is functional. World is imperative."
+exampleText = "Hello world! Hello Haskell. Haskell is functional. World is imperative."module WordFrequencyCounter where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+type WordCount = Map.Map T.Text Int
+
+countWords :: T.Text -> WordCount
+countWords = Map.fromListWith (+) . map (\w -> (w, 1)) . filter (not . T.null) . map normalize . T.words
+  where
+    normalize = T.toLower . T.filter Char.isLetter
+
+sortByFrequency :: WordCount -> [(T.Text, Int)]
+sortByFrequency = List.sortBy (\(_, c1) (_, c2) -> compare c2 c1) . Map.toList
+
+formatOutput :: [(T.Text, Int)] -> T.Text
+formatOutput = T.unlines . map (\(word, count) -> T.pack (show count) <> " " <> word)
+
+processText :: T.Text -> T.Text
+processText = formatOutput . sortByFrequency . countWords
+
+main :: IO ()
+main = do
+    input <- TIO.getContents
+    TIO.putStr $ processText input
