236292855bbe66c4112c524ff78e144583eac23d
heyQuim <hey.quim@hotmail.com>
2026-02-28 23:53:59 +0000

    Add memoized Fibonacci function using Data.Map for performance

--- src/FibonacciMemoized.hs
+++ src/FibonacciMemoized.hs
@@ -12,4 +12,36 @@ fibonacci = memoize fib
 main :: IO ()
 main = do
     putStrLn "First 20 Fibonacci numbers:"
-    mapM_ (print . fibonacci) [0..19]
+    mapM_ (print . fibonacci) [0..19]module FibonacciMemoized where
+
+import qualified Data.Map as Map
+
+fib :: Int -> Integer
+fib n = fst $ fibMemo Map.empty n
+  where
+    fibMemo :: Map.Map Int (Integer, Integer) -> Int -> (Integer, Map.Map Int (Integer, Integer))
+    fibMemo cache 0 = (0, Map.insert 0 (0, 1) cache)
+    fibMemo cache 1 = (1, Map.insert 1 (1, 1) cache)
+    fibMemo cache n =
+      case Map.lookup n cache of
+        Just (result, _) -> (result, cache)
+        Nothing ->
+          let (a, cache1) = fibMemo cache (n - 1)
+              (b, cache2) = fibMemo cache1 (n - 2)
+              result = a + b
+          in (result, Map.insert n (result, a) cache2)
+
+fibFast :: Int -> Integer
+fibFast n = fst $ fibMemoFast Map.empty n
+  where
+    fibMemoFast :: Map.Map Int Integer -> Int -> (Integer, Map.Map Int Integer)
+    fibMemoFast cache n
+      | n <= 1 = (fromIntegral n, cache)
+      | otherwise =
+          case Map.lookup n cache of
+            Just result -> (result, cache)
+            Nothing ->
+              let (a, cache1) = fibMemoFast cache (n - 1)
+                  (b, cache2) = fibMemoFast cache1 (n - 2)
+                  result = a + b
+              in (result, Map.insert n result cache2)
