b34f23167ff8edc5b927dfe81948381cd8157f92
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-28 02:06:00 +0000

    Add word frequency counter module with basic text processing functions

--- test/EVM/Test/WordFrequencyCounter.hs
+++ test/EVM/Test/WordFrequencyCounter.hs
@@ -1,62 +1,28 @@
 module WordFrequencyCounter where
 
-import qualified Data.Char as Char
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-type WordFreq = Map.Map String Int
+type WordCount = Map String Int
 
-countWordFrequencies :: String -> WordFreq
-countWordFrequencies text =
-    let wordsList = filter (not . null) $ map normalize $ splitWords text
-    in foldr (\word -> Map.insertWith (+) word 1) Map.empty wordsList
-
-splitWords :: String -> [String]
-splitWords = words . map (\c -> if Char.isAlpha c then c else ' ')
-
-normalize :: String -> String
-normalize = map Char.toLower . filter Char.isAlpha
-
-getTopWords :: Int -> WordFreq -> [(String, Int)]
-getTopWords n freqMap =
-    take n $ List.sortBy (\(_, cnt1) (_, cnt2) -> compare cnt2 cnt1) $ Map.toList freqMap
-
-processText :: String -> IO ()
-processText text = do
-    let frequencies = countWordFrequencies text
-    let topWords = getTopWords 10 frequencies
-    
-    putStrLn "Top 10 most frequent words:"
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
-    
-    putStrLn $ "\nTotal unique words: " ++ show (Map.size frequencies)
-module WordFrequencyCounter where
-
-import qualified Data.Char as Char
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
-import System.Environment
-
-type WordCount = Map.Map String Int
-
+-- | Count word frequencies in a text string
 countWords :: String -> WordCount
-countWords = foldr incrementWord Map.empty . words
-  where
-    incrementWord word = Map.insertWith (+) (normalize word) 1
-    normalize = map Char.toLower . filter Char.isAlphaNum
+countWords text = Map.fromListWith (+) [(w, 1) | w <- words (normalizeText text)]
 
-formatResults :: WordCount -> String
-formatResults = unlines . map formatEntry . List.sortOn snd . Map.toList
-  where
-    formatEntry (word, count) = word ++ ": " ++ show count
+-- | Normalize text by converting to lowercase and removing non-alphabetic characters
+normalizeText :: String -> String
+normalizeText = unwords . map (filter isAlpha . map toLower) . words
 
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [] -> putStrLn "Usage: wordfreq <text>"
-    textPieces -> do
-      let text = unwords textPieces
-      let frequencies = countWords text
-      putStrLn "Word frequencies:"
-      putStrLn $ formatResults frequencies
+-- | Get top N most frequent words
+topNWords :: Int -> WordCount -> [(String, Int)]
+topNWords n = take n . sortOn (\(_, count) -> negate count) . Map.toList
+
+-- | Pretty print word frequencies
+printWordFrequencies :: [(String, Int)] -> String
+printWordFrequencies freqs = unlines [word ++ ": " ++ show count | (word, count) <- freqs]
+
+-- | Process a text and return top N words
+analyzeText :: Int -> String -> String
+analyzeText n text = printWordFrequencies $ topNWords n (countWords text)
