66d67c10638638ac35d01d208674133036f82208
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-28 00:18:57 +0000

    Add function to calculate moving average of a list

--- doc/src/DataProcessor.hs
+++ doc/src/DataProcessor.hs
@@ -1,37 +1,29 @@
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . tails
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+smoothData :: [Double] -> [Double]
+smoothData = movingAverage 3
 
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * x + 1)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print result
+calculateTrend :: [Double] -> Maybe Double
+calculateTrend [] = Nothing
+calculateTrend [_] = Nothing
+calculateTrend values = Just slope
+  where
+    n = fromIntegral $ length values
+    indices = [0..n-1]
+    sumX = sum indices
+    sumY = sum values
+    sumXY = sum $ zipWith (*) indices values
+    sumX2 = sum $ map (^2) indices
+    slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX)
