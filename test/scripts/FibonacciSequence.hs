fb020e1ebdb6e24f78dc5414a9ea1c5405d10637
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-28 20:08:28 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/scripts/FibonacciSequence.hs
+++ test/scripts/FibonacciSequence.hs
@@ -8,4 +8,14 @@ fibonacci n = takeWhile (<= n) fibs
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+    putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
+    mapM_ (putStrLn . show) (fibonacci n)
