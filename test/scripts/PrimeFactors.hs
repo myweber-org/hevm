60b3131c9350acb4e0c5978d2d515e25a2c5e493
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-28 19:33:49 +0000

    Add function to compute prime factors of an integer

--- test/scripts/PrimeFactors.hs
+++ test/scripts/PrimeFactors.hs
@@ -31,3 +31,16 @@ primeFactors n = factors n 2
       | m < p * p = [m]
       | m `mod` p == 0 = p : factors (m `div` p) p
       | otherwise = factors m (p + 1)
+module PrimeFactors (primeFactors) where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = factorize n 2
+  where
+    factorize 1 _ = []
+    factorize m p
+      | m < p * p = [m]
+      | m `mod` p == 0 = p : factorize (m `div` p) p
+      | otherwise = factorize m (nextPrime p)
+    
+    nextPrime 2 = 3
+    nextPrime p = p + 2
