4febef34f8b9706fdbbd7d0bb3afc705cffea124
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-01 14:22:47 +0000

    Add function to filter and transform a list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -10,38 +10,4 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-main :: IO ()
-main = do
-    let numbers = [-3, 1, 0, 5, -2, 8]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-import Data.List (tails)
-
-movingAverage :: Int -> [Double] -> [Double]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | n > length xs = error "Window size exceeds list length"
-    | otherwise = map average $ filter (\window -> length window == n) $ tails xs
-  where
-    average :: [Double] -> Double
-    average window = sum window / fromIntegral n
-
--- Example usage function
-exampleUsage :: IO ()
-exampleUsage = do
-    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-    putStrLn "Original data:"
-    print dataSeries
-    putStrLn "\n3-point moving average:"
-    print $ movingAverage 3 dataSeries
-    putStrLn "\n5-point moving average:"
-    print $ movingAverage 5 dataSeries
+    print result
