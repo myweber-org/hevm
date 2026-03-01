a665619cbf70ecea10a39bbc99233348d89420fe
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-01 16:10:58 +0000

    Add a function to generate the Fibonacci sequence up to n terms

--- test/EVM/FibonacciSequence.hs
+++ test/EVM/FibonacciSequence.hs
@@ -22,4 +22,9 @@ main = do
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
