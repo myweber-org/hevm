744fe881927f5bfe4a80d68d30333d5b38e6fe5d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-01 18:55:16 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -28,4 +28,16 @@ fibonacci n = take n fibs
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+    putStrLn "Enter the number of Fibonacci terms to generate:"
+    input <- getLine
+    let n = read input :: Int
+    print (fibonacci n)
