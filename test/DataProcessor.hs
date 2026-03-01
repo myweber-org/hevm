f29d9061890a145fe2221660f946405175336acb
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-01 23:13:57 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -6,29 +6,8 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-main :: IO ()
-main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
-    print resultmodule DataProcessor where
+validateData :: [Int] -> Bool
+validateData xs = all (> 0) xs && length xs > 3
 
-processData :: [Int] -> [Int]
-processData = map (^2) . filter (>0)module DataProcessor where
-
-processNumbers :: [Int] -> [Int]
-processNumbers = map (*2) . filter (>0)
-
-sampleData :: [Int]
-sampleData = [1, -5, 3, 0, -2, 8]
-
-main :: IO ()
-main = do
-    let result = processNumbers sampleData
-    putStrLn $ "Original list: " ++ show sampleData
-    putStrLn $ "Processed list: " ++ show resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+combineResults :: [Int] -> [Int] -> [Int]
+combineResults xs ys = zipWith (+) (processData xs) (processData ys)
