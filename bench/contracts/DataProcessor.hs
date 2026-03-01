9a14f4f654c11b8c2d788ac566aa1264977b01ca
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-01 01:07:12 +0000

    Add function to calculate moving average of a list

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -20,4 +20,13 @@ processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
 sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processData
+sumPositiveDoubles = sum . processDatamodule DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
+    average zs = sum zs / fromIntegral (length zs)
