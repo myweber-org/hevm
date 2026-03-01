eff2c24c9f07ea716a8fc952f59633fa6a13c678
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-01 21:01:44 +0000

    Add word frequency counter with case-insensitive sorting and filtering

--- doc/WordFrequencyCounter.hs
+++ doc/WordFrequencyCounter.hs
@@ -36,4 +36,37 @@ main :: IO ()
 main = do
     let sampleText = "Hello world! This is a test. Hello again world. Testing testing one two three."
     let results = analyzeText sampleText 1 5
-    displayResults results
+    displayResults resultsmodule WordFrequencyCounter where
+
+import Data.Char (toLower)
+import Data.List (sortOn, group, sort)
+import Data.Ord (Down(..))
+
+type WordCount = (String, Int)
+
+countWords :: String -> [WordCount]
+countWords text = 
+    let wordsList = words text
+        lowerWords = map (map toLower) wordsList
+        sortedWords = sort lowerWords
+        grouped = group sortedWords
+        counts = map (\ws -> (head ws, length ws)) grouped
+        sortedCounts = sortOn (Down . snd) counts
+    in sortedCounts
+
+filterByMinFrequency :: Int -> [WordCount] -> [WordCount]
+filterByMinFrequency minFreq = filter (\(_, count) -> count >= minFreq)
+
+getTopNWords :: Int -> [WordCount] -> [WordCount]
+getTopNWords n = take n
+
+displayWordCounts :: [WordCount] -> String
+displayWordCounts counts = 
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+
+processText :: String -> Int -> Int -> String
+processText text minFreq topN =
+    let counts = countWords text
+        filtered = filterByMinFrequency minFreq counts
+        topWords = getTopNWords topN filtered
+    in displayWordCounts topWords
