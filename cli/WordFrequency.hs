d3733ce245e1695878518e9944f2e1b65707e5c9
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-02 16:08:34 +0000

    Add word frequency counter for text analysis

--- cli/WordFrequency.hs
+++ cli/WordFrequency.hs
@@ -21,4 +21,26 @@ readAndAnalyze filepath = do
     content <- readFile filepath
     let topWords = topNWords 10 content
     putStrLn "Top 10 most frequent words:"
-    mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) topWords
+    mapM_ (\(w, c) -> putStrLn $ w ++ ": " ++ show c) topWordsmodule WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (isAlpha, toLower)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords = foldr updateCount Map.empty . words
+  where
+    updateCount word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
+
+topWords :: Int -> String -> [(String, Int)]
+topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
+
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequencies . topWords n
