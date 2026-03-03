b0d99796ba50e67eebcb1a9bd7bf930fc173aebf
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-03 00:52:30 +0000

    Add function to calculate moving average of a list

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -51,4 +51,22 @@ exampleUsage = do
     putStrLn "\nMoving average with window 3:"
     print $ movingAverage 3 dataSeries
     putStrLn "\nSafe moving average with invalid window 15:"
-    print $ safeMovingAverage 15 dataSeries
+    print $ safeMovingAverage 15 dataSeriesmodule DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+    where
+        windows :: Int -> [a] -> [[a]]
+        windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+        
+        average :: [Double] -> Double
+        average ys = sum ys / fromIntegral (length ys)
+
+smoothData :: [Double] -> [Double]
+smoothData = movingAverage 3
+
+testData :: [Double]
+testData = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
