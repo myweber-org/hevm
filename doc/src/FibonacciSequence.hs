f985284d26941ee9bf4d940ffa959bad4dbda750
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-03 14:20:19 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- doc/src/FibonacciSequence.hs
+++ doc/src/FibonacciSequence.hs
@@ -3,4 +3,9 @@ module FibonacciSequence where
 fibonacci :: Integer -> [Integer]
 fibonacci n = takeWhile (<= n) fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
