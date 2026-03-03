adaf2816dd3d2a4fdf3d5656b7ef8b2b3f6cb05f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-03 15:50:35 +0000

    Implement a binary search tree with insertion and in-order traversal functions

--- doc/BinarySearchTree.hs
+++ doc/BinarySearchTree.hs
@@ -55,3 +55,17 @@ isBST tree = isSorted (inOrder tree)
     where isSorted [] = True
           isSorted [_] = True
           isSorted (x:y:xs) = x <= y && isSorted (y:xs)
+module BinarySearchTree where
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert x Empty = Node x Empty Empty
+insert x (Node y left right)
+    | x < y     = Node y (insert x left) right
+    | x > y     = Node y left (insert x right)
+    | otherwise = Node y left right
+
+inOrder :: Tree a -> [a]
+inOrder Empty = []
+inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
