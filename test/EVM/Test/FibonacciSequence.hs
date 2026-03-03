5bb3b4ffe7258258c7231c29675f404be434c992
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-03 01:33:49 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- test/EVM/Test/FibonacciSequence.hs
+++ test/EVM/Test/FibonacciSequence.hs
@@ -19,4 +19,16 @@ fibonacci n = take n fibs
 printFibonacci :: Int -> IO ()
 printFibonacci n = do
   putStrLn $ "First " ++ show n ++ " Fibonacci numbers:"
-  mapM_ (putStrLn . show) (fibonacci n)
+  mapM_ (putStrLn . show) (fibonacci n)module FibonacciSequence where
+
+import Data.Function (fix)
+
+fibMemo :: Int -> Integer
+fibMemo = (map fib [0..] !!)
+  where
+    fib 0 = 0
+    fib 1 = 1
+    fib n = fibMemo (n - 1) + fibMemo (n - 2)
+
+fibonacci :: Int -> Integer
+fibonacci = fix (\f n -> if n < 2 then fromIntegral n else f (n - 1) + f (n - 2))
