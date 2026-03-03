a6b18f6c333419b51b49ba5524979222ac9b4063
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-03 15:50:04 +0000

    Implement Sieve of Eratosthenes for prime number generation up to a given limit

--- cli/PrimeSieve.hs
+++ cli/PrimeSieve.hs
@@ -1,10 +1,13 @@
-
 module PrimeSieve where
 
+sieve :: Int -> [Int]
+sieve limit
+    | limit < 2 = []
+    | otherwise = sieveHelper [2..limit] []
+
+sieveHelper :: [Int] -> [Int] -> [Int]
+sieveHelper []     primes = reverse primes
+sieveHelper (p:xs) primes = sieveHelper (filter (\x -> x `mod` p /= 0) xs) (p:primes)
+
 primesUpTo :: Int -> [Int]
-primesUpTo n
-    | n < 2     = []
-    | otherwise = sieve [2..n]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+primesUpTo = sieve
