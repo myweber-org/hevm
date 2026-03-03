af1f89fa02daf9a74472747c225d07265f4f1def
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-03 14:20:28 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- test/FibonacciSequence.hs
+++ test/FibonacciSequence.hs
@@ -22,3 +22,9 @@ fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
