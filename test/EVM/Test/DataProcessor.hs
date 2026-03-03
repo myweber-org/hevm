ae06f34aa3c62809b46bae88182597c892e3bb77
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-03 20:36:02 +0000

    Add function to calculate moving average of a list

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -52,4 +52,23 @@ validateInput xs =
     else Nothing
 
 safeProcess :: [Int] -> Maybe Int
-safeProcess = fmap sumProcessed . validateInput
+safeProcess = fmap sumProcessed . validateInputmodule DataProcessor where
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows :: Int -> [a] -> [[a]]
+    windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+    
+    average :: [Double] -> Double
+    average ys = sum ys / fromIntegral (length ys)
+
+smoothData :: [Double] -> [Double]
+smoothData = movingAverage 3
+
+calculateTrend :: [Double] -> Maybe Double
+calculateTrend [] = Nothing
+calculateTrend xs = Just (last xs - head xs)
