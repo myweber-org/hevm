f311364ee76213468046e1d087d183fd81ab3e05
FelixMakes <felix.makes@hotmail.com>
2026-03-03 01:13:01 +0000

    Add word frequency counter for text analysis

--- ethjet/WordFrequency.hs
+++ ethjet/WordFrequency.hs
@@ -9,28 +9,20 @@ type WordCount = (String, Int)
 countWords :: String -> [WordCount]
 countWords text = 
     let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleaned = filter (all isAlpha) wordsList
-        grouped = groupWords cleaned
+        cleanedWords = map (map toLower) wordsList
+        grouped = foldr countWord [] cleanedWords
     in take 10 $ sortOn (Down . snd) grouped
   where
-    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-    
-    groupWords :: [String] -> [WordCount]
-    groupWords = foldr incrementCount []
-    
-    incrementCount :: String -> [WordCount] -> [WordCount]
-    incrementCount word [] = [(word, 1)]
-    incrementCount word ((w, c):rest)
+    cleanWord = filter isAlpha
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
         | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : incrementCount word rest
+        | otherwise = (w, c) : countWord word rest
 
 displayResults :: [WordCount] -> IO ()
 displayResults counts = do
     putStrLn "Top 10 most frequent words:"
-    putStrLn "---------------------------"
     mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
 
 analyzeText :: String -> IO ()
-analyzeText text = do
-    let frequencies = countWords text
-    displayResults frequencies
+analyzeText text = displayResults $ countWords text
