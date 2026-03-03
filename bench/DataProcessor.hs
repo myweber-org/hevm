b4cf4c05b00601bc51d729508f90268070cbc13e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-03 15:27:49 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -1,62 +1,10 @@
-
 module DataProcessor where
 
-import Data.List (intercalate)
-import Text.Read (readMaybe)
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
 
-type Row = [String]
-type CSVData = [Row]
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-parseCSV :: String -> CSVData
-parseCSV content = map (splitOn ',') (lines content)
-  where
-    splitOn :: Char -> String -> [String]
-    splitOn delimiter = foldr splitHelper [""]
-      where
-        splitHelper char acc@(current:rest)
-          | char == delimiter = "":acc
-          | otherwise = (char:current):rest
-
-safeReadDouble :: String -> Maybe Double
-safeReadDouble = readMaybe
-
-computeColumnStats :: CSVData -> Int -> Maybe (Double, Double, Double)
-computeColumnStats rows colIndex
-  | null rows = Nothing
-  | otherwise = do
-      let columnValues = mapMaybe (safeReadDouble . (!! colIndex)) rows
-      if null columnValues
-        then Nothing
-        else let sumVal = sum columnValues
-                 count = fromIntegral (length columnValues)
-                 avg = sumVal / count
-                 minVal = minimum columnValues
-                 maxVal = maximum columnValues
-             in Just (avg, minVal, maxVal)
-
-mapMaybe :: (a -> Maybe b) -> [a] -> [b]
-mapMaybe _ [] = []
-mapMaybe f (x:xs) = case f x of
-  Just y -> y : mapMaybe f xs
-  Nothing -> mapMaybe f xs
-
-formatStats :: (Double, Double, Double) -> String
-formatStats (avg, minVal, maxVal) =
-  "Average: " ++ show avg ++
-  ", Min: " ++ show minVal ++
-  ", Max: " ++ show maxVal
-
-processCSVFile :: String -> Int -> String
-processCSVFile content colIndex =
-  case computeColumnStats (parseCSV content) colIndex of
-    Just stats -> formatStats stats
-    Nothing -> "Unable to compute statistics for column " ++ show colIndex
-
-sampleCSV :: String
-sampleCSV = intercalate "\n"
-  [ "Name,Age,Salary"
-  , "Alice,30,50000"
-  , "Bob,25,45000"
-  , "Charlie,35,60000"
-  , "Diana,28,52000"
-  ]
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
