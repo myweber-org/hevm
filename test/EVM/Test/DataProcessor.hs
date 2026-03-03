242a80db1a87ecd40d5542ce399ae84c64b21700
InkspillVerse <inkspill.verse@outlook.com>
2026-03-03 23:05:13 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -72,3 +72,17 @@ smoothData = movingAverage 3
 calculateTrend :: [Double] -> Maybe Double
 calculateTrend [] = Nothing
 calculateTrend xs = Just (last xs - head xs)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processData
