8fb8678a0bb88ad05ed4bfaf0334c767642272da
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-03 21:12:38 +0000

    Add prime factorization function using trial division

--- test/contracts/pass/PrimeFactors.hs
+++ test/contracts/pass/PrimeFactors.hs
@@ -16,4 +16,18 @@ primeFactors n = factors n 2
     factors m p
       | m < p * p = [m]
       | m `mod` p == 0 = p : factors (m `div` p) p
-      | otherwise = factors m (p + 1)
+      | otherwise = factors m (p + 1)module PrimeFactors where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n
+  | n <= 1 = []
+  | otherwise = factorize n 2
+  where
+    factorize 1 _ = []
+    factorize m p
+      | m `mod` p == 0 = p : factorize (m `div` p) p
+      | p * p > m = [m]
+      | otherwise = factorize m (nextPrime p)
+    
+    nextPrime 2 = 3
+    nextPrime p = p + 2
