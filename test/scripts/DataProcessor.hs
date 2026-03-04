ab6abd0430a8c67b54039fa492b9d2bbe55ebabc
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-04 16:12:00 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -1,65 +1,14 @@
+
 module DataProcessor where
-
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-  where
-    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
-    average zs = sum zs / fromIntegral (length zs)
-
--- Helper function similar to Data.List.tails
-tails :: [a] -> [[a]]
-tails [] = [[]]
-tails (x:xs) = (x:xs) : tails xsmodule DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * 2 + 1)
-
-main :: IO ()
-main = do
-    let numbers = [1..20]
-    let result = processNumbers numbers
-    putStrLn $ "Original list: " ++ show numbers
-    putStrLn $ "Processed list: " ++ show resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
-safeHead :: [Int] -> Maybe Int
-safeHead [] = Nothing
-safeHead (x:_) = Just x
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
 
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles xs = 
-    let processed = processNumbers xs
-    in sum processed
-
-main :: IO ()
-main = do
-    let testData = [-3, 2, 0, 5, -1, 8]
-    putStrLn $ "Original: " ++ show testData
-    putStrLn $ "Processed: " ++ show (processNumbers testData)
-    putStrLn $ "Sum: " ++ show (sumPositiveDoubles testData)
-    putStrLn $ "Head: " ++ show (safeHead testData)
-module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
-
-sumProcessedList :: [Int] -> Int
-sumProcessedList = foldl' (+) 0 . processEvenSquares
-  where
-    foldl' f acc [] = acc
-    foldl' f acc (x:xs) = foldl' f (f acc x) xs
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -100) xs then Just xs else Nothing
