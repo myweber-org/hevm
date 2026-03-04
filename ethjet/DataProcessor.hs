3cc6e221b82ee369bb90fe385c7cd73a2719ceca
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-04 16:59:48 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives.

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -5,3 +5,7 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+processNumbers :: [Int] -> [Int]
+processNumbers = map (^2) . filter (>0)
