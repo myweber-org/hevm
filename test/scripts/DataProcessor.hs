2e843099f0c08336fa954cff621a9c1e9cee24f9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-04 02:28:34 +0000

    Add function to filter and transform list of integers

--- test/scripts/DataProcessor.hs
+++ test/scripts/DataProcessor.hs
@@ -12,4 +12,17 @@ movingAverage n xs
 -- Helper function similar to Data.List.tails
 tails :: [a] -> [[a]]
 tails [] = [[]]
-tails (x:xs) = (x:xs) : tails xs
+tails (x:xs) = (x:xs) : tails xsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * 2 + 1)
+
+main :: IO ()
+main = do
+    let numbers = [1..20]
+    let result = processNumbers numbers
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show result
