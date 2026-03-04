ebe76080522c26d0574b8b337031999db7d2e43a
heyQuim <hey.quim@hotmail.com>
2026-03-04 22:52:36 +0000

    Add function to filter and transform list of integers

--- test/EVM/DataProcessor.hs
+++ test/EVM/DataProcessor.hs
@@ -18,4 +18,14 @@ parseDateFromRecord :: Record -> Maybe Day
 parseDateFromRecord record =
     case record of
         (dateStr:_) -> parseTimeM True defaultTimeLocale "%Y-%m-%d" dateStr
-        _ -> Nothing
+        _ -> Nothingmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processNumbers
