648d7dafb2ae0d0f0aa54550c426757979473611
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-04 15:50:12 +0000

    Add a function to generate Fibonacci sequence up to a given number

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -40,4 +40,9 @@ main = do
     putStrLn "Enter the number of Fibonacci terms to generate:"
     input <- getLine
     let n = read input :: Int
-    print (fibonacci n)
+    print (fibonacci n)module FibonacciSequence where
+
+fibonacci :: Int -> [Int]
+fibonacci n = takeWhile (<= n) fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
