5d06d283e7ad504b18310cc484b40136a943841e
heyQuim <hey.quim@hotmail.com>
2026-03-04 01:24:48 +0000

    Add word frequency analysis function

--- test/contracts/lib/WordFrequency.hs
+++ test/contracts/lib/WordFrequency.hs
@@ -17,4 +17,28 @@ topWords :: Int -> WordCount -> [(String, Int)]
 topWords n = take n . sortOn (Down . snd) . Map.toList
 
 analyzeText :: Int -> String -> [(String, Int)]
-analyzeText n = topWords n . countWords
+analyzeText n = topWords n . countWordsmodule WordFrequency where
+
+import qualified Data.Map.Strict as Map
+import Data.Char (toLower, isAlpha)
+import Data.List (sortOn)
+import Data.Ord (Down(..))
+
+type FrequencyMap = Map.Map String Int
+
+countWords :: String -> FrequencyMap
+countWords text = 
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+    in foldr (\word -> Map.insertWith (+) word 1) Map.empty wordsList
+  where
+    cleanWord = map toLower . filter isAlpha
+
+getTopWords :: Int -> String -> [(String, Int)]
+getTopWords n text = 
+    take n $ sortOn (Down . snd) $ Map.toList $ countWords text
+
+analyzeText :: String -> IO ()
+analyzeText text = do
+    let topWords = getTopWords 10 text
+    putStrLn "Top 10 most frequent words:"
+    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
