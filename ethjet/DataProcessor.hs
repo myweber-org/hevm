9e06a95598c5a150e463901e9ba05e2a97000565
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-04 14:40:41 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,47 +1,7 @@
 module DataProcessor where
 
-import Data.List (intercalate)
-import Data.Char (isDigit, isAlpha)
-
-type CSVRow = [String]
-type ValidationError = String
-
-validateCSVRow :: CSVRow -> Either ValidationError CSVRow
-validateCSVRow [] = Left "Empty row"
-validateCSVRow row
-    | length row < 2 = Left "Row must have at least 2 columns"
-    | not (isValidId (head row)) = Left "Invalid ID format"
-    | not (isValidName (row !! 1)) = Left "Invalid name format"
-    | otherwise = Right row
-  where
-    isValidId :: String -> Bool
-    isValidId = all isDigit
-
-    isValidName :: String -> Bool
-    isValidName name = length name >= 2 && all isAlpha name
-
-processCSVData :: [CSVRow] -> Either ValidationError [CSVRow]
-processCSVData rows = traverse validateCSVRow rows
-
-formatValidRows :: [CSVRow] -> String
-formatValidRows rows = intercalate "\n" (map formatRow rows)
-  where
-    formatRow :: CSVRow -> String
-    formatRow = intercalate ","
-
-safeCSVProcessor :: [CSVRow] -> Either ValidationError String
-safeCSVProcessor rows = do
-    validated <- processCSVData rows
-    return $ formatValidRows validatedmodule DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Bool
-validateInput = all (\x -> x >= -100 && x <= 100)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
