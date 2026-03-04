6501c53dc644323976f2c4ade8d9d87010320282
InkspillVerse <inkspill.verse@outlook.com>
2026-03-04 17:19:56 +0000

    Add word frequency counter for text analysis

--- test/contracts/pass/WordFrequency.hs
+++ test/contracts/pass/WordFrequency.hs
@@ -2,48 +2,23 @@ module WordFrequency where
 
 import qualified Data.Map.Strict as Map
 import Data.Char (isAlpha, toLower)
-
-countWords :: String -> Map.Map String Int
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-    in foldr (\word -> Map.insertWith (+) word 1) Map.empty wordsList
-  where
-    cleanWord = map toLower . filter isAlpha
-
-printWordFrequencies :: String -> IO ()
-printWordFrequencies text = do
-    let freqMap = countWords text
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) 
-          (Map.toList freqMap)module WordFrequency where
-
-import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
 
-type WordCount = (String, Int)
+type FrequencyMap = Map.Map String Int
 
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map clean $ words text
-        cleaned = map toLower <$> wordsList
-        counts = foldr (\word acc -> 
-            case lookup word acc of
-                Just n -> (word, n+1) : filter ((/= word) . fst) acc
-                Nothing -> (word, 1) : acc) [] cleaned
-    in sortOn (Down . snd) counts
-    where
-        clean = filter isAlpha
+countWords :: String -> FrequencyMap
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
 
-formatOutput :: [WordCount] -> String
-formatOutput counts = 
-    unlines $ map (\(word, count) -> word ++ ": " ++ show count) counts
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
 
-processText :: String -> String
-processText = formatOutput . countWords
-
-main :: IO ()
-main = do
-    putStrLn "Enter text to analyze word frequency:"
-    input <- getContents
-    putStrLn "\nWord frequencies:"
-    putStrLn $ processText input
+analyzeText :: String -> IO ()
+analyzeText text = do
+  putStrLn "Top 10 most frequent words:"
+  mapM_ printWord (topNWords 10 text)
+  where
+    printWord (word, count) = putStrLn $ word ++ ": " ++ show count
