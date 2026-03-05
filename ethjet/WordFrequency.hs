92a956cdd157461e30d43a21736e8fa7a37264d4
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-05 16:40:58 +0000

    Add word frequency counter for text analysis

--- ethjet/WordFrequency.hs
+++ ethjet/WordFrequency.hs
@@ -1,51 +1,23 @@
 module WordFrequency where
 
-import Data.Char (toLower, isAlpha)
-import Data.List (sortOn)
-import Data.Ord (Down(..))
-
-type WordCount = (String, Int)
-
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleanedWords = map (map toLower) wordsList
-        grouped = foldr countWord [] cleanedWords
-    in take 10 $ sortOn (Down . snd) grouped
-  where
-    cleanWord = filter isAlpha
-    countWord word [] = [(word, 1)]
-    countWord word ((w, c):rest)
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord word rest
-
-displayResults :: [WordCount] -> IO ()
-displayResults counts = do
-    putStrLn "Top 10 most frequent words:"
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) counts
-
-analyzeText :: String -> IO ()
-analyzeText text = displayResults $ countWords textmodule WordFrequency where
-
+import qualified Data.Map.Strict as Map
 import Data.Char (isAlpha, toLower)
 import Data.List (sortOn)
-import Data.Map (Map)
-import qualified Data.Map as Map
 import Data.Ord (Down(..))
 
-type WordCount = Map String Int
+type FrequencyMap = Map.Map String Int
 
-countWords :: String -> WordCount
+countWords :: String -> FrequencyMap
 countWords = foldr incrementWord Map.empty . words
   where
-    incrementWord w m = Map.insertWith (+) (normalize w) 1 m
+    incrementWord word = Map.insertWith (+) (normalize word) 1
     normalize = map toLower . filter isAlpha
 
 topWords :: Int -> String -> [(String, Int)]
 topWords n text = take n $ sortOn (Down . snd) $ Map.toList (countWords text)
 
-displayFrequency :: [(String, Int)] -> String
-displayFrequency = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies = unlines . map (\(w, c) -> w ++ ": " ++ show c)
 
 analyzeText :: Int -> String -> String
-analyzeText n = displayFrequency . topWords n
+analyzeText n = displayFrequencies . topWords n
