4728fd6744ef6d62304c431b03a686d25a4d0d85
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-05 20:27:55 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- bench/FibonacciSequence.hs
+++ bench/FibonacciSequence.hs
@@ -83,4 +83,14 @@ main = do
     putStrLn "Enter the number of terms:"
     input <- getLine
     let n = read input :: Int
-    print $ fibonacci n
+    print $ fibonacci nmodule FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+    putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
+    mapM_ (putStrLn . show) (fibonacci n)
