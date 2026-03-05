39b83476af3fe26fff45207e3cb908e6d6701058
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-05 20:10:48 +0000

    Add a simple word counting utility for text processing tasks

--- test/EVM/Test/WordCounter.hs
+++ test/EVM/Test/WordCounter.hs
@@ -65,4 +65,33 @@ main = do
     args <- getArgs
     case args of
         [filename] -> processFile filename
-        _ -> putStrLn "Usage: wordcounter <filename>"
+        _ -> putStrLn "Usage: wordcounter <filename>"module WordCounter where
+
+import Data.Char (isSpace)
+import qualified Data.Map.Strict as Map
+import Data.List (sortOn)
+
+type WordCount = Map.Map String Int
+
+countWords :: String -> WordCount
+countWords = foldr incrementWord Map.empty . words
+  where
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter (not . isSpace)
+    toLower c
+      | c >= 'A' && c <= 'Z' = toEnum (fromEnum c + 32)
+      | otherwise = c
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (negate . snd) $ Map.toList (countWords text)
+
+wordFrequencyReport :: String -> String
+wordFrequencyReport text = unlines $ map formatEntry $ topNWords 10 text
+  where
+    formatEntry (word, count) = word ++ ": " ++ show count
+
+processTextFile :: FilePath -> IO ()
+processTextFile path = do
+  content <- readFile path
+  putStrLn $ "Top 10 words in " ++ path ++ ":"
+  putStrLn $ wordFrequencyReport content
