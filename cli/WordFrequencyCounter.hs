7758b00f21a04dfc118fb570c7386c3264426cff
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-05 14:50:30 +0000

    Add word frequency counter with sorting and output formatting

--- cli/WordFrequencyCounter.hs
+++ cli/WordFrequencyCounter.hs
@@ -355,4 +355,31 @@ displayFrequencies freqList =
     unlines $ map (\(word, count) -> word ++ ": " ++ show count) freqList
 
 processText :: String -> String
-processText = displayFrequencies . countWordFrequencies
+processText = displayFrequencies . countWordFrequenciesmodule WordFrequencyCounter where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+type WordCount = Map.Map T.Text Int
+
+countWords :: T.Text -> WordCount
+countWords = Map.fromListWith (+) . map (\w -> (w, 1)) . filter (not . T.null) . map normalize . T.words
+  where
+    normalize = T.toLower . T.filter Char.isLetter
+
+sortWordCounts :: WordCount -> [(T.Text, Int)]
+sortWordCounts = List.sortBy (\(_, cnt1) (_, cnt2) -> compare cnt2 cnt1) . Map.toList
+
+formatOutput :: [(T.Text, Int)] -> T.Text
+formatOutput = T.unlines . map (\(word, count) -> T.concat [word, T.pack ": ", T.pack (show count)])
+
+processText :: T.Text -> T.Text
+processText = formatOutput . sortWordCounts . countWords
+
+main :: IO ()
+main = do
+    input <- TIO.getContents
+    TIO.putStrLn $ processText input
