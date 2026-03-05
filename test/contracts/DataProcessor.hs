fb2faadca05c075686e4ebba38141d6c6fe0a342
VioletInquiry <violet.inquiry@outlook.com>
2026-03-05 01:19:53 +0000

    Add function to parse CSV data and compute summary statistics

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -1,13 +1,47 @@
+
 module DataProcessor where
 
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
+import Data.List (foldl')
+import Text.CSV (parseCSV, Record)
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+type SummaryStats = (Double, Double, Double, Double)
 
-validateData :: [Int] -> Bool
-validateData xs = all (> 0) xs && length xs > 3
+computeStats :: [Double] -> SummaryStats
+computeStats [] = (0, 0, 0, 0)
+computeStats xs = (minimum xs, maximum xs, mean, stdDev)
+  where
+    n = fromIntegral (length xs)
+    mean = sum xs / n
+    variance = sum (map (\x -> (x - mean) ** 2) xs) / n
+    stdDev = sqrt variance
 
-combineResults :: [Int] -> [Int] -> [Int]
-combineResults xs ys = zipWith (+) (processData xs) (processData ys)
+parseNumericColumn :: String -> Maybe [Double]
+parseNumericColumn input = case parseCSV "" input of
+    Left _ -> Nothing
+    Right csv -> extractColumn csv
+  where
+    extractColumn :: [Record] -> Maybe [Double]
+    extractColumn [] = Nothing
+    extractColumn (header:rows) = 
+        if null rows then Nothing
+        else traverse parseRow rows
+    
+    parseRow :: Record -> Maybe Double
+    parseRow [] = Nothing
+    parseRow (cell:_) = 
+        case reads cell of
+            [(val, "")] -> Just val
+            _ -> Nothing
+
+processCSVData :: String -> Maybe SummaryStats
+processCSVData csvContent = do
+    numericData <- parseNumericColumn csvContent
+    return $ computeStats numericData
+
+formatStats :: SummaryStats -> String
+formatStats (minVal, maxVal, meanVal, stdVal) =
+    "Statistics:\n" ++
+    "  Minimum: " ++ show minVal ++ "\n" ++
+    "  Maximum: " ++ show maxVal ++ "\n" ++
+    "  Mean: " ++ show meanVal ++ "\n" ++
+    "  Std Dev: " ++ show stdVal
