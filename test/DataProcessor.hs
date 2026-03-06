c847432595b91194832efe740ed69fb839f5d794
VioletInquiry <violet.inquiry@outlook.com>
2026-03-06 15:59:51 +0000

    Add function to filter and transform a list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -103,4 +103,16 @@ isWithinDateRange start end record =
         _ -> False
 
 parseDate :: String -> Maybe Day
-parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
+parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
