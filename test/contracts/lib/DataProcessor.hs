8e6411826bc4fd332436c527800275df15b5e0db
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-06 21:01:03 +0000

    Add function to filter and transform list of integers

--- test/contracts/lib/DataProcessor.hs
+++ test/contracts/lib/DataProcessor.hs
@@ -6,44 +6,5 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Bool
-validateInput xs = all (\x -> x >= -100 && x <= 100) xs
-
-safeProcess :: [Int] -> Maybe [Int]
-safeProcess xs
-  | validateInput xs = Just (processData xs)
-  | otherwise = Nothingmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform even (\x -> x * x + 1)
-
-main :: IO ()
-main = do
-    let numbers = [1..10]
-    let result = processNumbers numbers
-    print resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
 sumProcessedData :: [Int] -> Int
 sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (> -1000) xs then Just xs else Nothing
-
-main :: IO ()
-main = do
-    let sampleData = [1, -2, 3, 0, 5, -8]
-    case validateInput sampleData of
-        Just validData -> do
-            putStrLn $ "Original data: " ++ show validData
-            putStrLn $ "Processed data: " ++ show (processData validData)
-            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
-        Nothing -> putStrLn "Input contains values below -1000"
