7ee11e8e6f4dbd05a2606cee7ed2ec0b7796f4d0
FelixMakes <felix.makes@hotmail.com>
2026-03-06 01:28:31 +0000

    Add function to filter and transform a list of integers

--- doc/DataProcessor.hs
+++ doc/DataProcessor.hs
@@ -111,4 +111,10 @@ main = do
     let sampleData = [1, -5, 3, 0, 8, -2]
     case validateInput sampleData of
         Just validData -> print $ processData validData
-        Nothing -> putStrLn "Invalid input detected"
+        Nothing -> putStrLn "Invalid input detected"module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
