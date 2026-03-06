2c566b88cf72e6a48e55fb29096b93b265ac14fd
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-06 19:24:17 +0000

    Add function to filter and transform a list of integers, removing negatives and squaring positives

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -69,4 +69,13 @@ sumProcessed = sum . processNumbers
 
 safeHead :: [Int] -> Maybe Int
 safeHead [] = Nothing
-safeHead (x:_) = Just x
+safeHead (x:_) = Just xmodule DataProcessor where
+
+processData :: [Int] -> [Int]
+processData = map (^2) . filter (>0)
+
+validateData :: [Int] -> Bool
+validateData xs = all (>=0) (processData xs)
+
+sampleData :: [Int]
+sampleData = [1, -2, 3, -4, 5]
