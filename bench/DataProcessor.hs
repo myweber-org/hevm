0d960230750239e3a76d6e4306b9a45c0cef1159
InkspillVerse <inkspill.verse@outlook.com>
2026-03-06 19:03:43 +0000

    Add function to parse CSV data and calculate average values

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -22,4 +22,47 @@ sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
 
 validateInput :: [Int] -> Maybe [Int]
-validateInput xs = if all (> -100) xs then Just xs else Nothing
+validateInput xs = if all (> -100) xs then Just xs else Nothingmodule DataProcessor where
+
+import Data.List.Split (splitOn)
+
+type Row = [String]
+type CSVData = [Row]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
+
+numericColumns :: CSVData -> [Int]
+numericColumns [] = []
+numericColumns (header:_) = 
+    [i | (i, cell) <- zip [0..] header, all isNumericChar cell]
+  where
+    isNumericChar c = c `elem` "0123456789.-"
+
+columnAverage :: CSVData -> Int -> Maybe Double
+columnAverage rows colIndex
+    | null numericValues = Nothing
+    | otherwise = Just (sum numericValues / fromIntegral (length numericValues))
+  where
+    numericValues = [read cell | row <- rows, 
+                   length row > colIndex,
+                   let cell = row !! colIndex,
+                   all (\c -> c `elem` "0123456789.-") cell]
+
+processCSVFile :: String -> IO ()
+processCSVFile filename = do
+    content <- readFile filename
+    let dataRows = parseCSV content
+    case dataRows of
+        [] -> putStrLn "Empty CSV file"
+        (header:rows) -> do
+            putStrLn $ "Processing CSV with columns: " ++ unwords header
+            let numCols = numericColumns dataRows
+            mapM_ (\col -> 
+                case columnAverage (header:rows) col of
+                    Just avg -> putStrLn $ 
+                        "Average for column " ++ show col ++ 
+                        " (" ++ header !! col ++ "): " ++ show avg
+                    Nothing -> putStrLn $
+                        "No numeric data in column " ++ show col
+                ) numCols
