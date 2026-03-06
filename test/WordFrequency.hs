f0a2ca283adcced37a599661e2cb9b62fa80b12a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-06 20:07:54 +0000

    Add word frequency counting functionality to analyze text

--- test/WordFrequency.hs
+++ test/WordFrequency.hs
@@ -2,29 +2,22 @@ module WordFrequency where
 
 import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
-import Data.Ord (Down(..))
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-type WordCount = (String, Int)
+type FrequencyMap = Map String Int
 
-countWords :: String -> [WordCount]
-countWords text = 
-    let wordsList = filter (not . null) $ map cleanWord $ words text
-        cleanedWords = filter (all isAlpha) wordsList
-        grouped = foldr countWord [] cleanedWords
-    in take 10 $ sortOn (Down . snd) grouped
+countWords :: String -> FrequencyMap
+countWords = foldr incrementWord Map.empty . words
   where
-    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
-    
-    countWord :: String -> [WordCount] -> [WordCount]
-    countWord word [] = [(word, 1)]
-    countWord word ((w, c):rest)
-        | w == word = (w, c + 1) : rest
-        | otherwise = (w, c) : countWord word rest
+    incrementWord word = Map.insertWith (+) (normalize word) 1
+    normalize = map toLower . filter isAlpha
 
-displayResults :: [WordCount] -> String
-displayResults counts = 
-    "Top 10 most frequent words:\n" ++
-    unlines (map (\(word, count) -> word ++ ": " ++ show count) counts)
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text = take n $ sortOn (negate . snd) $ Map.toList $ countWords text
 
-processText :: String -> String
-processText = displayResults . countWords
+displayFrequency :: [(String, Int)] -> String
+displayFrequency = unlines . map (\(w, c) -> w ++ ": " ++ show c)
+
+analyzeText :: Int -> String -> String
+analyzeText n = displayFrequency . topNWords n
