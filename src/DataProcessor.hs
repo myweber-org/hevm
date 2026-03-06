00513b50a026db9c84c87ee905a501b909ec1b9c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-06 00:15:13 +0000

    Add function to filter and transform list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -163,4 +163,20 @@ processFinancialData filePath start end = do
         Left err -> putStrLn $ "Error: " ++ err
         Right results -> 
             mapM_ (\(name, date, amount) -> 
-                putStrLn $ name ++ " on " ++ show date ++ ": $" ++ show amount) results
+                putStrLn $ name ++ " on " ++ show date ++ ": $" ++ show amount) resultsmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (\x -> x * 2 + 1)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+main :: IO ()
+main = do
+    let numbers = [1..20]
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show (processNumbers numbers)
+    putStrLn $ "Sum of processed: " ++ show (sumProcessed numbers)
