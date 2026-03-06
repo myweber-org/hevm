46a92d12d8266b58caefe9163859700720da96c6
InkspillVerse <inkspill.verse@outlook.com>
2026-03-06 23:37:15 +0000

    Add word frequency counter for text analysis

--- test/contracts/WordCounter.hs
+++ test/contracts/WordCounter.hs
@@ -63,3 +63,33 @@ analyzeText text = do
   putStrLn "\nWord frequencies:"
   mapM_ (\(w, f) -> putStrLn $ "  " ++ w ++ ": " ++ show (round (f * 10000) / 100) ++ "%") 
         $ take 5 $ wordFrequency text
+module WordCounter where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map.Strict as Map
+
+type WordFreq = Map.Map String Int
+
+countWords :: String -> WordFreq
+countWords text =
+    let wordsList = filter (not . null) $ map normalize $ splitWords text
+    in foldr (\word -> Map.insertWith (+) word 1) Map.empty wordsList
+
+splitWords :: String -> [String]
+splitWords = words . map (\c -> if Char.isAlpha c then Char.toLower c else ' ')
+
+normalize :: String -> String
+normalize = filter Char.isAlpha
+
+topNWords :: Int -> String -> [(String, Int)]
+topNWords n text =
+    take n $ List.sortBy (\(_, cnt1) (_, cnt2) -> compare cnt2 cnt1) $
+    Map.toList $ countWords text
+
+displayFrequencies :: [(String, Int)] -> String
+displayFrequencies freqs =
+    unlines $ map (\(word, count) -> word ++ ": " ++ show count) freqs
+
+processText :: Int -> String -> String
+processText n text = displayFrequencies $ topNWords n text
