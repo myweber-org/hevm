bdcd56fe7801ceb73be800ccf7dfc8a646126b78
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-07 14:52:18 +0000

    Add Fibonacci sequence generator with memoization

--- doc/theme/FibonacciSequence.hs
+++ doc/theme/FibonacciSequence.hs
@@ -54,4 +54,14 @@ fibMemo = fix memoize
   where
     memoize _ 0 = 0
     memoize _ 1 = 1
-    memoize f n = f (n - 1) + f (n - 2)
+    memoize f n = f (n - 1) + f (n - 2)module FibonacciSequence where
+
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    mapM_ (print . fibonacci) [0..19]
