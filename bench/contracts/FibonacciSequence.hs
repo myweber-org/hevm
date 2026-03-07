cb012605d5a60c3b3a7a64c913e120d636347b20
VioletInquiry <violet.inquiry@outlook.com>
2026-03-07 22:42:14 +0000

    Add a function to generate Fibonacci sequence up to n terms

--- bench/contracts/FibonacciSequence.hs
+++ bench/contracts/FibonacciSequence.hs
@@ -53,4 +53,14 @@ printFibonacciSequence n = do
 fibonacci :: Int -> [Integer]
 fibonacci n = take n fibs
   where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)module FibonacciSequence where
+
+fibonacci :: Int -> [Integer]
+fibonacci n = take n fibs
+  where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+printFibonacci :: Int -> IO ()
+printFibonacci n = do
+    putStrLn $ "Fibonacci sequence up to " ++ show n ++ " terms:"
+    mapM_ (putStrLn . show) (fibonacci n)
