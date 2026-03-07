da7977bbedb6dfee8ec928cd5267bfd0dff22f12
FelixMakes <felix.makes@hotmail.com>
2026-03-07 23:10:41 +0000

    Add function to calculate the depth of a binary tree

--- test/EVM/BinaryTreeDepth.hs
+++ test/EVM/BinaryTreeDepth.hs
@@ -1,57 +1,5 @@
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
-
-treeDepth :: Tree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show, Eq)
+data BinaryTree a = Leaf | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 treeDepth :: BinaryTree a -> Int
 treeDepth Leaf = 0
-treeDepth (Node left _ right) = 1 + max (treeDepth left) (treeDepth right)module BinaryTreeDepth where
-
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: BinaryTree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty Empty)module BinaryTreeDepth where
-
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: BinaryTree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty Empty)data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
-
-treeDepth :: Tree a -> Int
-treeDepth Empty = 0
-treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
-
-sampleTree :: Tree Int
-sampleTree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty (Node 6 Empty Empty))
-
-main :: IO ()
-main = do
-    let depth = treeDepth sampleTree
-    putStrLn $ "Maximum depth of the sample tree is: " ++ show depth
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
-
-treeDepth :: BinaryTree a -> Int
-treeDepth Empty = 0
 treeDepth (Node _ left right) = 1 + max (treeDepth left) (treeDepth right)
