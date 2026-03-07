b9dc27fd22ad1ecb114e82f0cfce87a1d9f70be9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-07 20:20:00 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -39,3 +39,24 @@ runDataProcessor :: [String] -> String
 runDataProcessor input = 
     let processed = processData input
     in generateReport processed
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 10) (* 2)
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> 0) xs then Just xs else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [5, 12, 8, 20, 3, 15]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn "Original data:"
+            print validData
+            putStrLn "Processed data (values > 10 doubled):"
+            print $ processData validData
+        Nothing -> putStrLn "Invalid input: all values must be positive"
