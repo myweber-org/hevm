834fac904cd5d2eee35461c83ff2877b72cf68c4
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-07 16:59:26 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -214,3 +214,18 @@ processCSVFile content colIndex = do
     parsed <- parseCSV content
     average <- calculateColumnAverage parsed colIndex
     Right (formatCSVOutput parsed, average)
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateInput :: [Int] -> Bool
+validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+
+safeProcess :: [Int] -> Maybe [Int]
+safeProcess xs
+    | validateInput xs = Just (processData xs)
+    | otherwise = Nothing
