8b4bb630c12cb0bb401e27a7b8dedbe3f4474007
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-08 19:51:33 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -171,4 +171,30 @@ main = do
     putStrLn "Testing moving average with window size 3:"
     print $ movingAverage 3 testData
     putStrLn "\nTesting moving average with window size 5:"
-    print $ movingAverage 5 testData
+    print $ movingAverage 5 testDatamodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = 
+    if all (\x -> x >= -100 && x <= 100) xs
+        then Just xs
+        else Nothing
+
+main :: IO ()
+main = do
+    let sampleData = [-5, 2, 0, 8, -1, 10]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original data: " ++ show validData
+            putStrLn $ "Processed data: " ++ show (processData validData)
+            putStrLn $ "Sum of positive doubles: " ++ show (sumPositiveDoubles validData)
+        Nothing -> putStrLn "Input validation failed: values out of range"
