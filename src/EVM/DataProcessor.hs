5a01ce533c3e28337285d092161f03954020b187
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-08 19:50:39 +0000

    Add function to calculate moving average of a list

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -62,4 +62,26 @@ main :: IO ()
 main = do
     let numbers = [-3, 1, 0, 5, -2, 8]
     let result = processNumbers numbers
-    print result
+    print resultmodule DataProcessor where
+
+import Data.List (tails)
+
+movingAverage :: Int -> [Double] -> [Double]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | n > length xs = error "Window size exceeds list length"
+    | otherwise = map avg $ filter (\w -> length w == n) $ tails xs
+  where
+    avg window = sum window / fromIntegral n
+
+smoothData :: Int -> [Double] -> [Double]
+smoothData windowSize = movingAverage windowSize
+
+calculateTrend :: [Double] -> Double
+calculateTrend values = (last values - head values) / fromIntegral (length values - 1)
+
+processDataset :: Int -> [Double] -> (Double, [Double])
+processDataset windowSize dataset =
+    let smoothed = smoothData windowSize dataset
+        trend = calculateTrend smoothed
+    in (trend, smoothed)
