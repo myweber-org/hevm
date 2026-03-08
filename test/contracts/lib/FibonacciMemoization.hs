ee264896a6c7e2d923d5c125ca03977b1a955f31
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-08 19:54:47 +0000

    Implement memoized Fibonacci sequence using a lazy infinite list for efficient computation

--- test/contracts/lib/FibonacciMemoization.hs
+++ test/contracts/lib/FibonacciMemoization.hs
@@ -8,4 +8,17 @@ fib n = Map.findWithDefault 0 n memo
     memo = Map.fromList [(i, fib' i) | i <- [0..n]]
     fib' 0 = 0
     fib' 1 = 1
-    fib' x = memo Map.! (x-1) + memo Map.! (x-2)
+    fib' x = memo Map.! (x-1) + memo Map.! (x-2)module FibonacciMemoization where
+
+fibs :: [Integer]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+fibonacci :: Int -> Integer
+fibonacci n = fibs !! n
+
+main :: IO ()
+main = do
+    putStrLn "First 20 Fibonacci numbers:"
+    print $ take 20 fibs
+    putStrLn "\nFibonacci number at position 50:"
+    print $ fibonacci 50
