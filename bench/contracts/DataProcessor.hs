fd5355e0b9c761fe164145f458829bd702f921e0
InkspillVerse <inkspill.verse@outlook.com>
2026-03-08 15:43:57 +0000

    Add function to filter and transform a list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -4,10 +4,10 @@ filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
-processData = filterAndTransform even (\x -> x * x + 1)
+processData = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let input = [1..10]
+    let input = [1, -2, 3, -4, 5]
     let result = processData input
     print result
