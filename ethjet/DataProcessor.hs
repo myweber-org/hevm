3fc42db14861ba1b8f39e652eda92e60b7840610
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-08 21:12:02 +0000

    Add function to filter and transform list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -19,4 +19,13 @@ processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)module DataProcessor where
 
 processData :: [Int] -> [Int]
-processData xs = map (^2) (filter even xs)
+processData xs = map (^2) (filter even xs)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
