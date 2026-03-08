56174df8c30e484b0dfe7051e0263d929846d248
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-08 15:12:04 +0000

    Add function to calculate moving average of a list

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -133,4 +133,13 @@ exampleUsage = do
         Just (name, phone, length) -> 
             putStrLn $ "Processed: " ++ name ++ " | " ++ phone ++ " | Length: " ++ length
         Nothing -> 
-            putStrLn "Invalid input data"
+            putStrLn "Invalid input data"module DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+  where
+    windows m ys = take (length ys - m + 1) $ zipWith (++) (tails ys) (repeat [])
+    average zs = sum zs / fromIntegral (length zs)
