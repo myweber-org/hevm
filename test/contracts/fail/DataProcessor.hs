c12090ac9ac0dc4974a6b7856df765ec68a1e19f
InkspillVerse <inkspill.verse@outlook.com>
2026-03-08 22:47:14 +0000

    Add function to parse CSV data and calculate average values

--- test/contracts/fail/DataProcessor.hs
+++ test/contracts/fail/DataProcessor.hs
@@ -7,4 +7,30 @@ processNumbers :: [Int] -> [Int]
 processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processNumbers
+sumProcessed = sum . processNumbersmodule DataProcessor where
+
+import Data.List.Split (splitOn)
+
+type CSVRow = [String]
+type CSVData = [CSVRow]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
+
+extractNumericColumn :: CSVData -> Int -> [Double]
+extractNumericColumn rows colIndex = 
+    [ read cell | row <- rows, 
+                  length row > colIndex, 
+                  let cell = row !! colIndex, 
+                  all (\c -> c `elem` "0123456789.-") cell ]
+
+calculateAverage :: [Double] -> Double
+calculateAverage [] = 0.0
+calculateAverage xs = sum xs / fromIntegral (length xs)
+
+processCSVFile :: String -> Int -> IO Double
+processCSVFile filePath columnIndex = do
+    content <- readFile filePath
+    let parsedData = parseCSV content
+    let numericData = extractNumericColumn parsedData columnIndex
+    return $ calculateAverage numericData
