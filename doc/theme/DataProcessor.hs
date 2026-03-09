af92be9b4a15f2ab0347be882023498cefdf572b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-09 20:12:58 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -10,4 +10,21 @@ validateData :: [Int] -> Bool
 validateData xs = all (> 0) xs && length xs > 3
 
 combineResults :: [Int] -> [Int] -> [Int]
-combineResults xs ys = zipWith (+) (processData xs) (processData ys)
+combineResults xs ys = zipWith (+) (processData xs) (processData ys)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processData
+
+main :: IO ()
+main = do
+    let testData = [-3, 2, 0, 5, -1, 8]
+    putStrLn $ "Original data: " ++ show testData
+    putStrLn $ "Processed data: " ++ show (processData testData)
+    putStrLn $ "Sum of positive doubles: " ++ show (sumPositiveDoubles testData)
