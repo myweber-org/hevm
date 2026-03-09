0aade1ba5668f359354686d78eeb67788dd642cd
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-09 20:34:58 +0000

    Add function to parse CSV data and calculate average values

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -31,4 +31,41 @@ main :: IO ()
 main = do
     let input = [1, -2, 3, -4, 5]
     let result = processData input
-    print result
+    print resultmodule DataProcessor where
+
+import Data.List.Split (splitOn)
+
+type CSVRow = [String]
+type CSVData = [CSVRow]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
+
+extractNumericColumn :: CSVData -> Int -> Maybe [Double]
+extractNumericColumn dataRows colIndex
+    | null dataRows = Nothing
+    | otherwise = sequence $ map (safeRead . (!! colIndex)) dataRows
+  where
+    safeRead s = case reads s of
+        [(val, "")] -> Just val
+        _ -> Nothing
+
+calculateAverage :: [Double] -> Maybe Double
+calculateAverage vals
+    | null vals = Nothing
+    | otherwise = Just (sum vals / fromIntegral (length vals))
+
+processCSVFile :: String -> Int -> IO (Maybe Double)
+processCSVFile filePath columnIndex = do
+    content <- readFile filePath
+    let parsedData = parseCSV content
+    case extractNumericColumn parsedData columnIndex of
+        Nothing -> return Nothing
+        Just numbers -> return $ calculateAverage numbers
+
+main :: IO ()
+main = do
+    result <- processCSVFile "data.csv" 2
+    case result of
+        Nothing -> putStrLn "Failed to process CSV data"
+        Just avg -> putStrLn $ "Average: " ++ show avg
