e8b8cefbe761442bb526a2fe88d01a3131c87105
VioletInquiry <violet.inquiry@outlook.com>
2026-03-09 00:44:34 +0000

    Add function to filter and transform a list of integers

--- src/EVM/DataProcessor.hs
+++ src/EVM/DataProcessor.hs
@@ -37,4 +37,16 @@ exampleUsage = do
         Just (user, mail, tel) -> 
             putStrLn $ "Processed: " ++ user ++ ", " ++ mail ++ ", " ++ tel
         Nothing -> 
-            putStrLn "Invalid username provided"
+            putStrLn "Invalid username provided"module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [-3, 1, 0, 5, -2, 8]
+    let result = processNumbers numbers
+    print result
