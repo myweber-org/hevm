3f9b1db55b6641cb883116f4ac0970f1b093ac4b
FelixMakes <felix.makes@hotmail.com>
2026-03-10 22:03:30 +0000

    Add function to calculate moving average of a list

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -104,4 +104,22 @@ processData = filterAndTransform (> 0) (* 2)
 validateAndProcess :: [Int] -> Maybe [Int]
 validateAndProcess xs
     | null xs = Nothing
-    | otherwise = Just $ processData xs
+    | otherwise = Just $ processData xsmodule DataProcessor where
+
+movingAverage :: Fractional a => Int -> [a] -> [a]
+movingAverage n xs
+    | length xs < n = []
+    | otherwise = avg : movingAverage n (tail xs)
+    where
+        window = take n xs
+        avg = sum window / fromIntegral n
+
+-- Example usage with a helper function
+exampleUsage :: IO ()
+exampleUsage = do
+    let dataSeries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    let ma3 = movingAverage 3 dataSeries
+    let ma5 = movingAverage 5 dataSeries
+    putStrLn $ "Original data: " ++ show dataSeries
+    putStrLn $ "3-period moving average: " ++ show ma3
+    putStrLn $ "5-period moving average: " ++ show ma5
