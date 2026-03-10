059b12f67280f30d69bddce8ca8212510f16a028
heyQuim <hey.quim@hotmail.com>
2026-03-10 16:45:43 +0000

    Add function to filter and transform list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -297,4 +297,18 @@ createUserReport name phone =
     case processUserData name phone of
         Left err -> "Error: " ++ err
         Right (processedName, processedPhone) ->
-            "User: " ++ processedName ++ ", Phone: " ++ processedPhone
+            "User: " ++ processedName ++ ", Phone: " ++ processedPhonemodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
