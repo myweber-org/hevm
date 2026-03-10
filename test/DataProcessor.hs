e7f66bb49274ba6e97ebf701a3567da2a544e693
FelixMakes <felix.makes@hotmail.com>
2026-03-10 00:21:43 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -55,4 +55,29 @@ computeStats xs = Just Stats {
 
 -- Process data pipeline: filter, transform, and compute statistics
 processDataPipeline :: [Double] -> Maybe Stats
-processDataPipeline = computeStats . map (* 2) . filter (> 0)
+processDataPipeline = computeStats . map (* 2) . filter (> 0)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = 
+    map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 10) (* 2)
+
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs
+    | null xs = Nothing
+    | any (< 0) xs = Nothing
+    | otherwise = Just xs
+
+main :: IO ()
+main = do
+    let sampleData = [5, 12, 8, 20, 3, 15]
+    case validateInput sampleData of
+        Nothing -> putStrLn "Invalid input data"
+        Just validData -> do
+            let result = sumProcessedData validData
+            putStrLn $ "Sum of processed data: " ++ show result
