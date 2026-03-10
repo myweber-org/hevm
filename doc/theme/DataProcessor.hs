7fee41e9637f9407f832f490a72897cd211d65a4
FelixMakes <felix.makes@hotmail.com>
2026-03-10 00:30:50 +0000

    Add data processing module with validation and transformation functions

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -28,3 +28,48 @@ main = do
     putStrLn $ "Original data: " ++ show testData
     putStrLn $ "Processed data: " ++ show (processData testData)
     putStrLn $ "Sum of positive doubles: " ++ show (sumPositiveDoubles testData)
+module DataProcessor where
+
+import Data.Char (isDigit, isAlpha, toUpper)
+
+-- Validate if a string contains only digits
+validateNumeric :: String -> Bool
+validateNumeric = all isDigit
+
+-- Validate if a string contains only alphabetic characters
+validateAlpha :: String -> Bool
+validateAlpha = all isAlpha
+
+-- Convert string to uppercase
+toUppercase :: String -> String
+toUppercase = map toUpper
+
+-- Process a list of strings: validate and transform
+processData :: [String] -> [String]
+processData = map processItem
+  where
+    processItem item
+      | validateNumeric item = "NUMERIC: " ++ item
+      | validateAlpha item = "ALPHA: " ++ toUppercase item
+      | otherwise = "INVALID: " ++ item
+
+-- Filter valid numeric entries
+filterNumeric :: [String] -> [String]
+filterNumeric = filter validateNumeric
+
+-- Filter valid alphabetic entries
+filterAlpha :: [String] -> [String]
+filterAlpha = filter validateAlpha
+
+-- Example usage function
+exampleUsage :: IO ()
+exampleUsage = do
+  let testData = ["123", "abc", "456def", "XYZ", "789"]
+  putStrLn "Original data:"
+  print testData
+  putStrLn "\nProcessed data:"
+  print $ processData testData
+  putStrLn "\nNumeric entries:"
+  print $ filterNumeric testData
+  putStrLn "\nAlphabetic entries:"
+  print $ filterAlpha testData
