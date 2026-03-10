d21e17019ace856eaedaaee6aece6620ae694af3
InkspillVerse <inkspill.verse@outlook.com>
2026-03-10 15:52:45 +0000

    Implement binary search tree insertion and in-order traversal

--- cli/BinarySearchTree.hs
+++ cli/BinarySearchTree.hs
@@ -10,129 +10,14 @@ insert x (Node y left right)
     | x > y     = Node y left (insert x right)
     | otherwise = Node y left right
 
-inOrder :: Tree a -> [a]
-inOrder Empty = []
-inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
-
-fromList :: Ord a => [a] -> Tree a
-fromList = foldr insert Empty
-module BinarySearchTree where
-
-data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
-
-insert :: Ord a => a -> BST a -> BST a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
-
-inorder :: BST a -> [a]
-inorder Empty = []
-inorder (Node left val right) = inorder left ++ [val] ++ inorder right
-
-fromList :: Ord a => [a] -> BST a
-fromList = foldr insert Empty
-
-toSortedList :: BST a -> [a]
-toSortedList = inorder
-module BinarySearchTree where
-
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
-
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Empty = Node x Empty Empty
-insert x (Node y left right)
-    | x < y     = Node y (insert x left) right
-    | x > y     = Node y left (insert x right)
-    | otherwise = Node y left right
-
 inorder :: Tree a -> [a]
 inorder Empty = []
 inorder (Node x left right) = inorder left ++ [x] ++ inorder right
 
 fromList :: Ord a => [a] -> Tree a
-fromList = foldr insert Emptymodule BinarySearchTree where
-
-data Tree a = Empty | Node (Tree a) a (Tree a) deriving (Show, Eq)
-
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
-
-inorder :: Tree a -> [a]
-inorder Empty = []
-inorder (Node left val right) = inorder left ++ [val] ++ inorder right
-
-buildTree :: Ord a => [a] -> Tree a
-buildTree = foldr insert Empty
+fromList = foldr insert Empty
 
 main :: IO ()
 main = do
-    let values = [5, 3, 7, 2, 4, 6, 8]
-    let bst = buildTree values
-    putStrLn "In-order traversal:"
-    print $ inorder bst
-module BinarySearchTree where
-
-data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
-
-insert :: Ord a => a -> BST a -> BST a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
-
-inorder :: BST a -> [a]
-inorder Empty = []
-inorder (Node left val right) = inorder left ++ [val] ++ inorder right
-
-fromList :: Ord a => [a] -> BST a
-fromList = foldr insert Empty
-
-toSortedList :: BST a -> [a]
-toSortedList = inorder
-
-contains :: Ord a => a -> BST a -> Bool
-contains _ Empty = False
-contains x (Node left val right)
-    | x == val  = True
-    | x < val   = contains x left
-    | otherwise = contains x right
-module BinarySearchTree where
-
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
-
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Empty = Node x Empty Empty
-insert x (Node y left right)
-    | x < y     = Node y (insert x left) right
-    | x > y     = Node y left (insert x right)
-    | otherwise = Node y left right
-
-inOrder :: Tree a -> [a]
-inOrder Empty = []
-inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
-
-fromList :: Ord a => [a] -> Tree a
-fromList = foldr insert Emptymodule BinarySearchTree where
-
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
-
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Empty = Node x Empty Empty
-insert x (Node y left right)
-    | x < y     = Node y (insert x left) right
-    | x > y     = Node y left (insert x right)
-    | otherwise = Node y left right
-
-inOrder :: Tree a -> [a]
-inOrder Empty = []
-inOrder (Node x left right) = inOrder left ++ [x] ++ inOrder right
-
-fromList :: Ord a => [a] -> Tree a
-fromList = foldr insert Empty
+    let tree = fromList [5, 3, 7, 1, 4, 6, 8]
+    putStrLn $ "In-order traversal: " ++ show (inorder tree)
