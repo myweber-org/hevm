bdecd032f6055136447c7e578727a66f91a29f2b
ElaraWrites <elara.writes@outlook.com>
2026-03-10 21:46:34 +0000

    Add in-order traversal for binary trees using continuation passing style

--- ethjet/BinaryTreeTraversal.hs
+++ ethjet/BinaryTreeTraversal.hs
@@ -15,4 +15,37 @@ postorder Leaf = []
 postorder (Node left value right) = postorder left ++ postorder right ++ [value]
 
 exampleTree :: BinaryTree Int
-exampleTree = Node (Node Leaf 2 Leaf) 1 (Node (Node Leaf 4 Leaf) 3 Leaf)
+exampleTree = Node (Node Leaf 2 Leaf) 1 (Node (Node Leaf 4 Leaf) 3 Leaf)module BinaryTreeTraversal where
+
+data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show, Eq)
+
+inOrderCPS :: BinaryTree a -> ([a] -> r) -> r
+inOrderCPS Leaf k = k []
+inOrderCPS (Node left val right) k = 
+    inOrderCPS left $ \leftList ->
+        inOrderCPS right $ \rightList ->
+            k (leftList ++ [val] ++ rightList)
+
+inOrder :: BinaryTree a -> [a]
+inOrder tree = inOrderCPS tree id
+
+preOrderCPS :: BinaryTree a -> ([a] -> r) -> r
+preOrderCPS Leaf k = k []
+preOrderCPS (Node left val right) k = 
+    k ([val] ++) $ \prefix ->
+        preOrderCPS left $ \leftList ->
+            preOrderCPS right $ \rightList ->
+                prefix (leftList ++ rightList)
+
+preOrder :: BinaryTree a -> [a]
+preOrder tree = preOrderCPS tree id
+
+postOrderCPS :: BinaryTree a -> ([a] -> r) -> r
+postOrderCPS Leaf k = k []
+postOrderCPS (Node left val right) k = 
+    postOrderCPS left $ \leftList ->
+        postOrderCPS right $ \rightList ->
+            k (leftList ++ rightList ++ [val])
+
+postOrder :: BinaryTree a -> [a]
+postOrder tree = postOrderCPS tree id
