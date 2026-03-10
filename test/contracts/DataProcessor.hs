8ac1afa78fe2d5b3ceec96badde44739f8a72496
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-10 18:08:37 +0000

    Add function to parse and filter CSV data by date range

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -65,3 +65,47 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
+module DataProcessor where
+
+import Data.Time
+import Data.Time.Format
+import Data.List
+import Data.Char
+
+parseDate :: String -> Maybe Day
+parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
+
+data Record = Record
+    { recordDate :: Day
+    , recordValue :: Double
+    , recordCategory :: String
+    } deriving (Show, Eq)
+
+parseRecord :: String -> Maybe Record
+parseRecord line = case words line of
+    [dateStr, valueStr, category] -> do
+        date <- parseDate dateStr
+        value <- readMaybe valueStr
+        return Record { recordDate = date, recordValue = value, recordCategory = category }
+    _ -> Nothing
+  where
+    readMaybe :: String -> Maybe Double
+    readMaybe s = case reads s of
+        [(x, "")] -> Just x
+        _ -> Nothing
+
+filterByDateRange :: Day -> Day -> [Record] -> [Record]
+filterByDateRange start end = filter (\r -> recordDate r >= start && recordDate r <= end)
+
+summarizeByCategory :: [Record] -> [(String, Double)]
+summarizeByCategory records =
+    let grouped = groupBy (\a b -> recordCategory a == recordCategory b) $
+                  sortOn recordCategory records
+    in map (\group -> (recordCategory (head group), sum (map recordValue group))) grouped
+
+processData :: String -> Day -> Day -> Either String [(String, Double)]
+processData content start end = do
+    let lines' = filter (not . all isSpace) (lines content)
+    records <- mapM parseRecord lines'
+    let filtered = filterByDateRange start end records
+    return $ summarizeByCategory filtered
