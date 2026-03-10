9c10179b5021ecf8dd8d923f050851ab582fe39e
InkspillVerse <inkspill.verse@outlook.com>
2026-03-10 18:12:52 +0000

    Add function to filter and transform list of integers

--- test/contracts/DataProcessor.hs
+++ test/contracts/DataProcessor.hs
@@ -108,4 +108,17 @@ processData content start end = do
     let lines' = filter (not . all isSpace) (lines content)
     records <- mapM parseRecord lines'
     let filtered = filterByDateRange start end records
-    return $ summarizeByCategory filtered
+    return $ summarizeByCategory filteredmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+safeHead :: [Int] -> Maybe Int
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+sumPositiveDoubles :: [Int] -> Int
+sumPositiveDoubles = sum . processData
