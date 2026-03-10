4dd9665ceeef085c59891539af21cd0aa6e20f69
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-10 23:34:23 +0000

    Add function to filter and transform list of integers

--- bench/DataProcessor.hs
+++ bench/DataProcessor.hs
@@ -230,4 +230,20 @@ exampleUsage = do
             putStrLn "Parsed CSV data:"
             mapM_ print data
             putStrLn "\nFormatted output:"
-            putStrLn $ formatCSV data
+            putStrLn $ formatCSV datamodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+validateData :: [Int] -> Bool
+validateData = all (> 0)
+
+main :: IO ()
+main = do
+    let sampleData = [1, -2, 3, -4, 5]
+    putStrLn $ "Original data: " ++ show sampleData
+    putStrLn $ "Processed data: " ++ show (processData sampleData)
+    putStrLn $ "Is valid? " ++ show (validateData sampleData)
