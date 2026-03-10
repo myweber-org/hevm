ed35c97f3bd1c20c3c5634694140656745165a27
heyQuim <hey.quim@hotmail.com>
2026-03-10 01:47:37 +0000

    Add function to filter and transform list of integers

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -247,4 +247,13 @@ main = do
             case calculateColumnAverage validData 2 of
                 Right avg -> putStrLn $ "Average age: " ++ show avg
                 Left err -> putStrLn err
-        Left err -> putStrLn $ "Error: " ++ err
+        Left err -> putStrLn $ "Error: " ++ errmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
