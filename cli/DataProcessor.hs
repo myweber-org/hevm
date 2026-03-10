d68d9a4ef76c45e2b21b25502e6ad30c4ee0fa5e
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-10 18:41:53 +0000

    Add function to filter and transform list of integers

--- cli/DataProcessor.hs
+++ cli/DataProcessor.hs
@@ -1,30 +1,17 @@
-
 module DataProcessor where
 
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processEvenSquares :: [Int] -> [Int]
-processEvenSquares = filterAndTransform even (\x -> x * x)
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
 
-sumProcessedList :: [Int] -> Int
-sumProcessedList = sum . processEvenSquares
+validateInput :: [Int] -> Bool
+validateInput xs = not (null xs) && all (\x -> x >= -100 && x <= 100) xs
 
 main :: IO ()
 main = do
-    let sampleData = [1..10]
-    let result = sumProcessedList sampleData
-    putStrLn $ "Sum of squares of even numbers from 1 to 10: " ++ show resultmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processNumbers :: [Int] -> [Int]
-processNumbers = filterAndTransform (> 0) (* 2)
-
-sumPositiveDoubles :: [Int] -> Int
-sumPositiveDoubles = sum . processNumbersmodule DataProcessor where
-
-processData :: [Int] -> [Int]
-processData = map (^2) . filter (>0)
+    let sampleData = [1, -2, 3, 0, 5, -8]
+    if validateInput sampleData
+        then print $ processData sampleData
+        else putStrLn "Invalid input data"
