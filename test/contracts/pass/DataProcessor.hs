d0d3bd23e675e5ada9772494b9a01ba84f908434
ElaraWrites <elara.writes@outlook.com>
2026-03-11 23:34:15 +0000

    Add function to filter and transform a list of integers

--- test/contracts/pass/DataProcessor.hs
+++ test/contracts/pass/DataProcessor.hs
@@ -314,4 +314,16 @@ safeHead [] = Nothing
 safeHead (x:_) = Just xmodule DataProcessor where
 
 processEvenSquares :: [Int] -> [Int]
-processEvenSquares = map (^2) . filter even
+processEvenSquares = map (^2) . filter evenmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
