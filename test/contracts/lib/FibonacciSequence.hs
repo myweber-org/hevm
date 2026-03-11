7a07e5ed33fd9f239f921f99a9cdfb92a5940c6d
InkspillVerse <inkspill.verse@outlook.com>
2026-03-11 14:42:30 +0000

    Add a function to generate the nth Fibonacci number using memoization

--- test/contracts/lib/FibonacciSequence.hs
+++ test/contracts/lib/FibonacciSequence.hs
@@ -1,35 +1,25 @@
 module FibonacciSequence where
 
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Function (fix)
 
 fibonacci :: Int -> Integer
-fibonacci n = fst (fibMemo n Map.empty)
-
-fibMemo :: Int -> Map Int Integer -> (Integer, Map Int Integer)
-fibMemo n memo
-    | n <= 1 = (fromIntegral n, memo)
-    | otherwise = case Map.lookup n memo of
-        Just val -> (val, memo)
-        Nothing -> let
-            (a, memo1) = fibMemo (n-1) memo
-            (b, memo2) = fibMemo (n-2) memo1
-            result = a + b
-            in (result, Map.insert n result memo2)
-
-fibonacciList :: Int -> [Integer]
-fibonacciList n = map fibonacci [0..n-1]
-
-main :: IO ()
-main = do
-    putStrLn "First 20 Fibonacci numbers:"
-    print $ fibonacciList 20
-    putStrLn "\nIndividual Fibonacci numbers:"
-    print $ fibonacci 10
-    print $ fibonacci 20
-    print $ fibonacci 30module FibonacciSequence where
-
-fibonacci :: Integer -> [Integer]
-fibonacci n = takeWhile (<= n) fibs
+fibonacci n = fibList !! n
   where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+    fibList = 0 : 1 : zipWith (+) fibList (tail fibList)
+
+-- Alternative implementation using fix for memoization
+fibonacci' :: Int -> Integer
+fibonacci' = fix (\rec n -> if n < 2 then fromIntegral n else rec (n-1) + rec (n-2))
+
+-- Fast doubling algorithm for O(log n) Fibonacci
+fibFastDoubling :: Int -> Integer
+fibFastDoubling n
+    | n < 0     = error "Negative input not supported"
+    | otherwise = fst (fibPair n)
+  where
+    fibPair 0 = (0, 1)
+    fibPair k
+        | even k    = (a * (2*b - a), a*a + b*b)
+        | otherwise = (a*a + b*b, b * (2*a + b))
+      where
+        (a, b) = fibPair (k `div` 2)
