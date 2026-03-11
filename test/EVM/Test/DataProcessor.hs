7a836e8c1b7a4014e0e199eef11461aa9c1b59f9
FelixMakes <felix.makes@hotmail.com>
2026-03-11 15:22:15 +0000

    Add function to filter and transform list of integers

--- test/EVM/Test/DataProcessor.hs
+++ test/EVM/Test/DataProcessor.hs
@@ -6,14 +6,18 @@ filterAndTransform predicate transformer = map transformer . filter predicate
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Bool
-validateInput xs = all (\x -> x >= -100 && x <= 100) xs
+sumProcessedData :: [Int] -> Int
+sumProcessedData = sum . processData
+
+validateInput :: [Int] -> Maybe [Int]
+validateInput xs = if all (> -1000) xs then Just xs else Nothing
 
 main :: IO ()
 main = do
-    let inputData = [1, -2, 3, 0, 5, -10]
-    if validateInput inputData
-        then do
-            let result = processData inputData
-            putStrLn $ "Processed data: " ++ show result
-        else putStrLn "Invalid input data detected"
+    let sampleData = [1, -2, 3, 0, 5, -8]
+    case validateInput sampleData of
+        Just validData -> do
+            putStrLn $ "Original data: " ++ show validData
+            putStrLn $ "Processed data: " ++ show (processData validData)
+            putStrLn $ "Sum of processed data: " ++ show (sumProcessedData validData)
+        Nothing -> putStrLn "Input validation failed: values too low"
