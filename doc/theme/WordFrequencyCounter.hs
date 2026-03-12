3e3a5dd8aaa66a81ae069874b2868a33adcd858d
FelixMakes <felix.makes@hotmail.com>
2026-03-12 00:28:21 +0000

    Add word frequency counter module with basic text processing functions

--- doc/theme/WordFrequencyCounter.hs
+++ doc/theme/WordFrequencyCounter.hs
@@ -1,104 +1,29 @@
 module WordFrequencyCounter where
 
-import qualified Data.Map as Map
 import Data.Char (toLower, isAlpha)
 import Data.List (sortOn)
 import Data.Ord (Down(..))
 
-type WordFrequency = Map.Map String Int
+type WordCount = (String, Int)
 
-countWords :: String -> WordFrequency
+countWords :: String -> [WordCount]
 countWords text = 
-    let words' = filter (not . null) $ map (filter isAlpha . map toLower) $ words text
-    in foldr (\word -> Map.insertWith (+) word 1) Map.empty words'
-
-getTopWords :: Int -> WordFrequency -> [(String, Int)]
-getTopWords n freqMap = 
-    take n $ sortOn (Down . snd) $ Map.toList freqMap
-
-filterByMinFrequency :: Int -> WordFrequency -> WordFrequency
-filterByMinFrequency minFreq = Map.filter (>= minFreq)
-
-analyzeText :: String -> Int -> Int -> IO ()
-analyzeText text topN minFreq = do
-    let freqMap = countWords text
-    let filteredMap = filterByMinFrequency minFreq freqMap
-    let topWords = getTopWords topN filteredMap
-    
-    putStrLn "Top words by frequency:"
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) topWords
-    
-    putStrLn $ "\nTotal unique words: " ++ show (Map.size freqMap)
-    putStrLn $ "Words meeting minimum frequency (" ++ show minFreq ++ "): " ++ show (Map.size filteredMap)
-
-sampleText :: String
-sampleText = "The quick brown fox jumps over the lazy dog. The dog barks at the fox."
-
-main :: IO ()
-main = do
-    putStrLn "Word Frequency Analysis"
-    putStrLn "======================="
-    analyzeText sampleText 5 2module WordFrequencyCounter where
-
-import qualified Data.Char as Char
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-
-type WordCount = Map.Map T.Text Int
-
-countWords :: T.Text -> WordCount
-countWords = Map.fromListWith (+) . map (\w -> (w, 1)) . filter (not . T.null) . map normalize . T.words
+    let wordsList = filter (not . null) $ map cleanWord $ words text
+        cleanedWords = filter (all isAlpha) wordsList
+        grouped = foldr countWord [] cleanedWords
+    in take 10 $ sortOn (Down . snd) grouped
   where
-    normalize = T.toCaseFold . T.filter Char.isLetter
+    cleanWord = map toLower . filter (\c -> isAlpha c || c == '\'')
+    
+    countWord word [] = [(word, 1)]
+    countWord word ((w, c):rest)
+        | w == word = (w, c + 1) : rest
+        | otherwise = (w, c) : countWord word rest
 
-sortByFrequency :: WordCount -> [(T.Text, Int)]
-sortByFrequency = List.sortBy (\(_, c1) (_, c2) -> compare c2 c1) . Map.toList
+formatResults :: [WordCount] -> String
+formatResults counts = 
+    "Top 10 most frequent words:\n" ++
+    unlines (map (\(word, count) -> word ++ ": " ++ show count) counts)
 
-formatOutput :: [(T.Text, Int)] -> T.Text
-formatOutput = T.unlines . map (\(word, count) -> T.pack (show count) <> " " <> word)
-
-processText :: T.Text -> T.Text
-processText = formatOutput . sortByFrequency . countWords
-
-main :: IO ()
-main = do
-    input <- TIO.getContents
-    TIO.putStr $ processText inputmodule WordFrequencyCounter where
-
-import Data.Char (toLower)
-import Data.List (sortOn)
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Ord (Down(..))
-
-type WordCount = Map String Int
-
-countWords :: String -> WordCount
-countWords = foldr incrementWord Map.empty . words
-  where
-    incrementWord word = Map.insertWith (+) (normalize word) 1
-    normalize = map toLower
-
-filterByMinFrequency :: Int -> WordCount -> WordCount
-filterByMinFrequency minFreq = Map.filter (>= minFreq)
-
-getTopNWords :: Int -> WordCount -> [(String, Int)]
-getTopNWords n = take n . sortOn (Down . snd) . Map.toList
-
-analyzeText :: String -> Int -> Int -> [(String, Int)]
-analyzeText text minFreq topN = 
-    getTopNWords topN $ filterByMinFrequency minFreq $ countWords text
-
-displayAnalysis :: String -> Int -> Int -> IO ()
-displayAnalysis text minFreq topN = do
-    let results = analyzeText text minFreq topN
-    putStrLn "Top words by frequency:"
-    mapM_ (\(word, count) -> putStrLn $ word ++ ": " ++ show count) results
-
-sampleText :: String
-sampleText = "The quick brown fox jumps over the lazy dog. The dog barks at the fox."
-
-main :: IO ()
-main = displayAnalysis sampleText 2 5
+processText :: String -> String
+processText = formatResults . countWords
