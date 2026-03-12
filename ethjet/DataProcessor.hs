58a95379aed5140c35076496536eef55d05d5eaa
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-12 22:02:37 +0000

    Add function to calculate moving average of a list

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,53 +1,13 @@
 module DataProcessor where
 
-movingAverage :: Fractional a => Int -> [a] -> [a]
-movingAverage windowSize xs
-    | windowSize <= 0 = error "Window size must be positive"
-    | length xs < windowSize = []
-    | otherwise = map average $ windows windowSize xs
-  where
-    windows n = takeWhile ((== n) . length) . map (take n) . tails
-    average ys = sum ys / fromIntegral (length ys)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs
-    | null xs = Nothing
-    | any (< -100) xs = Nothing
-    | any (> 100) xs = Nothing
-    | otherwise = Just xsmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = 
-    map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
-
-validateInput :: [Int] -> Maybe [Int]
-validateInput xs
-    | null xs = Nothing
-    | any (< -100) xs = Nothing
-    | any (> 100) xs = Nothing
-    | otherwise = Just xsmodule DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
-
-sumProcessedData :: [Int] -> Int
-sumProcessedData = sum . processData
+movingAverage :: (Fractional a) => Int -> [a] -> [a]
+movingAverage n xs
+    | n <= 0 = error "Window size must be positive"
+    | length xs < n = []
+    | otherwise = map average $ windows n xs
+    where
+        windows :: Int -> [a] -> [[a]]
+        windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
+        
+        average :: (Fractional a) => [a] -> a
+        average ys = sum ys / fromIntegral (length ys)
