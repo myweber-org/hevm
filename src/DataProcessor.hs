a93cda07493014b60cc89e065ff928794db29f58
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-12 00:21:38 +0000

    Add function to filter and transform a list of integers

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -143,4 +143,16 @@ processCSVFile :: String -> Int -> Either String (String, Double)
 processCSVFile content colIndex = do
   let parsed = parseCSV content
   average <- calculateColumnAverage parsed colIndex
-  return (formatCSVOutput parsed, average)
+  return (formatCSVOutput parsed, average)module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processData :: [Int] -> [Int]
+processData = filterAndTransform (> 0) (* 2)
+
+main :: IO ()
+main = do
+    let input = [1, -2, 3, -4, 5]
+    let result = processData input
+    print result
