f4fe94e8dad41f8074819913a6e040ab88ab0521
ElaraWrites <elara.writes@outlook.com>
2026-03-12 16:59:53 +0000

    Implement a simple prime sieve using the Sieve of Eratonsthenes algorithm

--- test/scripts/PrimeSieve.hs
+++ test/scripts/PrimeSieve.hs
@@ -158,4 +158,17 @@ sieve limit
     go (x:xs) acc = go (filter (\n -> n `mod` x /= 0) xs) (x:acc)
 
 primesUpTo :: Int -> [Int]
-primesUpTo = sieve
+primesUpTo = sievemodule PrimeSieve where
+
+primes :: [Int]
+primes = sieve [2..]
+  where
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
+
+isPrime :: Int -> Bool
+isPrime n
+    | n < 2 = False
+    | otherwise = all (\p -> n `mod` p /= 0) (takeWhile (\p -> p * p <= n) primes)
