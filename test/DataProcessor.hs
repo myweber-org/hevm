b1c76cfb1840900f509983793548a01d8e4fa767
VioletInquiry <violet.inquiry@outlook.com>
2026-03-12 23:34:53 +0000

    Add function to filter and transform list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -1,58 +1,20 @@
 
 module DataProcessor where
 
-import Data.Time
-import Text.CSV
-
-filterCSVByDate :: String -> Day -> Day -> Either String [Record]
-filterCSVByDate csvContent startDate endDate = do
-    csv <- parseCSV "input" csvContent
-    let filtered = filter (isWithinDateRange startDate endDate) csv
-    return filtered
-
-isWithinDateRange :: Day -> Day -> Record -> Bool
-isWithinDateRange start end record =
-    case record of
-        (dateStr:_) -> 
-            case parseDate dateStr of
-                Just date -> date >= start && date <= end
-                Nothing -> False
-        _ -> False
-
-parseDate :: String -> Maybe Day
-parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform even (\x -> x * x + 1)
-
-main :: IO ()
-main = do
-    let inputData = [1..10]
-    let result = processData inputData
-    putStrLn $ "Input: " ++ show inputData
-    putStrLn $ "Result: " ++ show resultmodule DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
 processData :: [Int] -> [Int]
 processData = filterAndTransform (> 0) (* 2)
 
-validateInput :: [Int] -> Bool
-validateInput xs = not (null xs) && all (\x -> x >= -100 && x <= 100) xs
+validateAndProcess :: [Int] -> Maybe [Int]
+validateAndProcess xs
+    | null xs = Nothing
+    | otherwise = Just $ processData xs
 
 main :: IO ()
 main = do
-    let sampleData = [1, -2, 3, 0, 5, -10]
-    if validateInput sampleData
-        then print $ processData sampleData
-        else putStrLn "Invalid input data"
+    let inputData = [1, -2, 3, 0, 5, -8]
+    case validateAndProcess inputData of
+        Nothing -> putStrLn "Empty input list"
+        Just result -> print result
