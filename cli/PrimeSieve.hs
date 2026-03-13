0b64d1ae54e12bc85135a30c19028c6b1c1561e0
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-13 20:15:40 +0000

    Implement a simple prime number sieve using the Sieve of Eratosthenes algorithm

--- cli/PrimeSieve.hs
+++ cli/PrimeSieve.hs
@@ -40,4 +40,17 @@ primesUpTo n
 
 -- Example usage (commented out for library use):
 -- main :: IO ()
--- main = print $ primesUpTo 100
+-- main = print $ primesUpTo 100module PrimeSieve where
+
+primes :: [Int]
+primes = sieve [2..]
+  where
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+nthPrime :: Int -> Int
+nthPrime n = primes !! (n - 1)
+
+isPrime :: Int -> Bool
+isPrime n
+    | n < 2     = False
+    | otherwise = n == head (dropWhile (< n) primes)
