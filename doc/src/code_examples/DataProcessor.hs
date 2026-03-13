523b1e2667c001331215ed9aff831aa5b53bf755
VioletInquiry <violet.inquiry@outlook.com>
2026-03-13 00:25:15 +0000

    Add function to parse CSV data and compute basic statistics

--- doc/src/code_examples/DataProcessor.hs
+++ doc/src/code_examples/DataProcessor.hs
@@ -8,3 +8,47 @@ processNumbers = filterAndTransform (> 0) (* 2)
 
 sumProcessed :: [Int] -> Int
 sumProcessed = sum . processNumbers
+module DataProcessor where
+
+import Data.List (intercalate)
+import Data.List.Split (splitOn)
+
+type Row = [String]
+type CSVData = [Row]
+
+parseCSV :: String -> CSVData
+parseCSV content = map (splitOn ",") (lines content)
+
+computeColumnAverages :: CSVData -> [Double]
+computeColumnAverages [] = []
+computeColumnAverages rows@(header:_) = 
+    map (computeAverage . map read) (transpose numericRows)
+  where
+    numericRows = map (filter (all isNumeric)) (tail rows)
+    isNumeric str = case reads str :: [(Double, String)] of
+        [(_, "")] -> True
+        _         -> False
+
+computeAverage :: [Double] -> Double
+computeAverage [] = 0.0
+computeAverage xs = sum xs / fromIntegral (length xs)
+
+transpose :: [[a]] -> [[a]]
+transpose [] = []
+transpose ([]:xss) = transpose xss
+transpose ((x:xs):xss) = 
+    (x : [h | (h:_) <- xss]) : transpose (xs : [t | (_:t) <- xss])
+
+formatResults :: [Double] -> String
+formatResults averages = 
+    "Column averages:\n" ++ 
+    intercalate "\n" (zipWith formatLine [1..] averages)
+  where
+    formatLine idx avg = "Column " ++ show idx ++ ": " ++ show avg
+
+processCSVFile :: String -> IO ()
+processCSVFile filename = do
+    content <- readFile filename
+    let parsed = parseCSV content
+    let averages = computeColumnAverages parsed
+    putStrLn $ formatResults averages
