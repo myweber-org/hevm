05d939e47892cac93c3f6c00bac49b2c25d3ea9b
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-13 18:59:43 +0000

    Add function to filter and transform list of integers

--- bench/contracts/DataProcessor.hs
+++ bench/contracts/DataProcessor.hs
@@ -45,4 +45,13 @@ safeHead [] = Nothing
 safeHead (x:_) = Just x
 
 sumProcessed :: [Int] -> Int
-sumProcessed = sum . processEvenSquares
+sumProcessed = sum . processEvenSquaresmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
+
+sumProcessed :: [Int] -> Int
+sumProcessed = sum . processNumbers
