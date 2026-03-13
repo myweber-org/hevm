eb989dcca4d426b335935437fe7bbb841869d6bd
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-13 15:47:01 +0000

    Add function to filter and transform a list of integers

--- test/DataProcessor.hs
+++ test/DataProcessor.hs
@@ -17,4 +17,17 @@ main = do
     let inputData = [1, -2, 3, 0, 5, -8]
     case validateAndProcess inputData of
         Nothing -> putStrLn "Empty input list"
-        Just result -> print result
+        Just result -> print resultmodule DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform even (* 2)
+
+main :: IO ()
+main = do
+    let numbers = [1..10]
+    let result = processNumbers numbers
+    putStrLn $ "Original list: " ++ show numbers
+    putStrLn $ "Processed list: " ++ show result
