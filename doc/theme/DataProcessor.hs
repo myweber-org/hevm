59ce9ffeb90f6b4c51abeeb275870d3b9943a057
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-13 14:16:53 +0000

    Add function to filter and transform list of integers

--- doc/theme/DataProcessor.hs
+++ doc/theme/DataProcessor.hs
@@ -119,3 +119,19 @@ processCSVFile filename = do
             Just avg -> putStrLn $ "Column " ++ show col ++ ": " ++ show avg
             Nothing -> putStrLn $ "Column " ++ show col ++ ": No numeric data"
         ) numericCols
+module DataProcessor where
+
+filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
+filterAndTransform predicate transformer = map transformer . filter predicate
+
+processEvenSquares :: [Int] -> [Int]
+processEvenSquares = filterAndTransform even (\x -> x * x)
+
+sumProcessedList :: [Int] -> Int
+sumProcessedList = sum . processEvenSquares
+
+main :: IO ()
+main = do
+    let sampleData = [1..10]
+    let result = sumProcessedList sampleData
+    putStrLn $ "Sum of squares of even numbers from 1 to 10: " ++ show result
