c8bd2682b475283996d6ceabb477dec2b9f5cd18
heyQuim <hey.quim@hotmail.com>
2026-03-13 23:30:04 +0000

    Add function to filter and transform a list of integers

--- ethjet/DataProcessor.hs
+++ ethjet/DataProcessor.hs
@@ -1,31 +1,13 @@
 module DataProcessor where
 
-movingAverage :: (Fractional a) => Int -> [a] -> [a]
-movingAverage n xs
-    | n <= 0 = error "Window size must be positive"
-    | length xs < n = []
-    | otherwise = map average $ windows n xs
-    where
-        windows :: Int -> [a] -> [[a]]
-        windows m = takeWhile ((== m) . length) . map (take m) . iterate tail
-        
-        average :: (Fractional a) => [a] -> a
-        average ys = sum ys / fromIntegral (length ys)module DataProcessor where
-
 filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
 filterAndTransform predicate transformer = map transformer . filter predicate
 
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)module DataProcessor where
-
-filterAndTransform :: (Int -> Bool) -> (Int -> Int) -> [Int] -> [Int]
-filterAndTransform predicate transformer = map transformer . filter predicate
-
-processData :: [Int] -> [Int]
-processData = filterAndTransform (> 0) (* 2)
+processNumbers :: [Int] -> [Int]
+processNumbers = filterAndTransform (> 0) (* 2)
 
 main :: IO ()
 main = do
-    let input = [1, -2, 3, -4, 5]
-    let result = processData input
+    let numbers = [-3, 1, 0, 5, -2, 8]
+    let result = processNumbers numbers
     print result
