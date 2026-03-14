4784ddd56b5bd3745322150e431f98748252d332
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-14 02:05:13 +0000

    Implement a binary search tree with insertion and in-order traversal

--- test/EVM/BinarySearchTree.hs
+++ test/EVM/BinarySearchTree.hs
@@ -6,28 +6,8 @@ data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
 insert :: Ord a => a -> BST a -> BST a
 insert x Empty = Node Empty x Empty
 insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
-    | otherwise = Node left val right
-
-search :: Ord a => a -> BST a -> Bool
-search _ Empty = False
-search x (Node left val right)
-    | x < val   = search x left
-    | x > val   = search x right
-    | otherwise = True
-
-inorder :: BST a -> [a]
-inorder Empty = []
-inorder (Node left val right) = inorder left ++ [val] ++ inorder rightmodule BinarySearchTree where
-
-data BST a = Empty | Node (BST a) a (BST a) deriving (Show, Eq)
-
-insert :: Ord a => a -> BST a -> BST a
-insert x Empty = Node Empty x Empty
-insert x (Node left val right)
-    | x < val   = Node (insert x left) val right
-    | x > val   = Node left val (insert x right)
+    | x < val  = Node (insert x left) val right
+    | x > val  = Node left val (insert x right)
     | otherwise = Node left val right
 
 inOrder :: BST a -> [a]
@@ -36,6 +16,3 @@ inOrder (Node left val right) = inOrder left ++ [val] ++ inOrder right
 
 fromList :: Ord a => [a] -> BST a
 fromList = foldr insert Empty
-
-toSortedList :: BST a -> [a]
-toSortedList = inOrder
